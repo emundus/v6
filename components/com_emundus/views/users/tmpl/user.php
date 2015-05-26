41107c66a68c1e368f9e3ddfcb0e924c8f6fc3dd
QuentinLagadec <quentin.lgdc@gmail.com>
2015-05-26 17:30:26 +0200

    correctif pagination users

--- components/com_emundus/views/users/tmpl/user.php
+++ components/com_emundus/views/users/tmpl/user.php
@@ -6,6 +6,7 @@
  * Time: 09:15
  */
 ?>
+<input type="hidden" id="view" name="view" value="users">
 <?php if(!empty($this->users)):?>
 	<div>
 		<?php echo $this->pagination->getResultsCounter();
@@ -129,49 +130,49 @@
 		</table>
 	</div>
 	<div class="well">
-		<label for = "pager-select"><?php echo JText::_('DISPLAY')?></label>
-		<select name="pager-select" class="chzn-select" id="pager-select">
-			<option value="0" <?php if($this->pagination->limit == 0){echo "selected=true";}?>><?php echo JText::_('ALL')?></option>
-			<option value="5" <?php if($this->pagination->limit == 5){echo "selected=true";}?>>5</option>
-			<option value="10" <?php if($this->pagination->limit == 10){echo "selected=true";}?>>10</option>
-			<option value="15" <?php if($this->pagination->limit == 15){echo "selected=true";}?>>15</option>
-			<option value="20" <?php if($this->pagination->limit == 20){echo "selected=true";}?>>20</option>
-			<option value="25" <?php if($this->pagination->limit == 25){echo "selected=true";}?>>25</option>
-			<option value="30" <?php if($this->pagination->limit == 30){echo "selected=true";}?>>30</option>
-			<option value="50" <?php if($this->pagination->limit == 50){echo "selected=true";}?>>50</option>
-			<option value="100" <?php if($this->pagination->limit == 100){echo "selected=true";}?>>100</option>
-		</select>
-		<div>
-			<ul class="pagination pagination-sm">
-				<li><a href="#em-data" id="<?php echo $this->pagination->{'pagesStart'}?>"><<</a></li>
-				<?php if($this->pagination->{'pagesTotal'} > 15):?>
+        <label for = "pager-select"><?php echo JText::_('DISPLAY')?></label>
+        <select name="pager-select" class="chzn-select" id="pager-select">
+            <option value="0" <?php if($this->pagination->limit == 100000){echo "selected=true";}?>><?php echo JText::_('ALL')?></option>
+            <option value="5" <?php if($this->pagination->limit == 5){echo "selected=true";}?>>5</option>
+            <option value="10" <?php if($this->pagination->limit == 10){echo "selected=true";}?>>10</option>
+            <option value="15" <?php if($this->pagination->limit == 15){echo "selected=true";}?>>15</option>
+            <option value="20" <?php if($this->pagination->limit == 20){echo "selected=true";}?>>20</option>
+            <option value="25" <?php if($this->pagination->limit == 25){echo "selected=true";}?>>25</option>
+            <option value="30" <?php if($this->pagination->limit == 30){echo "selected=true";}?>>30</option>
+            <option value="50" <?php if($this->pagination->limit == 50){echo "selected=true";}?>>50</option>
+            <option value="100" <?php if($this->pagination->limit == 100){echo "selected=true";}?>>100</option>
+        </select>
+        <div>
+            <ul class="pagination pagination-sm">
+                <li><a href="#em-data" id="<?php echo $this->pagination->{'pagesStart'}?>"><<</a></li>
+                <?php if($this->pagination->{'pagesTotal'} > 15):?>
 
-					<?php for($i = 1; $i <= 5; $i++ ):?>
-						<li <?php if($this->pagination->{'pagesCurrent'} == $i){echo 'class="active"';}?>><a id="<?php echo $i?>" href="#em-data"><?php echo $i?></a></li>
-					<?php endfor;?>
-					<li class="disabled"><span>...</span></li>
-					<?php if($this->pagination->{'pagesCurrent'} <= 5):?>
-						<?php for($i = 6; $i <= 10; $i++ ):?>
-							<li <?php if($this->pagination->{'pagesCurrent'} == $i){echo 'class="active"';}?>><a id="<?php echo $i?>" href="#em-data"><?php echo $i?></a></li>
-						<?php endfor;?>
-					<?php else:?>
-						<?php for($i = ($this->pagination->{'pagesCurrent'} - 2); $i <= ($this->pagination->{'pagesCurrent'} + 2); $i++ ):?>
-							<li <?php if($this->pagination->{'pagesCurrent'} == $i){echo 'class="active"';}?>><a id="<?php echo $i?>" href="#em-data"><?php echo $i?></a></li>
-						<?php endfor;?>
-					<?php endif;?>
-					<li class="disabled"><span>...</span></li>
-					<?php for($i = ($this->pagination->{'pagesTotal'} - 4); $i <= $this->pagination->{'pagesTotal'}; $i++ ):?>
-						<li <?php if($this->pagination->{'pagesCurrent'} == $i){echo 'class="active"';}?>><a id="<?php echo $i?>" href="#em-data"><?php echo $i?></a></li>
-					<?php endfor;?>
-				<?php else:?>
-					<?php for($i = 1; $i <= $this->pagination->{'pagesStop'}; $i++ ):?>
-						<li <?php if($this->pagination->{'pagesCurrent'} == $i){echo 'class="active"';}?>><a id="<?php echo $i?>" href="#em-data"><?php echo $i?></a></li>
-					<?php endfor;?>
-				<?php endif;?>
-				<li><a href="#em-data" id="<?php echo $this->pagination->{'pagesTotal'}?>">>></a></li>
-			</ul>
-		</div>
-	</div>
+                    <?php for($i = 1; $i <= 5; $i++ ):?>
+                        <li <?php if($this->pagination->{'pagesCurrent'} == $i){echo 'class="active"';}?>><a id="<?php echo $i?>" href="#em-data"><?php echo $i?></a></li>
+                    <?php endfor;?>
+                    <li class="disabled"><span>...</span></li>
+                    <?php if($this->pagination->{'pagesCurrent'} <= 5):?>
+                        <?php for($i = 6; $i <= 10; $i++ ):?>
+                            <li <?php if($this->pagination->{'pagesCurrent'} == $i){echo 'class="active"';}?>><a id="<?php echo $i?>" href="#em-data"><?php echo $i?></a></li>
+                        <?php endfor;?>
+                    <?php else:?>
+                        <?php for($i = ($this->pagination->{'pagesCurrent'} - 2); $i <= ($this->pagination->{'pagesCurrent'} + 2); $i++ ):?>
+                            <li <?php if($this->pagination->{'pagesCurrent'} == $i){echo 'class="active"';}?>><a id="<?php echo $i?>" href="#em-data"><?php echo $i?></a></li>
+                        <?php endfor;?>
+                    <?php endif;?>
+                    <li class="disabled"><span>...</span></li>
+                    <?php for($i = ($this->pagination->{'pagesTotal'} - 4); $i <= $this->pagination->{'pagesTotal'}; $i++ ):?>
+                        <li <?php if($this->pagination->{'pagesCurrent'} == $i){echo 'class="active"';}?>><a id="<?php echo $i?>" href="#em-data"><?php echo $i?></a></li>
+                    <?php endfor;?>
+                <?php else:?>
+                    <?php for($i = 1; $i <= $this->pagination->{'pagesStop'}; $i++ ):?>
+                        <li <?php if($this->pagination->{'pagesCurrent'} == $i){echo 'class="active"';}?>><a id="<?php echo $i?>" href="#em-data"><?php echo $i?></a></li>
+                    <?php endfor;?>
+                <?php endif;?>
+                <li><a href="#em-data" id="<?php echo $this->pagination->{'pagesTotal'}?>">>></a></li>
+            </ul>
+        </div>
+    </div>
 
 <?php else:?>
 	<?php echo JText::_('NO_RESULT'); ?>
