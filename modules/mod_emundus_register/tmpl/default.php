f08dadac04079f3b889f9c3f6fba57602b72302e
Benjamin <benjamin@emundus.fr>
2015-06-15 18:32:01 +0200

    add class btn btn-info

--- modules/mod_emundus_register/tmpl/default.php
+++ modules/mod_emundus_register/tmpl/default.php
@@ -5,7 +5,7 @@ echo '
 <div class="dropdown clearfix">
    <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu" style="display:block;position:static;margin-bottom:5px;">
 	  '.JText::_( 'ETAPES').'
-	  <li><a tabindex="-1" class="btn" href ="'.$urlresult.$result['code'].'">S\'inscrire à ce programme</a></li>
+	  <li><a tabindex="-1" class="btn btn-info" href ="'.$urlresult.$result['code'].'">S\'inscrire à ce programme</a></li>
     </ul>
   </div>';
 ?>
