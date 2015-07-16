3e3f3645928f232a06e06ccda7299de989bb5da7
Benjamin <benjamin@emundus.fr>
2015-07-16 10:09:10 +0200

    check $params['published']

--- components/com_emundus/models/files.php
+++ components/com_emundus/models/files.php
@@ -615,8 +615,8 @@ class EmundusModelFiles extends JModelLegacy
     {
         $params = JFactory::getSession()->get('filt_params');
         $db = JFactory::getDBO();
-   var_dump(intval($params['published']));
-        if(!is_int(intval($params['published'])) || is_null($params['published']))
+
+        if(!is_numeric($params['published']) || is_null($params['published']))
             $params['published'] = 1;
 
         $query = array('q' => '', 'join' => '');
