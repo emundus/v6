ee8d681bc34b2b54ebfcb466abe421764ec4bf4f
Benjamin <benjamin@emundus.fr>
2016-03-10 17:07:39 +0100

    manage global renew application option

--- modules/mod_emundus_applications/mod_emundus_applications.php
+++ modules/mod_emundus_applications/mod_emundus_applications.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * @package		Joomla.Site
- * @subpackage	mod_users_latest
- * @copyright	Copyright (C) 2005 - 2014 Open Source Matters, Inc. All rights reserved.
+ * @package		Joomla
+ * @subpackage	mod_emundus_applications
+ * @copyright	Copyright (C) 2015 emundus.fr. All rights reserved.
  * @license		GNU General Public License version 2 or later; see LICENSE.txt
  */
 
@@ -14,11 +14,17 @@ require_once dirname(__FILE__).'/helper.php';
 include_once(JPATH_BASE.DS.'components'.DS.'com_emundus'.DS.'models'.DS.'application.php');
 require_once(JPATH_SITE.DS.'components'.DS.'com_emundus'.DS.'models'.DS.'checklist.php');
 include_once(JPATH_BASE.DS.'components'.DS.'com_emundus'.DS.'helpers'.DS.'menu.php');
+
 $app 						= JFactory::getApplication();
 $Itemid 					= $app->input->getInt('Itemid', null, 'int');
+
+$eMConfig 					= JComponentHelper::getParams('com_emundus');
+$applicant_can_renew 		= $eMConfig->get('applicant_can_renew', '0');
+
 $description		 		= $params->get('description', 1);
 $show_add_application 		= $params->get('show_add_application', 1);
 $position_add_application 	= (int)$params->get('position_add_application', 0);
+
 $applications		= modemundusApplicationsHelper::getApplications($params);
 $linknames 			= $params->get('linknames', 0);
 $moduleclass_sfx 	= htmlspecialchars($params->get('moduleclass_sfx'));
