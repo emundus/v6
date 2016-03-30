96715906485f2eb6ed48282fa4b3981adaef8195
Benjamin <benjamin@emundus.fr>
2016-03-30 17:29:52 +0200

    add paiement step to emundus flow module

--- modules/mod_emundusflow/mod_emundusflow.php
+++ modules/mod_emundusflow/mod_emundusflow.php
@@ -2,7 +2,7 @@
 /**
 * @version		$Id: mod_emundusflow.php 7692 2007-06-08 20:41:29Z tcp $
 * @package		Joomla
-* @copyright	Copyright (C) 2005 - 2007 Open Source Matters. All rights reserved.
+* @copyright	Copyright (C) 2016 emundus.fr. All rights reserved.
 * @license		GNU/GPL, see LICENSE.php
 * Joomla! is free software. This version may have been modified pursuant
 * to the GNU General Public License, and as distributed it includes or
@@ -18,13 +18,20 @@ require_once (JPATH_SITE.DS.'components'.DS.'com_emundus'.DS.'helpers'.DS.'menu.
 require_once (JPATH_SITE.DS.'components'.DS.'com_emundus'.DS.'models'.DS.'checklist.php');
 require_once (JPATH_SITE.DS.'components'.DS.'com_emundus'.DS.'models'.DS.'application.php');
 
-$user = JFactory::getUser();
+$document = JFactory::getDocument();
+$document->addStyleSheet( JURI::base()."media/com_emundus/lib/Semantic-UI-CSS-master/semantic.min.css" );
 
+$jinput = JFactory::getApplication()->input;
+$option = $jinput->get('option');
+$view = $jinput->get('view');
+
+$user = JFactory::getUser();
 $db = JFactory::getDBO();
 
 $eMConfig = JComponentHelper::getParams('com_emundus');
 $applicant_can_renew = $eMConfig->get('applicant_can_renew');
 $show_programme = $params->get('show_programme', 1);
+
 $checklist = new EmundusModelChecklist;
 $application = new EmundusModelApplication;
 
@@ -33,7 +40,8 @@ if (isset($user->fnum) && !empty($user->fnum)) {
 	$forms = $application->getFormsProgress($user->id, $user->profile, $user->fnum);
 	$current_application = $application->getApplication($user->fnum);
 	$sent = $checklist->getSent();
-	$confirm_form_url = $checklist->getConfirmUrl(); 
+	$confirm_form_url = $checklist->getConfirmUrl();
+	$paid = 1;
 
 	require(JModuleHelper::getLayoutPath('mod_emundusflow'));
 }
--- modules/mod_emundusflow/tmpl/default.php
+++ modules/mod_emundusflow/tmpl/default.php
@@ -1,39 +1,46 @@
 <?php // no direct access
 defined('_JEXEC') or die('Restricted access'); 
 
-$document = JFactory::getDocument();
-$document->addStyleSheet( JURI::base()."media/com_emundus/lib/semantic/packaged/css/semantic.min.css" );
+
 if($show_programme) {
 ?>
 	<h3> <?php echo $user->campaign_name; ?> <?php echo @$renew; ?></h3>
 <?php 
 }
 $renew='';
+$step_form = $forms<100?'':'completed';
+$step_attachment = $attachments<100?'':'completed';
+$step_paiement = $paid==0?'':'completed';;
+$step_sent = '';
 if ($sent>0) {
-	// Apply again
-	/*$query='SELECT count(id) as cpt FROM #__emundus_setup_campaigns 
-			WHERE id NOT IN (
-			select campaign_id FROM #__emundus_campaign_candidature WHERE applicant_id='.$user->id.' AND fnum like '.$db->Quote($user->fnum).'
-			)';
-	$db->setQuery($query);
-	$cpt = $db->loadResult();*/
-	$complete =  '<i class="large ok sign icon"></i>'.JText::_('APPLICATION_SENT');
+	$step_sent = 'completed';
+	$complete =  JText::_('APPLICATION_SENT');
 	if($applicant_can_renew)
 		$renew =  ' <a href="index.php?option=com_emundus&view=renew_application"><i class="large repeat icon"></i>'.JText::_('RENEW_APPLICATION').'</a>';
 } else {
-	$complete =  '<i class="large time icon"></i><a href="'.$confirm_form_url.'&usekey=fnum&rowid='.$user->fnum.'" title="'.JText::_('APPLICATION_NOT_SENT').'">'.JText::_('APPLICATION_NOT_SENT').'</a>';
+	$complete =  '<a href="'.$confirm_form_url.'&usekey=fnum&rowid='.$user->fnum.'" title="'.JText::_('APPLICATION_NOT_SENT').'">'.JText::_('APPLICATION_NOT_SENT').'</a>';
 }
 ?>
-<div class="ui small steps">
-  <div class="ui <?php echo $forms<100?"disabled":"active"; ?> step">
-    <?php echo  '<i class="large text file outline icon"></i> '.$forms.'% '.JText::_('FORM_FILLED'); ?>
+<div class="ui tablet stackable steps">
+  <div class="<?php echo ($view=="form")?"active":""; ?><?php echo $step_form; ?> step">
+  	<i class="large text file outline icon"></i>
+  	<div class="content">
+    	<div class="description"><?php echo  $forms.'% '.JText::_('FORM_FILLED'); ?></div>
+    </div>
   </div>
-  <div class="ui <?php echo $attachments<100?"disabled":"active"; ?> step">
-    <?php echo  '<i class="large attachment icon"></i> '.$attachments.'% '.JText::_('ATTACHMENT_SENT'); ?>
+  <div class="<?php echo ($view=="checklist")?"active":""; ?><?php echo $step_attachment; ?> step">
+  	<i class="large attach outline icon"></i>
+    <?php echo  $attachments.'% '.JText::_('ATTACHMENT_SENT'); ?>
   </div>
-  <div class="ui <?php echo $sent<=0?"disabled red":"active"; ?> step">
-    <?php echo $complete; ?>
-    <span class="label label-<?php echo $current_application->class; ?>"> <?php echo $current_application->value; ?></span>
+  <div class="<?php echo ($option=="com_hikashop")?"active":""; ?><?php echo $step_paiement; ?> step">
+  	<i class="large payment outline icon"></i>
+    <?php echo  JText::_('APPLICATION_PAID'); ?>
   </div>
+  <div class="<?php echo $step_sent; ?> step">
+  	<i class="large time outline icon"></i>
+  	<div class="content">
+    	<div class="description"><?php echo $complete; ?></div>
+    	<div class="description"><span class="label label-<?php echo $current_application->class; ?>"> <?php echo $current_application->value; ?></span></div>
+    </div>
   </div>
 </div>
