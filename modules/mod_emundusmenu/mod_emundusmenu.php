92aa8ff5b912953b5c21eb66a97b2a3411f7d2d8
Benjamin <benjamin@emundus.fr>
2016-04-05 18:56:36 +0200

    add emundus parameters

--- modules/mod_emundusmenu/mod_emundusmenu.php
+++ modules/mod_emundusmenu/mod_emundusmenu.php
@@ -1,8 +1,8 @@
 <?php
 /**
  * @package		Joomla.Site
- * @subpackage	mod_menu
- * @copyright	Copyright (C) 2005 - 2012 Open Source Matters, Inc. All rights reserved.
+ * @subpackage	mod_emundusmenu
+ * @copyright	Copyright (C) 2016 emundus.fr, Inc. All rights reserved.
  * @license		GNU General Public License version 2 or later; see LICENSE.txt
  */
 
@@ -12,19 +12,28 @@ defined('_JEXEC') or die;
 // Include the syndicate functions only once
 require_once dirname(__FILE__).'/helper.php';
 
-$user	= JFactory::getUser();
-$list = array();
-if (isset($user->menutype) && empty($user->fnum))
-	$list	= modEmundusMenuHelper::getList($params);
-$app	= JFactory::getApplication();
-$menu	= $app->getMenu();
-$active	= $menu->getActive();
-$active_id = isset($active) ? $active->id : $menu->getDefault()->id;
-$path	= isset($active) ? $active->tree : array();
-$showAll	= $params->get('showAllChildren');
-$class_sfx	= htmlspecialchars($params->get('class_sfx'));
+$params = JComponentHelper::getParams('com_emundus');
+$display_applicant_menu = $params->get('display_applicant_menu', 1);
 
-if(count($list)) {
-	require JModuleHelper::getLayoutPath('mod_emundusmenu', $params->get('layout', 'default'));
+$user	= JFactory::getUser();
+
+if (!empty($user->fnum) && $display_applicant_menu==0) 
+	return;
+
+if (isset($user->menutype)) {
+	$list = array();
+	$list	= modEmundusMenuHelper::getList($params);
+
+	$app	= JFactory::getApplication();
+	$menu	= $app->getMenu();
+	$active	= $menu->getActive();
+	$active_id = isset($active) ? $active->id : $menu->getDefault()->id;
+	$path	= isset($active) ? $active->tree : array();
+	$showAll	= $params->get('showAllChildren');
+	$class_sfx	= htmlspecialchars($params->get('class_sfx'));
+
+	if(count($list)) {
+		require JModuleHelper::getLayoutPath('mod_emundusmenu', $params->get('layout', 'default'));
+	}
 }
 ?>
