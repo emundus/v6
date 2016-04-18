612873eaafd89dc3dd43dca7a6dca0bf4c2d7906
Benjamin <benjamin@emundus.fr>
2016-04-18 20:46:34 +0200

    manage menu icons

--- modules/mod_emunduspanel/mod_emunduspanel.php
+++ modules/mod_emunduspanel/mod_emunduspanel.php
@@ -1,8 +1,8 @@
 <?php
 /**
-* @version		$Id: mod_emunduspanel.php 7692 2011-05-26 20:41:29Z mguillossou $
+* @version		$Id: mod_emunduspanel.php 7692 2016-04-18 rivalland $
 * @package		Joomla
-* @copyright	Copyright (C) 2005 - 2007 Open Source Matters. All rights reserved.
+* @copyright	Copyright (C) 2016 emundus.fr. All rights reserved.
 * @license		GNU/GPL, see LICENSE.php
 * Joomla! is free software. This version may have been modified pursuant
 * to the GNU General Public License, and as distributed it includes or
@@ -13,87 +13,121 @@
 
 // no direct access
 defined('_JEXEC') or die('Restricted access');
-JHTML::stylesheet( 'emundus.css', JURI::Base().'modules/mod_emunduspanel/style/' );
+//JHTML::stylesheet( 'emundus.css', JURI::Base().'modules/mod_emunduspanel/style/' );
+
+$document = JFactory::getDocument();
+$document->addStyleSheet( JURI::base()."media/com_emundus/lib/Semantic-UI-CSS-master/semantic.min.css" );
 $db	= JFactory::getDBO();
-/*$eMConfig = JComponentHelper::getParams('com_emundus');
-$applicant_can_renew = $eMConfig->get('applicant_can_renew'); 
-$applicant_can_renew_campaign = $eMConfig->get('applicant_can_renew_campaign'); */
+
 
 $user = JFactory::getUser();
-	if(isset($user->menutype)) $user_menutype = $user->menutype;
-	else $user_menutype = 'mainmenu';
-	$folder = $params->get('folder', '');
-	$show_profile_link = $params->get('show_profile_link', 1);
-	$text = $params->get($user_menutype, '');
-	$img = $params->get($user_menutype.'_img', '');
-	$is_text = $params->get($user_menutype.'_text', '');
-	$img = explode(',',$img);
-	$col = 0;
-	$t__ = '';
-	$i = 1;
+if(isset($user->menutype)) $user_menutype = $user->menutype;
+else $user_menutype = 'mainmenu';
+$folder = $params->get('folder', '');
+$show_profile_link = $params->get('show_profile_link', 1);
+$text = $params->get($user_menutype, '');
+$img = $params->get($user_menutype.'_img', '');
+$is_text = $params->get($user_menutype.'_text', '');
+$img = explode(',',$img);
+$col = 0;
+$t__ = '';
+$i = 1;
 
-	if(is_array($text) && !empty($text)){
-		foreach($text as $t){
-			if(count($text) != $i) $t__ .= $t.',';
-			else $t__ .= $t;
-			$i++;
-		}
-	}else{
-		$t__ = $text;
+if(is_array($text) && !empty($text)){
+	foreach($text as $t){
+		if(count($text) != $i) $t__ .= $t.',';
+		else $t__ .= $t;
+		$i++;
 	}
-	if (!empty($t__)) { 
-		
-		if($user_menutype == 'mainmenu')
-			$query = 'SELECT m.menutype, m.title, m.alias, m.link, m.id FROM #__menu m WHERE m.id IN ('.$t__.') ORDER BY m.lft ASC';
-		else
-			$query = 'SELECT m.menutype, m.title, m.alias, m.link, m.id FROM #__menu m WHERE m.id IN ('.$t__.') ORDER BY m.lft ASC';
-		$db->setQuery($query);
-		$res = $db->loadObjectList();
+}else{
+	$t__ = $text;
+}
 
-		if (count($res > 0)) {
-			$tab = array();
-			$tab_temp = array();
-			echo '<div class="emundus_home_page">';
-			$j = 0;
-			foreach($res as $r){
+$btn_profile = '<button class="circular ui icon button"><a href="'.JRoute::_('index.php?option=com_users&view=profile&layout=edit').'"><i class="user icon"></i>'.JText::_('PROFILE').'</a></button>';
+
+
+if (!empty($t__)) { 
+	
+	/*if($user_menutype == 'mainmenu')
+		$query = 'SELECT m.menutype, m.title, m.alias, m.link, m.id, m.params FROM #__menu m WHERE m.id IN ('.$t__.') ORDER BY m.lft ASC';
+	else */
+	
+	$query = 'SELECT m.menutype, m.title, m.alias, m.link, m.id, m.params 
+				FROM #__menu m 
+				WHERE m.id IN ('.$t__.') 
+				ORDER BY m.lft ASC';
+	$db->setQuery($query);
+	$res = $db->loadObjectList();
+
+	if (count($res > 0)) {
+		$tab = array();
+
+		echo '<div class="emundus_home_page">';
+		$j = 0;
+		foreach($res as $r){
+			$menu_params = json_decode($r->params, true);
+			if (empty($img[$j]) && !empty($menu_params['menu_image']) && empty($menu_params['menu-anchor_css']))
+				$src = JURI::base().$menu_params['menu_image'];
+			else
 				$src = $folder.''.$img[$j];
-				$str = '<a href="'.$r->link.'&Itemid='.$r->id.'"><img src="'.JURI::Base().'/'.$src.'" /></a>';
-				if($is_text == '1')
-					$str .= '<br/><a class="text" href="'.$r->link.'&Itemid='.$r->id.'">'.$r->title.'</a>';
-				$tab[] = $str;
-				$j++;
-			}
-			/*if($is_text == '1')
-				$tab = array_merge($tab,$tab_temp);*/
-			$col = count($tab);
-	
-			echo '</div>';
-		}
-	} elseif(!$user->guest) { 
-		$query = 'SELECT m.menutype, m.title, m.alias, m.link, m.id
-                  FROM #__menu m
-                  WHERE published=1
-                  AND menutype="'.$user_menutype.'"
-                  AND m.link <> ""
-                  AND m.link <> "#"
-                  ORDER BY m.parent_id DESC, m.lft, m.level, m.menutype, m.id ASC';
-		$db->setQuery($query);
-		$res = $db->loadObjectList();
-		if (count($res > 0)) {
-			$tab = array();
-			$tab_temp = array();
-			echo '<div class="emundus_home_page">';
-			foreach($res as $r){
-				$src = $folder.'files_grey.png';
-				$str = '<a href="'.$r->link.'&Itemid='.$r->id.'"><img src="'.JURI::Base().'/'.$src.'" /></a>';
+
+			if (!empty($menu_params['menu-anchor_css']))
+				$glyphicon = '<i class="'.$menu_params['menu-anchor_css'].'"></i>';
+			else
+				$glyphicon = '';
+
+			$str = '<a href="'.JRoute::_($r->link.'&Itemid='.$r->id).'">'.$glyphicon.'<img src="'.JURI::Base().'/'.$src.'" /></a>';
+			if($is_text == '1')
 				$str .= '<br/><a class="text" href="'.$r->link.'&Itemid='.$r->id.'">'.$r->title.'</a>';
-				$tab[] = $str;
-			}
-			$col = count($tab);
-	
-			echo '</div>';
+			$tab[] = $str;
+			$j++;
 		}
 
+		$col = count($tab);
 	}
+
+} elseif(!$user->guest) { 
+	$query = 'SELECT m.menutype, m.title, m.alias, m.link, m.id, m.params
+              FROM #__menu m
+              WHERE published=1
+              AND menutype="'.$user_menutype.'"
+              AND m.link <> ""
+              AND m.link <> "#"
+              ORDER BY m.parent_id DESC, m.lft, m.level, m.menutype, m.id ASC';
+	$db->setQuery($query);
+	$res = $db->loadObjectList();
+	if (count($res > 0)) {
+		$tab = array();
+		$tab_temp = array();
+
+
+		if($user->applicant == 1)
+			$btn_start = '<button class="circular ui icon button"><a href="'.JRoute::_($res[0]->link.'&Itemid='.$res[0]->id).'"><i class="right arrow icon"></i>'.JText::_('START').'</a></button>';
+		else
+			$btn_start = '';
+
+		foreach($res as $r){
+			$menu_params = json_decode($r->params, true); 
+
+			if (!empty($menu_params['menu-anchor_css'])) {
+				$glyphicon = '<i class="'.$menu_params['menu-anchor_css'].'"></i>';
+				$icon = '';
+			} else {
+				$glyphicon = '';
+				if (!empty($menu_params['menu_image']))
+					$icon = '<img src="'.JURI::base().$menu_params['menu_image'].'" />';
+				else
+					$icon = '<img src="'.JURI::Base().$folder.'files_grey.png" />';
+			}
+			
+			$str = '<a href="'.JRoute::_($r->link.'&Itemid='.$r->id).'">'.$glyphicon.$icon.' <br />'.$r->title.'</a>';
+			//$str .= '<br/><a class="text" href="'.$r->link.'&Itemid='.$r->id.'">'.$r->title.'</a>';
+			$tab[] = $str;
+		}
+		$col = count($tab);
+
+	}
+
+}
 
 require(JModuleHelper::getLayoutPath('mod_emunduspanel'));
--- modules/mod_emunduspanel/tmpl/default.php
+++ modules/mod_emunduspanel/tmpl/default.php
@@ -4,27 +4,28 @@ JHTML::stylesheet( 'emundus.css', JURI::Base().'modules/mod_emunduspanel/style/'
 
 if (!empty($tab)) {
 ?>
+<div class="emundus_home_page">
 <fieldset class="ui existing segment">
-	<?php
+    <?php
     if(isset($user->profile) && $user->profile>0) {
 
         $title = $user->profile_label;
-        //$title = strlen($user->campaign_name)>3?$user->campaign_name:$user->profile_label;
+
         if($show_profile_link == 1)
-        	echo '<a href="index.php?option=com_users&view=profile&layout=edit"><legend>'.$title. ' <span class="icon-cog"></span></legend></a>';
+            echo '<legend>'.$title.' '.$btn_profile.' '.$btn_start.'</legend>';
         else
-        	echo '<legend>'.$title.'</legend>';
+            echo '<legend>'.$title.'</legend>';
     }
     
     ?>
     <div class="emundus_home_page" ><div class="rt-grid-9">
     <?php 
     $i=1; $j=1;$k=0;
-	$l = (@$user->candidature_posted == 1 && @$user->candidature_incomplete == 0 ) ? 2 : '999';
-	//die(print_r($user));
+    $l = (@$user->candidature_posted == 1 && @$user->candidature_incomplete == 0 ) ? 2 : '999';
+    //die(print_r($user));
 
     foreach ($tab as $t){ 
-		if ($j>$l) {
+        if ($j>$l) {
             break;
         } else {
             echo '<div class="rt-grid-3 element_home_emundus">' . $t . '</div>';
@@ -34,11 +35,12 @@ if (!empty($tab)) {
                 $k=0;
             }
         }
-		$j++;
+        $j++;
     } 
-   	?>
+    ?>
     </div>
     </div>
     
 </fieldset>
 <?php } ?>
+</div>
