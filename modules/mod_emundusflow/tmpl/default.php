cc8e0dac51959f3bf996989af1483e458e319adf
Benjamin <benjamin@emundus.fr>
2016-05-02 11:08:03 +0200

    add return button

--- modules/mod_emundusflow/tmpl/default.php
+++ modules/mod_emundusflow/tmpl/default.php
@@ -8,26 +8,31 @@ $step_attachment = $attachments<100?'':'completed';
 $step_paiement = @$paid==0?'':'completed';;
 $step_sent = '';
 if ($sent>0) {
-	$step_sent = 'completed';
-	$complete =  JText::_('APPLICATION_SENT');
-	if($applicant_can_renew)
-		$renew =  ' <a href="index.php?option=com_emundus&view=renew_application"><i class="large repeat icon"></i>'.JText::_('RENEW_APPLICATION').'</a>';
+  $step_sent = 'completed';
+  $complete =  JText::_('APPLICATION_SENT');
+  if($applicant_can_renew)
+    $renew =  ' <a href="index.php?option=com_emundus&view=renew_application"><i class="large repeat icon"></i>'.JText::_('RENEW_APPLICATION').'</a>';
 } else {
-	$complete =  '<a href="'.$confirm_form_url.'&usekey=fnum&rowid='.$user->fnum.'" title="'.JText::_('APPLICATION_NOT_SENT').'">'.JText::_('APPLICATION_NOT_SENT').'</a>';
+  $complete =  '<a href="'.$confirm_form_url.'&usekey=fnum&rowid='.$user->fnum.'" title="'.JText::_('APPLICATION_NOT_SENT').'">'.JText::_('APPLICATION_NOT_SENT').'</a>';
 }
 ?>
 <div class="ui attached segment">
-  <p><?php echo ($show_programme==1)?$user->campaign_name.' '.@$renew:''; ?></p>
+  <p><?php echo ($show_programme==1)?'<b>'.$user->campaign_name.'</b> '.@$renew:''; ?></p>
 </div>
 <div class="ui tablet stackable bottom attached steps">
+  <div class="step">
+    <a href="index.php" title="<?php echo  JText::_('RETURN'); ?>">
+      <i class="large arrow left outline icon"></i> <?php echo  JText::_('RETURN'); ?>
+    </a>
+  </div>
   <div class="<?php echo ($view=="form")?"active":""; ?> <?php echo $step_form; ?> step">
-  	<i class="large text file outline icon"></i>
-  	<div class="content">
-    	<div class="description"><?php echo  $forms.'% '.JText::_('FORM_FILLED'); ?></div>
+    <i class="large text file outline icon"></i>
+    <div class="content">
+      <div class="description"><?php echo  $forms.'% '.JText::_('FORM_FILLED'); ?></div>
     </div>
   </div>
   <div class="<?php echo ($view=="checklist")?"active":""; ?> <?php echo $step_attachment; ?> step">
-  	<i class="large attach outline icon"></i>
+    <i class="large attach outline icon"></i>
     <div class="content">
       <div class="description"><?php echo  $attachments.'% '.JText::_('ATTACHMENT_SENT'); ?></div>
     </div>
@@ -35,7 +40,7 @@ if ($sent>0) {
 
 <?php if ($application_fee == 1) { ?>
   <div class="<?php echo ($option=="com_hikashop")?"active":""; ?> <?php echo $step_paiement; ?> step">
-  	<i class="large payment outline icon"></i>
+    <i class="large payment outline icon"></i>
     <div class="content">
       <div class="description"><?php echo  $paid>0?JText::_('APPLICATION_PAID'):JText::_('APPLICATION_NOT_PAID'); ?></div>
       <div class="description"><?php echo  $paid>0?'':'<a href="index.php?option=com_hikashop&ctrl=product&task=updatecart&quantity=1&checkout=1&product_id=1" title="'.JText::_('ORDER_NOW').'">'.JText::_('ORDER_NOW').'</a>'; ?></div>
@@ -43,10 +48,10 @@ if ($sent>0) {
   </div>
 <?php } ?>
   <div class="<?php echo $step_sent; ?> step">
-  	<i class="large time outline icon"></i>
-  	<div class="content">
-    	<div class="description"><?php echo $complete; ?></div>
-    	<div class="description"><span class="label label-<?php echo $current_application->class; ?>"> <?php echo $current_application->value; ?></span></div>
+    <i class="large time outline icon"></i>
+    <div class="content">
+      <div class="description"><?php echo $complete; ?></div>
+      <div class="description"><span class="label label-<?php echo $current_application->class; ?>"> <?php echo $current_application->value; ?></span></div>
     </div>
   </div>
 </div>
