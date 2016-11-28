dfc28864bf299a1d6c1faf0170b79d2ae6a83c1a
Benjamin <benjamin@emundus.fr>
2016-11-28 17:37:56 +0100

    fixe language on button register

--- modules/mod_emundus_register/tmpl/default.php
+++ modules/mod_emundus_register/tmpl/default.php
@@ -5,7 +5,7 @@ echo '
 <div class="dropdown clearfix">
    <ul role="menu" aria-labelledby="dropdownMenu" style="display:block;position:static;margin-left:5px;">
 	  '.JText::_( 'ETAPES').'
-	  <li><a tabindex="-1" class="btn btn-info" href ="'.$urlresult.$result['code'].'">S\'inscrire à ce programme</a></li>
+	  <li><a tabindex="-1" class="btn btn-info" href ="'.$urlresult.$result['code'].'">'.JText::_('MOD_EM_REGISTER').'</a></li>
     </ul>
   </div>';
 ?>
