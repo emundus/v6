70bb5190fa57365bc7a4a8f9682a2c1e97d552be
Benjamin <benjamin@emundus.fr>
2016-11-28 17:39:19 +0100

    set default var value

--- modules/mod_emundusflow/tmpl/default.php
+++ modules/mod_emundusflow/tmpl/default.php
@@ -7,6 +7,7 @@ $renew='';
 $step_form = $forms<100?'':'completed';
 $step_attachment = $attachments<100?'':'completed';
 $step_paiement = @$paid==0?'':'completed';
+$btn_send = '';
 if ($forms>=100 && $attachments>=100 && $paid>0 && $sent==0) {
   $btn_send = '
   <div class="ui vertical mini button">
