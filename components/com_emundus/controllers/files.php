5bec24f0a074d8f1bf06707eb1eb1a92d99383ef
Benjamin <benjamin@emundus.fr>
2017-01-27 17:46:39 +0100

    change access right check on export CSV

--- components/com_emundus/controllers/files.php
+++ components/com_emundus/controllers/files.php
@@ -1119,7 +1119,7 @@ class EmundusControllerFiles extends JControllerLegacy
         $validFnums = array();
         foreach($fnums as $fnum)
         {
-            if(EmundusHelperAccess::asAccessAction(13, 'u', $this->_user->id, $fnum)&& $fnum != 'em-check-all-all' && $fnum != 'em-check-all')
+            if(EmundusHelperAccess::asAccessAction(1, 'r', $this->_user->id, $fnum)&& $fnum != 'em-check-all-all' && $fnum != 'em-check-all')
             {
                 $validFnums[] = $fnum;
             }
