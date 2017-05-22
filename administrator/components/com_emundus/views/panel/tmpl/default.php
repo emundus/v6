d001a5722763bcedac3c8005effe919b6aed2167
Benjamin <benjamin@emundus.fr>
2017-05-22 14:57:06 +0200

    cleanup

--- administrator/components/com_emundus/views/panel/tmpl/default.php
+++ administrator/components/com_emundus/views/panel/tmpl/default.php
@@ -11,12 +11,12 @@ defined('_JEXEC') or die('RESTRICTED');
 <html><body bgcolor="#FFFFFF">
 <table width="100%" border="0">
   <tr>
-    <td width="80%"><p align="center"><a href="http://www.emundus.fr" target="_blank" title="Candidate Relation Management"><img src="components/com_emundus/logo.png" alt="Decision Publique" border="0"></a></p>
+    <td width="80%"><p align="center"><a href="http://www.emundus.fr" target="_blank" title="Candidate Relation Management"><img src="components/com_emundus/logo.png" alt="eMundus" border="0"></a></p>
       <p align="center"><?php echo JText::_("FIND_PRESTATION"); ?></p>
       <p align="center"><a href="http://www.emundus.fr" target="_blank">www.emundus.fr</a></p></td>
     <td width="20%"><p align="center"><strong><?php echo JText::_("EMUNDUS_COMPONENT"); ?></strong></p>
       <p align="center"><?php echo JText::_("GESTIONNAIRE_CANDIDATURE"); ?></p>
-      <p align="center">Version 6.1 - 06/2015</p>
+      <p align="center">Version 6.2 - 06/2017</p>
       <p align="center"><h4><?php echo JText::_("AUTHOR"); ?> </h4>
         <em>Benjamin Rivalland</em>
       </p>
@@ -29,6 +29,7 @@ defined('_JEXEC') or die('RESTRICTED');
         <em>Robin Colet</em>,<br>
         <em>Yohan Durand</em>,<br>
         <em>Quentin Lagadec</em><br>
+        <em>Florian Gohard</em><br>
       </p>
     </td>
   </tr>
