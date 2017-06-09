2d7852f220a0c5d2995398423d55fb2be2fbc90a
Benjamin <benjamin@emundus.fr>
2017-06-09 16:50:59 +0200

    add Fabrik form JS for SU

---
+++ components/com_fabrik/js/form_363.js
@@ -0,0 +1,55 @@
+requirejs(['fab/fabrik'], function () {
+	Fabrik.addEvent('fabrik.form.group.delete.end', function (form, event) {
+		var montant_idex=0;
+		for(i=0;i<100;i++){
+		  if($('jos_emundus_inov_budget_508_repeat___montant_idex_'+i) !== null){
+			tmp = parseFloat($('jos_emundus_inov_budget_508_repeat___montant_idex_'+i).value.replace(/[^\d\][^.]/gi, ''));
+			if(tmp === null || tmp==="" || isNaN(tmp)) {tmp=0;}
+			montant_idex = parseFloat(montant_idex) + parseFloat(tmp);
+		  }
+		}
+
+		if(montant_idex === null || montant_idex==="" || isNaN(montant_idex)) {montant_idex=0;}
+
+		$('jos_emundus_inov_budget___total_montant_idex').value=parseFloat(montant_idex);
+		$('jos_emundus_inov_budget___total').value=parseFloat($('jos_emundus_inov_budget___total_montant_idex').value)+parseFloat($('jos_emundus_inov_budget___total_montant_idex1').value)+parseFloat($('jos_emundus_inov_budget___total_investissement').value)+parseFloat($('jos_emundus_inov_budget___total_amortissement').value);
+		
+		var montant_idex=0;
+		for(i=0;i<100;i++){
+		  if($('jos_emundus_inov_budget_509_repeat___montant_idex_'+i) !== null){
+			tmp = parseFloat($('jos_emundus_inov_budget_509_repeat___montant_idex_'+i).value.replace(/[^\d\][^.]/gi, ''));
+			if(tmp === null || tmp==="" || isNaN(tmp)) {tmp=0;}
+			montant_idex = parseFloat(montant_idex) + parseFloat(tmp);
+		  }
+		}
+
+		if(montant_idex === null || montant_idex==="" || isNaN(montant_idex)) {montant_idex=0;}
+
+		$('jos_emundus_inov_budget___total_montant_idex1').value=parseFloat(montant_idex); 
+
+		$('jos_emundus_inov_budget___total').value=parseFloat($('jos_emundus_inov_budget___total_montant_idex').value)+parseFloat($('jos_emundus_inov_budget___total_montant_idex1').value)+parseFloat($('jos_emundus_inov_budget___total_investissement').value)+parseFloat($('jos_emundus_inov_budget___total_amortissement').value);
+		
+		var montant_idex=0;
+		var montant_investissement=0;
+		for(i=0;i<100;i++){
+		  if($('jos_emundus_inov_budget_531_repeat___montant_amortissement_'+i) !== null){
+			tmp = parseFloat($('jos_emundus_inov_budget_531_repeat___montant_amortissement_'+i).value.replace(/[^\d\][^.]/gi, ''));
+			if(tmp === null || tmp==="" || isNaN(tmp)) {tmp=0;}
+			montant_idex = parseFloat(montant_idex) + parseFloat(tmp);
+			
+			investissement = parseFloat($('jos_emundus_inov_budget_531_repeat___montant_investissement_'+i).value.replace(/[^\d\][^.]/gi, ''));
+			if(investissement === null || investissement==="" || isNaN(investissement)) {investissement=0;}
+			montant_investissement = parseFloat(montant_investissement) + parseFloat(investissement);
+		  }
+		}
+
+		if(montant_idex === null || montant_idex==="" || isNaN(montant_idex)) {montant_idex=0;}
+		if(montant_investissement === null || montant_investissement==="" || isNaN(montant_investissement)) {montant_investissement=0;}
+
+		$('jos_emundus_inov_budget___total_investissement').value=parseFloat(montant_investissement);
+		$('jos_emundus_inov_budget___total_amortissement').value=parseFloat(montant_idex);
+
+		$('jos_emundus_inov_budget___total').value=parseFloat($('jos_emundus_inov_budget___total_montant_idex').value)+parseFloat($('jos_emundus_inov_budget___total_montant_idex1').value)+parseFloat($('jos_emundus_inov_budget___total_investissement').value)+parseFloat($('jos_emundus_inov_budget___total_amortissement').value);
+
+	});
+})
