ccfad09b975ebc60bf9afb934e9e16c097084d46
Hugo Moracchini <hugo4400@gmail.com>
2017-09-08 14:58:20 +0200

    Fixed bug not displaying contents of non-boolean radio buttons in Admission list view

--- components/com_emundus/views/admission/tmpl/data.php
+++ components/com_emundus/views/admission/tmpl/data.php
@@ -83,7 +83,7 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
 										</a>
 									<?php elseif ($k == 'admission_id' || $k == 'recorded_by') :?>
 									<?php else:?>
-
+										
 										<?php if ($value->type == 'text' ) :?>
 											<?php echo strip_tags($value->val); ?>
 										<?php elseif ($value->type == 'textarea') :?>
@@ -104,13 +104,17 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
 												</strong>
 											</h5>
 										<?php elseif ($value->type == 'radiobutton') :?>
-											<?php if (strtolower($value->val) == 'yes' || $value->val == '1' || strtolower($value->val) == 'oui') :?>
-												<span class="glyphicon glyphicon-ok em-radio" id="<?php echo $cfnum.'-'.$value->id.'-'.$value->val; ?>" aria-hidden="true" style="color:green;"></span>
-											<?php elseif (strtolower($value->val) == 'no' || $value->val == '0' || strtolower($value->val) == 'non') :?>
-												<span class="glyphicon glyphicon-remove em-radio" id="<?php echo $cfnum.'-'.$value->id.'-'.$value->val; ?>" aria-hidden="true" style="color:red;"></span>
-											<?php else :?>
+											<?php if(isset($value->val)) :?>
+												<?php if (strtolower($value->val) == 'yes' || $value->val == '1' || strtolower($value->val) == 'oui') :?>
+													<span class="glyphicon glyphicon-ok em-radio" id="<?php echo $cfnum.'-'.$value->id.'-'.$value->val; ?>" aria-hidden="true" style="color:green;"></span>
+												<?php elseif (strtolower($value->val) == 'no' || $value->val == '0' || strtolower($value->val) == 'non') :?>
+													<span class="glyphicon glyphicon-remove em-radio" id="<?php echo $cfnum.'-'.$value->id.'-'.$value->val; ?>" aria-hidden="true" style="color:red;"></span>
+												<?php else :?>
+													<p> <?php echo $value->val ?> </p>
+												<?php endif; ?>
+											<?php else: ?>
 												<span class="glyphicon glyphicon-warning-sign em-radio" id="<?php echo $cfnum.'-'.$value->id.'-'.$value->val; ?>" aria-hidden="true" style="color:orange;"></span>
-											<?php endif; ?>
+											<?php endif; ?> 
 										<?php elseif ($value->type == 'field'):?>
 											<input class="admission_input" type="text" id="<?php echo $cfnum.'-'.$value->id; ?>" name="<?php echo $value->val ?>" value="<?php echo $value->val ?>"></input>
 											<span class="glyphicon glyphicon-share-alt em-field" id="<?php echo $cfnum.'-'.$value->id.'-span'; ?>" aria-hidden="true" style="color:black;"></span>
