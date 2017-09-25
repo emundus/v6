902c76b5031c2cf92c2c0c9e1cdf1acc9a6a4fe7
Hugo Moracchini <hugo4400@gmail.com>
2017-09-25 12:37:00 +0200

    Fix eventy deletion script

--- plugins/fabrik_form/php/scripts/emundus-calendar_delete-event.php
+++ plugins/fabrik_form/php/scripts/emundus-calendar_delete-event.php
@@ -27,111 +27,61 @@ JLog::addLogger(
     array('com_emundus')
 );
 
-$user = JFactory::getUser();
-$mainframe = JFactory::getApplication();
-$db = JFactory::getDBO();
-$jinput = $mainframe->input;
+$user       = JFactory::getUser();
+$mainframe  = JFactory::getApplication();
+$db         = JFactory::getDBO();
+$jinput     = $mainframe->input;
 
-$eMConfig = JComponentHelper::getParams('com_emundus');
-$id_applicants = $eMConfig->get('id_applicants', '0');
-$applicants = explode(',',$id_applicants);
-$calendarID1 = $eMConfig->get('calendarId1');
-$calendarID2 = $eMConfig->get('calendarId2');
-$calendarID3 = $eMConfig->get('calendarId3');
-$calendarID4 = $eMConfig->get('calendarId4');
-$calendarID5 = $eMConfig->get('calendarId5');
-$calendarID6 = $eMConfig->get('calendarId6');
-$calendarID7 = $eMConfig->get('calendarId7');
-$calendarID8 = $eMConfig->get('calendarId8');
-$calendarID9 = $eMConfig->get('calendarId9');
-$calendarID10 = $eMConfig->get('calendarId10');
-$calendarID11 = $eMConfig->get('calendarId11');
-$calendarID12 = $eMConfig->get('calendarId12');
-$calendarID13 = $eMConfig->get('calendarId13');
-$calendarID14 = $eMConfig->get('calendarId14');
-$calendarID15 = $eMConfig->get('calendarId15');
-$calendarID16 = $eMConfig->get('calendarId16');
-$calendarID17 = $eMConfig->get('calendarId17');
-$calendarID18 = $eMConfig->get('calendarId18');
-$calendarID19 = $eMConfig->get('calendarId19');
-$calendarID20 = $eMConfig->get('calendarId20');
+$eMConfig       = JComponentHelper::getParams('com_emundus');
+$id_applicants  = $eMConfig->get('id_applicants', '0');
+$applicants     = explode(',',$id_applicants);
+$calendarIDs    = $eMConfig->get('calendarIds');
 
-$fnum = $jinput->get('rowid', null);
-$itemid = $jinput->get('Itemid'); 
-$reload = $jinput->get('rq', 0);
-$cal = $jinput->get('catid', 105);
-$valeurCatId = $jinput->getValue('jos_dpcalendar_events___catid');
-$title = $jinput->getValue('jos_dpcalendar_events___title');
-$description = $jinput->getValue('jos_dpcalendar_events___description');
-$valStartDateTime = $jinput->getValue('jos_dpcalendar_events___start_date');
-$valEndDateTime = $jinput->getValue('jos_dpcalendar_events___end_date');
-$startTime = $jinput->getValue('jos_dpcalendar_events___start_time');
-$endTime = $jinput->getValue('jos_dpcalendar_events___end_time');
-$startDateTime = explode(' ', $valStartDateTime[date]);
-$startDate = $startDateTime[0];
-$startTime = $startDateTime[1];
+$fnum               = $jinput->get('rowid', null);
+$itemid             = $jinput->get('Itemid'); 
+$reload             = $jinput->get('rq', 0);
+$cal                = $jinput->get('catid', 105);
+$valeurCatId        = $jinput->getValue('jos_dpcalendar_events___catid');
+$title              = $jinput->getValue('jos_dpcalendar_events___title');
+$description        = $jinput->getValue('jos_dpcalendar_events___description');
+$valStartDateTime   = $jinput->getValue('jos_dpcalendar_events___start_date');
+$valEndDateTime     = $jinput->getValue('jos_dpcalendar_events___end_date');
+$startTime          = $jinput->getValue('jos_dpcalendar_events___start_time');
+$endTime            = $jinput->getValue('jos_dpcalendar_events___end_time');
+$startDateTime      = explode(' ', $valStartDateTime[date]);
+$startDate          = $startDateTime[0];
+$startTime          = $startDateTime[1];
 
 $endDateTime = explode(' ', $valEndDateTime[date]);
 $endDate = $endDateTime[0];
 $endTime = $endDateTime[1];
 
-$myUrl = "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; 
-$urlExplode = explode('?', $myUrl);
-$url = $urlExplode[1];
-$rowIdExplode = explode('&', $url);
-$rowId = $rowIdExplode[3];
-$valueRowId = explode('=', $rowId);
-$eventId = $valueRowId[1];
-$compareForm = $valueRowId[0];
 
+// The category ID is only used as a means to get to the calendarID.
+$catIdValue = $jinput->getValue('jos_dpcalendar_events___catid');
 
+// Using the category ID we can get the current calendars ID.
+$query = "SELECT calId FROM jos_categories WHERE id = ".$catIdValue[0];
 
-if ($valeurCatId == array('105')) {
-    $calendarID = $calendarID1;
-} elseif ($valeurCatId == array('106')){
-   $calendarID = $calendarID2;
-} elseif ($valeurCatId == array('107')){
-   $calendarID = $calendarID3;
-} elseif ($valeurCatId == array('108')){
-   $calendarID = $calendarID4;
-} elseif ($valeurCatId == array('109')){
-   $calendarID = $calendarID5;
-} elseif ($valeurCatId == array('110')){
-   $calendarID = $calendarID6;
-} elseif ($valeurCatId == array('111')){
-   $calendarID = $calendarID7;
-} elseif ($valeurCatId == array('112')){
-   $calendarID = $calendarID8;
-} elseif ($valeurCatId == array('113')){
-   $calendarID = $calendarID9;
-} elseif ($valeurCatId == array('114')){
-   $calendarID = $calendarID10;
-} elseif ($valeurCatId == array('115')){
-   $calendarID = $calendarID11;
-} elseif ($valeurCatId == array('116')){
-   $calendarID = $calendarID12;
-} elseif ($valeurCatId == array('117')){
-   $calendarID = $calendarID13;
-} elseif ($valeurCatId == array('118')){
-   $calendarID = $calendarID14;
-} elseif ($valeurCatId == array('119')){
-   $calendarID = $calendarID15;
-} elseif ($valeurCatId == array('120')){
-   $calendarID = $calendarID16;
-} elseif ($valeurCatId == array('121')){
-   $calendarID = $calendarID17;
-} elseif ($valeurCatId == array('122')){
-   $calendarID = $calendarID18;
-} elseif ($valeurCatId == array('123')){
-   $calendarID = $calendarID19;
-} elseif ($valeurCatId == array('124')){
-   $calendarID = $calendarID20;
+try {
+    $db->setQuery($query);
+    $calendarID = $db->loadResult(); 
+} catch (Exception $e) {
+    die($e->getMessage());
 }
+
+// Here we are simply checking that the calendar ID is in the params, this isn't really useful but helps add a safety check.
+if (!in_array($calendarID, explode(",", $calendarIDs)))
+    die('Error: current calendar ID cannot be found in params.');
+
+// Gets the arguements to the URI, used for knowing whether we are updating an event or making a new one.
+// uri['rowid'] = event ID
+parse_str($_SERVER['REQUEST_URI'], $uri);
 
 try {
 
     $sync = new EmundusModelCalendar;
-    $sync->deleteEvent($calendarID,$eventId);
+    $sync->deleteEvent($calendarID, $uri['rowid']);
     die("<div class='col-md-12'><center><h1>Record Deleted.</h1></center></div>");
 
 } catch(Exception $e) {
