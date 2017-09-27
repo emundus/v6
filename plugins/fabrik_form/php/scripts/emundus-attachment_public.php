9981b37984a3c90968f5b42fa8af0d3b9098df05
Benjamin <benjamin@emundus.fr>
2017-09-28 01:01:09 +0200

    add try catch

--- plugins/fabrik_form/php/scripts/emundus-attachment_public.php
+++ plugins/fabrik_form/php/scripts/emundus-attachment_public.php
@@ -45,89 +45,91 @@ JLog::addLogger(
     array('com_emundus')
 );
 
-$db->setQuery('SELECT student_id, attachment_id, keyid FROM #__emundus_files_request WHERE keyid="'.mysql_real_escape_string($key_id).'"');
-$file_request=$db->loadObject();
+try {
 
-if ($files['jos_emundus_uploads___filename']['size'] == 0) {
-		$link_upload = $baseurl.'index.php?option=com_fabrik&view=form&formid=68&jos_emundus_uploads___user_id[value]='.$sid.'&jos_emundus_uploads___attachment_id[value]='.$file_request->attachment_id.'&sid='.$sid.'&keyid='.$key_id;
-		if($files['jos_emundus_uploads___filename']['error'] == 4)
-			JError::raiseWarning(500, JText::_('WARNING: No file selected, please select a file','error')); // no file
-		else
-			JError::raiseWarning(500, JText::_('WARNING: You just upload an empty file, please check out your file','error')); // file empty
-		$mainframe->redirect($link_upload);
-		exit();
-}
+	$db->setQuery('SELECT student_id, attachment_id, keyid FROM #__emundus_files_request WHERE keyid="'.mysql_real_escape_string($key_id).'"');
+	$file_request=$db->loadObject();
 
-if ($user_id != $file_request->student_id || $attachment_id != $file_request->attachment_id) {
-	// die('data1:'.$file_request->student_id.'-'.$user_id.'-'.$file_request->attachment_id.'-'.$attachment_id.'-'.$key_id.'-'.$db->getErrorMsg());
-	JLog::add("PLUGIN emundus-attachment_public [".$key_id."]: ".JText::_("ERROR_ACCESS_DENIED"), JLog::ERROR, 'com_emundus');
-	header('Location: '.$baseurl.'index.php');
-	exit();
-}
-
-$student = &JUser::getInstance($user_id);
-
-
-if (!isset($student)) {
-	// die('data2:'.$key_id.'-'.$user_id.'-'.$attachment_id);
-	JLog::add("PLUGIN emundus-attachment_public [".$key_id."]: ".JText::_("ERROR_STUDENT_NOT_SET"), JLog::ERROR, 'com_emundus');
-	header('Location: '.$baseurl.'index.php');
-	exit();
-}
-
-$query = 'SELECT profile FROM #__emundus_users WHERE user_id='.$user_id.'';
-$db->setQuery( $query );
-$profile=$db->loadResult();
-//die('data2:'.$profile.'-'.print_r($attachement_params, true).'-'.print_r($upload, true).'-'.$db->getErrorMsg());
-
-// 1. Récupération des informations sur l'étudiant et le fichier qui doit être chargé par la tierce personne
-$query = 'SELECT ap.displayed, attachment.lbl, attachment.value
-			FROM #__emundus_setup_attachments AS attachment
-			LEFT JOIN #__emundus_setup_attachment_profiles AS ap ON attachment.id = ap.attachment_id AND ap.profile_id='.$profile.'
-			WHERE attachment.id ='.$attachment_id.' ';
-$db->setQuery( $query );
-$attachement_params=$db->loadObject();
-//die('data3:'.$attachement_params->displayed.'-'.$attachement_params->lbl.'-'.$attachment_id.'-'.$profile.'-'.$db->getErrorMsg().'-'.$db->getQuery());
-// 2. Récupération des données du fichier qui vient d'être uplodé par la tierce personne
-$query = 'SELECT id, filename FROM #__emundus_uploads WHERE attachment_id='.$attachment_id.' AND user_id='.$user_id.' ORDER BY id DESC';
-$db->setQuery( $query );
-$upload=$db->loadObject();
-
-if (count($upload) == 0) {
-	JLog::add("PLUGIN emundus-attachment_public [".$key_id."]: ".JText::_("ERROR_FILE_NOT_FOUND"), JLog::ERROR, 'com_emundus');
-	die(JText::_("ERROR_FILE_NOT_FOUND"));
-}
-$nom = strtolower(preg_replace(array('([\40])','([^a-zA-Z0-9-])','(-{2,})'),array('_','','_'),preg_replace('/&([A-Za-z]{1,2})(grave|acute|circ|cedil|uml|lig);/','$1',htmlentities($student->name,ENT_NOQUOTES,'UTF-8'))));
-if(!isset($attachement_params->displayed) || $attachement_params->displayed === '0') $nom.= "_locked";
-$nom .= $attachement_params->lbl.rand().'.'.end(explode('.', $upload->filename));
-
-if(!file_exists(EMUNDUS_PATH_ABS.$user_id)) {
-	if (!mkdir(EMUNDUS_PATH_ABS.$user_id, 0777, true) || !copy(EMUNDUS_PATH_ABS.'index.html', EMUNDUS_PATH_ABS.$user_id.DS.'index.html')) {
-		JLog::add("PLUGIN emundus-attachment_public [".$key_id."]: ".JText::_("ERROR_CANNOT_CREATE_USER_FILE"), JLog::ERROR, 'com_emundus');
-		die(JError::raiseWarning(500, 'ERROR_CANNOT_CREATE_USER_FILE'));
+	if ($files['jos_emundus_uploads___filename']['size'] == 0) {
+			$link_upload = $baseurl.'index.php?option=com_fabrik&view=form&formid=68&jos_emundus_uploads___user_id[value]='.$sid.'&jos_emundus_uploads___attachment_id[value]='.$file_request->attachment_id.'&sid='.$sid.'&keyid='.$key_id;
+			if($files['jos_emundus_uploads___filename']['error'] == 4)
+				JError::raiseWarning(500, JText::_('WARNING: No file selected, please select a file','error')); // no file
+			else
+				JError::raiseWarning(500, JText::_('WARNING: You just upload an empty file, please check out your file','error')); // file empty
+			$mainframe->redirect($link_upload);
+			exit();
 	}
-}
-	
 
-if (!rename(JPATH_SITE.$upload->filename, EMUNDUS_PATH_ABS.$user_id.DS.$nom)) {
-	JLog::add("PLUGIN emundus-attachment_public [".$key_id."]: ".JText::_("ERROR_MOVING_UPLOAD_FILE"), JLog::ERROR, 'com_emundus');
-	die(JText::_("ERROR_MOVING_UPLOAD_FILE"));
-}
+	if ($user_id != $file_request->student_id || $attachment_id != $file_request->attachment_id) {
+		// die('data1:'.$file_request->student_id.'-'.$user_id.'-'.$file_request->attachment_id.'-'.$attachment_id.'-'.$key_id.'-'.$db->getErrorMsg());
+		JLog::add("PLUGIN emundus-attachment_public [".$key_id."]: ".JText::_("ERROR_ACCESS_DENIED"), JLog::ERROR, 'com_emundus');
+		header('Location: '.$baseurl.'index.php');
+		exit();
+	}
 
-$db->setQuery('UPDATE #__emundus_uploads SET filename="'.$nom.'" WHERE id='.$upload->id);
-$db->execute();
-$query = 'UPDATE #__emundus_files_request SET uploaded=1, filename="'.$nom.'" WHERE keyid="'.$key_id.'"';
-$db->setQuery( $query );
-$db->execute();
+	$student = &JUser::getInstance($user_id);
 
-// Récupération des données du mail
-$query = 'SELECT id, subject, emailfrom, name, message
-				FROM #__emundus_setup_emails
-				WHERE lbl="attachment"';
-$db->setQuery( $query );
-$obj=$db->loadObject();
-	
-	
+
+	if (!isset($student)) {
+		// die('data2:'.$key_id.'-'.$user_id.'-'.$attachment_id);
+		JLog::add("PLUGIN emundus-attachment_public [".$key_id."]: ".JText::_("ERROR_STUDENT_NOT_SET"), JLog::ERROR, 'com_emundus');
+		header('Location: '.$baseurl.'index.php');
+		exit();
+	}
+
+	$query = 'SELECT profile FROM #__emundus_users WHERE user_id='.$user_id.'';
+	$db->setQuery( $query );
+	$profile=$db->loadResult();
+	//die('data2:'.$profile.'-'.print_r($attachement_params, true).'-'.print_r($upload, true).'-'.$db->getErrorMsg());
+
+	// 1. Récupération des informations sur l'étudiant et le fichier qui doit être chargé par la tierce personne
+	$query = 'SELECT ap.displayed, attachment.lbl, attachment.value
+				FROM #__emundus_setup_attachments AS attachment
+				LEFT JOIN #__emundus_setup_attachment_profiles AS ap ON attachment.id = ap.attachment_id AND ap.profile_id='.$profile.'
+				WHERE attachment.id ='.$attachment_id.' ';
+	$db->setQuery( $query );
+	$attachement_params=$db->loadObject();
+	//die('data3:'.$attachement_params->displayed.'-'.$attachement_params->lbl.'-'.$attachment_id.'-'.$profile.'-'.$db->getErrorMsg().'-'.$db->getQuery());
+	// 2. Récupération des données du fichier qui vient d'être uplodé par la tierce personne
+	$query = 'SELECT id, filename FROM #__emundus_uploads WHERE attachment_id='.$attachment_id.' AND user_id='.$user_id.' ORDER BY id DESC';
+	$db->setQuery( $query );
+	$upload=$db->loadObject();
+
+	if (count($upload) == 0) {
+		JLog::add("PLUGIN emundus-attachment_public [".$key_id."]: ".JText::_("ERROR_FILE_NOT_FOUND"), JLog::ERROR, 'com_emundus');
+		die(JText::_("ERROR_FILE_NOT_FOUND"));
+	}
+	$nom = strtolower(preg_replace(array('([\40])','([^a-zA-Z0-9-])','(-{2,})'),array('_','','_'),preg_replace('/&([A-Za-z]{1,2})(grave|acute|circ|cedil|uml|lig);/','$1',htmlentities($student->name,ENT_NOQUOTES,'UTF-8'))));
+	if(!isset($attachement_params->displayed) || $attachement_params->displayed === '0') $nom.= "_locked";
+	$nom .= $attachement_params->lbl.rand().'.'.end(explode('.', $upload->filename));
+
+	if(!file_exists(EMUNDUS_PATH_ABS.$user_id)) {
+		if (!mkdir(EMUNDUS_PATH_ABS.$user_id, 0777, true) || !copy(EMUNDUS_PATH_ABS.'index.html', EMUNDUS_PATH_ABS.$user_id.DS.'index.html')) {
+			JLog::add("PLUGIN emundus-attachment_public [".$key_id."]: ".JText::_("ERROR_CANNOT_CREATE_USER_FILE"), JLog::ERROR, 'com_emundus');
+			die(JError::raiseWarning(500, 'ERROR_CANNOT_CREATE_USER_FILE'));
+		}
+	}
+		
+
+	if (!rename(JPATH_SITE.$upload->filename, EMUNDUS_PATH_ABS.$user_id.DS.$nom)) {
+		JLog::add("PLUGIN emundus-attachment_public [".$key_id."]: ".JText::_("ERROR_MOVING_UPLOAD_FILE"), JLog::ERROR, 'com_emundus');
+		die(JText::_("ERROR_MOVING_UPLOAD_FILE"));
+	}
+
+	$db->setQuery('UPDATE #__emundus_uploads SET filename="'.$nom.'" WHERE id='.$upload->id);
+	$db->execute();
+	$query = 'UPDATE #__emundus_files_request SET uploaded=1, filename="'.$nom.'" WHERE keyid="'.$key_id.'"';
+	$db->setQuery( $query );
+	$db->execute();
+
+	// Récupération des données du mail
+	$query = 'SELECT id, subject, emailfrom, name, message
+					FROM #__emundus_setup_emails
+					WHERE lbl="attachment"';
+	$db->setQuery( $query );
+	$obj=$db->loadObject();
+		
+		
 	//$user = JFactory::getUser();
 	
 	$patterns = array ('/\[ID\]/', '/\[NAME\]/', '/\[EMAIL\]/','/\n/');
@@ -175,6 +177,11 @@ $obj=$db->loadObject();
         $db->setQuery($sql);
         $db->execute();
     }
+}
+catch (Exception $e) {
+    // catch any database errors.
+    JLog::add(JUri::getInstance().' :: USER ID : '.JFactory::getUser()->id.' -> '.$query, JLog::ERROR, 'com_emundus');
+}
 	
 // if($alert_new_attachment == 1){
 //	// Envoie d'une copie au user, le CC ou BCC n'est pas utilisé car cela bug avec les serveurs de Paris1
