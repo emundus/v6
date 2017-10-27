9c8b0690850578fd5cc837ae99379e78e87d2c0c
Hugo Moracchini <hugo4400@gmail.com>
2017-10-27 09:59:49 +0200

    fix merge issue

--- components/com_emundus/views/evaluation/tmpl/data.php
+++ components/com_emundus/views/evaluation/tmpl/data.php
@@ -36,7 +36,7 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
 										<strong>
 											<?php echo JText::_($v); ?>
 										</strong>
-										
+
 									<?php elseif( $kl == 'check'):?>
 										<label for="em-check-all">
 											<input type="checkbox" value="-1" id="em-check-all" class="em-check" style="width:20px !important;"/>
@@ -52,11 +52,11 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
 								</p>
 							</th>
 						<?php endforeach; ?>
-						
+
 					</tr>
-					
+
 				</thead>
-				<tbody>							
+				<tbody>
 				<?php foreach ($this->datas as $key => $line):?>
 					<?php if($key != 0): ?>
 						<tr>
@@ -68,7 +68,7 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
 										<?php if($k == 'check'): ?>
 											<label for = "<?php echo $line['fnum']->val ?>_check">
 												<input type="checkbox" name="<?php echo $line['fnum']->val ?>_check" id="<?php echo $line['fnum']->val ?>_check" class='em-check' style="width:20px !important;"/>
-												<?php 
+												<?php
 													$tab = explode('-', $key);
 													echo ($tab[1] + 1 + $this->pagination->limitstart);
 													?>
@@ -79,8 +79,9 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
 											<a href="#<?php echo $value->val ?>|open" id="<?php echo $value->val ?>" class="em_file_open">
 												<div class="em_list_photo"><?php echo $value->photo; ?></div>
 												<div class="em_list_text">
-													<span class="em_list_text" title="<?php echo $value->val ?>"> <strong> <?php echo strtoupper($value->emUser['lastname'])." ".ucfirst(strtolower($value->emUser['firstname'])); ?></strong></span>
+													<span class="em_list_text" title="<?php echo $value->val ?>"> <strong> <?php echo $value->user->name; ?></strong></span>
 													<div class="em_list_email"><?php echo $value->user->email; ?></div>
+													<div class="em_list_email"><?php echo $value->user->id; ?></div>
 												</div>
 											</a>
 										<?php elseif($k == "access"):?>
@@ -107,7 +108,7 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
 											<?php endif; ?>
 										<?php endif; ?>
 									</div>
-									
+
 								</td>
 							<?php endif; ?>
 							<?php endforeach; ?>
@@ -131,30 +132,9 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
 				<option value="100" <?php if($this->pagination->limit == 100){echo "selected=true";}?>>100</option>
 			</select>
 			<div>
-<<<<<<< HEAD
-				<div style="width: 20%; float:left; margin-top:19px;">
-					<label for = "pager-select"><?php echo JText::_('DISPLAY')?></label>
-					<select name="pager-select" class="chzn-select" id="pager-select" style="width: 70px;margin-top:0px; margin-right:50px;">
-						<option value="0" <?php if($this->pagination->limit == 0){echo "selected=true";}?>><?php echo JText::_('ALL')?></option>
-						<option value="5" <?php if($this->pagination->limit == 5){echo "selected=true";}?>>5</option>
-						<option value="10" <?php if($this->pagination->limit == 10){echo "selected=true";}?>>10</option>
-						<option value="15" <?php if($this->pagination->limit == 15){echo "selected=true";}?>>15</option>
-						<option value="20" <?php if($this->pagination->limit == 20){echo "selected=true";}?>>20</option>
-						<option value="25" <?php if($this->pagination->limit == 25){echo "selected=true";}?>>25</option>
-						<option value="30" <?php if($this->pagination->limit == 30){echo "selected=true";}?>>30</option>
-						<option value="50" <?php if($this->pagination->limit == 50){echo "selected=true";}?>>50</option>
-						<option value="100" <?php if($this->pagination->limit == 100){echo "selected=true";}?>>100</option>
-					</select>
-				</div>
-				<div style="width: 80%; float:left;">
-					<ul class="pagination pagination-sm">
-						<li><a href="#em-data" id="<?php echo $this->pagination->{'pagesStart'}?>"><<</a></li>
-						<?php if($this->pagination->{'pagesTotal'} > 15):?>
-=======
 				<ul class="pagination pagination-sm">
 					<li><a href="#em-data" id="<?php echo $this->pagination->{'pagesStart'}?>"><<</a></li>
 					<?php if($this->pagination->{'pagesTotal'} > 15):?>
->>>>>>> canceling thead fixing. automatic sorting little icon on final grade resolved.user deletion and adding(with or not email config.). page refresh after an action.
 
 						<?php for($i = 1; $i <= 5; $i++ ):?>
 							<li <?php if($this->pagination->{'pagesCurrent'} == $i){echo 'class="active"';}?>><a id="<?php echo $i?>" href="#em-data"><?php echo $i?></a></li>
@@ -169,116 +149,6 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
 								<li <?php if($this->pagination->{'pagesCurrent'} == $i){echo 'class="active"';}?>><a id="<?php echo $i?>" href="#em-data"><?php echo $i?></a></li>
 							<?php endfor;?>
 						<?php endif;?>
-<<<<<<< HEAD
-						<li><a href="#em-data" id="<?php echo $this->pagination->{'pagesTotal'}?>">>></a></li>
-					</ul>
-				</div>
-			</div>
-			<?php echo $this->pagination->getResultsCounter(); ?>
-		</div>
-		<div class="em-data-container">
-			<div id="table-scroll" class="table-scroll">
-				<div id="false-table" class="false-table" ></div>
-				<div class="table-wrap" id="table-wrap">
- 					<table class="table table-striped table-hover" id="em-data">
-						<thead>
-							<tr>
-								<?php foreach($this->datas[0] as $kl => $v): ?>
-									<th title="<?php echo strip_tags(JText::_($v)); ?>" id="<?php echo $kl?>" >
-										<p class="em-cell">
-											<?php if($kl == 'check'): ?>
-												<label for="em-check-all">
-													<input type="checkbox" value="-1" id="em-check-all" class="em-check" style="width:20px !important;"/>
-													<span>#</span>
-												</label>
-												<label class="em-hide em-check-all-all" for="em-check-all-all">
-													<input class="em-check-all-all em-hide" type="checkbox" name="check-all-all" value="all" id="em-check-all-all" style="width:20px !important;"/>
-													<span class="em-hide em-check-all-all"><?php echo JText::_('COM_EMUNDUS_CHECK_ALL_ALL')?></span>
-												</label>
-											<?php elseif(@$this->lists['order'] == $kl):?>
-												<?php if(@$this->lists['order_dir'] == 'desc'):?>
-													<span class="glyphicon glyphicon-sort-by-attributes-alt"></span>
-												<?php else:?>
-													<span class="glyphicon glyphicon-sort-by-attributes"></span>
-												<?php endif;?>
-												<strong>
-													<?php echo JText::_($v); ?>
-												</strong>
-											<?php else:?>
-												<?php echo JText::_($v);?>
-											<?php endif;?>
-										</p>
-									</th>
-								<?php endforeach; ?>
-								<th></th>
-								<th></th>
-							</tr>
-
-						</thead>
-
-						<tbody>
-						<?php foreach ($this->datas as $key => $line):?>
-							<?php if($key != 0): ?>
-								<tr>
-									<?php foreach ($line as $k => $value):?>
-									<?php if($k != 'evaluation_id'): ?>
-
-										<td <?php if($k == 'check' && $value->class != null) {echo 'class="'.$value->class.'"';}?>>
-											<div class="em-cell" >
-												<?php if($k == 'check'): ?>
-													<label for = "<?php echo $line['fnum']->val ?>_check">
-														<input type="checkbox" name="<?php echo $line['fnum']->val ?>_check" id="<?php echo $line['fnum']->val ?>_check" class='em-check' style="width:20px !important;"/>
-														<?php
-															$tab = explode('-', $key);
-															echo ($tab[1] + 1 + $this->pagination->limitstart);
-															?>
-													</label>
-												<?php elseif($k == 'status'):?>
-													<span class="label label-<?php echo $value->status_class ?>" title="<?php echo $value->val ?>"><?php echo $value->val ?></span>
-												<?php elseif($k == 'fnum'):?>
-													<a href="#<?php echo $value->val ?>|open" id="<?php echo $value->val ?>" class="em_file_open">
-														<div class="em_list_photo"><?php echo $value->photo; ?></div>
-														<div class="em_list_text">
-															<span class="em_list_text" title="<?php echo $value->val ?>"> <strong> <?php echo $value->user->name; ?></strong></span>
-															<div class="em_list_email"><?php echo $value->user->email; ?></div>
-															<div class="em_list_email"><?php echo $value->user->id; ?></div>
-														</div>
-													</a>
-												<?php elseif($k == "access"):?>
-													<?php echo $this->accessObj[$line['fnum']->val]?>
-												<?php elseif($k == "id_tag"):?>
-													<?php echo $this->colsSup['id_tag'][$line['fnum']->val]?>
-												<?php else:?>
-													<?php if ($value->type == 'text' ) :?>
-														<?php echo strip_tags($value->val); ?>
-													<?php elseif ($value->type == "textarea" && !empty($value->val) && strlen($value->val) > 200) :?>
-														<?php echo substr($value->val,0,200)." ..."; ?>
-													<?php elseif ($value->type == "date")  :?>
-														<strong>
-															<?php if (!isset($value->val) || $value->val == "0000-00-00 00:00:00") :?>
-															<?php else: ?>
-																<?php
-																	$formatted_date = DateTime::createFromFormat('Y-m-d H:i:s', $value->val);
-																	echo $formatted_date->format("M j, Y, H:i");
-																?>
-															<?php endif; ?>
-														</strong>
-													<?php else: ?>
-														<?php echo $value->val; ?>
-													<?php endif; ?>
-												<?php endif; ?>
-											</div>
-
-										</td>
-									<?php endif; ?>
-									<?php endforeach; ?>
-								</tr>
-							<?php endif;?>
-						<?php  endforeach;?>
-						</tbody>
-					</table>
-				</div>
-=======
 						<li class="disabled"><span>...</span></li>
 						<?php for($i = ($this->pagination->{'pagesTotal'} - 4); $i <= $this->pagination->{'pagesTotal'}; $i++ ):?>
 							<li <?php if($this->pagination->{'pagesCurrent'} == $i){echo 'class="active"';}?>><a id="<?php echo $i?>" href="#em-data"><?php echo $i?></a></li>
@@ -290,7 +160,6 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
 					<?php endif;?>
 					<li><a href="#em-data" id="<?php echo $this->pagination->{'pagesTotal'}?>">>></a></li>
 				</ul>
->>>>>>> canceling thead fixing. automatic sorting little icon on final grade resolved.user deletion and adding(with or not email config.). page refresh after an action.
 			</div>
 		</div>
 	<?php else:?>
