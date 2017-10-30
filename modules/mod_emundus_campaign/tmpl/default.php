a2971bbe18de1a9453dd9319e6faf6a8b548f627
Hugo Moracchini <hugo4400@gmail.com>
2017-10-30 09:46:40 +0100

    Fixed mod_campaign using a version of bootstrap that caused glitches

--- modules/mod_emundus_campaign/tmpl/default.php
+++ modules/mod_emundus_campaign/tmpl/default.php
@@ -350,7 +350,7 @@ if ($locallang == "fr-FR") {
 </form>
 
 <script src="media/com_emundus/js/jquery.cookie.js" type="text/javascript"></script>
-<script src="media/com_emundus/lib/bootstrap-336/js/bootstrap.min.js" type="text/javascript"></script>
+<script src="media/jui/js/bootstrap.min.js" type="text/javascript"></script>
 <script type="text/javascript">
     jQuery(document).ready(function() {
         var tabsidshow = jQuery.cookie("tabactive");
