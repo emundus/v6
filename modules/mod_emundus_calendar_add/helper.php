67df3118c5e3fdd051424493a72cbc5eb497c6ba
Hugo Moracchini <hugo4400@gmail.com>
2017-11-02 14:08:42 +0100

    Remove inclusions of google calendar API in module.

--- modules/mod_emundus_calendar_add/helper.php
+++ modules/mod_emundus_calendar_add/helper.php
@@ -1,16 +1,6 @@
 <?php
     defined('_JEXEC') or die('Access Deny');
 
-    require_once(JPATH_LIBRARIES.DS."php-google-api-client".DS."vendor".DS.'autoload.php');
-    define('APPLICATION_NAME', 'Google Calendar API PHP Emundus');
-    define('CREDENTIALS_PATH', JPATH_LIBRARIES.DS.'php-google-api-client'.DS.'credentials'.DS.'calendar-php-quickstart.json');
-    define('CLIENT_SECRET_PATH', JPATH_LIBRARIES.DS.'php-google-api-client'.DS.'certificates'.DS.'client_secret.json');
-
-    define('SCOPES', implode(' ', array(
-        Google_Service_Calendar::CALENDAR) // CALENDAR_READONLY
-    ));
-
-
     class modEmundusCalendarAddHelper {
 
         public function getPrograms() {
