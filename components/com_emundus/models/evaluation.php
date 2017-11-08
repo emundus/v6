890dd7f34174d7be682513bd30c5517997b35b3d
Hugo Moracchini <hugo4400@gmail.com>
2017-11-08 11:30:21 +0100

    Fixed pre-filters on status.

--- components/com_emundus/models/evaluation.php
+++ components/com_emundus/models/evaluation.php
@@ -957,7 +957,7 @@ class EmundusModelEvaluation extends JModelList
 						break;
 					case 'status':
 						// Code is redundant because $filt_menu has the status value as well.
-						/*if ($value)
+						if ($value)
 						{
 							if ( $value[0] == "%" || !isset($value[0]) )
 								$query['q'] .= ' ';
@@ -966,7 +966,7 @@ class EmundusModelEvaluation extends JModelList
 								//$query['q'] .= ' and c.status IN (' . implode(',', $value) . ') ';
 							}
 						}
-						break;*/
+						break;
 					case 'tag':
                         if ($value)
                         {
