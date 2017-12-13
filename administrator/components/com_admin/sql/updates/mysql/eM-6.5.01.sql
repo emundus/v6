9b32b037f95fc8d75c8712d7ba0bb31cbcb0fd82
Hugo Moracchini <hugo4400@gmail.com>
2017-12-13 16:51:10 +0100

    Added a missing SQL migration.

---
+++ administrator/components/com_admin/sql/updates/mysql/eM-6.5.01.sql
@@ -0,0 +1,1 @@
+ALTER TABLE `jos_emundus_setup_programmes` ADD `fabrik_admission_group_id` INT(7) NULL AFTER `url`, ADD `fabrik_applicant_admission_group_id` INT(7) NULL AFTER `fabrik_admission_group_id`;
