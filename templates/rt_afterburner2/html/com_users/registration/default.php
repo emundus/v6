0a2400218a27cbf363ca83a44769c347131bd59e
latifouassirou <latifouassirou9@gmail.com>
2018-02-20 12:07:02 +0100

    redirection after send mail (account already exists)

--- templates/rt_afterburner2/html/com_users/registration/default.php
+++ templates/rt_afterburner2/html/com_users/registration/default.php
@@ -116,6 +116,10 @@ foreach ($messages as $message) {
 
 				$msg = JText::_('COM_EMUNDUS_EMAIL_SENT');
 			}
+			$app->enqueueMessage(JText::_('COM_USERS_REGISTER_CHECK_YOUR_MAIL'), 'notice');
+			$app->redirect('index.php');
+			
+
 
 
 			echo json_encode((object)array('status' => $res, 'msg' => $msg));
