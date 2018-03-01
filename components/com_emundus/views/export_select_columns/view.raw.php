78748a71d2bd918179ca3c8dbae58b18b1f32bff
Hugo Moracchini <hugo4400@gmail.com>
2018-03-01 10:21:45 +0100

    Fix strange git issue: export_select_columns:views.raw.php

--- components/com_emundus/views/export_select_columns/view.raw.php
+++ components/com_emundus/views/export_select_columns/view.raw.php
@@ -7,34 +7,34 @@
  * @license    GNU/GPL
  * @author     Benjamin Rivalland
 */
- 
+
 // no direct access
- 
+
 defined( '_JEXEC' ) or die( 'Restricted access' );
- 
+
 jimport( 'joomla.application.component.view');
- 
+
 /**
  * HTML View class for the Emundus Component
  *
  * @package    Emundus
  */
- 
+
 class EmundusViewExport_select_columns extends JViewLegacy
 {
 	var $_user = null;
 	var $_db = null;
-	
+
 	function __construct($config = array()){
 		require_once (JPATH_COMPONENT.DS.'helpers'.DS.'files.php');
 		require_once (JPATH_COMPONENT.DS.'helpers'.DS.'access.php');
-		
+
 		$this->_user = JFactory::getUser();
 		$this->_db = JFactory::getDBO();
 
 		parent::__construct($config);
 	}
-	
+
     function display($tpl = null) {
 
         require_once (JPATH_COMPONENT.DS.'models'.DS.'admission.php');
@@ -44,16 +44,20 @@ class EmundusViewExport_select_columns extends JViewLegacy
         $prg    = $jinput->get('code', null);
         $view   = $jinput->get('viewcall', null);
         $form   = $jinput->get('form', null);
-        
+        $year   = $jinput->get('year', null);
+
 
         $code       = array();
+        $years       = array();
         $code[]     = $prg;
-		
+        $years[]     = $year;
+
         $current_user = JFactory::getUser();
 
         if ($view == "evaluation" || $form == "decision" || $form == "admission" || $form == "evaluation") {
             $session = JFactory::getSession();
             $params['programme'] = $code;
+            $params['schoolyear'] = $years;
             $session->set('filt_params', $params);
         }
 
@@ -67,12 +71,12 @@ class EmundusViewExport_select_columns extends JViewLegacy
         if ($form == "decision")
             $elements = $m_admission->getAdmissionElementsName(1, 1);
         elseif ($form == "admission")
-            $elements = $m_admission->getApplicantAdmissionElementsName(0, 0);     
+            $elements = $m_admission->getApplicantAdmissionElementsName(0, 0);
         elseif ($form == "evaluation")
-            $elements = $m_eval->getEvaluationElementsName(1, 1);  
+            $elements = $m_eval->getEvaluationElementsName(1, 1);
         else
 		    $elements = EmundusHelperFiles::getElements($code, $years);
-		
+
         $this->assignRef('elements', $elements);
         $this->assignRef('form', $form);
 		parent::display($tpl);
