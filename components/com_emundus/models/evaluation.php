28e14a228dca3df9094129a836fd5f38f7449a35
Hugo Moracchini <hugo4400@gmail.com>
2018-03-07 17:37:22 +0100

    Fix a bug breaking the status filters on evaluations.

--- components/com_emundus/models/evaluation.php
+++ components/com_emundus/models/evaluation.php
@@ -967,7 +967,7 @@ class EmundusModelEvaluation extends JModelList
 						break;
 					case 'status':
 						// Code is redundant because $filt_menu has the status value as well.
-						/*if ($value)
+						if ($value)
 						{
 							if ( $value[0] == "%" || !isset($value[0]) )
 								$query['q'] .= ' ';
@@ -975,7 +975,7 @@ class EmundusModelEvaluation extends JModelList
 							{
 								//$query['q'] .= ' and c.status IN (' . implode(',', $value) . ') ';
 							}
-						}*/
+						}
 						break;
 					case 'tag':
                         if ($value)
