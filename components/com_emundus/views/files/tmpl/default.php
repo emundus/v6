a2739c63e8d5aaec5bf73cd32ab28b8eaa82fb19
latifouassirou <latifouassirou9@gmail.com>
2018-03-15 18:09:35 +0100

    modal body bugfix

--- components/com_emundus/views/files/tmpl/default.php
+++ components/com_emundus/views/files/tmpl/default.php
@@ -90,48 +90,7 @@ JHTML::stylesheet('media/com_emundus/css/emundus_panel.css' );
 </div>
 
 
-<div class="modal fade" id="em-modal-actions" style="z-index:99999" tabindex="-1" role="dialog" aria-labelledby="em-modal-actions" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-				<table style="width: 95%">
-					<tr>
-						<td>
-							<h4 class="modal-title" id="em-modal-actions-title"><?php echo JText::_('TITLE');?></h4>
-						</td>
-						<td style="text-align: right;" id="can-val">
-							<button type="button" class="btn btn-danger" data-dismiss="modal"><?php echo JText::_('CANCEL')?></button>
-							<button type="button" class="btn btn-success"><?php echo JText::_('OK');?></button>
-						</td>
-					</tr>
-				</table>
-      </div>
-      <div class="modal-body">
 
-      </div>
-      <div class="modal-footer">
-        
-      </div>
-    </div>
-  </div>
-</div>
-<div class="modal fade" id="em-modal-form" style="z-index:99999" tabindex="-1" role="dialog" aria-labelledby="em-modal-actions" aria-hidden="true">
-  <div class="modal-dialog modal-lg">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-        <h4 class="modal-title" id="em-modal-actions-title"><?php echo JText::_('LOADING');?></h4>
-      </div>
-      <div class="modal-body">
-      	<img src="<?php echo JURI::base(true); ?>media/com_emundus/images/icones/loader-line.gif">
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-danger" data-dismiss="modal"><?php echo JText::_('CANCEL')?></button>
-      </div>
-    </div>
-  </div>
-</div>
 
 
 
