f97e0114c446074d0511ff6f21418b45fffc0249
latifouassirou <latifouassirou9@gmail.com>
2018-05-31 11:31:24 +0200

    route to files, eval, users in homepage bugfix

--- modules/mod_emunduspanel/tmpl/default.php
+++ modules/mod_emunduspanel/tmpl/default.php
@@ -48,11 +48,6 @@ if (isset($user->profile) && $user->profile > 0) {
     <?php
     if(!in_array($user->profile, $applicant_profiles) || $user->fnum){
         foreach ($tab as $t) {
-            $t = str_replace("?view=files","",$t);
-            $t = str_replace("?view=evaluation","",$t);
-            $t = str_replace("?view=decision","",$t);
-            $t = str_replace("?view=admission","",$t);
-
             echo '<div class="five wide column element_home_emundus">' . $t . '</div>';
         }
     }
