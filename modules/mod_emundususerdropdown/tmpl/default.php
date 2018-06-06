c0ab0580a093a83508b3cc03cbba94a2a2e28f29
Hugo Moracchini <hugo4400@gmail.com>
2018-06-06 15:58:04 +0200

    Created a new module that allows for e new and better looking user menu.

---
+++ modules/mod_emundususerdropdown/tmpl/default.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * @package		Joomla.Site
+ * @subpackage	mod_menu
+ * @copyright	Copyright (C) 2005 - 2012 Open Source Matters, Inc. All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// No direct access.
+defined('_JEXEC') or die;
+
+// Note. It is important to remove spaces between elements.
+?>
+
+<!-- Button which opens up the dropdown menu. -->
+<div class='dropdown' id="userDropdown">
+    <div class="em-user-dropdown-button" id="userDropdownLabel" aria-haspopup="true" aria-expanded="false">
+        <i class="big circular user outline icon" style="background-color: #<?php echo $icon_color; ?>; border: solid 1px white"></i>
+    </div>
+    <ul class="dropdown-menu" aria-labelledby="userDropdownLabel">
+        <li class="dropdown-header"><?php echo $user->name; ?></li>
+        <li class="dropdown-header"><?php echo $user->email; ?></li>
+        <?php if (!empty($list)) :?>
+            <li role="separator" class="divider"></li>
+            <?php foreach ($list as $i => $item) :?>
+                <li class="<?php echo ($item->id == $active_id)?'active':''; ?>"><a href="<?php echo $item->flink ?>"><?php echo $item->title; ?></a></li>
+            <?php endforeach; ?>
+        <?php endif; ?>
+        <li role="separator" class="divider"></li>
+        <?php
+            $userToken = JSession::getFormToken();
+            echo '<li><a href="index.php?option=com_users&task=user.logout&' . $userToken . '=1">'.JText::_('LOGOUT').'</a></li>';
+        ?>
+    </ul>
+</div>
+
+<script>
+    // This counters all of the issues linked to using BootstrapJS.
+    document.getElementById('userDropdownLabel').onclick = function(e) {
+        var dropdown = document.getElementById('userDropdown');
+
+        if (dropdown.hasClass('open'))
+            dropdown.removeClass('open');
+        else
+            dropdown.addClass('open');
+    };
+</script>
+
