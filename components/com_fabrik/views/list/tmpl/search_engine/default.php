25d5c0ce055cfc4d432175f09ede84a9c376c8b7
latifouassirou <latifouassirou9@gmail.com>
2018-06-18 16:22:46 +0200

    search engine bugfixes

--- components/com_fabrik/views/list/tmpl/search_engine/default.php
+++ components/com_fabrik/views/list/tmpl/search_engine/default.php
@@ -57,7 +57,7 @@ echo $this->table->intro;
 	}
 	.em-search-engine-div-data {
 		width:95%; 
-		height:100px; 
+		height:auto; 
 		text-align:justify;
 		border: 1px solid;
 		border-radius:5px;
@@ -65,157 +65,178 @@ echo $this->table->intro;
 		box-shadow: 5px 10px #a22727;
 	}
 	.em-search-engine-filters {
-		width:60%;
+		width:55%;
 		float:left;
 		position:absolute;
+		padding-left:2%;
+		height: -webkit-fill-available;
+
 		
 	}
 	.em-search-engine-data {
-		width:68%;
-		float:right;
-		position:relative
+		margin-left:38%;
+		min-width:400px;
 	}
 	.fabrikDataContainer{
 		border-radius:5px;
-		padding-bottom:500px;
+		display: grid;
+		grid-gap: 5px;
 	}
 	.filtertable{
 		border: 1px solid #b9b9b9f5; 
 		border-radius: 5px;
 		border-style: hidden; 
-		box-shadow: 0 0 10px 0 #a22727
+		box-shadow: 0 0 10px 0 #a22727;
+		float:left
 	}
 	.fabrikButtonsContainer{
 		background-color:#d6d6d6;
 		border-radius:3px;
-		height:40px
+		height:53px
 	}
+	.form{
+		margin-right: 15%;
+	}
+	
+	.main{
+		display:inline-flex;
+	}
+
+
+
+	
 	
 
 </style>
-<form class="fabrikForm form-search" action="<?php echo $this->table->action;?>" method="post" id="<?php echo $this->formid;?>" name="fabrikList">
-	
-	<?php
-	if ($this->hasButtons):
-		echo $this->loadTemplate('buttons');
-	endif;
+<div class="main">
+	<div class="form">
+		<form class="fabrikForm form-search" action="<?php echo $this->table->action;?>" method="post" id="<?php echo $this->formid;?>" name="fabrikList">
+			
+			<?php
+			if ($this->hasButtons):
+				echo $this->loadTemplate('buttons');
+			endif;
 
-	
-	//for some really ODD reason loading the headings template inside the group
-	//template causes an error as $this->_path['template'] doesn't contain the correct
-	// path to this template - go figure!
-	/*$headingsHtml = $this->loadTemplate('headings');
-	echo $this->loadTemplate('tabs');*/
-	?>
+			
+			//for some really ODD reason loading the headings template inside the group
+			//template causes an error as $this->_path['template'] doesn't contain the correct
+			// path to this template - go figure!
+			/*$headingsHtml = $this->loadTemplate('headings');
+			echo $this->loadTemplate('tabs');*/
+			?>
 
-	<div class="fabrikDataContainer">
-		
-		<?php foreach ($this->pluginBeforeList as $c) :
-			echo $c;
-		endforeach;
+			<div class="fabrikDataContainer">
+				
+				<?php foreach ($this->pluginBeforeList as $c) :
+					echo $c;
+				endforeach;
 
 
-		$data = array();
-		$i = 0;
-		if(!empty($this->rows[0])){
-			foreach($this->rows[0] as $k=>$v){
-				foreach ($this->headings as $key => $val) {
-					if(array_key_exists($key, $v->data)){
-						if(strcasecmp($v->data->$key , "oui") == 0)
-							$data[$i][$val] = $v->data->$key;
-						else
-							$data[$i][$key] = $v->data->$key;		
+				$data = array();
+				$i = 0;
+				if(!empty($this->rows[0])){
+					foreach($this->rows[0] as $k=>$v){
+						foreach ($this->headings as $key => $val) {
+							if(array_key_exists($key, $v->data)){
+								if(strcasecmp($v->data->$key , "oui") == 0)
+									$data[$i][$val] = $v->data->$key;
+								else
+									$data[$i][$key] = $v->data->$key;		
+							}
+						}
+						$i = $i + 1;
 					}
 				}
-				$i = $i + 1;
-			}
-		}
-		
-		//var_dump($data);die;
-
-		?>												
-
-		<div class="em-search-engine-filters" >
-			<?php
-				if ($this->showFilters && $this->bootShowFilters) :
-					echo $this->layoutFilters();
-				endif; 
-			?>
-		</div>
-		
-		<div class="em-search-engine-data">
-			<table>
-				<thead>
-					<tr>
-						<td><h3>RESULTAT DE LA RECHERCHE</h3></td>
-					</tr>
-				</thead>
-				<tfoot>
-					<tr class="fabrik___heading">
-						<td colspan="<?php echo count($this->headings);?>">
-							<?php echo $this->nav;?>
-						</td>
-					</tr>
-				</tfoot>
 				
-				<tbody>
-				
-					<?php  
-						$region=""; $department=""; $chercheur=""; $cherches=""; $themes="";
-						$gCounter = 0;
-						//var_dump($data);die;
-						foreach($data as $d){
-							$region 	= $d['data_regions___name'];
-							$department = $d['data_departements___departement_nom'];
-							$chercheur 	= strtolower($d['jos_emundus_setup_profiles___label']);
+				//var_dump($data);die;
 
-							if(count(array_keys($d, "oui")) > 1){
-								$cherches  	= implode(",", array_keys($d, "oui"));
-								$cherches 	= strtolower(str_replace(","," </b></i>et<i><b> ", $cherches));	
+				?>												
 
-							}else{
-								$cherches	= strtolower(array_search("oui", $d));
-							}
-								$themes     = $d['jos_emundus_projet_620_repeat___themes'];
-							echo '<tr>
-									<td>
-										<div class="em-search-engine-div-data">
-											<p>En région <i><b>'.$region.'</b></i>, dans le département <i><b>'.$department.'</b></i>, un <i><b>'.$chercheur.'</b></i> cherche <i><b>'.$cherches.'</b></i> sur le thème <i><b>'.$themes.'</b></i></p>
-											<a class="em-search-engine-learn-more" >'.$d['fabrik_actions'].' Connectez-vous pour en savoir plus</a>
-										</div>
-									</td>
-								</tr>';
-							unset($cherches);
-							unset($themes);
-							$gCounter++;
-
-						}
-
+				<div class="em-search-engine-filters" >
+					<?php
+						if ($this->showFilters && $this->bootShowFilters) :
+							echo $this->layoutFilters();
+						endif; 
 					?>
-				</tbody>
-				<?php if ($this->hasCalculations) : ?>
-					<tfoot>
-						<tr class="fabrik_calculations">
+				</div>
+				<div class="em-search-engine-data">
+					<table>
+						<thead>
+							<tr>
+								<td><h3>RESULTAT DE LA RECHERCHE</h3></td>
+							</tr>
+						</thead>
+						<tfoot>
+							<tr class="fabrik___heading">
+								<td colspan="<?php echo count($this->headings);?>">
+									<?php echo $this->nav;?>
+								</td>
+							</tr>
+						</tfoot>
+						
+						<tbody>
+						
+							<?php  
+								$region=""; $department=""; $chercheur=""; $cherches=""; $themes="";
+								$gCounter = 0;
+								//var_dump($data);die;
+								foreach($data as $d){
+									$region 	= $d['data_regions___name'];
+									$department = $d['data_departements___departement_nom'];
+									$chercheur 	= strtolower($d['jos_emundus_setup_profiles___label']);
 
-						<?php
-						foreach ($this->headings as $key => $heading) :
-							$h = $this->headingClass[$key];
-							$style = empty($h['style']) ? '' : 'style="' . $h['style'] . '"';?>
-							<td class="<?php echo $h['class']?>" <?php echo $style?>>
+									if(count(array_keys($d, "oui")) > 1){
+										$cherches  	= implode(",", array_keys($d, "oui"));
+										$cherches 	= strtolower(str_replace(","," </b></i>et<i><b> ", $cherches));	
+
+									}else{
+										$cherches	= strtolower(array_search("oui", $d));
+									}
+										$themes     = $d['jos_emundus_projet_620_repeat___themes'];
+									echo '<tr>
+											<td>
+												<div class="em-search-engine-div-data">
+													<p>En région <i><b>'.$region.'</b></i>, dans le département <i><b>'.$department.'</b></i>, un <i><b>'.$chercheur.'</b></i> cherche <i><b>'.$cherches.'</b></i> sur le thème <i><b>'.$themes.'</b></i></p>
+													<a class="em-search-engine-learn-more" >'.$d['fabrik_actions'].' Connectez-vous pour en savoir plus</a>
+												</div>
+											</td>
+										</tr>';
+									unset($cherches);
+									unset($themes);
+									$gCounter++;
+
+								}
+
+							?>
+						</tbody>
+						<?php if ($this->hasCalculations) : ?>
+							<tfoot>
+								<tr class="fabrik_calculations">
+
 								<?php
-								$cal = $this->calculations[$key];
-								echo array_key_exists($groupedBy, $cal->grouped) ? $cal->grouped[$groupedBy] : $cal->calc;
+								foreach ($this->headings as $key => $heading) :
+									$h = $this->headingClass[$key];
+									$style = empty($h['style']) ? '' : 'style="' . $h['style'] . '"';?>
+									<td class="<?php echo $h['class']?>" <?php echo $style?>>
+										<?php
+										$cal = $this->calculations[$key];
+										echo array_key_exists($groupedBy, $cal->grouped) ? $cal->grouped[$groupedBy] : $cal->calc;
+										?>
+									</td>
+								<?php
+								endforeach;
 								?>
-							</td>
-						<?php
-						endforeach;
-						?>
 
-						</tr>
-					</tfoot>
-				<?php endif ?>
-			</table>
-		</div>
-		<?php print_r($this->hiddenFields);?>
+								</tr>
+							</tfoot>
+						<?php endif ?>
+					</table>
+				</div>
+				
+				
+				<?php print_r($this->hiddenFields);?>
+			</div>
+		</form>
 	</div>
-</form>
+	
+</div>
