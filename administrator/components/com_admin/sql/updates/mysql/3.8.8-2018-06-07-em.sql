edbe09561c26330af6bdea383e573cf2536bc85f
Hugo Moracchini <hugo4400@gmail.com>
2018-06-20 10:03:20 +0200

    jos_emundus_logs SQL improved with a CREATE TABLE IF NOT EXISTS and additional indexes.

--- administrator/components/com_admin/sql/updates/mysql/3.8.8-2018-06-07-em.sql
+++ administrator/components/com_admin/sql/updates/mysql/3.8.8-2018-06-07-em.sql
@@ -1,6 +1,6 @@
 
 -- This table is used for logging actions done on a User or an fnum.
-CREATE TABLE `jos_emundus_logs` (
+CREATE TABLE IF NOT EXISTS `jos_emundus_logs` (
   `id` int(11) NOT NULL,
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `user_id_from` int(11) NOT NULL,
@@ -12,15 +12,14 @@ CREATE TABLE `jos_emundus_logs` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 ALTER TABLE `jos_emundus_logs`
-  ADD PRIMARY KEY (`id`);
+ ADD PRIMARY KEY (`id`), ADD KEY `actions` (`action_id`), ADD KEY `fnum to` (`fnum_to`), ADD KEY `user from` (`user_id_from`), ADD KEY `user to` (`user_id_to`);
 
 
 ALTER TABLE `jos_emundus_logs`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 ALTER TABLE `jos_emundus_logs`
-  ADD CONSTRAINT `actions` FOREIGN KEY (`action_id`) REFERENCES `jos_emundus_setup_actions` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `fnum to` FOREIGN KEY (`fnum_to`) REFERENCES `jos_emundus_campaign_candidature` (`fnum`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `user from` FOREIGN KEY (`user_id_from`) REFERENCES `jos_emundus_users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `user to` FOREIGN KEY (`user_id_to`) REFERENCES `jos_emundus_users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ADD CONSTRAINT `actions` FOREIGN KEY (`action_id`) REFERENCES `jos_emundus_setup_actions` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `fnum to` FOREIGN KEY (`fnum_to`) REFERENCES `jos_emundus_campaign_candidature` (`fnum`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `user from` FOREIGN KEY (`user_id_from`) REFERENCES `jos_emundus_users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `user to` FOREIGN KEY (`user_id_to`) REFERENCES `jos_emundus_users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
