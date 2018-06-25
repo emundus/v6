8108fdb3a08bbe32cf5ab790d8ec3488b3e273a2
Hugo Moracchini <hugo4400@gmail.com>
2018-06-25 14:59:46 +0200

    Changed the search engine details template to show everything in one page. Added logging of opening an offer.

--- components/com_fabrik/views/details/tmpl/search_engine/default.php
+++ components/com_fabrik/views/details/tmpl/search_engine/default.php
@@ -13,8 +13,7 @@
 defined('_JEXEC') or die('Restricted access');
 
 $user = JFactory::getUser();
-if ($user->guest)
-{
+if ($user->guest) {
 	JFactory::getApplication()->redirect(JRoute::_('index.php?option=com_users&view=login&return=' . base64_encode(JFactory::getURI())), JText::_('JGLOBAL_YOU_MUST_LOGIN_FIRST'), 'warning');
 	return;
 }
@@ -39,115 +38,59 @@ if ($this->params->get('show-title', 1)) :?>
 endif;
 
 echo $form->intro;
-
-?>
-	<div class="fabrikForm fabrikDetails" id="<?php echo $form->formid; ?>">
-<?php
-echo $this->plugintop;
-?>
-
-<div class="fabrikMainError alert alert-error fabrikError<?php echo $active?>">
-	<button class="close" data-dismiss="alert">×</button>
-	<?php echo $form->error?>
-</div>
-
-<div class="row-fluid nav">
-	<div class="span6 pull-right">
-		<?php
-		echo $this->loadTemplate('buttons');
-		?>
-	</div>
-	<div class="span6">
-		<?php
-		echo $this->loadTemplate('relateddata');
-		?>
-	</div>
-</div>
-	<?php
-	$tabs = array();
-	$i = 0;
-
-	foreach ($this->groups as $group) :
-		$tabId = $this->form->id . '_' . (int)$this->rowid . '_' . $i;
-		// If this is multi-page then groups are consolidated until a group with a page break
-		// So we should only show a tab if: it is first tab, or if it is a page break
-		if (!$model->isMultiPage() || $i === 0 || $group->splitPage) :
-			$tab = new stdClass;
-			$tab->class = $i === 0 ? 'active' : '';
-			$tab->css = $group->css;
-			$tab->href = 'group-tab' . $tabId;
-			$tab->id = 'group' . $group->id . '_tab';
-			$tab->label = !empty($group->title) ? $group->title : $group->name;;
-			$tabs[] = $tab;
-			$i ++;
-		endif;
-	endforeach;
-	?>
-<?php
-echo FabrikHelperHTML::getLayout('fabrik-tabs')->render((object) array('tabs' => $tabs));
-?>
-<div class="tab-content">
-	<?php
-
-	$i = 0;
-	foreach ($this->groups as $group) :
-		$this->group = $group;
-		$tabId = $this->form->id . '_' . (int)$this->rowid . '_' . $i;
-
-		if ($i == 0 || !$model->isMultiPage() || $group->splitPage) :
-			if ($i != 0) :
-				echo '</div>';
-			endif;
-			?>
-			<div role="tabpanel" class="tab-pane<?php if ($i == 0) echo " active"?>" id="group-tab<?php echo $tabId;?>">
-			<?php
-			$i++;
-		endif; ?>
-			<fieldset class="<?php echo $group->class; ?>" id="group<?php echo $group->id;?>" style="<?php echo $group->css;?>">
-				<?php
-				if ($group->showLegend) :?>
-					<legend class="legend"><?php echo $group->title;?></legend>
-				<?php
-				endif;
-
-				if (!empty($group->intro)) : ?>
-					<div class="groupintro"><?php echo $group->intro ?></div>
-				<?php
-				endif;
-
-				/* Load the group template - this can be :
-				 *  * default_group.php - standard group non-repeating rendered as an unordered list
-				 *  * default_repeatgroup.php - repeat group rendered as an unordered list
-				 *  * default_repeatgroup_table.php - repeat group rendered in a table.
-				 */
-				$this->elements = $group->elements;
-				echo $this->loadTemplate($group->tmpl);
-
-				if (!empty($group->outro)) : ?>
-					<div class="groupoutro"><?php echo $group->outro ?></div>
-				<?php
-				endif;
-			?>
-			</fieldset>
-		<?php
-	endforeach;
-	?>
-	</div>
-</div>
-<?php
-if ($model->editable) : ?>
-<div class="fabrikHiddenFields">
-	<?php echo $this->hiddenFields; ?>
-</div>
-<?php
+if ($this->isMambot) :
+	echo '<div class="fabrikForm fabrikDetails fabrikIsMambot" id="' . $form->formid . '">';
+else :
+	echo '<div class="fabrikForm fabrikDetails" id="' . $form->formid . '">';
 endif;
+echo $this->plugintop;
+echo $this->loadTemplate('buttons');
+echo $this->loadTemplate('relateddata');
+foreach ($this->groups as $group) :
+	$this->group = $group;
+	?>
+
+    <div class="<?php echo $group->class; ?>" id="group<?php echo $group->id;?>" style="<?php echo $group->css;?>">
+
+		<?php
+		if ($group->showLegend) :?>
+            <h3 class="legend">
+                <span><?php echo $group->title;?></span>
+            </h3>
+		<?php endif;
+
+		if (!empty($group->intro)) : ?>
+            <div class="groupintro"><?php echo $group->intro ?></div>
+		<?php
+		endif;
+
+		// Load the group template - this can be :
+		//  * default_group.php - standard group non-repeating rendered as an unordered list
+		//  * default_repeatgroup.php - repeat group rendered as an unordered list
+		//  * default_repeatgroup_table.php - repeat group rendered in a table.
+
+		$this->elements = $group->elements;
+		echo $this->loadTemplate($group->tmpl);
+
+		if (!empty($group->outro)) : ?>
+            <div class="groupoutro"><?php echo $group->outro ?></div>
+		<?php
+		endif;
+		?>
+    </div>
+<?php
+endforeach;
+
+// This is currently the only way of getting the fnum.
+// CHANGING THE NAME OF THE GROUP 'search engine data' WILL BREAK THIS LOGGING.
+$fnum = $this->groups['search engine data']->elements['fnum']->value;
+
+// Log the action of opening the persons form.
+require_once (JPATH_BASE.DS.'components'.DS.'com_emundus'.DS.'models'.DS.'logs.php');
+EmundusModelLogs::log($user->id, (int)substr($fnum,-7), $fnum, 33, 'r', 'COM_EMUNDUS_LOGS_OPEN_OFFER');
 
 echo $this->pluginbottom;
 echo $this->loadTemplate('actions');
-?>
-
-</div>
-<?php
+echo '</div>';
 echo $form->outro;
 echo $this->pluginend;
-echo FabrikHelperHTML::keepalive();
