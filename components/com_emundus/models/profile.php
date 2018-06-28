74f7a0f30827fd3bf41fe0dfa765299a4a1ff250
Benjamin Rivalland <benjamin.rivalland@emundus.fr>
2018-06-28 12:46:09 +0200

    avoid error for empty profile

--- components/com_emundus/models/profile.php
+++ components/com_emundus/models/profile.php
@@ -632,10 +632,11 @@ class EmundusModelProfile extends JModelList
 			$campaign 	= $this->getCurrentCampaignInfoByApplicant($current_user->id);
             $incomplete = $this->getCurrentIncompleteCampaignByApplicant($current_user->id);
 			$p 			= $this->isProfileUserSet($current_user->id);
-			$profile 	= $this->getProfileByCampaign($campaign["id"]);
 
 			if (empty($p['profile']) || empty($campaign["id"]) || !isset($p['profile']) || !isset($campaign["id"]) )
 				$app->redirect(JRoute::_('index.php?option=com_fabrik&view=form&formid=102&random=0'));
+			
+			$profile 	= $this->getProfileByCampaign($campaign["id"]);
 
 			$emundus_user->profile 				  = $profile["profile_id"];
 			$emundus_user->profile_label          = $profile["label"];
