bed976b91e1bf787c8f56de84b4febd6fc426c6d
latifouassirou <latifouassirou9@gmail.com>
2018-07-10 11:35:18 +0200

    some bugfixes

--- administrator/components/com_admin/sql/updates/mysql/3.8.8-2018-06-22-em.sql
+++ administrator/components/com_admin/sql/updates/mysql/3.8.8-2018-06-22-em.sql
@@ -1,5 +1,7 @@
 /* add a column to jos_emundus_uploads and jos_emundus_setup_attachments for photos and passports validation */
 
-ALTER TABLE `jos_emundus_uploads` ADD `is_validated` DOUBLE NOT NULL DEFAULT -2
-ALTER TABLE `jos_emundus_setup_attachments` ADD `ocr_keywords` TEXT  DEFAULT NULL
-UPDATE `jos_emundus_setup_attachments` SET `ocr_keywords` = 'passport;passeport' WHERE lbl LIKE '%_passport%'
+ALTER TABLE `jos_emundus_uploads` ADD `is_validated` DOUBLE NOT NULL DEFAULT -2 ;
+
+ALTER TABLE `jos_emundus_setup_attachments` ADD `ocr_keywords` TEXT  DEFAULT NULL ;
+
+UPDATE `jos_emundus_setup_attachments` SET `ocr_keywords` = 'passport;passeport' WHERE lbl LIKE '%_passport%' ;
