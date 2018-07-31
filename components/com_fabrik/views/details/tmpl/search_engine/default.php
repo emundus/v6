04c85b523aaec8f9cf19402f36801f8372593200
Hugo Moracchini <hugo4400@gmail.com>
2018-07-31 11:31:08 +0200

    Changed search details page to be a bit better.

--- components/com_fabrik/views/details/tmpl/search_engine/default.php
+++ components/com_fabrik/views/details/tmpl/search_engine/default.php
@@ -24,6 +24,10 @@ $user = JFactory::getSession()->get('emundusUser');
 $fnum = $this->data["jos_emundus_recherche___fnum_raw"];
 $author = JFactory::getUser((int)substr($fnum,-7));
 
+require_once (JPATH_BASE.DS.'components'.DS.'com_emundus'.DS.'models'.DS.'files.php');
+$m_files = new EmundusModelFiles();
+$fnumInfos = $m_files->getFnumInfos($fnum);
+
 $form = $this->form;
 $model = $this->getModel();
 $groupTmpl = $model->editable ? 'group' : 'group_details';
@@ -31,10 +35,9 @@ $active = ($form->error != '') ? '' : ' fabrikHide';
 
 if ($this->params->get('show_page_heading', 1)) : ?>
 	<div class="componentheading<?php echo $this->params->get('pageclass_sfx')?>">
-	<?php echo $this->escape($this->params->get('page_heading')); ?>
+	    <?php echo $this->escape($this->params->get('page_heading')); ?>
 	</div>
-<?php
-endif;
+<?php endif;
 
 
 echo $this->plugintop;
@@ -56,7 +59,7 @@ $profile    = $this->data['jos_emundus_setup_profiles___id_raw'][0];
 </p>
 
 <div class="em-offre-meta">
-    <p>Sujet déposé le <strong class="em-highlight"><!-- TODO: Get the submitted date here --></strong></p>
+    <p>Sujet déposé le <strong class="em-highlight"><?php echo date('d/m/y H:i:s', strtotime($fnumInfos['date_submitted'])); ?></strong></p>
     <p>Par un <strong class="em-highlight"><?php echo $chercheur; ?></strong></p>
 </div>
 
@@ -65,7 +68,7 @@ $profile    = $this->data['jos_emundus_setup_profiles___id_raw'][0];
 <div class="em-offre-author">
     <h1 class="em-offre-title"> Le déposant </h1>
     <div class="em-offre-subtitle">Profil du déposant</div>
-    <strong>Nom : </strong><?php echo $author->name; ?>
+    <strong>Nom : </strong><div class="em-offre-author-name"><?php echo $author->name; ?></div>
 
     <?php
     // We need to change up the page based on if the person is viewing an offer from a lab, a future PHd, or a municiplaity.
@@ -78,17 +81,18 @@ $profile    = $this->data['jos_emundus_setup_profiles___id_raw'][0];
         $m_cifre = new EmundusModelCifre();
         $laboratoire = $m_cifre->getUserLaboratory($author->id);
         ?>
-        <div> <strong>Laboratoire :</strong>
-        <?php
-        if (!empty($laboratoire->website)) {
-            $parse = parse_url($laboratoire->website, PHP_URL_SCHEME) === null ? 'http://' . $laboratoire->website: $laboratoire->website;
-            echo '<a target="_blank " href="'.$parse.'">';
-        }
+        <strong>Laboratoire :</strong>
+        <div class="em-offre-institution">
+            <?php
+            if (!empty($laboratoire->website)) {
+                $parse = parse_url($laboratoire->website, PHP_URL_SCHEME) === null ? 'http://' . $laboratoire->website: $laboratoire->website;
+                echo '<a target="_blank " href="'.$parse.'">';
+            }
 
-        echo $laboratoire->name;
-        if (!empty($laboratoire->website))
-            echo '</a>';
-        ?>
+            echo $laboratoire->name;
+            if (!empty($laboratoire->website))
+                echo '</a>';
+            ?>
         </div>
         <a class="btn btn-default" href="/index.php?option=com_fabrik&task=details.view&formid=308&listid=318&rowid=<?php echo $laboratoire->id; ?>">Cliquez ici pour plus d'information</a>
     <?php elseif ($profile == '1008') :?>
@@ -97,17 +101,17 @@ $profile    = $this->data['jos_emundus_setup_profiles___id_raw'][0];
         $m_cifre = new EmundusModelCifre();
         $institution = $m_cifre->getUserInstitution($author->id);
         ?>
-        <div> <strong>Structure :</strong>
-        <?php
-
-        if (!empty($institution->website)) {
-            $parse = parse_url($institution->website, PHP_URL_SCHEME) === null ? 'http://' . $institution->website: $institution->website;
-            echo '<a target="_blank " href="'.$parse.'">';
-        }
-        echo $institution->nom_de_structure;
-        if (!empty($institution->website))
-            echo '</a>';
-        ?>
+        <strong>Structure :</strong>
+        <div class="em-offre-institution">
+            <?php
+            if (!empty($institution->website)) {
+                $parse = parse_url($institution->website, PHP_URL_SCHEME) === null ? 'http://' . $institution->website: $institution->website;
+                echo '<a target="_blank " href="'.$parse.'">';
+            }
+            echo $institution->nom_de_structure;
+            if (!empty($institution->website))
+                echo '</a>';
+            ?>
         </div>
         <a class="btn btn-default" href="/index.php?option=com_fabrik&task=details.view&formid=307&listid=317&rowid=<?php echo $institution->id; ?>">Cliquez ici pour plus d'information</a>
     <?php endif; ?>
@@ -124,17 +128,17 @@ $profile    = $this->data['jos_emundus_setup_profiles___id_raw'][0];
 
     <!-- Project context -->
     <p class="em-offre-contexte">
-    <div class="em-offre-subtitle">Contexte : </div><?php echo $this->data['jos_emundus_projet___contexte_raw'][0]; ?>
+        <div class="em-offre-subtitle">Contexte : </div><?php echo $this->data['jos_emundus_projet___contexte_raw'][0]; ?>
     </p>
 
     <!-- Project question -->
     <p class="em-offre-question">
-    <div class="em-offre-subtitle">Grande question posée : </div><?php echo $this->data['jos_emundus_projet___question_raw'][0]; ?>
+        <div class="em-offre-subtitle">Grande question posée : </div><?php echo $this->data['jos_emundus_projet___question_raw'][0]; ?>
     </p>
 
     <!-- Project methodology -->
     <p class="em-offre-methodologie">
-    <div class="em-offre-subtitle">Méthodologie proposée : </div><?php echo $this->data['jos_emundus_projet___methodologie_raw'][0]; ?>
+        <div class="em-offre-subtitle">Méthodologie proposée : </div><?php echo $this->data['jos_emundus_projet___methodologie_raw'][0]; ?>
     </p>
 
 </div>
@@ -145,89 +149,89 @@ $profile    = $this->data['jos_emundus_setup_profiles___id_raw'][0];
     <p class="em-contact-item"><strong>Nom : </strong><?php echo !empty($this->data['jos_emundus_projet___contact_nom_raw'][0])?$this->data['jos_emundus_projet___contact_nom_raw'][0]:'Aucun nom renseigné'; ?></p>
     <p class="em-contact-item"><strong>Mail : </strong><?php echo !empty($this->data['jos_emundus_projet___contact_mail_raw'][0])?$this->data['jos_emundus_projet___contact_mail_raw'][0]:'Aucun mail renseingé'; ?></p>
     <p class="em-contact-item"><strong>Tel : </strong><?php echo !empty($this->data['jos_emundus_projet___contact_tel_raw'][0])?$this->data['jos_emundus_projet___contact_tel_raw'][0]:'Aucun numéro renseigné'; ?></p>
-</div>
 
-<?php
-// Log the action of opening the persons form.
-require_once (JPATH_BASE.DS.'components'.DS.'com_emundus'.DS.'models'.DS.'logs.php');
-EmundusModelLogs::log($user->id, $author->id, $fnum, 33, 'r', 'COM_EMUNDUS_LOGS_OPEN_OFFER');
+    <?php
+    // Log the action of opening the persons form.
+    require_once (JPATH_BASE.DS.'components'.DS.'com_emundus'.DS.'models'.DS.'logs.php');
+    EmundusModelLogs::log($user->id, $author->id, $fnum, 33, 'r', 'COM_EMUNDUS_LOGS_OPEN_OFFER');
 
-// Action button types:
-// // NO BUTTON : if the offer belongs to the user.
-// // ENTREZ EN CONTACT : If the user has not already contacted.
-// // REPONDRE : If the user has already been contacted for this offer but has not answered.
-// // RELANCE : If the user has contacted but not been answered yet.
-// // BREAK UP : If the user is collaborating with the other.
-require_once (JPATH_BASE.DS.'components'.DS.'com_emundus'.DS.'controllers'.DS.'cifre.php');
-$c_ciffe = new EmundusControllerCifre();
-$action_button = $c_ciffe->getActionButton($fnum);
-?>
+    // Action button types:
+    // // NO BUTTON : if the offer belongs to the user.
+    // // ENTREZ EN CONTACT : If the user has not already contacted.
+    // // REPONDRE : If the user has already been contacted for this offer but has not answered.
+    // // RELANCE : If the user has contacted but not been answered yet.
+    // // BREAK UP : If the user is collaborating with the other.
+    require_once (JPATH_BASE.DS.'components'.DS.'com_emundus'.DS.'controllers'.DS.'cifre.php');
+    $c_ciffe = new EmundusControllerCifre();
+    $action_button = $c_ciffe->getActionButton($fnum);
+    ?>
 
-<!-- Button used for matching with the offer -->
-<div class="em-search-item-action">
+    <!-- Button used for matching with the offer -->
+    <div class="em-search-item-action">
 
-    <span class="alert alert-danger hidden" id="em-action-text"></span>
+        <span class="alert alert-danger hidden" id="em-action-text"></span>
 
-    <div id="em-search-item-action-button">
+        <div id="em-search-item-action-button">
 
-    <?php if ($action_button == 'contact') :?>
+        <?php if ($action_button == 'contact') :?>
 
-        <?php $offers = $c_ciffe->getOwnOffers($fnum); ?>
+            <?php $offers = $c_ciffe->getOwnOffers($fnum); ?>
 
-        <button type="button" class="btn btn-success" data-toggle="modal" data-target="#contactModal">
-	        Entrer en contact
-        </button>
+            <button type="button" class="btn btn-success" data-toggle="modal" data-target="#contactModal">
+                Entrer en contact
+            </button>
 
-        <div class="modal fade" id="contactModal" tabindex="-1" role="dialog">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title">Demande de contact</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                        <p>Veuillez confirmer que vous souhaitez contacter le créateur de cette offre.</p>
-                        <?php if (!empty($offers)) :?>
-                            <p>Si vous le souhaitez: vous pouvez joindre une de vos offres.</p>
-                            <select id="em-join-offer">
-                                <option value="">Je ne souhaite pas joindre mes offres</option>
-                                <?php foreach ($offers as $offer) :?>
-                                    <option value="<?php echo $offer->fnum; ?>"><?php echo $offer->titre; ?></option>
-                                <?php endforeach; ?>
-                            </select>
-                        <?php endif; ?>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-primary" data-dismiss="modal" onclick="actionButton('contact')">Evoyer la demande de contact</button>
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Annuler</button>
+            <div class="modal fade" id="contactModal" tabindex="-1" role="dialog">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title">Demande de contact</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <p>Veuillez confirmer que vous souhaitez contacter le créateur de cette offre.</p>
+                            <?php if (!empty($offers)) :?>
+                                <p>Si vous le souhaitez: vous pouvez joindre une de vos offres.</p>
+                                <select id="em-join-offer">
+                                    <option value="">Je ne souhaite pas joindre mes offres</option>
+                                    <?php foreach ($offers as $offer) :?>
+                                        <option value="<?php echo $offer->fnum; ?>"><?php echo $offer->titre; ?></option>
+                                    <?php endforeach; ?>
+                                </select>
+                            <?php endif; ?>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-primary" data-dismiss="modal" onclick="actionButton('contact')">Evoyer la demande de contact</button>
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Annuler</button>
+                        </div>
                     </div>
                 </div>
             </div>
+
+        <?php elseif ($action_button == 'reply') :?>
+            <button type="button" class="btn btn-primary" onclick="actionButton('reply')">
+                Répondre
+            </button>
+            <button type="button" class="btn btn-primary" onclick="breakUp()">
+                Ignorer
+            </button>
+
+        <?php elseif ($action_button == 'retry') :?>
+            <button type="button" class="btn btn-primary" onclick="actionButton('retry')">
+                Relancer
+            </button>
+
+        <?php elseif ($action_button == 'breakup') :?>
+            <button type="button" class="btn btn-primary" onclick="breakUp()">
+                Couper contact
+            </button>
+        <?php endif; ?>
+
         </div>
 
-    <?php elseif ($action_button == 'reply') :?>
-        <button type="button" class="btn btn-primary" onclick="actionButton('reply')">
-            Répondre
-        </button>
-        <button type="button" class="btn btn-primary" onclick="breakUp()">
-            Ignorer
-        </button>
-
-    <?php elseif ($action_button == 'retry') :?>
-        <button type="button" class="btn btn-primary" onclick="actionButton('retry')">
-            Relancer
-        </button>
-
-    <?php elseif ($action_button == 'breakup') :?>
-        <button type="button" class="btn btn-primary" onclick="breakUp()">
-            Couper contact
-        </button>
-    <?php endif; ?>
-
     </div>
-
 </div>
 
 <script>
