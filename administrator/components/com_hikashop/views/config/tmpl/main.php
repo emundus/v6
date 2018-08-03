056dedb4ae50fbc99df6f7c2f929a31b9bbed641
Hugo Moracchini <hugo4400@gmail.com>
2018-08-03 14:58:04 +0200

    Updated Gantry 5, Helium, Hikashop, Admintools.

--- administrator/components/com_hikashop/views/config/tmpl/main.php
+++ administrator/components/com_hikashop/views/config/tmpl/main.php
@@ -32,7 +32,7 @@ echo $this->leftmenu(
 	<tr>
 		<td class="hk_tbl_key"<?php echo $this->docTip('version');?>><?php echo JText::_('VERSION');?></td>
 		<td>
-			HikaShop <?php echo $this->config->get('level').' '.$this->config->get('version'); ?> [1807311515]
+			HikaShop <?php echo $this->config->get('level').' '.$this->config->get('version'); ?> [1808011106]
 		</td>
 	</tr>
 	<tr>
