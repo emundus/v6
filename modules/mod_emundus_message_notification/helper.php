31f8c4e03e32384ac79a99378a50f6623e81f91c
james dean <jdean162@gmail.com>
2018-08-28 18:14:57 +0200

    created message module

---
+++ modules/mod_emundus_message_notification/helper.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * @copyright	Copyright (C) 2005 - 2012 Open Source Matters, Inc. All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// no direct access
+defined('_JEXEC') or die;
+
+/**
+ * @package		Joomla.Site
+ * @subpackage	mod_emundusmenu
+ * @since		1.5
+ * author       James Dean
+ */
+class modEmundusMessageNotificationHelper {
+
+    public function getMessages($user)
+    {
+
+        $db = JFactory::getDbo();
+        $query = $db->getQuery(true);
+
+        $query
+            ->select('COUNT(message_id)')
+            ->from($db->quoteName('#__messages'))
+            ->where($db->quoteName('folder_id') . ' = 2 AND ' . $db->quoteName('user_id_to') . ' = ' . $user . ' AND ' . $db->quoteName('state') . ' = 1');
+
+        $db->setQuery($query);
+
+        //var_dump($query->__toString()).die();
+
+        try {
+
+            return $db->loadResult();
+
+        } catch (Exception $e) {
+            JLog::add('Error getting account type stats from mod_graphs helper at query: ' . $query->__toString(), JLog::ERROR, 'com_emundus');
+        }
+
+    }
+
+    public function getContacts($user) {
+
+        $db = JFactory::getDbo();
+
+        $query = "  select jos_messages.*, user_from.name as name_from, sp_from.label as profile_from, user_to.name as name_to, sp_to.label as profile_to, uploadTo.attachment_id as photo_to, uploadFrom.attachment_id as photo_from
+                from jos_messages
+                LEFT JOIN jos_users user_to ON user_to.id = jos_messages.user_id_to 
+                LEFT JOIN jos_users user_from ON user_from.id = jos_messages.user_id_from 
+                LEFT JOIN jos_emundus_users eu_to ON eu_to.user_id = user_to.id
+                LEFT JOIN jos_emundus_users eu_from ON eu_from.user_id = user_from.id
+                LEFT JOIN jos_emundus_setup_profiles sp_to ON sp_to.id =  eu_to.profile
+                LEFT JOIN jos_emundus_setup_profiles sp_from ON sp_from.id =  eu_from.profile
+                
+                LEFT JOIN jos_emundus_uploads uploadTo ON uploadTo.user_id = user_to.id AND uploadTo.attachment_id = 10
+                LEFT JOIN jos_emundus_uploads uploadFrom ON uploadFrom.user_id = user_from.id AND uploadFrom.attachment_id = 10
+              
+                where (jos_messages.folder_id = 2) 
+                AND(least(`user_id_from`, `user_id_to`), greatest(`user_id_from`, `user_id_to`), `date_time`)       
+                in 
+                (
+                    select 
+                       least(`user_id_from`, `user_id_to`) as x, greatest(`user_id_from`, `user_id_to`) as y, 
+                       max(`date_time`) as msg_time
+                    from jos_messages 
+                    group by x, y
+                )
+                AND (`user_id_to` = ".$user." OR `user_id_from` = ".$user.")
+                ORDER BY jos_messages.date_time DESC ";
+
+
+        $db->setQuery($query);
+        try {
+
+
+            return $db->loadObjectList();
+
+        } catch (Exception $e) {
+            JLog::add('Error getting candidate file attachment name in model/messages at query: '.$query, JLog::ERROR, 'com_emundus');
+            return false;
+        }
+    }
+
+
+
+
+}
