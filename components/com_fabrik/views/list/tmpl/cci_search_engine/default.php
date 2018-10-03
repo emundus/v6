bf9cb885773024c0f1d2fa928ab8c3262ad7380d
Hugo Moracchini <hugo4400@gmail.com>
2018-10-03 11:42:33 +0200

    🎁 Added single CPF and certification checkboxes that toggle all certs or CPF items.

--- components/com_fabrik/views/list/tmpl/cci_search_engine/default.php
+++ components/com_fabrik/views/list/tmpl/cci_search_engine/default.php
@@ -262,11 +262,45 @@ if(!empty($category)) {
                 no_results_text: "<?php echo JText::_('CHOSEN_NO_RESULTS'); ?>"
             });
 
-            <?php if(!empty(JFactory::getApplication()->input->post->get('search'))) :?>
+            <?php if (!empty(JFactory::getApplication()->input->post->get('search'))) :?>
                 var searchValue = '<?php echo JFactory::getApplication()->input->post->get('search'); ?>';
                 document.getElementById("formation-search").value = searchValue;
             <?php endif; ?>
-        });
-    </script>
 
+            singleToggleCheckboxes();
+        });
+
+        function singleToggleCheckboxes() {
+
+            var cpfContainer = jQuery('[data-filter-row="jos_emundus_setup_programmes___numcpf"]');
+            cpfContainer.children().hide();
+
+            cpfContainer.append('' +
+                '<div class="row-fluid">\n' +
+                    '<div class="fabrikgrid_checkbox span12">' +
+                        '<label for="cpfCheckbox" class="checkbox">\n' +
+                        '\t<input type="checkbox" class="fabrikinput  fabrik_filter" name="cpfCheckbox" id="cpfCheckbox"  onchange="toggleCPF(this);"><span>éligible au CPF</span></label>\n' +
+                    '</div>\n' +
+                '</div>')
+
+            var certContainer = jQuery('[data-filter-row="jos_emundus_setup_programmes___certificate"]');
+            certContainer.children().hide();
+
+            certContainer.append('' +
+                '<div class="row-fluid">\n' +
+                    '<div class="fabrikgrid_checkbox span12">' +
+                        '<label for="certCheckbox" class="checkbox">\n' +
+                        '\t<input type="checkbox" class="fabrikinput  fabrik_filter" name="certCheckbox" id="certCheckbox"  onchange="toggleCert(this);"><span>certifiante ou diplômante</span></label>\n' +
+                    '</div>\n' +
+                '</div>')
+
+        }
+        
+        function toggleCPF(checkbox) {
+            jQuery('[data-filter-row="jos_emundus_setup_programmes___numcpf"] input:checkbox').not(checkbox).prop('checked', checkbox.checked);
+        }
+        function toggleCert(checkbox) {
+            jQuery('[data-filter-row="jos_emundus_setup_programmes___certificate"] input:checkbox').not(checkbox).prop('checked', checkbox.checked);
+        }
+    </script>
 </div>
