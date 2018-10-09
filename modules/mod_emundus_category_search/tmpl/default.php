af34679a76da6ed5f1427781e1c0a05214db6444
Hugo Moracchini <hugo4400@gmail.com>
2018-10-09 15:39:40 +0200

    🖥♻ Updated category search module HTML structure to ease integration.

--- modules/mod_emundus_category_search/tmpl/default.php
+++ modules/mod_emundus_category_search/tmpl/default.php
@@ -12,8 +12,9 @@ defined('_JEXEC') or die;
 
 <div class="em-category-search-module">
     <?php echo $heading; ?>
-    <?php foreach ($categories as $category) :?>
-        <div class="em-themes em-theme-<?php echo $category->color ?>"><a href="<?php echo $search_page; ?>?category=<?php echo str_replace(['é','è','ê'],'e', html_entity_decode(mb_strtolower(str_replace(' ','-', $category->title)))); ?>"><?php echo $category->label; ?></a></div>
-    <?php endforeach; ?>
-
+    <div class="em-themes">
+        <?php foreach ($categories as $category) :?>
+            <div class="em-theme-<?php echo $category->color ?>"><a href="<?php echo $search_page; ?>?category=<?php echo str_replace(['é','è','ê'],'e', html_entity_decode(mb_strtolower(str_replace(' ','-', $category->title)))); ?>" title="<?php echo $category->label; ?>"><?php echo $category->label; ?></a></div>
+        <?php endforeach; ?>
+    </div>
 </div>
