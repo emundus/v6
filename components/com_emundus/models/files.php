03006d750565f408deff712c306f9192d77147ea
james dean <jdean162@gmail.com>
2018-10-19 14:28:25 +0200

    Adding completefile + publishfile functions to files.php

--- components/com_emundus/models/files.php
+++ components/com_emundus/models/files.php
@@ -3073,7 +3073,101 @@ die();*/
         }
     }
 
+    /* complete file */
+    function completefile() {
+        $app = JFactory::getApplication();
+        $jinput = $app->input;
+        $m_profile = new EmundusModelProfile;
 
+        $student_id    = $jinput->get->get('sid', null);
+        $fnum          = $jinput->get->get('fnum', null);
+        $redirect      = $jinput->get->getBase64('redirect', null);
+        // Redirect URL is currently only used in Hesam template of mod_emundus_application, it allows for the module to be located on a page other than index.php.
+
+        if (empty($redirect))
+            $redirect = 'index.php';
+        else
+            $redirect = base64_decode($redirect);
+
+        if (empty($fnum))
+            $app->redirect($redirect);
+
+        $current_user  = JFactory::getSession()->get('emundusUser');
+        $m_files = $this->getModel('files');
+
+        if (EmundusHelperAccess::isApplicant($current_user->id) && in_array($fnum, array_keys($current_user->fnums))){
+            $user = $current_user;
+            $result = $m_files->completeFile($fnum);
+
+        } elseif(EmundusHelperAccess::asAccessAction(1, 'd', $current_user->id, $fnum) ||
+            EmundusHelperAccess::asAdministratorAccessLevel($current_user->id)) {
+            $user = $m_profile->getEmundusUser($student_id);
+
+        } else {
+            JError::raiseError(500, JText::_('ACCESS_DENIED'));
+            $app->redirect($redirect);
+
+            return false;
+        }
+
+        if (in_array($user->fnum, array_keys($user->fnums))) {
+            $app->redirect($redirect);
+        } else {
+            $fnum = array_shift($current_user->fnums);
+            $app->redirect($redirect);
+        }
+
+        return true;
+
+    }
+
+    /* publish file */
+    function publishfile() {
+        $app = JFactory::getApplication();
+        $jinput = $app->input;
+        $m_profile = new EmundusModelProfile;
+
+        $student_id    = $jinput->get->get('sid', null);
+        $fnum          = $jinput->get->get('fnum', null);
+        $redirect      = $jinput->get->getBase64('redirect', null);
+        // Redirect URL is currently only used in Hesam template of mod_emundus_application, it allows for the module to be located on a page other than index.php.
+
+        if (empty($redirect))
+            $redirect = 'index.php';
+        else
+            $redirect = base64_decode($redirect);
+
+        if (empty($fnum))
+            $app->redirect($redirect);
+
+        $current_user  = JFactory::getSession()->get('emundusUser');
+        $m_files = $this->getModel('files');
+
+        if (EmundusHelperAccess::isApplicant($current_user->id) && in_array($fnum, array_keys($current_user->fnums))){
+            $user = $current_user;
+            $result = $m_files->publishFile($fnum);
+
+        } elseif(EmundusHelperAccess::asAccessAction(1, 'd', $current_user->id, $fnum) ||
+            EmundusHelperAccess::asAdministratorAccessLevel($current_user->id)) {
+            $user = $m_profile->getEmundusUser($student_id);
+
+        } else {
+            JError::raiseError(500, JText::_('ACCESS_DENIED'));
+            $app->redirect($redirect);
+
+            return false;
+        }
+
+        if (in_array($user->fnum, array_keys($user->fnums))) {
+            $app->redirect($redirect);
+        } else {
+            $fnum = array_shift($current_user->fnums);
+            $app->redirect($redirect);
+        }
+
+        return true;
+
+    }
     /*
      * CCIRS functions
      * function to get all sessions linked to a program
