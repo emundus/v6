29ac717b8fe058a65d76c9acc9a634cd7d2642b7
James dean <jdean162@gmail.com>
2018-12-18 14:56:51 +0100

    program redirect if logged in

--- templates/g5_helium/html/com_users/login/default.php
+++ templates/g5_helium/html/com_users/login/default.php
@@ -10,21 +10,23 @@
 defined('_JEXEC') or die;
 
 $cookieLogin = $this->user->get('cookieLogin');
+$this->redirect   = $jinput->get->getBase64('redirect');
 if (!empty($cookieLogin) || $this->user->get('guest'))
 {
     // Get campaign ID and course from url
     $jinput = JFactory::getApplication()->input;
     $this->campaign = $jinput->get->get('cid');
     $this->course   = $jinput->get->get('course');
-    $this->redirect   = $jinput->get->getBase64('redirect');
 
-    if (!empty($this->redirect) && !$this->user->get('guest'))
-        $app->redirect(JRoute::_($this->redirect));
+
+
 	// The user is not logged in or needs to provide a password.
 	echo $this->loadTemplate('login');
 }
 else
 {
+    if (!empty($this->redirect))
+        $app->redirect(JRoute::_($this->redirect));
 	// The user is already logged in.
 	echo $this->loadTemplate('logout');
 }
