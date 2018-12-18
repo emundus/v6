5b4aa55d046a80691a8072fff95e85dc60c14402
James dean <jdean162@gmail.com>
2018-12-18 18:48:16 +0100

    Application module modifications

--- modules/mod_emundus_applications/tmpl/ccirs.php
+++ modules/mod_emundus_applications/tmpl/ccirs.php
@@ -10,10 +10,7 @@ defined('_JEXEC') or die;
 //var_dump($user->fnums); echo "<hr>"; var_dump($applications);
 echo $description;
 ?>
-<?php if ($show_add_application && ($position_add_application == 0 || $position_add_application == 2) && $applicant_can_renew) : ?>
-    <a class="btn btn-success" href="<?php echo JURI::base(); ?>index.php?option=com_emundus&view=renew_application"><span class="icon-plus-sign"> <?php echo JText::_('ADD_APPLICATION_FILE'); ?></span></a>
-    <hr>
-<?php endif; ?>
+
 <?php if (!empty($applications)) : ?>
     <div class="<?php echo $moduleclass_sfx ?>">
         <?php foreach($applications as $application) : ?>
@@ -57,34 +54,6 @@ echo $description;
 <?php else :
     echo JText::_('NO_FILE');
     ?>
-<?php endif; ?>
-
-<?php if ($show_add_application && $position_add_application > 0 && $applicant_can_renew) : ?>
-    <a class="btn btn-success" href="<?php echo JURI::base(); ?>index.php?option=com_emundus&view=renew_application"><span class="icon-plus-sign"> <?php echo JText::_('ADD_APPLICATION_FILE'); ?></span></a>
-    <hr>
-<?php endif; ?>
-
-<?php if (!empty($filled_poll_id) && !empty($poll_url) && $filled_poll_id == 0 && $poll_url != "") : ?>
-    <div class="modal fade" id="em-modal-form" style="z-index:99999" tabindex="-1" role="dialog" aria-labelledby="em-modal-form" aria-hidden="true">
-        <div class="modal-dialog modal-lg">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                </div>
-                <div class="modal-body">
-                    <h4 class="modal-title" id="em-modal-form-title"><?php echo JText::_('LOADING');?></h4>
-                    <img src="<?php echo JURI::base(); ?>media/com_emundus/images/icones/loader-line.gif">
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <script type="text/javascript">
-        var poll_url = "<?php echo $poll_url; ?>";
-        $(".modal-body").html('<iframe src="'+poll_url+'" style="width:'+window.getWidth()*0.8+'px; height:'+window.getHeight()*0.8+'px; border:none"></iframe>');
-        setTimeout(function(){$('#em-modal-form').modal({backdrop:true, keyboard:true},'toggle');}, 1000);
-    </script>
-
 <?php endif; ?>
 
 <script type="text/javascript">
