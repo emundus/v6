358b84fc45d2e68335c6975a32b1a3bf9cf6e66d
Hugo Moracchini <hugo4400@gmail.com>
2018-12-27 10:55:49 +0100

    Renamed plugin and changed a bit of code to make it ambiguous in the user being inserted and unambiguous in the profile.

---
+++ plugins/fabrik_form/php/scripts/emundus-CCIRS-DRH-assign-company.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: James Dean
+ * Date: 2018-12-26
+ * Time: 12:08
+ */
+
+
+jimport('joomla.log.log');
+JLog::addLogger(['text_file' => 'com_emundus.evaluation.php'], JLog::ALL, ['com_emundus']);
+
+$db = JFactory::getDBO();
+
+$current_user = JFactory::getSession()->get('emundusUser');
+$mainframe = JFactory::getApplication();
+
+$user = $formModel->getElementData('jos_emundus_entreprise___user')[0];
+$cid = $formModel->getElementData('jos_emundus_entreprise___id');
+
+if (empty($user) || empty($cid))
+	return false;
+
+try {
+
+    // Insert columns.
+    $columns = array('user', 'cid', 'profile');
+
+    // Insert values.
+    $values = array($current_user->id, $cid, '1002');
+
+    $query = $db->getQuery(true);
+    $query->insert($db->quoteName('#__emundus_user_entreprise'))
+        ->columns($db->quoteName($columns))
+        ->values(implode(',', $values));
+    $db->setQuery($query);
+    $db->execute();
+
+} catch (Exception $e) {
+    JLog::add('Error setting status in plugin at query : '.$query->__toString(), JLog::ERROR, 'com_emundus');
+}
