de1b543d1a6d9454aac79a9238cdd5d140b05860
James dean <jdean162@gmail.com>
2018-12-28 12:41:33 +0100

    insert into user_entreprise if user doesn't exist

--- plugins/fabrik_form/php/scripts/emundus-CCIRS-DHR-create-associate.php
+++ plugins/fabrik_form/php/scripts/emundus-CCIRS-DHR-create-associate.php
@@ -15,22 +15,49 @@ JLog::addLogger(array('text_file' => 'com_emundus.HRcreateassociate.php'), JLog:
 
 $current_user = JFactory::getSession()->get('emundusUser');
 $email = $formModel->getElementData('jos_emundus_users___email');
+$user = $formModel->getElementData('jos_emundus_entreprise___user');
+$cid = $formModel->getElementData('jos_emundus_users___company_id');
+
 $mainframe = JFactory::getApplication();
 
 
 if (empty($current_user))
     return false;
 
+require_once (JPATH_BASE.DS.'components'.DS.'com_emundus'.DS.'models'.DS.'formations.php');
+$m_formations = new EmundusModelFormations();
+
+// Check that the user is in the company
+if (!$m_formations->checkCompanyUser($user, $company_id)) {
+    JLog::add('User: '.$user_id.' is not in the company: '.$company_id, JLog::ERROR, 'com_emundus');
+    return false;
+}
+
 
 try {
 
+    $columns = array('user', 'cid', 'profile');
+    $values = array($user, $cid, '1001');
+
     $query = $db->getQuery(true);
     $query
-        ->select($db->quoteName('u.id'))
-        ->from($db->quoteName('#__users'), 'u')
-        ->where($db->quoteName('u.email') . ' LIKE "'.$email.'"');
+        ->insert($db->quoteName('#__emundus_user_entreprise'))
+        ->columns($db->quoteName($columns))
+        ->values(implode(',', $values));
+    $db->setQuery($query);
+    $db->execute();
+
+
+    $query->clear()
+        ->select($db->quoteName('eu.id'))
+        ->from($db->quoteName('#__emundus_users'), 'eu')
+        ->where($db->quoteName('eu.email') . ' LIKE "'.$email.'"');
 
     $db->setQuery($query);
+
+    if (!empty($db->loadResult())) {
+        $mainframe->redirect('/mon-espace-decideur-rh');
+    }
     
 
 } catch (Exception $e) {
