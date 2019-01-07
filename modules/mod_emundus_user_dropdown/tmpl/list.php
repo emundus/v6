4102db28bcefc7c8448801c3fc4db67196140109
James dean <jdean162@gmail.com>
2019-01-07 17:03:11 +0100

    add class on logout link

--- modules/mod_emundus_user_dropdown/tmpl/list.php
+++ modules/mod_emundus_user_dropdown/tmpl/list.php
@@ -14,12 +14,12 @@
         <ul>
             <?php if (!empty($list)) :?>
                 <?php foreach ($list as $i => $item) :?>
-                    <li class="<?php echo ($item->id)?'menu-item':'logout'; ?>"><a href="<?php echo $item->flink ?>"><?php echo $item->title; ?></a></li>
+                    <li class="<?php echo ($item->id)?'menu-item':''; ?>"><a href="<?php echo $item->flink ?>"><?php echo $item->title; ?></a></li>
                 <?php endforeach; ?>
             <?php endif; ?>
             <?php
             $userToken = JSession::getFormToken();
-            echo '<li><a href="/index.php?option=com_users&task=user.logout&' . $userToken . '=1">'.JText::_('LOGOUT').'</a></li>';
+            echo '<li class="user-logout"><a href="/index.php?option=com_users&task=user.logout&' . $userToken . '=1">'.JText::_('LOGOUT').'</a></li>';
             ?>
         </ul>
     </div>
