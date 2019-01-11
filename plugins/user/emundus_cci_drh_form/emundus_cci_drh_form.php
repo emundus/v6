cb22a2c7c6bc439367517d06264649735255e8cc
Hugo Moracchini <hugo4400@gmail.com>
2019-01-11 16:16:07 +0100

    Rewrote plugin to user user params in order to force redirection.

--- plugins/user/emundus_cci_drh_form/emundus_cci_drh_form.php
+++ plugins/user/emundus_cci_drh_form/emundus_cci_drh_form.php
@@ -8,10 +8,29 @@ if (!defined('DS')) {
     define('DS',DIRECTORY_SEPARATOR);
 }
 
-class plgUserCci_drh_form extends JPlugin {
+class plgUserEmundus_cci_drh_form extends JPlugin {
 	
 	function _construct(& $subject, $config) {
 		parent::__construct($subject, $config);
+
+		$this->loadLanguage();
+		$user = JFactory::getUser();
+
+		if (!$user->guest) {
+
+			// need to load fresh instance
+			$table = JTable::getInstance('user', 'JTable');
+			$table->load($user->id);
+
+			// Reidrect the user if the param is present.
+			$user_params = new JRegistry($table->params);
+			if ($user_params->get('needs_company', 'false') == "true") {
+				$application = JFactory::getApplication();
+				$application->enqueueMessage('Afin de pouvoir continuer en tant que Décideur RH, vous devez déclarer votre entreprise.');
+				$application->redirect($this->params->get('url', null));
+			}
+		}
+
 	}
 	
 	function onLoginUser($user, $option) {
@@ -27,17 +46,15 @@ class plgUserCci_drh_form extends JPlugin {
 
 		$username  = JRequest::getVar('username');
 		$userid    = JUserHelper::getUserId($username);
-		
+
 		// For Guest, do nothing and just return, let joomla handle it
 		if (!$userid) {
 			return;
 		}
-			
-		$plugin = JPluginHelper::getPlugin('user', 'emundus_cci_drh_form');
- 		$params = new JRegistry($plugin->params);
 
- 		$lastvisitdate = JFactory::getUser($userid)->lastvisitDate;
-		$block		   = JFactory::getUser($userid)->block;
+		$current_user = JFactory::getUser($userid);
+ 		$lastvisitdate = $current_user->lastvisitDate;
+		$block		   = $current_user->block;
 
  		// Check for first login
 		if ($lastvisitdate == "0000-00-00 00:00:00" && $block == 0) {
@@ -74,7 +91,16 @@ class plgUserCci_drh_form extends JPlugin {
 					return;
 				}
 
-				JFactory::getApplication()->redirect($params->get('url'));
+				// Set the user param to notify him of the fact that he needs a company.
+				$current_user->setParam('needs_company', 'true');
+
+				// Get the raw User Parameters
+				$params = $current_user->getParameters();
+
+				// Set the user table instance to include the new token.
+				$table = JTable::getInstance('user', 'JTable');
+				$table->load($userid);
+				$table->params = $params->toString();
 			}
 
 		}
