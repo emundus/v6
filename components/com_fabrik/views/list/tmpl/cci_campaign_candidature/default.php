facc5946a817357d1e143e079818c01196c850cc
Hugo Moracchini <hugo4400@gmail.com>
2019-02-04 16:11:12 +0100

    Fixed PHP quotes causing errors.

--- components/com_fabrik/views/list/tmpl/cci_campaign_candidature/default.php
+++ components/com_fabrik/views/list/tmpl/cci_campaign_candidature/default.php
@@ -117,9 +117,9 @@ echo $this->table->intro;
                         ?>
 
                         <div class="em-result accordion-container accordion-container-<?php echo $this->table->renderid; ?>">
-                            <div class="em-top-details">
+                            <div class="em-top-details article-title article-title-<?php echo $this->table->renderid; ?>">
                                 <div class="g-block size-50 em-formation-title">
-                                    <h2 class="article-title article-title-<?php echo $this->table->renderid; ?>"><?php echo "<a href='".$d['fabrik_view_url'].'?rowid='.$d['jos_emundus_setup_programmes___id_raw'];"' >".$title."</a>"; ?></h2>
+                                    <h2><?php echo '<a href="'.$d['fabrik_view_url'].'?rowid='.$d['jos_emundus_setup_programmes___id_raw'].'" >'.$title."</a>"; ?></h2>
                                 </div>
                                 <div class="g-block size-50 em-status">
                                     <span class="label label-<?php echo $d['jos_emundus_setup_status___class_raw']; ?>">
