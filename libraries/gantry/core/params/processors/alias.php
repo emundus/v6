8d3dbd0c08f58179ab52bdafcc32f5bcec2736c3
James dean <jdean162@gmail.com>
2019-02-15 14:19:25 +0100

    Gantry 4 update

--- libraries/gantry/core/params/processors/alias.php
+++ libraries/gantry/core/params/processors/alias.php
@@ -2,7 +2,7 @@
 /**
  * @version   $Id: alias.php 6491 2013-01-15 02:25:56Z btowles $
  * @author    RocketTheme http://www.rockettheme.com
- * @copyright Copyright (C) 2007 - 2018 RocketTheme, LLC
+ * @copyright Copyright (C) 2007 - 2019 RocketTheme, LLC
  * @license   http://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 only
  *
  * Gantry uses the Joomla Framework (http://www.joomla.org), a GNU/GPLv2 content management system
