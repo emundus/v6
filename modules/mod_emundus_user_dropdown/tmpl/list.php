561109a42c766cda01a46b4c68d97412cac00925
Hugo Moracchini <hugo4400@gmail.com>
2019-03-01 16:31:29 +0100

    Added active class to currently active menu item in list view.

--- modules/mod_emundus_user_dropdown/tmpl/list.php
+++ modules/mod_emundus_user_dropdown/tmpl/list.php
@@ -14,7 +14,7 @@
         <ul>
             <?php if (!empty($list)) :?>
                 <?php foreach ($list as $i => $item) :?>
-                    <li class="<?php echo ($item->id)?'menu-item':''; ?>"><a href="<?php echo $item->flink ?>"><?php echo $item->title; ?></a></li>
+                    <li class="<?php echo ($item->id)?'menu-item':''; echo ($item->id == $active_id)?'active':''; ?>"><a href="<?php echo $item->flink ?>"><?php echo $item->title; ?></a></li>
                 <?php endforeach; ?>
             <?php endif; ?>
             <?php
