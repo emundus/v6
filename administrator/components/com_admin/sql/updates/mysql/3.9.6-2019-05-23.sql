4ee479d02e29f251198e265d1850153a964504b8
Benjamin Rivalland <benjamin.rivalland@emundus.fr>
2019-06-13 19:06:16 +0200

    some fixe on sql update

--- administrator/components/com_admin/sql/updates/mysql/3.9.6-2019-05-23.sql
+++ administrator/components/com_admin/sql/updates/mysql/3.9.6-2019-05-23.sql
@@ -3,5 +3,5 @@ CREATE TABLE `jos_emundus_updated` (
   `fnum` varchar(255) NOT NULL,
   `user_id` int(11) NOT NULL,
   `date_time` datetime NOT NULL,
-  `fabrik_element_id` json NOT NULL
+  `fabrik_element_id` longtext NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
