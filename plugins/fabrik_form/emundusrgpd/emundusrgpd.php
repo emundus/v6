d62d362f7f188000bb994cc7414e9a86f571df2b
Benjamin.R <benjamin.retord@orange.fr>
2019-07-07 16:01:13 +0200

    Remove obsolete condition and field

--- plugins/fabrik_form/emundusrgpd/emundusrgpd.php
+++ plugins/fabrik_form/emundusrgpd/emundusrgpd.php
@@ -16,36 +16,32 @@ defined('_JEXEC') or die();
 class PlgFabrik_FormEmundusrgpd extends plgFabrik_Form {
 
     public function onAfterProcess(){
-        $checkbox = $this->params->get('emunduscheckbox_rgpd');
+
         $subject = $this->params->get('emundussubject_rgpd');
         $body = $this->params->get('emundusbody_rgpd');
         $formModel = $this->getModel();
         $uid = $formModel->formDataWithTableName['jos_emundus_users___user_id'];
 
         $created = date('Y-m-d H:i:s');
-        
-        if(!empty($checkbox)){
-            $checked = $formModel->getElementData($checkbox);
-            if($checked){
-                //insert
-                $userNote = (object) array(
-                    'user_id' => $uid,
-                    'state'   => 0,
-                    'subject' => $subject,
-                    'body'    => $body,
-                    'created' => $created,
-                );
 
-                try
-                {
-                    $db = JFactory::getDBO();
-                    $db->insertObject('#__privacy_consents', $userNote);
-                }
-                catch (Exception $e)
-                {
-                    // Do nothing if the save fails
-                }
-            }
+        //insert
+        $userNote = (object) array(
+            'user_id' => $uid,
+            'state'   => 0,
+            'created' => $created,
+            'subject' => $subject,
+            'body'    => $body
+
+        );
+
+        try
+        {
+            $db = JFactory::getDBO();
+            $db->insertObject('#__privacy_consents', $userNote);
+        }
+        catch (Exception $e)
+        {
+            // Do nothing if the save fails
         }
     }
 }
