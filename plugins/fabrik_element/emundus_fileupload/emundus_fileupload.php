84b4bea85508a4ea1d39daf925ba2f816dab9025
Hugo Moracchini <hugo4400@gmail.com>
2019-09-02 18:49:52 +0200

    Added byte formatting for fileupload.

--- plugins/fabrik_element/emundus_fileupload/emundus_fileupload.php
+++ plugins/fabrik_element/emundus_fileupload/emundus_fileupload.php
@@ -190,6 +190,8 @@ class PlgFabrik_ElementEmundus_fileupload extends PlgFabrik_Element {
                     }
                 }
 
+                $size = $this->formatBytes($size);
+
                 if ($lengthFile > $nbMaxFile) {
                     $nbMax = false;
                 }
@@ -205,6 +207,15 @@ class PlgFabrik_ElementEmundus_fileupload extends PlgFabrik_Element {
         echo json_encode($result);
         return true;
     }
+
+	private function formatBytes($bytes, $precision = 2) {
+		$units = array('KB', 'MB', 'GB', 'TB');
+		$bytes = max($bytes, 0);
+		$pow = floor(($bytes ? log($bytes) : 0) / log(1024));
+		$pow = min($pow, count($units) - 1);
+		$bytes /= pow(1024, $pow);
+		return round($bytes, $precision) . ' ' . $units[$pow];
+	}
 
 
     /**
