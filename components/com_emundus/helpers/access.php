237bcabfd989a33749c72ef21e73b2e570a362e3
Hugo Moracchini <hugo4400@gmail.com>
2019-09-27 17:25:14 +0200

    Added function to check if user is allowed to see applicant's persional info.

--- components/com_emundus/helpers/access.php
+++ components/com_emundus/helpers/access.php
@@ -238,6 +238,38 @@ class EmundusHelperAccess {
 
 
 	/**
+	 * @param $user_id
+	 *
+	 * @return bool
+	 *
+	 * @since version
+	 */
+	public static function isDataAnonymized($user_id) {
+		JLog::addLogger(['text_file' => 'com_emundus.access.error.php'], JLog::ERROR, 'com_emundus');
+		require_once (JPATH_BASE.DS.'components'.DS.'com_emundus'.DS.'models'.DS.'users.php');
+		$m_users = new EmundusModelUsers();
+
+		$group_ids = $m_users->getUserGroups($user_id);
+		// NOTE: The unorthodox array_keys_flip is actually faster than doing array_unique(). The first array_keys is because the function used returns an assoc array [id => name].
+		$group_ids = array_keys(array_flip(array_keys($group_ids)));
+
+		$db = JFactory::getDbo();
+		$query = $db->getQuery(true);
+		$query->select($db->quoteName('anonymize'))
+			->from($db->quoteName('#__emundus_setup_groups'))
+			->where($db->quoteName('id').' IN ('.implode(',', $group_ids).')');
+		$db->setQuery($query);
+
+		try {
+			return in_array('1', $db->loadColumn());
+		} catch (Exception $e) {
+			JLog::add('Error seeing if user can access non anonymous data. -> '.$e->getMessage(), JLog::ERROR, 'com_emundus');
+			return false;
+		}
+	}
+
+
+	/**
 	 *
 	 * @return JCrypt
 	 *
