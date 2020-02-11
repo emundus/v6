cc4ad6c52432800b50037b268ef1f514a9fea836
Benjamin Rivalland <benjamin.rivalland@emundus.fr>
2020-02-11 18:23:23 +0100

    Add link to file request in logged messages sent to referees Change NOW() to local time

--- plugins/fabrik_form/php/scripts/emundus-referent_letter.php
+++ plugins/fabrik_form/php/scripts/emundus-referent_letter.php
@@ -75,6 +75,16 @@ $patterns = array ('/\[ID\]/', '/\[NAME\]/', '/\[EMAIL\]/', '/\[UPLOAD_URL\]/', 
 
 // setup mail
 $app = JFactory::getApplication();
+
+$offset = $app->get('offset', 'UTC');
+try {
+    $dateTime = new DateTime(gmdate("Y-m-d H:i:s"), new DateTimeZone('UTC'));
+    $dateTime = $dateTime->setTimezone(new DateTimeZone($offset));
+    $now = $dateTime->format('Y-m-d H:i:s');
+} catch (Exception $e) {
+    echo $e->getMessage() . '<br />';
+}
+
 $email_from_sys = $app->getCfg('mailfrom');
 
 $from = $obj->emailfrom;
@@ -107,6 +117,7 @@ foreach ($recipients as $recipient) {
                           VALUES (NOW(), '.$student->id.', "'.$key.'", "'.$attachment_id.'", '.$current_user->fnum.', '.$db->Quote($recipient['email']).')';
             $db->setQuery($query);
             $db->execute();
+            $request_id = $db->insertid();
             
             // 3. Envoi du lien vers lequel le professeur va pouvoir uploader la lettre de référence
             $link_upload = $baseurl.'index.php?option=com_fabrik&c=form&view=form&formid=68&tableid=71&keyid='.$key.'&sid='.$student->id;
@@ -141,6 +152,8 @@ foreach ($recipients as $recipient) {
                 JLog::add($send->__toString(), JLog::ERROR, 'com_emundus');
             } else {
                 JFactory::getApplication()->enqueueMessage(JText::_('MESSAGE_SENT').' : '.$recipient['email'], 'message');
+                $body = JText::_('SENT_TO'). ' '.$recipient['email'].'<br><a href="index.php?option=com_fabrik&view=details&formid=264&rowid='.$request_id.'&listid=273" target="_blank">'.JText::_('INVITATION_LINK').'</a><br>'.$body;
+
                 $sql = "INSERT INTO `#__messages` (`user_id_from`, `user_id_to`, `subject`, `message`, `date_time`)
                             VALUES ('62', '-1', ".$db->quote($subject).", ".$db->quote($body).", NOW())";
                 $db->setQuery($sql);
