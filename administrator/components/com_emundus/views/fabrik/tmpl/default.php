760c2139ce235725993d97f9516d65dd129424f6
James dean <jdean162@gmail.com>
2020-02-24 17:55:12 +0100

    New Dev : Clean fabrik elements and their columns in the DB

---
+++ administrator/components/com_emundus/views/fabrik/tmpl/default.php
@@ -0,0 +1,7 @@
+<?php
+/**
+* @package Joomla
+* @subpackage eMundus
+* @copyright Copyright (C) 2015 emundus.fr. All rights reserved.
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
+*/
