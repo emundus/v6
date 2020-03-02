6a3e1704e8bb0c2139c0816dd172093875b9e7e2
Hugo Moracchini <hugo4400@gmail.com>
2020-03-02 17:14:37 +0100

    Updated menu item link.

--- administrator/components/com_admin/sql/updates/mysql/3.9.15-2020-02-26-em.sql
+++ administrator/components/com_admin/sql/updates/mysql/3.9.15-2020-02-26-em.sql
@@ -1,3 +1,3 @@
-UPDATE jos_menu SET note = '18|c|1' WHERE id = 1386;
+UPDATE jos_menu SET note = '18|c|1', link = 'index.php?option=com_emundus&view=email&tmpl=component&fnums={fnums}&layout=expert&Itemid={Itemid}' WHERE id = 1386;
 
 drop index `key` on jos_emundus_files_request;
