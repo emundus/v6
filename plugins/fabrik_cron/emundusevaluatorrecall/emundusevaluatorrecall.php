246c3f6a6ca60d47fdfbcd161a54b8f1fe2b3fcc
James dean <jdean162@gmail.com>
2020-03-05 16:17:48 +0100

    emundus evaluator recall template fix

--- plugins/fabrik_cron/emundusevaluatorrecall/emundusevaluatorrecall.php
+++ plugins/fabrik_cron/emundusevaluatorrecall/emundusevaluatorrecall.php
@@ -24,27 +24,27 @@ require_once COM_FABRIK_FRONTEND . '/models/plugin-cron.php';
 
 class PlgFabrik_Cronemundusevaluatorrecall extends PlgFabrik_Cron {
 
-	/**
-	 * Check if the user can use the plugin
-	 *
-	 * @param   string  $location  To trigger plugin on
-	 * @param   string  $event     To trigger plugin on
-	 *
-	 * @return  bool can use or not
-	 */
-	public function canUse($location = null, $event = null) {
-		return true;
-	}
+    /**
+     * Check if the user can use the plugin
+     *
+     * @param   string  $location  To trigger plugin on
+     * @param   string  $event     To trigger plugin on
+     *
+     * @return  bool can use or not
+     */
+    public function canUse($location = null, $event = null) {
+        return true;
+    }
 
-	/**
-	 * Do the plugin action
-	 *
-	 * @param array  &$data data
-	 *
-	 * @return  mixed  number of records updated
-	 * @throws Exception
-	 */
-	public function process(&$data, &$listModel) {
+    /**
+     * Do the plugin action
+     *
+     * @param array  &$data data
+     *
+     * @return  mixed  number of records updated
+     * @throws Exception
+     */
+    public function process(&$data, &$listModel) {
 
         jimport('joomla.mail.helper');
 
@@ -53,21 +53,28 @@ class PlgFabrik_Cronemundusevaluatorrecall extends PlgFabrik_Cron {
         JLog::addLogger(['text_file' => 'com_emundus.evaluatorrecall.info.php'], JLog::INFO, 'com_emundus');
         JLog::addLogger(['text_file' => 'com_emundus.evaluatorrecall.error.php'], JLog::ERROR, 'com_emundus');
 
+        $params = $this->getParams();
 
-
-		$params = $this->getParams();
-
-		$reminder_mail_id = $params->get('reminder_mail_id', '');
+        $reminder_mail_id = $params->get('reminder_mail_id', '');
         $acl_aro_groups = $params->get('acl_aro_groups', '13');
         $reminder_deadline = $params->get('reminder_deadline', '14,7,1');
         $status_for_send = $params->get('reminder_status', '');
 
 
-        $users = $this->getEmundusUsers($acl_aro_groups);
+        $empty_evals = $this->getEmptyEvals($reminder_deadline, $status_for_send);
 
-		if (!empty($users)) {
-		    // Store the evaluators
-		    $evaluators = [];
+        if (empty($empty_evals)) {
+            return false;
+        }
+
+        $users = $this->getEmundusUsers($acl_aro_groups);
+        $group_users = $this->getGroupsByProg($acl_aro_groups);
+
+        $users = array_unique(array_merge($users, $group_users));
+
+        if (!empty($users)) {
+            // Store the evaluators
+            $evaluators = [];
 
             foreach ($users as $user) {
                 $fnums = $this->getFnumAssoc($user);
@@ -77,18 +84,16 @@ class PlgFabrik_Cronemundusevaluatorrecall extends PlgFabrik_Cron {
             }
 
             if (!empty($evaluators)) {
+                include_once (JPATH_SITE.DS.'components'.DS.'com_emundus'.DS.'models'.DS.'profile.php');
+                include_once (JPATH_SITE.DS.'components'.DS.'com_emundus'.DS.'controllers'.DS.'messages.php');
 
-                include_once(JPATH_SITE.'/components/com_emundus/models/emails.php');
-                $m_emails = new EmundusModelEmails;
-                
-                $email = $m_emails->getEmailById($reminder_mail_id);
-                $EmptyEvals = $this->getEmptyEvals($reminder_deadline, $status_for_send);
-                
+                $m_profile = new EmundusModelProfile();
+                $c_messages = new EmundusControllerMessages();
+
                 $emailArray = [];
 
-
                 foreach ($evaluators as $evaluator => $val) {
-                    foreach ($EmptyEvals as $applicant) {
+                    foreach ($empty_evals as $applicant) {
                         // We check if the fnum is in the list of associated fnums 
                         // If it is, this means we have to notify the evaluator, meaning, adding it in the emailArray
                         if (array_search($applicant->fnum, $val) !== false) {
@@ -107,77 +112,53 @@ class PlgFabrik_Cronemundusevaluatorrecall extends PlgFabrik_Cron {
                              * }
                              *
                              **/
-                            
+
                             $emailArray[$evaluator][$applicant->campaign_id]["label"] = $applicant->label;
                             $emailArray[$evaluator][$applicant->campaign_id]["eval_end_date"] = $applicant->eval_end_date;
                             $emailArray[$evaluator][$applicant->campaign_id]["fnums"][] = $applicant->fnum;
                         }
                     }
                 }
+
                 if (!empty($emailArray)) {
+                    $menu = JMenu::getInstance('site');
+
                     foreach ($emailArray as $emailUser => $emailCampaigns) {
                         foreach ($emailCampaigns as $campaign) {
                             // We send a email for each campaign for each user
-                            $mailer = JFactory::getMailer();
                             $user = JFactory::getUser($emailUser);
 
+                            $menutype = $m_profile->getProfileByApplicant($user->id)['menutype'];
+                            $items = $menu->getItems('menutype', $menutype);
+
+                            // We're getting the first link in the user's menu that's from com_emundus, which is PROBABLY a files/evaluation view, but this does not guarantee it.
+                            $index = 0;
+                            foreach ($items as $k => $item) {
+                                if ($item->component === 'com_emundus') {
+                                    $index = $k;
+                                    break;
+                                }
+                            }
+
+                            if (JFactory::getConfig()->get('sef') == 1) {
+                                $userLink = $items[$index]->alias;
+                            } else {
+                                $userLink = $items[$index]->link.'&Itemid='.$items[0]->id;
+                            }
+
+                            $fnumList = '<ul>';
+                            foreach ($campaign['fnums'] as $fnum) {
+                                $fnumList .= '<li><a href="'.JURI::root().$userLink.'#'.$fnum.'">'.$fnum.'</a></li>';
+                            }
+                            $fnumList .= '</ul>';
+
                             $post = array(
-                                'FNUM' => '<ul><li>' . implode('</li><li>',$campaign['fnums']) . '</li></ul>',
+                                'FNUMS' => $fnumList,
                                 'CAMPAIGN_LABEL' => $campaign['label'],
                                 'EVALUATION_END' => strftime("%d/%m/%Y %H:%M", strtotime($campaign['eval_end_date'])),
                                 'NAME' => $user->name,
                             );
-
-                            $tags = $m_emails->setTags($user->id, $post);
-
-                            $from = preg_replace($tags['patterns'], $tags['replacements'], $email->emailfrom);
-                            $from_id = 62;
-                            $fromname = preg_replace($tags['patterns'], $tags['replacements'], $email->name);
-                            $to = $user->email;
-                            $to_id = $user->id;
-                            $subject = preg_replace($tags['patterns'], $tags['replacements'], $email->subject);
-                            $body = preg_replace($tags['patterns'], $tags['replacements'], $email->message);
-
-                            $config = JFactory::getConfig();
-                            $email_from_sys = $config->get('mailfrom');
-                            $email_from = $email->emailfrom;
-
-                            // If the email sender has the same domain as the system sender address.
-                            if (!empty($email_from) && substr(strrchr($email_from, "@"), 1) === substr(strrchr($email_from_sys, "@"), 1)) {
-                                $mail_from_address = $email_from;
-                            } else {
-                                $mail_from_address = $email_from_sys;
-                            }
-
-                            // Set sender
-                            $sender = [
-                                $mail_from_address,
-                                $fromname
-                            ];
-                            
-                            $mailer->setSender($sender);
-                            $mailer->addReplyTo($from, $fromname);
-                            $mailer->addRecipient($to);
-                            $mailer->setSubject($subject);
-                            $mailer->isHTML(true);
-                            $mailer->Encoding = 'base64';
-                            $mailer->setBody($body);
-
-                            // Send emails
-                            $send = $mailer->Send();
-                            if ($send !== true) {
-	                            JLog::add("Error sending email to: " . $to. '\n message: '.$body, JLog::ERROR, 'com_emundus');
-                            } else {
-                                $message = array(
-                                    'user_id_from' => $from_id,
-                                    'user_id_to' => $to_id,
-                                    'subject' => $subject,
-                                    'message' => '<i>'.JText::_('MESSAGE').' '.JText::_('SENT').' '.JText::_('TO').' '.$to.'</i><br>'.$body
-                                );
-                                $m_emails->logEmail($message);
-                            }
-                            // to avoid been considered as a spam process or DDoS
-                            sleep(0.1);
+                            $c_messages->sendEmailNoFnum($user->email, $reminder_mail_id, $post);
                         }
                     }
                     JLog::add("\n process " . sizeof($emailArray) . " emails sent", JLog::INFO, 'com_emundus');
@@ -186,9 +167,8 @@ class PlgFabrik_Cronemundusevaluatorrecall extends PlgFabrik_Cron {
             }
         }
         return false;
-	}
+    }
 
-	
     /**
      * Gets all eMundus users linked to the Joomla acl_groups set in XML params
      *
@@ -204,7 +184,8 @@ class PlgFabrik_Cronemundusevaluatorrecall extends PlgFabrik_Cron {
             ->select(['DISTINCT(eu.user_id)'])
             ->from($db->quoteName('#__emundus_users', 'eu'))
             ->leftJoin($db->quoteName('#__emundus_users_profiles', 'eup') . ' ON ' . $db->quoteName('eu.user_id') . ' = '. $db->quoteName('eup.user_id'))
-            ->where($db->quoteName('eu.profile') . ' IN (' . $profile. ') OR ' . $db->quoteName('eup.profile_id') . ' IN (' . $profile. ')');
+            ->leftJoin($db->quoteName('#__emundus_setup_profiles', 'esp') . ' ON ' . $db->quoteName('esp.id') . ' = '. $db->quoteName('eup.profile_id'))
+            ->where($db->quoteName('esp.acl_aro_groups') . ' IN (' . $profile. ')');
 
         try {
             $db->setQuery($query);
@@ -215,7 +196,35 @@ class PlgFabrik_Cronemundusevaluatorrecall extends PlgFabrik_Cron {
         }
     }
 
-    
+    /**
+     * Gets all eMundus users where the group is linked to a program
+     *
+     * @param string $profile Joomla acl_group
+     * @return mixed
+     *
+     */
+    public function getGroupsByProg($profile) {
+        $db = JFactory::getDBO();
+        $query = $db->getQuery(true);
+
+        $query
+            ->select(['DISTINCT(eu.user_id)'])
+            ->from($db->quoteName('#__emundus_users', 'eu'))
+            ->leftJoin($db->quoteName('#__emundus_setup_profiles', 'esp') . ' ON ' . $db->quoteName('esp.id') . ' = '. $db->quoteName('eu.profile'))
+            ->leftJoin($db->quoteName('#__emundus_groups', 'eg') . ' ON ' . $db->quoteName('eg.user_id') . ' = '. $db->quoteName('eu.user_id'))
+            ->leftJoin($db->quoteName('#__emundus_setup_groups_repeat_course', 'esgrc') . ' ON ' . $db->quoteName('esgrc.parent_id') . ' = '. $db->quoteName('eg.group_id'))
+            ->leftJoin($db->quoteName('#__emundus_acl', 'ea') . ' ON ' . $db->quoteName('eg.group_id') . ' = '. $db->quoteName('ea.group_id'))
+            ->where($db->quoteName('esp.acl_aro_groups') . ' IN(' . $profile . ') AND ' . $db->quoteName('ea.action_id') . ' = 5 AND ' . $db->quoteName('ea.c') . ' = 1');
+
+        try {
+            $db->setQuery($query);
+            return $db->loadColumn();
+        } catch (Exception $e) {
+            JLog::add('SQL Error -> '.$query->__toString(), JLog::ERROR, 'com_emundus');
+            return false;
+        }
+    }
+
     /**
      * Gets all fnums linked to a user through the user assoc table
      *
@@ -241,7 +250,6 @@ class PlgFabrik_Cronemundusevaluatorrecall extends PlgFabrik_Cron {
         }
     }
 
-    
     /**
      * Gets all fnums linked to a user through the group assoc table
      *
@@ -273,7 +281,7 @@ class PlgFabrik_Cronemundusevaluatorrecall extends PlgFabrik_Cron {
      * Gets all empty evals linked to a program that have been sent and have a Date Diff with the evaluation end date
      *
      * @param string $reminder_deadline Days before eval end
-     * @param string $status_for_send Statuses to exclude 
+     * @param string $status_for_send Statuses to exclude
      * @return mixed
      *
      */
@@ -282,7 +290,7 @@ class PlgFabrik_Cronemundusevaluatorrecall extends PlgFabrik_Cron {
         $query = $db->getQuery(true);
 
         $query
-            ->select([$db->quoteName('ecc.*'), $db->quoteName('esc.*')])
+            ->select(['ecc.*', 'esc.*'])
             ->from($db->quoteName('#__emundus_campaign_candidature', 'ecc'))
             ->leftJoin($db->quoteName('#__emundus_evaluations', 'ee') . ' ON ' . $db->quoteName('ecc.fnum') . ' = '. $db->quoteName('ee.fnum'))
             ->leftJoin($db->quoteName('#__emundus_setup_campaigns', 'esc') . ' ON ' . $db->quoteName('esc.id') . ' = '. $db->quoteName('ecc.campaign_id'))
@@ -297,3 +305,4 @@ class PlgFabrik_Cronemundusevaluatorrecall extends PlgFabrik_Cron {
         }
     }
 }
+
