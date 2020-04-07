a1380827808043c6cbe6c76c59e58018bf364a59
Hugo Moracchini <hugo4400@gmail.com>
2020-04-07 10:11:26 +0200

    Joomla 3.9.16

--- libraries/vendor/composer/autoload_static.php
+++ libraries/vendor/composer/autoload_static.php
@@ -33,12 +33,6 @@ class ComposerStaticInit205c915b9c7d3e718e7c95793ee67ffe
     );
 
     public static $prefixLengthsPsr4 = array (
-        'Z' => 
-        array (
-            'Zend\\Validator\\' => 15,
-            'Zend\\Stdlib\\' => 12,
-            'Zend\\Escaper\\' => 13,
-        ),
         'T' => 
         array (
             'TYPO3\\PharStreamWrapper\\' => 24,
@@ -61,8 +55,6 @@ class ComposerStaticInit205c915b9c7d3e718e7c95793ee67ffe
         array (
             'Psr\\Log\\' => 8,
             'Psr\\Container\\' => 14,
-            'PhpOffice\\PhpWord\\' => 18,
-            'PhpOffice\\Common\\' => 17,
         ),
         'J' => 
         array (
@@ -89,18 +81,6 @@ class ComposerStaticInit205c915b9c7d3e718e7c95793ee67ffe
     );
 
     public static $prefixDirsPsr4 = array (
-        'Zend\\Validator\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/zendframework/zend-validator/src',
-        ),
-        'Zend\\Stdlib\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/zendframework/zend-stdlib/src',
-        ),
-        'Zend\\Escaper\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/zendframework/zend-escaper/src',
-        ),
         'TYPO3\\PharStreamWrapper\\' => 
         array (
             0 => __DIR__ . '/..' . '/typo3/phar-stream-wrapper/src',
@@ -132,14 +112,6 @@ class ComposerStaticInit205c915b9c7d3e718e7c95793ee67ffe
         'Symfony\\Component\\Yaml\\' => 
         array (
             0 => __DIR__ . '/..' . '/symfony/yaml',
-        ),
-        'PhpOffice\\PhpWord\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/phpoffice/phpword/src/PhpWord',
-        ),
-        'PhpOffice\\Common\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/phpoffice/common/src/Common',
         ),
         'ReCaptcha\\' => 
         array (
