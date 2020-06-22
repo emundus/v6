a4d7e9be3563d9827fbb4fe3b84aa575efd8acec
jire18 <33605545+jire18@users.noreply.github.com>
2020-06-22 10:21:35 +0200

    sql

--- administrator/components/com_admin/sql/updates/mysql/3.9.19-2020-06-19-em.sql
+++ administrator/components/com_admin/sql/updates/mysql/3.9.19-2020-06-19-em.sql
@@ -105,3 +105,6 @@ SELECT MAX(jos_modules.id)  , 144 FROM jos_modules;
 COMMIT;
 
 SET autocommit = 1;
+UPDATE `jos_modules` SET `published` = '0' WHERE `jos_modules`.`id` = 155 AND `module` = 'mod_custom';
+
+UPDATE `jos_modules` SET `access` = 1 WHERE `module` = 'mod_emundus_user_dropdown';
