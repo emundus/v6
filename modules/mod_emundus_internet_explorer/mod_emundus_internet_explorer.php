341b9c91b249bade6bd50656b90a1b2856961fde
Hugo Moracchini <hugo4400@gmail.com>
2020-06-29 15:12:53 +0200

    Optimizations to internet explorer module.

--- modules/mod_emundus_internet_explorer/mod_emundus_internet_explorer.php
+++ modules/mod_emundus_internet_explorer/mod_emundus_internet_explorer.php
@@ -1,28 +1,23 @@
 <?php
-defined('_JEXEC') or die('Access Deny');
+defined('_JEXEC') or die('Access Denied');
 require_once(dirname(__FILE__).DS.'helper.php');
-require_once(JPATH_BASE.DS.'components'.DS.'com_emundus'.DS.'models'.DS.'stats.php');
 
-JHtml::script('media/com_emundus/js/jquery.cookie.js');
 JHtml::script('media/jui/js/bootstrap.min.js');
 
-$document   = JFactory::getDocument();
-$document->addStyleSheet("modules/mod_emundus_internet_explorer/style/mod_emundus_internet_explorer.css" );
+$document = JFactory::getDocument();
+$document->addStyleSheet("modules/mod_emundus_internet_explorer/style/mod_emundus_internet_explorer.css");
 
 $helper = new modEmundusInternetExplorerHelper;
 
 $message = str_replace("\r\n", "",nl2br(addslashes($params->get('message'))));
-$layout  = substr($params->get('layout', 'default'), 2);
+$layout = substr($params->get('layout', 'default'), 2);
 
-if($message === "")
+if ($message === "") {
 	$message = "TEXT_DEFAULT";
+}
 
 $session = JFactory::getSession();
 $agent = $_SERVER['HTTP_USER_AGENT'];
-if(($session->get('showInternetExplorer') === null || $session->get('showInternetExplorer') === true) && (preg_match('/MSIE/i',$agent) || preg_match('/Trident/i',$agent))) {
-	$show = true;
-} else {
-	$show = false;
+if (($session->get('showInternetExplorer') === null || $session->get('showInternetExplorer') === true) && (preg_match('/MSIE/i',$agent) || preg_match('/Trident/i',$agent))) {
+	require(JModuleHelper::getLayoutPath('mod_emundus_internet_explorer', $layout));
 }
-
-require(JModuleHelper::getLayoutPath('mod_emundus_internet_explorer',$layout));
