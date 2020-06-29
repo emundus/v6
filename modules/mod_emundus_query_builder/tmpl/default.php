5615cbae4fd95e0dc96ba90f1819f0d1b0aafcdf
jire18 <33605545+jire18@users.noreply.github.com>
2020-06-29 15:39:41 +0200

    all stat file default delete addScript and addStyleSheet

--- modules/mod_emundus_query_builder/tmpl/default.php
+++ modules/mod_emundus_query_builder/tmpl/default.php
@@ -1,13 +1,6 @@
 <?php
 defined('_JEXEC') or die;
 header('Content-Type: text/html; charset=utf-8');
-$document = JFactory::getDocument();
-//Chart.js is the libary used for this module's graphs
-$document->addScript('media'.DS.'com_emundus'.DS.'lib'.DS.'Chart.min.js');
-//moment.js is a Date libary, using to retrieve missing dates
-$document->addScript('media'.DS.'com_emundus'.DS.'lib'.DS.'moment.min.js');
-$document->addStyleSheet('media'.DS.'com_emundus'.DS.'lib'.DS.'bootstrap-336'.DS.'css'.DS.'bootstrap.min.css');
-$document->addStyleSheet('media'.DS.'com_emundus'.DS.'lib'.DS.'Semantic-UI-CSS-master'.DS.'semantic.min.css');
 ?>
 
 
