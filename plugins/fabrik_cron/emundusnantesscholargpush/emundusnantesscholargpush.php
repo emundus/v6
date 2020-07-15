4a7091b94e8aec3cff8cd4f843c1c2fb9d23e3a4
Hugo Moracchini <hugo4400@gmail.com>
2020-07-15 15:41:32 +0200

    Improvements to ScholarG plugin

--- plugins/fabrik_cron/emundusnantesscholargpush/emundusnantesscholargpush.php
+++ plugins/fabrik_cron/emundusnantesscholargpush/emundusnantesscholargpush.php
@@ -66,72 +66,78 @@ class PlgFabrik_Cronemundusnantesscholargpush extends PlgFabrik_Cron {
         $token = $params->get('token');
         $status = $params->get('status');
         $tag = $params->get('tag');
+        $profile = $params->get('profile');
 
 		$this->log = '';
 
 
 		$select = [
-			'pd.email AS adresseMailExterne',
-			'a.bac_year AS anneeBac',
-			'a.parcours_sup_annee AS anneeEnsSupInscription1',
-			'a.univ_year AS anneeInscription1',
-			'a.bac_serie AS codBac',
-			'pd.city_1 AS codCommuneAF',
-			'cc.id AS codDossier',
-			'a.bac_department AS codDptBac',
-			'a.univ_depatment AS codDptInscription1',
-			'pd.birth_department AS codDptNaissance',
+			$db->quoteName('pd.email','adresseMailExterne'),
+			$db->quoteName('a.bac_year', 'anneeBac'),
+			$db->quoteName('a.parcours_sup_annee','anneeEnsSupInscription1'),
+			$db->quoteName('a.univ_year','anneeInscription1'),
+			$db->quoteName('a.bac_serie','codBac'),
+			$db->quoteName('pd.city_1','codCommuneAF'),
+			$db->quoteName('cc.id','codDossier'),
+			$db->quoteName('a.bac_department','codDptBac'),
+			$db->quoteName('a.univ_depatment','codDptInscription1'),
+			$db->quoteName('pd.birth_department','codDptNaissance'),
 			'"EMUNDUS" AS codEtablissement',
-			'a.univ_establishment AS codEtbInscription1',
+			$db->quoteName('a.univ_establishment','codEtbInscription1'),
 			'"" AS codEtbLycee',
 			'"1" AS codFamiliale',
-			'a.bac_mention AS codMention',
+			$db->quoteName('a.bac_mention','codMention'),
 			'"N" AS codMilitaire',
-			'pd.nationality AS codNationalite',
-			'pd.country_1 AS codPaysAF',
-			'pd.birth_country AS codPaysNaissance',
+			$db->quoteName('c.scholarg_code','codNationalite'),
+			$db->quoteName('c2.scholarg_code','codPaysAF'),
+			$db->quoteName('c3.scholarg_code','codPaysNaissance'),
 			'"99" AS codPcsParent1',
-			'pd.zipcode_1 AS codPostalAF',
-			'a.bac_establishment_type AS codTypEtablissement',
-			'pd.birth_town AS codcommunenaissance',
+			$db->quoteName('pd.zipcode_1','codPostalAF'),
+			$db->quoteName('a.bac_establishment_type','codTypEtablissement'),
+			'"" AS codcommunenaissance',
 			'"99" AS codpcsParent2',
-			'pd.street_2 AS complementAF',
-			'pd.city_2 AS cpEtrangerAF',
-			'pd.birth_date AS datNaissance',
-			'pd.birth_town AS lieuNaissance',
-			'pd.city_1 AS localiteAF',
+			$db->quoteName('pd.street_2','complementAF'),
+			$db->quoteName('pd.city_2','cpEtrangerAF'),
+			$db->quoteName('pd.birth_date','datNaissance'),
+			$db->quoteName('pd.birth_town','lieuNaissance'),
+			$db->quoteName('fc.name','localiteAF'),
 			'"0" AS nbrEnfants',
-			'pd.last_name AS nomUsuel',
-			'a.parcours_sup_ine AS numIne',
-			'pd.telephone AS numPortable',
-			'pd.telephone AS numTelephoneAF',
-			'pd.street_1 AS numVoieAF',
-			'pd.nom_marital AS patronyme',
-			'pd.first_name AS prenom1',
-			'pd.second_name AS prenom2',
-			'pd.gender AS gender',
-			'pd.city_1 AS villeAF',
-			'pd.street_1 AS voieAF',
-			'cc.fnum'
+			$db->quoteName('pd.last_name','nomUsuel'),
+			$db->quoteName('a.parcours_sup_ine','numIne'),
+			$db->quoteName('pd.telephone','numPortable'),
+			$db->quoteName('pd.telephone','numTelephoneAF'),
+			$db->quoteName('pd.street_1','numVoieAF'),
+			$db->quoteName('pd.nom_marital','patronyme'),
+			$db->quoteName('pd.first_name','prenom1'),
+			$db->quoteName('pd.second_name','prenom2'),
+			$db->quoteName('pd.gender','gender'),
+			$db->quoteName('fc.name','villeAF'),
+			$db->quoteName('pd.street_1','voieAF'),
+			$db->quoteName('cc.fnum')
 		];
 
 		// TODO : codNationalite needs to look at a different table, not the eMundus one, fuze this.
-		// TODO : codecommunenaissance ugh...
 		// TODO : localieAF, where does it come from ?
 		// TODO : Check villeAF
 
         // Get list of files to push
         $query = $db->getQuery(true);
-        $query->select($db->quoteName($select))
+        $query->select($select)
 	        ->from($db->quoteName('jos_emundus_campaign_candidature','cc'))
 			->leftJoin($db->quoteName('jos_emundus_personal_detail', 'pd').' ON '.$db->quoteName('cc.fnum').' = '.$db->quoteName('pd.fnum'))
+	        ->leftJoin($db->quoteName('jos_emundus_setup_campaigns', 'sc').' ON '.$db->quoteName('cc.campaign_id').' = '.$db->quoteName('sc.id'))
 	        ->leftJoin($db->quoteName('jos_emundus_academic', 'a').' ON '.$db->quoteName('cc.fnum').' = '.$db->quoteName('a.fnum'))
-	        ->where($db->quoteName('cc.status').' = '.$status.' AND '.$db->quoteName('cc.fnum'). ' NOT IN (SELECT fnum FROM jos_emundus_tag_assoc as eta WHERE id_tag = '.$tag.')');
+	        ->leftJoin($db->quoteName('jos_emundus_country', 'c').' ON '.$db->quoteName('c.id').' = '.$db->quoteName('pd.nationality'))
+	        ->leftJoin($db->quoteName('jos_emundus_country', 'c2').' ON '.$db->quoteName('c2.id').' = '.$db->quoteName('pd.country_1'))
+	        ->leftJoin($db->quoteName('jos_emundus_country', 'c3').' ON '.$db->quoteName('c3.id').' = '.$db->quoteName('pd.birth_country'))
+	        ->leftJoin($db->quoteName('jos_emundus_french_cities', 'fc').' ON '.$db->quoteName('fc.insee_code').' LIKE '.$db->quoteName('pd.city_1'))
+	        ->where($db->quoteName('sc.profile_id').' = '.$db->quote($profile).' AND '.$db->quoteName('cc.status').' = '.$status.' AND '.$db->quoteName('cc.fnum'). ' NOT IN (SELECT fnum FROM jos_emundus_tag_assoc as eta WHERE id_tag = '.$tag.')');
         $db->setQuery($query);
 
         try {
         	$files = $db->loadObjectList('fnum');
         } catch (Exception $e) {
+        	echo '<pre>'; var_dump($e->getMessage()); echo '</pre>'; die;
         	JLog::add('Error getting files to be exported : '.$e->getMessage(), JLog::ERROR, 'com_emundus.emundusnantesscholargpush');
         	return false;
         }
@@ -142,8 +148,21 @@ class PlgFabrik_Cronemundusnantesscholargpush extends PlgFabrik_Cron {
 			// remove fnum from object being sent to API.
 			unset($file->fnum);
 
-			$response = $http->post($api_url.$api_route, json_encode($file), ['X-Auth-Token' => $token, 'Content-Type' => 'application/json']);
+			if (empty($file->codDptBac)) {
+				$file->codBac = "";
+			}
+			if (empty($file->codDptBac)) {
+				$file->codDptBac = "";
+			}
+			if (empty($file->codDptNaissance)) {
+				$file->codDptNaissance = "";
+			}
+			if (empty($file->codTypEtablissement)) {
+				$file->codTypEtablissement = "";
+			}
 
+			$response = $http->post($api_url.$api_route, json_encode($file), ['X-Auth-Token' => $token, 'Content-Type' => 'application/json']);
+			
 			if ($response->code === 200) {
 				$response->body = json_decode($response->body);
 
@@ -167,7 +186,6 @@ class PlgFabrik_Cronemundusnantesscholargpush extends PlgFabrik_Cron {
 
 			} else {
 				JLog::add('Error ('.$response->code.') from client API : '.$response->body);
-				continue;
 			}
 		}
 
