51479fb8a390dd4d80969b23f7beb37946902ef7
Hugo Moracchini <hugo4400@gmail.com>
2020-07-23 10:36:40 +0200

    Fixed EMPLOI fabrik form script.

--- plugins/fabrik_form/php/scripts/emundus-EMPLOI_isApplicationSent.php
+++ plugins/fabrik_form/php/scripts/emundus-EMPLOI_isApplicationSent.php
@@ -19,44 +19,39 @@ $mainframe = JFactory::getApplication();
 $jinput = $mainframe->input;
 $eMConfig = JComponentHelper::getParams('com_emundus');
 $can_edit_until_deadline = 0;
-$id_applicants 			 = $eMConfig->get('id_applicants', '0');
-$applicants 			 = explode(',',$id_applicants);
+$id_applicants = $eMConfig->get('id_applicants', '0');
+$applicants = explode(',',$id_applicants);
 
 $fnum = $jinput->get('rowid', null);
 
-if(!EmundusHelperAccess::asApplicantAccessLevel($user->id)) {
-    if ($jinput->get('tmpl')=='component') {
-        JHTML::stylesheet( JURI::base().'media/com_fabrik/css/fabrik.css' );
-        JHTML::stylesheet( JURI::base().'media/system/css/modal.css' );
-        $doc = JFactory::getDocument();
-        $doc->addScript("media/com_fabrik/js/window-min.js");
-        $doc->addScript("media/com_fabrik/js/lib/form_placeholder/Form.Placeholder.js");
-        $doc->addScript("templates/rt_afterburner2/js/rokmediaqueries.js");
-    }
-    //echo "<script>$('rt-header').remove(); $('rt-footer').remove(); $('gf-menu-toggle').remove();</script>";
-} else{
-    if (($user->fnum != $fnum && $fnum != -1) && !empty($fnum)) {
-        JError::raiseNotice('ERROR', JText::_('ERROR...'));
-        $mainframe->redirect("index.php");
-    }
+if (EmundusHelperAccess::asApplicantAccessLevel($user->id)) {
+	if (($user->fnum != $fnum && $fnum != -1) && !empty($fnum)) {
+		JError::raiseNotice('ERROR', JText::_('ERROR...'));
+		$mainframe->redirect("index.php");
+	}
+} else {
+	if ($jinput->get('tmpl') == 'component') {
+		JHTML::stylesheet(JURI::base().'media/com_fabrik/css/fabrik.css');
+		JHTML::stylesheet(JURI::base().'media/system/css/modal.css');
+		$doc = JFactory::getDocument();
+		$doc->addScript("media/com_fabrik/js/window-min.js");
+		$doc->addScript("media/com_fabrik/js/lib/form_placeholder/Form.Placeholder.js");
+		$doc->addScript("templates/rt_afterburner2/js/rokmediaqueries.js");
+	}
 }
 
 
-//$registered = $db->loadResult();
-if (EmundusHelperAccess::asCoordinatorAccessLevel($user->id)){
+if (EmundusHelperAccess::asCoordinatorAccessLevel($user->id)) {
     $sid = $jinput->get('sid', null, 'ALNUM');
-
     echo !empty($rowid)?'<h4 style="text-align:right">#'.$fnum.'</h4>':'';
-
-}
-else {
+} else {
 	if ($jinput->get('view') == 'form' && empty($fnum) && !isset($fnum)) {
 		$itemid = $jinput->get('Itemid');
 		// Si l'application Form a été envoyée par le candidat : affichage vue details
-		if($user->candidature_posted > 0  && $can_edit_until_deadline == 0) {
+		if ($user->candidature_posted > 0  && $can_edit_until_deadline == 0) {
 			$mainframe->redirect("index.php?option=com_fabrik&view=details&formid=".$jinput->get('formid')."&Itemid=".$itemid."&usekey=fnum&rowid=".$user->fnum);
-		} elseif(strtotime(date("Y-m-d H:m:i")) > strtotime($user->end_date) && !in_array($user->id, $applicants) ) {
-			JError::raiseNotice('CANDIDATURE_PERIOD_TEXT', utf8_encode(JText::sprintf('PERIOD', strftime("%d/%m/%Y %H:%M", strtotime($user->start_date) ), strftime("%d/%m/%Y %H:%M", strtotime($user->end_date) ))));
+		} elseif (strtotime(date("Y-m-d H:m:i")) > strtotime($user->end_date) && !in_array($user->id, $applicants) ) {
+			JError::raiseNotice('CANDIDATURE_PERIOD_TEXT', utf8_encode(JText::sprintf('PERIOD', strftime("%d/%m/%Y %H:%M", strtotime($user->start_date)), strftime("%d/%m/%Y %H:%M", strtotime($user->end_date)))));
 			$mainframe->redirect("index.php?option=com_fabrik&view=details&formid=".$jinput->get('formid')."&Itemid=".$itemid."&usekey=fnum&rowid=".$user->fnum);
 		} else {
 			if (empty($fnum) && !isset($fnum)) {
@@ -66,4 +61,3 @@ else {
 		}
 	}
 }
-?>
