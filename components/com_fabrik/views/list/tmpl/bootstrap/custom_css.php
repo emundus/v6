af475b01569374ab2eed41bed81e3676758c9f00
Hugo Moracchini <hugo4400@gmail.com>
2020-07-24 11:59:52 +0200

    Improvements and fixes to CSS

---
+++ components/com_fabrik/views/list/tmpl/bootstrap/custom_css.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Fabrik List Template: Default Custom CSS
+ *
+ * @package     Joomla
+ * @subpackage  Fabrik
+ * @copyright   Copyright (C) 2005-2020  Media A-Team, Inc. - All rights reserved.
+ * @license     GNU/GPL http://www.gnu.org/copyleft/gpl.html
+ */
+
+/**
+* If you need to make small adjustments or additions to the CSS for a Fabrik
+* list template, you can create a custom_css.php file, which will be loaded after
+* the main template_css.php for the template.
+*
+* This file will be invoked as a PHP file, so the list ID
+* can be used in order to narrow the scope of any style changes.  You do
+* this by prepending #listform_$c to any selectors you use.  This will become
+* (say) #listform_12, owhich will be the HTML ID of your list on the page.
+*
+* See examples below, which you should remove if you copy this file.
+*
+* Don't edit anything outside of the BEGIN and END comments.
+*
+* For more on custom CSS, see the Wiki at:
+*
+* http://www.fabrikar.com/forums/index.php?wiki/form-and-details-templates/#the-custom-css-file
+*
+* NOTE - for backward compatibility with Fabrik 2.1, and in case you
+* just prefer a simpler CSS file, without the added PHP parsing that
+* allows you to be be more specific in your selectors, we will also include
+* a custom.css we find in the same location as this file.
+*
+*/
+
+header('Content-type: text/css');
+echo <<<EOT
+/* BEGIN - Your CSS styling starts here */
+
+.g-back-office-emundus-tableau.view-list #g-container-main .page-header, 
+.g-back-office-emundus-tableau.view-form #g-container-main .page-header, 
+g-back-office-emundus-tableau.view-details #g-container-main .page-header {
+    padding-bottom: 30px;
+}
+
+/* PAGINATION */
+
+.list-footer .limit .input-prepend.input-append span.add-on {
+	padding: 9px !important;
+    height: 41px;
+}
+
+.list-footer .limit .input-prepend.input-append span.add-on label {
+	margin-bottom: 0 !important;
+}
+
+.list-footer .limit .input-prepend.input-append select.inputbox.input-mini {
+    padding: 10.6px !important;
+    width: 80px;
+}
+
+/* END - Your CSS styling ends here */
+EOT;
