3813f024ffb12322d1cc67f7104e6278e8a55f8a
James Dean <jdean162@gmail.com>
2020-08-18 12:09:27 +0200

    Add limit condition to campaign-check php script plugin
    Added form errors so we don't get redirected with a 404

--- plugins/fabrik_form/php/scripts/emundus-campaign-check.php
+++ plugins/fabrik_form/php/scripts/emundus-campaign-check.php
@@ -8,17 +8,29 @@ $campaign_id = $data['jos_emundus_campaign_candidature___campaign_id_raw'][0];
 $eMConfig = JComponentHelper::getParams('com_emundus');
 $applicant_can_renew = $eMConfig->get('applicant_can_renew', '0');
 
+require_once (JPATH_SITE.DS.'components'.DS.'com_emundus'.DS.'models'.DS.'campaign.php');
+$m_campaign     = new EmundusModelCampaign;
+$isLimitObtained = $m_campaign->isLimitObtained($campaign_id);
+
+if ($isLimitObtained) {
+    JLog::add('User: '.$user->id.' Campaign limit is obtained', JLog::ERROR, 'com_emundus');
+
+    $formModel->setFormErrorMsg("Campaign limit is obtained.");
+    $formModel->getForm()->error = JText::_('ERROR');
+    return false;
+}
 switch ($applicant_can_renew) {
 
     // Cannot create new campaigns at all.
     case 0:
         JLog::add('User: '.$user->id.' already has a file.', JLog::ERROR, 'com_emundus');
-        JError::raiseError(400, 'User already has a file open and cannot have multiple.');
-        exit;
+        $formModel->setFormErrorMsg('User already has a file open and cannot have multiple.');
+        $formModel->getForm()->error = JText::_('ERROR');
+        return false;
 
-	// If the applicant can only have one file per campaign.
-	case 2:
-		$query = 'SELECT id
+    // If the applicant can only have one file per campaign.
+    case 2:
+        $query = 'SELECT id
 					FROM #__emundus_setup_campaigns
 					WHERE published = 1
 					AND end_date >= NOW()
@@ -29,24 +41,25 @@ switch ($applicant_can_renew) {
 						where applicant_id='. $user->id.'
 					)';
 
-		try {
+        try {
 
             $db->setQuery($query);
-			if (!in_array($campaign_id, $db->loadColumn())) {
-				JLog::add('User: '.$user->id.' already has a file for campaign id: '.$campaign_id, JLog::ERROR, 'com_emundus');
-                JError::raiseError(400, 'User already has a file for this campaign.');
-                exit;
-			}
+            if (!in_array($campaign_id, $db->loadColumn())) {
+                JLog::add('User: '.$user->id.' already has a file for campaign id: '.$campaign_id, JLog::ERROR, 'com_emundus');
+                $formModel->setFormErrorMsg('User already has a file for this campaign.');
+                $formModel->getForm()->error = JText::_('ERROR');
+                return false;
+            }
 
-		} catch (Exception $e) {
-			JLog::add('plugin/emundus_campaign SQL error at query :'.$query, JLog::ERROR, 'com_emundus');
-		}
+        } catch (Exception $e) {
+            JLog::add('plugin/emundus_campaign SQL error at query :'.$query, JLog::ERROR, 'com_emundus');
+        }
 
-		break;
+        break;
 
     // If the applicant can only have one file per school year.
-	case 3:
-		$query = 'SELECT id
+    case 3:
+        $query = 'SELECT id
 					FROM #__emundus_setup_campaigns
 					WHERE published = 1
 					AND end_date >= NOW()
@@ -58,19 +71,20 @@ switch ($applicant_can_renew) {
 						where applicant_id='. $user->id.'
 					)';
 
-		try {
+        try {
 
             $db->setQuery($query);
-			if (!in_array($campaign_id, $db->loadColumn())) {
-				JLog::add('User: '.$user->id.' already has a file for year belong to campaign: '.$campaign_id, JLog::ERROR, 'com_emundus');
-                JError::raiseError(400, 'User already has a file for this year.');
-                exit;
-			}
+            if (!in_array($campaign_id, $db->loadColumn())) {
+                JLog::add('User: '.$user->id.' already has a file for year belong to campaign: '.$campaign_id, JLog::ERROR, 'com_emundus');
+                $formModel->setFormErrorMsg('User already has a file for this year.');
+                $formModel->getForm()->error = JText::_('ERROR');
+                return false;
+            }
 
-		} catch (Exception $e) {
-			JLog::add('plugin/emundus_campaign SQL error at query :'.$query, JLog::ERROR, 'com_emundus');
-		}
+        } catch (Exception $e) {
+            JLog::add('plugin/emundus_campaign SQL error at query :'.$query, JLog::ERROR, 'com_emundus');
+        }
 
-		break;
+        break;
 
 }
