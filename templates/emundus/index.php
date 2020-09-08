37f526d3cd2c6e9de4de1867fb0289afd955513a
Brice <b.hubinet@gmail.com>
2020-09-08 11:19:12 +0200

    [HOTFIX] Fix SaaS CSS

--- templates/emundus/index.php
+++ templates/emundus/index.php
@@ -59,23 +59,7 @@ $doc = JFactory::getDocument();
                     <jdoc:include type="modules" name="header-onboarding"/>
                 <?php endif; ?>
             </div>
-            <div id="moduletable-c" class="moduletable-c size-9">
-                <?php
-                $showcolumn = $this->countModules('top-a');
-                ?>
-                <?php if($showcolumn): ?>
-                    <jdoc:include type="modules" name="top-a"/>
-                <?php endif; ?>
-            </div>
             <div id="moduletable-d" class="moduletable-d size-30">
-                <?php
-                $showcolumn = $this->countModules('header-switch');
-                ?>
-                <?php if($showcolumn): ?>
-                    <jdoc:include type="modules" name="header-switch"/>
-                <?php endif; ?>
-            </div>
-            <div id="moduletable-e" class="moduletable-e size-0">
                 <?php
                 $showcolumn = $this->countModules('header-c');
                 ?>
