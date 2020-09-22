6f0086cf17712129cc24c0bea59d300a5174652d
Brice <b.hubinet@gmail.com>
2020-09-22 09:44:29 +0200

    Fix isemail validation parameters, fix multiple options at keyup enter

--- components/com_emundus_onboard/models/formbuilder.php
+++ components/com_emundus_onboard/models/formbuilder.php
@@ -1622,9 +1622,9 @@ class EmundusonboardModelformbuilder extends JModelList {
                     $element['params']['validations']['plugin_published'][] = "1";
                     $element['params']['validations']['validate_in'][] = "both";
                     $element['params']['validations']['validation_on'][] = "both";
-                    $element['params']['validations']['validate_hidden'][] = "1";
+                    $element['params']['validations']['validate_hidden'][] = "0";
                     $element['params']['validations']['must_validate'][] = "0";
-                    $element['params']['validations']['show_icon'][] = "1";
+                    $element['params']['validations']['show_icon'][] = "0";
                 } else {
                     //$element['params']['validations']['plugin'] = array_merge(array_diff($element['params']['validations']['plugin'], array("isemail")));
                     $key = array_search("isemail", $element['params']['validations']['plugin']);
