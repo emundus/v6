7e58f9b3ee93a459a91ca3b79eca6cc2e0ea5e83
Brice <b.hubinet@gmail.com>
2020-10-09 12:04:15 +0200

    Edit actions menu to close it, and fix transitions

--- components/com_emundus_onboard/src/views/formBuilder.vue
+++ components/com_emundus_onboard/src/views/formBuilder.vue
@@ -34,56 +34,71 @@
             @removeMenu="removeMenu"
     />
     <div class="row form-builder">
-      <div class="actions-menu menu-block">
-        <div>
-          <div class="heading-actions">
-            <label class="form-title" style="padding: 0; margin: 0">{{Actions}}</label>
-          </div>
-          <div class="action-links">
-              <a class="d-flex action-link" style="padding-top: 2em" @click="$modal.show('modalMenu')">
-                <em class="add-page-icon col-md-offset-1 col-sm-offset-1"></em>
-                <label class="action-label col-md-offset-2 col-sm-offset-1">{{addMenu}}</label>
-              </a>
-              <a class="d-flex action-link" @click="createGroup()">
-                <em class="add-group-icon col-md-offset-1 col-sm-offset-1"></em>
-                <label class="action-label col-md-offset-2 col-sm-offset-1">{{addGroup}}</label>
-              </a>
-              <a class="d-flex action-link" :class="{ 'disable-element': elementDisabled}" @click="showElements">
-                <em class="add-element-icon col-md-offset-1 col-sm-offset-1"></em>
-                <label class="action-label col-md-offset-2 col-sm-offset-1" :class="[{'disable-element': elementDisabled}, addingElement ? 'down-arrow' : 'right-arrow']">{{addItem}}</label>
-              </a>
-            <transition :name="'slide-down'" type="transition">
-              <draggable
-                      v-model="plugins"
-                      v-bind="dragOptions"
-                      v-if="addingElement"
-                      handle=".handle"
-                      @start="dragging = true;draggingIndex = index"
-                      @end="addingNewElement($event)"
-                      drag-class="plugin-drag"
-                      chosen-class="plugin-chosen"
-                      ghost-class="plugin-ghost"
-                      style="padding-bottom: 2em">
-                  <div class="d-flex plugin-link col-md-offset-3 col-sm-offset-2 handle" v-for="(plugin,index) in plugins" :id="'plugin_' + plugin.value" @dblclick="addingNewElementByDblClick(plugin.value)" :title="plugin.name">
-                    <em :class="plugin.icon"></em>
-                    <span class="ml-10px">{{plugin.name}}</span>
-                  </div>
-              </draggable>
-            </transition>
-          </div>
-        </div>
-        <a class="send-form-button" @click="sendForm">
-          <label style="cursor: pointer" class="mb-0">{{sendFormButton}}</label>
-          <em class="fas fa-paper-plane" style="font-size: 20px"></em>
-        </a>
-      </div>
-      <div class="col-md-8 col-sm-9 col-md-offset-4 col-sm-offset-4 menu-block">
+      <div class="sidebar-formbuilder">
         <div class="heading-block">
           <h1 class="form-title" style="padding: 0; margin: 0">{{profileLabel}}</h1>
           <a :href="'index.php?option=com_emundus_onboard&view=form&layout=add&pid=' + this.prid" style="margin-left: 1em" :title="Edit">
             <em class="fas fa-pencil-alt" data-toggle="tooltip" data-placement="top"></em>
           </a>
         </div>
+        <transition name="little-move-right">
+          <div v-if="!actions_menu">
+            <button class="actions-button" type="button" @click="actions_menu = true">
+              {{Actions}}
+              <em class="fas fa-plus-circle"></em>
+            </button>
+          </div>
+        </transition>
+        <transition name="move-right">
+          <div class="actions-menu menu-block" v-if="actions_menu">
+            <div>
+              <div class="heading-actions">
+                <label class="form-title" style="padding: 0; margin: 0">{{Actions}}</label>
+                <button type="button" class="btnCloseModal" @click="actions_menu = false">
+                  <em class="fas fa-times-circle pointer" ></em>
+                </button>
+              </div>
+              <div class="action-links">
+                  <a class="d-flex action-link" style="padding-top: 2em" @click="$modal.show('modalMenu')">
+                    <em class="add-page-icon col-md-offset-1 col-sm-offset-1"></em>
+                    <label class="action-label col-md-offset-2 col-sm-offset-1">{{addMenu}}</label>
+                  </a>
+                  <a class="d-flex action-link" @click="createGroup()">
+                    <em class="add-group-icon col-md-offset-1 col-sm-offset-1"></em>
+                    <label class="action-label col-md-offset-2 col-sm-offset-1">{{addGroup}}</label>
+                  </a>
+                  <a class="d-flex action-link" :class="{ 'disable-element': elementDisabled}" @click="showElements">
+                    <em class="add-element-icon col-md-offset-1 col-sm-offset-1"></em>
+                    <label class="action-label col-md-offset-2 col-sm-offset-1" :class="[{'disable-element': elementDisabled}, addingElement ? 'down-arrow' : 'right-arrow']">{{addItem}}</label>
+                  </a>
+                <transition :name="'slide-down'" type="transition">
+                  <draggable
+                          v-model="plugins"
+                          v-bind="dragOptions"
+                          v-if="addingElement"
+                          handle=".handle"
+                          @start="dragging = true;draggingIndex = index"
+                          @end="addingNewElement($event)"
+                          drag-class="plugin-drag"
+                          chosen-class="plugin-chosen"
+                          ghost-class="plugin-ghost"
+                          style="padding-bottom: 2em">
+                      <div class="d-flex plugin-link col-md-offset-3 col-sm-offset-2 handle" v-for="(plugin,index) in plugins" :id="'plugin_' + plugin.value" @dblclick="addingNewElementByDblClick(plugin.value)" :title="plugin.name">
+                        <em :class="plugin.icon"></em>
+                        <span class="ml-10px">{{plugin.name}}</span>
+                      </div>
+                  </draggable>
+                </transition>
+              </div>
+            </div>
+            <a class="send-form-button" @click="sendForm">
+              <label style="cursor: pointer" class="mb-0">{{sendFormButton}}</label>
+              <em class="fas fa-paper-plane" style="font-size: 20px"></em>
+            </a>
+          </div>
+        </transition>
+      </div>
+      <div  :class="actions_menu ? 'col-md-8 col-md-offset-4 col-sm-9 col-sm-offset-3' : 'col-md-10 col-md-offset-2 col-sm-11 col-sm-offset-1'" class="menu-block">
         <ul class="form-section">
           <li>
             <a :class="menuHighlight === 0 ? 'form-section__current' : ''" @click="menuHighlight = 0;indexHighlight = 0">{{FormPage}}</a>
@@ -199,6 +214,7 @@
     data() {
       return {
         // UX variables
+        actions_menu: true,
         UpdateUx: false,
         menuHighlight: 0,
         indexHighlight: "0",
@@ -771,70 +787,14 @@
 </script>
 
 <style scoped lang="scss">
-  .fa-li {
-    left: -0.45em;
-  }
-
-  .full-width {
-    width: 100vw;
-    position: relative;
-    margin-left: -50vw !important;
-    left: 50%;
-    margin-top: -4.2%;
-  }
-  .container {
-    margin-bottom: 5%;
-  }
-  h1 {
-    margin: 20px;
-    line-height: 20px;
-    font-family: "Open Sans", sans-serif;
-    box-sizing: border-box;
-  }
-  .sidebar {
-    padding-top: 20px;
-    background-color: #f0f0f0;
-    height: 100%;
-    width: 16.9%;
-  }
-  body {
-    background-color: #fafafa;
-  }
-  .Topbar {
-    text-align: center;
-    font-family: "Open Sans", sans-serif;
-    padding: 25px 0;
-    background-color: #f0f0f0;
-    height: 150px;
-  }
-  .separator {
-    border-right: 1px solid hsla(0, 0%, 81%, 0.5);
-  }
-
-  .btnreturn {
-    position: relative;
-    left: 37%;
-    top: 5%;
-    background-color: #1b1f3c;
-    border-radius: 28px;
-    border: 1px solid #1b1f3c;
-    display: inline-block;
-    cursor: pointer;
-    color: #ffffff;
-    font-family: Arial;
-    font-size: 17px;
-    padding: 12px 27px;
-    text-decoration: none;
-  }
-  .btnreturn:hover {
-    background-color: #ef6d3b;
-    border: 1px solid #ef6d3b;
-  }
-
   .form-builder{
     margin-top: 6em;
     padding: 1em;
     min-height: 50em;
+  }
+
+  .menu-block {
+    margin-top: 0;
   }
 
   .form-title{
@@ -850,47 +810,6 @@
       margin-top: 0;
     }
   }
-  .select-form{
-    display: flex;
-  }
-  .select-form select{
-    width: 75%;
-    margin-left: 1em;
-  }
-
-  .add-menu{
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    border: unset;
-    cursor: pointer;
-    align-self: baseline;
-  }
-
-  .add-menu:hover > .btnPM {
-    background-color: #1b1f3c;
-    color: white;
-  }
-
-  .dropdown-toggle{
-    height: auto;
-    background: white;
-  }
-  .divider-menu{
-    width: 100%;
-    margin: 0em;
-  }
-  .heading-block{
-    text-align: center;
-    margin-bottom: 1em;
-    margin-top: 2em;
-    width: 75%;
-  }
-  .edit-icon{
-    align-items: center;
-    display: flex;
-    justify-content: center;
-  }
   .container-fluid{
     margin-bottom: 10em;
   }
@@ -899,25 +818,6 @@
     position: relative;
     cursor: grab;
     left: 5px;
-  }
-  .heading-actions{
-    background: #1b1f3c;
-    height: 60px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    font-size: 18px;
-    color: #fff;
-  }
-  .action-link{
-    padding: 1em 10px 10px 5px;
-    cursor: pointer;
-  }
-  .action-link:hover > .action-label{
-    color: #de6339;
-  }
-  .action-links{
-    background: #fafafa;
   }
   .form-viewer-builder{
     background: #fafafa;
