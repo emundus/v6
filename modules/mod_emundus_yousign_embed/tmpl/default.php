5490d0a9101a0acae1b4538e721fda904fce6d8f
Hugo Moracchini <hugo4400@gmail.com>
2020-11-09 16:56:48 +0100

    Added iFrame module.

---
+++ modules/mod_emundus_yousign_embed/tmpl/default.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @package   Joomla.Site
+ * @subpackage  eMundus
+ * @copyright Copyright (C) 2020 emundus.fr. All rights reserved.
+ * @license   GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// no direct access
+defined('_JEXEC') or die;
+?>
+<div class="em-yousign-embed">
+    <iframe src="https://staging-app.yousign.com/procedure/sign?<?= $yousign_member_id; ?>"></iframe>
+</div>
