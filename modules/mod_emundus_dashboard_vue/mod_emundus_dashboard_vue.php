ee9adf554e9598348315b29f03ea292fbbc35665
Brice <b.hubinet@gmail.com>
2020-12-24 16:40:45 +0100

    Rename dashboard module and remove unused helper

--- modules/mod_emundus_dashboard_vue/mod_emundus_dashboard_vue.php
+++ modules/mod_emundus_dashboard_vue/mod_emundus_dashboard_vue.php
@@ -1,7 +1,6 @@
 <?php
 
 defined('_JEXEC') or die('Access Deny');
-require_once(dirname(__FILE__).DS.'helper.php');
 
 JHtml::script('media/com_emundus/js/jquery.cookie.js');
 JHtml::script('media/jui/js/bootstrap.min.js');
