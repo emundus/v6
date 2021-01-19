593b8ec6aa90822490dbc2366bfe88d9228b38be
Brice <b.hubinet@gmail.com>
2021-01-19 10:13:18 +0100

    Fix foreign key and change profile to int

--- administrator/components/com_admin/sql/updates/mysql/3.9.23-2021-01-12.sql
+++ administrator/components/com_admin/sql/updates/mysql/3.9.23-2021-01-12.sql
@@ -3,7 +3,7 @@ CREATE TABLE `jos_emundus_campaign_workflow` (
     `date_time` datetime DEFAULT NULL,
     `updated` datetime DEFAULT NULL,
     `campaign` int(11) DEFAULT NULL,
-    `profile` text DEFAULT NULL,
+    `profile` int(11) DEFAULT NULL,
     `status` int(2) DEFAULT NULL,
     PRIMARY KEY (`id`),
     KEY `fb_groupby_campaign_INDEX` (`campaign`)
@@ -13,4 +13,4 @@ CREATE TABLE `jos_emundus_campaign_workflow` (
 ALTER TABLE `jos_emundus_campaign_workflow`
     ADD CONSTRAINT jos_emundus_campaign_workflow_ibfk_1 FOREIGN KEY (`campaign`) REFERENCES `jos_emundus_setup_campaigns` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
     ADD CONSTRAINT jos_emundus_campaign_workflow_ibfk_2 FOREIGN KEY (`profile`) REFERENCES `jos_emundus_setup_profiles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-    ADD CONSTRAINT jos_emundus_campaign_workflow_ibfk_2 FOREIGN KEY (`status`) REFERENCES `jos_emundus_setup_status` (`step`) ON DELETE CASCADE ON UPDATE CASCADE;
+    ADD CONSTRAINT jos_emundus_campaign_workflow_ibfk_3 FOREIGN KEY (`status`) REFERENCES `jos_emundus_setup_status` (`step`) ON DELETE CASCADE ON UPDATE CASCADE;
