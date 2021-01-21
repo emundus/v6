a3488e0cf3960cc4f2d501b9bed79f0c27810c4c
Brice <b.hubinet@gmail.com>
2021-01-21 15:25:13 +0100

    Fix publish_up,down cannot be null

--- administrator/components/com_admin/sql/updates/mysql/3.9.23-2021-20-01.sql
+++ administrator/components/com_admin/sql/updates/mysql/3.9.23-2021-20-01.sql
@@ -18,7 +18,7 @@ WHERE reference_table = 'menu' and reference_id IN (
 
 # Insert new module not published by default to include image background on homepage #
 INSERT INTO jos_modules (asset_id, title, note, content, ordering, position, checked_out, checked_out_time, publish_up, publish_down, published, module, access, showtitle, params, client_id, language) VALUES (0, 'Homepage background', '', '<div class="homepage-background-relative"><img class="homepage-background" style="background: url(''images/custom/home_background.png'');" />
-<h1 class="welcome-message">Bienvenue sur votre espace de candidature</h1></div>', 1, 'content-top-a', 62, '2021-01-21 11:36:45', null, null, 0, 'mod_emundus_custom', 9, 0, '{"prepare_content":0,"layout":"_:default","moduleclass_sfx":"","cache":1,"cache_time":900,"cachemode":"static","module_tag":"div","bootstrap_size":"0","header_tag":"h3","header_class":"","style":"0"}', 0, '*');
+<h1 class="welcome-message">Bienvenue sur votre espace de candidature</h1></div>', 1, 'content-top-a', 62, '2021-01-21 11:36:45', '2021-01-21 11:36:45', '2025-01-21 11:36:45', 0, 'mod_emundus_custom', 9, 0, '{"prepare_content":0,"layout":"_:default","moduleclass_sfx":"","cache":1,"cache_time":900,"cachemode":"static","module_tag":"div","bootstrap_size":"0","header_tag":"h3","header_class":"","style":"0"}', 0, '*');
 SET @module_id := LAST_INSERT_ID();
 
 SELECT @menu_id := id
