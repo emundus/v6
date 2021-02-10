358633fd4d4ec165e2d5a9b8c09d31f539393f51
Benjamin Rivalland <benjamin.rivalland@emundus.fr>
2021-02-10 12:18:15 +0100

    css - remove height in Fabrik form template

--- components/com_fabrik/views/form/tmpl/bootstrap/custom_css.php
+++ components/com_fabrik/views/form/tmpl/bootstrap/custom_css.php
@@ -363,11 +363,11 @@ form legend a, form legend a:hover, form legend a:focus {
 .customsend-application-file a {
     color: #ffffff !important;
 }
-/* Fabrik application WYSIWYG height */
+/* Fabrik application WYSIWYG height 
 .view-form iframe {
     height: 200px !important;
 }
-
+*/
 .view-form .mce-tinymce .mce-statusbar .mce-path > .mce-path-item {
     display: none !important;
 }
