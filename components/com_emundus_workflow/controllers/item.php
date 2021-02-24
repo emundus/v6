fef34b21fb149000d9622efee08f297a1d85cf44
Duy Tran <duy.tran@emundus.fr>
2021-02-24 09:04:59 +0100

    model and controller for getAllItemTypes

--- components/com_emundus_workflow/controllers/item.php
+++ components/com_emundus_workflow/controllers/item.php
@@ -26,17 +26,42 @@ jimport('joomla.application.component.controller');
         var $model = null;
 
         public function __construct($config=array()) {
+            require_once (JPATH_COMPONENT.DS.'helpers'.DS.'access.php');
             parent::__construct($config);
             $this->model = $this->getModel('item'); //get item model
         }
 
-        public function createItem() {
-
-        }
+        public function createItem() {}
 
         public function deleteItem($id) {}
 
         public function updateItemOrder($id,$old_order,$new_order) {}
+
+        public function getallitems() {
+            $user = JFactory::getUser();
+
+            if(!EmundusworkflowHelperAccess::asCoordinatorAccessLevel($user->id)) {
+                $result = 0;
+                $tab = array('status' => $result, 'msg' => JText::_("ACCESS_DENIED"));
+            }
+            else {
+                $_wit = $this->model;
+                //$jinput = JFactory::getApplication()->input;
+
+                //do stuff
+                $items= $_wit->getAllItems();
+                if (count($items) > 0) {
+                    $tab = array('status' => 1, 'msg' => JText::_("ITEMS_RETRIEVED"), 'data' => $items);
+                }
+                else {
+                    $tab = array('status' => 0, 'msg' => JText::_("NO_ITEMS"), 'data' => $items);
+                }
+
+            }
+            echo json_encode((object)$tab);
+            exit;
+            //return json_encode((object)$tab);
+        }
 
         public function getOrder($id) {}
 
--- components/com_emundus_workflow/models/item.php
+++ components/com_emundus_workflow/models/item.php
@@ -22,4 +22,27 @@ class EmundusworkflowModelitem extends JModelList
     public function __construct($config = array()) {
         parent::__construct($config);
     }
+
+    //get all items from database
+    public function getAllItems() {
+        $db = JFactory::getDbo();
+        $query =$db->getQuery(true);
+
+        try {
+            //query string
+            $query->select('*')
+                ->from($db->quoteName('#__emundus_workflow_item_type'));
+
+            //execute query string
+            $db->setQuery($query);
+            return $db->loadObjectList();
+        }
+        catch(Exception $e) {
+            JLog::add('component/com_emundus_workflow/models/workflow | Cannot get all item types' . preg_replace("/[\r\n]/"," ",$query->__toString().' -> '.$e->getMessage()), JLog::ERROR, 'com_emundus_workflow');
+            return $e->getMessage();
+        }
+
+//        var_dump($query->__toString()); die;
+        //var_dump($db->loadObjectList()); die;
+    }
 }
