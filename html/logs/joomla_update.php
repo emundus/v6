9115e04aaad707e426aadf1919074e8e0bc83bd1
James Dean <jdean162@gmail.com>
2021-03-08 17:36:41 +0100

    Joomla 3.9.25

--- html/logs/joomla_update.php
+++ html/logs/joomla_update.php
@@ -12,3 +12,32 @@
 2020-12-08T08:44:55+00:00	INFO ::1	update	Effacement des fichiers et dossiers à supprimer.
 2020-12-08T08:44:57+00:00	INFO ::1	update	Mise en ordre après installation.
 2020-12-08T08:44:57+00:00	INFO ::1	update	La mise à jour vers la version 3.9.23 est achevée.
+2021-03-08T16:34:30+00:00	INFO ::1	update	Mise à jour lancée par l'utilisateur Administrator (62). L'ancienne version est 3.9.24.
+2021-03-08T16:34:33+00:00	INFO ::1	update	Téléchargement du fichier de mise à jour depuis https://s3-us-west-2.amazonaws.com/joomla-official-downloads/joomladownloads/joomla3/Joomla_3.9.25-Stable-Update_Package.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA6LXDJLNUINX2AVMH%2F20210308%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20210308T163430Z&X-Amz-Expires=60&X-Amz-SignedHeaders=host&X-Amz-Signature=4d5ce2c888bee20988458deb57983dd5fb87c910ac496674275ef2fd6fc48b79.
+2021-03-08T16:34:40+00:00	INFO ::1	update	Fichier Joomla_3.9.25-Stable-Update_Package.zip téléchargé avec succès.
+2021-03-08T16:34:40+00:00	INFO ::1	update	Lancement de l'installation de la nouvelle version.
+2021-03-08T16:34:45+00:00	INFO ::1	update	Conclusion de l'installation.
+2021-03-08T16:34:45+00:00	INFO ::1	update	Requête lancée à partir du fichier 3.9.24-2021-02-12-em. Texte de la requête: CREATE TABLE `jos_emundus_campaign_workflow` (     `id` int(11) NOT NULL AUTO_IN.
+2021-03-08T16:34:45+00:00	INFO ::1	update	Requête lancée à partir du fichier 3.9.24-2021-02-12-em. Texte de la requête: ALTER TABLE `jos_emundus_campaign_workflow`     ADD CONSTRAINT jos_emundus_campa.
+2021-03-08T16:34:45+00:00	INFO ::1	update	Requête lancée à partir du fichier 3.9.24-2021-02-12-em. Texte de la requête: INSERT INTO jos_fabrik_forms (label, record_in_database, error, intro, created, .
+2021-03-08T16:34:45+00:00	INFO ::1	update	Requête lancée à partir du fichier 3.9.24-2021-02-12-em. Texte de la requête: SET @form_id := LAST_INSERT_ID();.
+2021-03-08T16:34:45+00:00	INFO ::1	update	Requête lancée à partir du fichier 3.9.24-2021-02-12-em. Texte de la requête: INSERT INTO jos_fabrik_lists (label, introduction, form_id, db_table_name, db_pr.
+2021-03-08T16:34:45+00:00	INFO ::1	update	Requête lancée à partir du fichier 3.9.24-2021-02-12-em. Texte de la requête: INSERT INTO jos_fabrik_groups (name, css, label, published, created, created_by,.
+2021-03-08T16:34:45+00:00	INFO ::1	update	Requête lancée à partir du fichier 3.9.24-2021-02-12-em. Texte de la requête: SET @group_id := LAST_INSERT_ID();.
+2021-03-08T16:34:45+00:00	INFO ::1	update	Requête lancée à partir du fichier 3.9.24-2021-02-12-em. Texte de la requête: INSERT INTO jos_fabrik_formgroup (form_id, group_id, ordering) VALUES (@form_id,.
+2021-03-08T16:34:45+00:00	INFO ::1	update	Requête lancée à partir du fichier 3.9.24-2021-02-12-em. Texte de la requête: INSERT INTO jos_fabrik_elements (name, group_id, plugin, label, checked_out, che.
+2021-03-08T16:34:45+00:00	INFO ::1	update	Requête lancée à partir du fichier 3.9.24-2021-02-12-em. Texte de la requête: INSERT INTO jos_fabrik_elements (name, group_id, plugin, label, checked_out, che.
+2021-03-08T16:34:45+00:00	INFO ::1	update	Requête lancée à partir du fichier 3.9.24-2021-02-12-em. Texte de la requête: INSERT INTO jos_fabrik_elements (name, group_id, plugin, label, checked_out, che.
+2021-03-08T16:34:45+00:00	INFO ::1	update	Requête lancée à partir du fichier 3.9.24-2021-02-12-em. Texte de la requête: INSERT INTO jos_fabrik_elements (name, group_id, plugin, label, checked_out, che.
+2021-03-08T16:34:45+00:00	INFO ::1	update	Requête lancée à partir du fichier 3.9.24-2021-02-12-em. Texte de la requête: SET @campaign := LAST_INSERT_ID();.
+2021-03-08T16:34:45+00:00	INFO ::1	update	Requête lancée à partir du fichier 3.9.24-2021-02-12-em. Texte de la requête: INSERT INTO jos_fabrik_elements (name, group_id, plugin, label, checked_out, che.
+2021-03-08T16:34:45+00:00	INFO ::1	update	Requête lancée à partir du fichier 3.9.24-2021-02-12-em. Texte de la requête: SET @profile := LAST_INSERT_ID();.
+2021-03-08T16:34:45+00:00	INFO ::1	update	Requête lancée à partir du fichier 3.9.24-2021-02-12-em. Texte de la requête: INSERT INTO jos_fabrik_elements (name, group_id, plugin, label, checked_out, che.
+2021-03-08T16:34:45+00:00	INFO ::1	update	Requête lancée à partir du fichier 3.9.24-2021-02-12-em. Texte de la requête: SET @status := LAST_INSERT_ID();.
+2021-03-08T16:34:45+00:00	INFO ::1	update	Requête lancée à partir du fichier 3.9.24-2021-02-12-em. Texte de la requête: INSERT INTO jos_fabrik_joins (list_id, element_id, join_from_table, table_join, .
+2021-03-08T16:34:45+00:00	INFO ::1	update	Requête lancée à partir du fichier 3.9.24-2021-02-12-em. Texte de la requête: INSERT INTO jos_fabrik_joins (list_id, element_id, join_from_table, table_join, .
+2021-03-08T16:34:45+00:00	INFO ::1	update	Requête lancée à partir du fichier 3.9.24-2021-02-12-em. Texte de la requête: INSERT INTO jos_fabrik_joins (list_id, element_id, join_from_table, table_join, .
+2021-03-08T16:34:45+00:00	INFO ::1	update	Requête lancée à partir du fichier 3.9.24-2021-02-24-em. Texte de la requête: UPDATE jos_emundus_setup_tags set request=REPLACE(request, 'JPATH_BASE', 'JPATH_.
+2021-03-08T16:34:45+00:00	INFO ::1	update	Effacement des fichiers et dossiers à supprimer.
+2021-03-08T16:34:47+00:00	INFO ::1	update	Mise en ordre après installation.
+2021-03-08T16:34:47+00:00	INFO ::1	update	La mise à jour vers la version 3.9.25 est achevée.
