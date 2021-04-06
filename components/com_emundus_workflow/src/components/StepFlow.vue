bab7161f99677c360e23ef1170efd963ffdcd9cc
Duy Tran <duy.tran@emundus.fr>
2021-04-06 15:58:37 +0200

    create step flow (basic CSS) --> don't forget to call it in main.js

---
+++ components/com_emundus_workflow/src/components/StepFlow.vue
@@ -0,0 +1,81 @@
+<template>
+  <div id="stepflow">
+    <div class="min-h-screen flex overflow-x-scroll py-12">
+      <div v-for="column in columns" :key="column.title" class="bg-gray-100 rounded-lg px-3 py-3 column-width rounded mr-4">
+        <p>{{ column.title }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "stepflow",
+  components: {
+  },
+  data() {
+    return {
+      columns: [
+        { title: "Step 1", },
+        { title: "Step 2", },
+        { title: "Step 3", },
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+.column-width {
+  min-width: 450px;
+  width: 450px;
+}
+
+.px-3 {
+  padding-left: .75rem;
+  padding-right: .75rem;
+}
+
+.py-3 {
+  padding-top: .75rem;
+  padding-bottom: .75rem;
+}
+
+.mr-4 {
+  margin-right: 1rem;
+}
+
+.rounded-lg {
+  border-radius: .5rem;
+}
+
+.bg-gray-100 {
+  background-color: #c6e1dc;
+  background-image: radial-gradient(circle, black 1px, rgba(0, 0, 0, 0) 1px);
+  background-size: 3em 3em;
+}
+
+.py-12 {
+  padding-top: 3rem;
+  padding-bottom: 3rem;
+}
+
+.overflow-x-scroll {
+  overflow-x: scroll;
+}
+
+.min-h-screen {
+  min-height: 100vh;
+}
+
+.flex {
+  display: flex;
+}
+
+*, ::after, ::before {
+  box-sizing: border-box;
+  border-width: 0;
+  border-style: solid;
+  border-color: #e2e8f0;
+}
+</style>
