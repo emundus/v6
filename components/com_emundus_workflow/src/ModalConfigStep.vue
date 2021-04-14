93712a1664260faafd41d5146d7f531ef90b56a0
Duy Tran <duy.tran@emundus.fr>
2021-04-14 11:57:50 +0200

    fix hiding modal :) (one line code)

--- components/com_emundus_workflow/src/ModalConfigStep.vue
+++ components/com_emundus_workflow/src/ModalConfigStep.vue
@@ -195,7 +195,7 @@ export default {
             confirmButtonColor: '#28a745',
           }).then(result => {
             if(result.isConfirmed) {
-              this.exitModal();
+              this.$modal.hide("stepModal" + this.element.id);
             }
             // this.inStatusSelected = $( "#instatus-selected option:selected" ).text();
           })
