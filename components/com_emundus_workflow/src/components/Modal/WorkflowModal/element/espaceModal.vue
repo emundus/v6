6783f2f625a4f1517a57a69f2d9bddfb5ca285eb
Duy Tran <duy.tran@emundus.fr>
2021-05-17 11:31:14 +0200

    define common rules of using JText.Joomla._

--- components/com_emundus_workflow/src/components/Modal/WorkflowModal/element/espaceModal.vue
+++ components/com_emundus_workflow/src/components/Modal/WorkflowModal/element/espaceModal.vue
@@ -4,26 +4,26 @@
     <link type="text/css" rel="stylesheet" href="//unpkg.com/bootstrap-vue@latest/dist/bootstrap-vue.min.css" />
 
     <div class="row mb-3">
-      <label class="col-sm-6 col-form-label">{{ this.elementTitle.item_name_label }}</label>
+      <label class="col-sm-6 col-form-label">{{ espaceModal_Title.label }}</label>
       <div class="col-xs-8">
-        <textarea :id="'step_label'+element.id" rows="3" v-model="form.itemLabel" :placeholder="modalName" style="width: 95%; height: 35px !important"></textarea>
+        <textarea :id="'step_label'+element.id" rows="3" v-model="form.itemLabel" :placeholder="espaceModal_PlaceHolder.label" style="width: 95%; height: 35px !important"></textarea>
       </div>
     </div>
 
     <div class="row mb-3">
-      <label class="col-sm-6 col-form-label">{{ this.elementTitle.form_name_title }}</label>
+      <label class="col-sm-6 col-form-label">{{ espaceModal_Title.profile }}</label>
       <div class="col-xs-8">
         <select v-model="form.formNameSelected" class="form-control-select">
-          <option selected disabled>{{ profilePlaceholder }}</option>
+          <option selected disabled>{{ espaceModal_PlaceHolder.profile }}</option>
           <option v-for="form in this.$data.forms" :value="form.id"> {{ form.label }}</option>
         </select>
       </div>
     </div>
 
     <div class="row mb-3">
-      <label class="col-sm-6 col-form-label">{{ this.elementTitle.notes_title }}</label>
+      <label class="col-sm-6 col-form-label">{{ espaceModal_Title.supplementary_information }}</label>
       <div class="col-xs-8">
-        <textarea id="notes_form" rows="3" v-model="form.notes" :placeholder="notesPlaceholder" style="margin: -5px; width: 102%"></textarea>
+        <textarea id="notes_form" rows="3" v-model="form.notes" :placeholder="espaceModal_PlaceHolder.supplementary_information" style="margin: -5px; width: 102%"></textarea>
       </div>
     </div>
 
@@ -31,8 +31,6 @@
 </template>
 <script>
 import axios from 'axios';
-import Swal from "sweetalert2";
-import $ from 'jquery';     //using jquery
 
 const qs = require('qs');
 let _all = [];
@@ -43,24 +41,22 @@ export default {
   },
   data: function() {
     return {
-      modalName: Joomla.JText._("COM_EMUNDUS_WORKFLOW_ESPACE_PLACEHOLDER_LABEL"),
-      profilePlaceholder: Joomla.JText._("COM_EMUNDUS_WORKFLOW_ESPACE_PLACEHOLDER_FORM"),
-      notesPlaceholder: Joomla.JText._("COM_EMUNDUS_WORKFLOW_ESPACE_PLACEHOLDER_SUPPLEMENTARY_INFORMATION"),
-
-      elementTitle: {
-        item_name_label: Joomla.JText._("COM_EMUNDUS_WORKFLOW_ESPACE_MODAL_LABEL_TITLE"),
-        form_name_title: Joomla.JText._("COM_EMUNDUS_WORKFLOW_ESPACE_MODAL_FORM_LABEL"),
-        edited_status_title: Joomla.JText._("COM_EMUNDUS_WORKFLOW_ESPACE_MODAL_INPUT_STATUS_LABEL"),
-        output_status_title: Joomla.JText._("COM_EMUNDUS_WORKFLOW_ESPACE_MODAL_OUTPUT_STATUS_LABEL"),
-        notes_title: Joomla.JText._("COM_EMUNDUS_WORKFLOW_ESPACE_MODAL_SUPPLEMENTARY_INFORMATION_LABEL"),
+      espaceModal_PlaceHolder: {
+        label: Joomla.JText._("COM_EMUNDUS_WORKFLOW_ELEMENT_ESPACE_MODAL_PLACEHOLDER_LABEL"),
+        profile: Joomla.JText._("COM_EMUNDUS_WORKFLOW_ELEMENT_ESPACE_MODAL_PLACEHOLDER_PROFILE"),
+        supplementary_information: Joomla.JText._("COM_EMUNDUS_WORKFLOW_ELEMENT_ESPACE_MODAL_PLACEHOLDER_SUPPLEMENTARY_INFORMATION"),
       },
+
+      espaceModal_Title: {
+        label: Joomla.JText._("COM_EMUNDUS_WORKFLOW_ELEMENT_ESPACE_MODAL_TITLE_LABEL"),
+        profile: Joomla.JText._("COM_EMUNDUS_WORKFLOW_ELEMENT_ESPACE_MODAL_TITLE_PROFILE"),
+        supplementary_information: Joomla.JText._("COM_EMUNDUS_WORKFLOW_ELEMENT_ESPACE_MODAL_TITLE_SUPPLEMENTARY_INFORMATION"),
+      },
+
       form: {
         itemLabel: "",
         formNameSelected: '',
-        inputStatus: [],
-        outputStatus: '',
         notes: '',
-        color: "#0f4c81",
       },
       forms: [],
       status: [],
@@ -119,60 +115,6 @@ export default {
         console.log(error);
       })
     },
-
-    // getAvailableInStatus: async function(data) {
-    //   var _rawA2 = await axios.get('index.php?option=com_emundus_workflow&controller=item&task=getavailableinputstatus', { params: { data : data }});
-    //   var _rawAll = await axios.get('index.php?option=com_emundus_workflow&controller=common&task=getallstatus');
-    //
-    //   var _a2 = _rawA2.data.data;
-    //   var _all = _rawAll.data.data;
-    //
-    //   let _idiff = _all.filter(({ step: id1 }) => !_a2.some(({ step: id2 }) => id2 === id1));
-    //   let _iintersect = _all.filter(item1 => _a2.some(item2 => item1.step === item2.step));
-    //
-    //   if(_idiff.length !== 0 && _iintersect.length !== 0) {
-    //     _idiff.forEach(elt => elt['disabled'] = true);
-    //     _iintersect.forEach(elt => elt['disabled'] = false);
-    //     var _merge1 = _idiff.concat(_iintersect);
-    //   }
-    //
-    //   else {
-    //     if(_idiff.length !== 0 && _iintersect.length == 0) {
-    //       _all.forEach(elt => elt['disabled'] = true);
-    //       var _merge1 = _all;
-    //     }
-    //     else {
-    //       _all.forEach(elt => elt['disabled'] = false);
-    //       var _merge1 = _all;
-    //     }
-    //   }
-    //
-    //   this.$data.inStatus = _merge1;
-    // },
-
-    // getAvailableOutStatus: async function(data) {
-    //   var _rawB2 = await axios.get('index.php?option=com_emundus_workflow&controller=item&task=getavailableoutputstatus', { params: { data : data }});
-    //   var _rawAll = await axios.get('index.php?option=com_emundus_workflow&controller=common&task=getallstatus');
-    //
-    //   var _b2 = _rawB2.data.data;
-    //   var _all = _rawAll.data.data;
-    //
-    //   let _odiff = _all.filter(({ step: id1 }) => !_b2.some(({ step: id2 }) => id2 === id1));
-    //   let _ointersect = _all.filter(item1 => _b2.some(item2 => item1.step === item2.step));
-    //
-    //   if(_odiff.length !== 0 && _ointersect.length !== 0) {
-    //     _odiff.forEach(elt => elt['disabled'] = true);
-    //     _ointersect.forEach(elt => elt['disabled'] = false);
-    //     var _merge2 = _odiff.concat(_ointersect);
-    //   }
-    //
-    //   else {
-    //     _all.forEach(elt => elt['disabled'] = false);
-    //     var _merge2 = _all;
-    //   }
-    //
-    //   this.$data.outStatus = _merge2;
-    // },
 
     getNonStatusParams: function(id) {
       axios({
