dfc3b018de081f090bf0b6c22c1f28d09c4fb5f0
Duy Tran <duy.tran@emundus.fr>
2021-06-03 19:26:47 +0200

    [update paris 2]

--- media/com_emundus/js/em_files.js
+++ media/com_emundus/js/em_files.js
@@ -786,7 +786,11 @@ function generate_pdf(json,elements=undefined) {
                     params : elements               /// add params
                 },
                 success: function(result) {
-                    /// focus on back-end PHP
+                    $('#extractstep').replaceWith('<div id="extractstep"><p>' + Joomla.JText._('COM_EMUNDUS_PDF_GENERATION') + '</p></div>');
+                    $('#loadingimg').empty();
+                    $('#extractstep').replaceWith('<div class="alert alert-success" role="alert">' + Joomla.JText._('COM_EMUNDUS_EXPORT_FINISHED') + '</div>');
+                    $('#chargement').append('<button type="button" class="btn btn-default" id="back" onclick="back();"><span class="glyphicon glyphicon-arrow-left"></span>&nbsp;&nbsp;' + Joomla.JText._('BACK') + '</button>&nbsp;&nbsp;&nbsp;');
+                    $('#chargement').append('<a class="btn btn-link" title="' + Joomla.JText._('DOWNLOAD_PDF') + '" href="' + result.json.path + 'tmp/' + file + '" target="_blank"><span class="glyphicon glyphicon-download-alt"></span>  <span>' + Joomla.JText._('DOWNLOAD_PDF') + '</span></a>');
                 }, error: function(jqXHR) {
                     console.log(jqXHR.responseText);
                 }
