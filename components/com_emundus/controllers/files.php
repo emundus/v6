b2a8aadbd917b627cde9e6edcc5731a1664e764a
Duy Tran <duy.tran@emundus.fr>
2021-06-14 16:59:05 +0200

    [update kit] group letters by candidat

--- components/com_emundus/controllers/files.php
+++ components/com_emundus/controllers/files.php
@@ -4304,8 +4304,99 @@ require_once (JPATH_LIBRARIES . '/emundus/vendor/autoload.php');
             }
         }
 
+        // group letters by candidat
+        $fnumsInfos = $_mFile->getFnumsInfos($fnum_Array);
+
+        $applicant_id = [];
+        foreach($fnumsInfos as $key => $value) {
+            $applicant_id[] = $value['applicant_id'];
+        }
+
+        $applicant_id = array_unique(array_filter($applicant_id));
+        $zip_Url = [];
+
+        /// applicant_id = [95,164,165,166,167]
+        foreach($applicant_id as $key => $value) {
+
+            $_zipName = $value . '_' . date("Y-m-d") . '_' . uniqid() .'_x.zip';
+            $zip_Url[$value] = JPATH_BASE.DS . 'tmp' . DS . $_zipName;
+
+            $this->ZipLetter(EMUNDUS_PATH_ABS . $value, JPATH_BASE.DS.'tmp'.DS . $_zipName, 'true');
+
+        }
+
+        $res->zip_url = $zip_Url;
+
+        // group letters by document type
+
         echo json_encode($res);
         exit;
+    }
+
+    public static function ZipLetter($source, $destination, $include_dir = false)
+    {
+
+        if (!extension_loaded('zip') || !file_exists($source)) {
+            return false;
+        }
+
+        if (file_exists($destination)) {
+            unlink ($destination);
+        }
+
+        $zip = new ZipArchive();
+        if (!$zip->open($destination, ZIPARCHIVE::CREATE)) {
+            return false;
+        }
+        $source = str_replace('\\', '/', realpath($source));
+
+        if (is_dir($source) === true)
+        {
+
+            $files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($source), RecursiveIteratorIterator::SELF_FIRST);
+
+            if ($include_dir) {
+
+                $arr = explode("/",$source);
+                $maindir = $arr[count($arr)- 1];
+
+                $source = "";
+                for ($i=0; $i < count($arr) - 1; $i++) {
+                    $source .= '/' . $arr[$i];
+                }
+
+                $source = substr($source, 1);
+
+                $zip->addEmptyDir($maindir);
+
+            }
+
+            foreach ($files as $file)
+            {
+                $file = str_replace('\\', '/', $file);
+
+                // Ignore "." and ".." folders
+                if( in_array(substr($file, strrpos($file, '/')+1), array('.', '..')) )
+                    continue;
+
+                $file = realpath($file);
+
+                if (is_dir($file) === true)
+                {
+                    $zip->addEmptyDir(str_replace($source . '/', '', $file . '/'));
+                }
+                else if (is_file($file) === true)
+                {
+                    $zip->addFromString(str_replace($source . '/', '', $file), file_get_contents($file));
+                }
+            }
+        }
+        else if (is_file($source) === true)
+        {
+            $zip->addFromString(basename($source), file_get_contents($source));
+        }
+
+        return $zip->close();
     }
 }
 
