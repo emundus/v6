8609a0bce6b33f763dbc572d10f9bcd4fba9b89e
Brice <b.hubinet@gmail.com>
2021-08-17 10:53:50 +0200

    Add anonymous parameter, add new attachment category for messenger

---
+++ administrator/components/com_admin/sql/updates/mysql/3.9.28-2021-08-17-em.sql
@@ -0,0 +1,2 @@
+INSERT INTO jos_emundus_setup_attachments (lbl, value, description, allowed_types, nbmax, ordering, published, ocr_keywords, category, video_max_length)
+VALUES ('_messenger_attachments', 'Documents de la messagerie', null, 'pdf;doc;docx;xls;xlsx;jpg;odt', 1000, 0, 0, null, '2', null);
