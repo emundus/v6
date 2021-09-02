3efb5af63bdb6504c235483072fc47e7a1c51e42
Brice <b.hubinet@gmail.com>
2021-09-02 16:11:43 +0200

    WE HAVE THE DATE HELPER ! Now we have to do refactoring 😅

--- libraries/emundus/pdf.php
+++ libraries/emundus/pdf.php
@@ -780,6 +780,7 @@ function application_form_pdf($user_id, $fnum = null, $output = true, $form_post
     jimport('joomla.html.parameter');
     set_time_limit(0);
     require_once(JPATH_LIBRARIES . DS . 'emundus' . DS . 'tcpdf' . DS . 'tcpdf.php');
+    require_once (JPATH_SITE . DS . 'components' . DS . 'com_emundus' . DS . 'helpers' . DS . 'date.php');
 
     require_once(JPATH_SITE . DS . 'components' . DS . 'com_emundus' . DS . 'models' . DS . 'application.php');
     require_once(JPATH_SITE . DS . 'components' . DS . 'com_emundus' . DS . 'models' . DS . 'profile.php');
@@ -858,7 +859,7 @@ function application_form_pdf($user_id, $fnum = null, $output = true, $form_post
             $allowed_attachments = EmundusHelperAccess::getUserAllowedAttachmentIDs(JFactory::getUser()->id);
 
             if ($options[0] !== "0" && !$anonymize_data && ($allowed_attachments === true || in_array('10', $allowed_attachments))) {
-                $date_submitted = (!empty($item->date_submitted) && !strpos($item->date_submitted, '0000')) ? JHTML::_('date', $item->date_submitted, JText::_('DATE_FORMAT_LC2')) : JText::_('NOT_SENT');
+                $date_submitted = (!empty($item->date_submitted) && !strpos($item->date_submitted, '0000')) ? EmundusHelperDate::displayDate($item->date_submitted) : JText::_('NOT_SENT');
 
                 // Create an date object
                 $date_printed = new Date();
