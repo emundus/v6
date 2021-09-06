2105bf39f12c4b654cedd26eb99400c3a53c8e36
James Dean <jdean162@gmail.com>
2021-09-06 10:26:46 +0200

    Clean event plugin
    Small clean + Added TODO to set states as params

--- plugins/emundus/eb_book_event_status/eb_book_event_status.php
+++ plugins/emundus/eb_book_event_status/eb_book_event_status.php
@@ -31,19 +31,19 @@ class PlgEmundusEb_book_event_status extends JPlugin {
     function onAfterStatusChange($fnum,$state) {
         $status_to_check = $this->params->get('eb_book_event_status_step', '');
 
+        // No need to continue if the status is not in the list of statuses 
         if (!in_array($state,explode(',',$status_to_check))) {
             return false;
         }
 
-        $this->log = '';
-
-        if(!empty($_SERVER['HTTP_CLIENT_IP'])){
+        /// Set the IP
+        if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
             //ip from share internet
             $ip = $_SERVER['HTTP_CLIENT_IP'];
-        }elseif(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
+        } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
             //ip pass from proxy
             $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-        }else{
+        } else {
             $ip = $_SERVER['REMOTE_ADDR'];
         }
 
@@ -53,59 +53,72 @@ class PlgEmundusEb_book_event_status extends JPlugin {
         require_once(JPATH_SITE . DS . 'components' . DS . 'com_emundus' . DS . 'models' . DS . 'files.php');
         $m_files = new EmundusModelFiles();
 
-        jimport('joomla.log.log');
-        JLog::addLogger(['text_file' => 'com_emundus.hikashopAddToCart.php'], JLog::ALL, ['com_emundus']);
-
         try {
             $fnumInfos = $m_files->getFnumInfos($fnum);
 
-            $query->select('cc.eb_registration,sc.event')
+            $query
+                ->select('cc.eb_registration,sc.event')
                 ->from($db->quoteName('#__emundus_campaign_candidature','cc'))
                 ->leftJoin($db->quoteName('#__emundus_setup_campaigns','sc').' ON '.$db->quoteName('sc.id').' = '.$db->quoteName('cc.campaign_id'))
                 ->where($db->quoteName('cc.fnum') . ' = ' . $db->quote($fnum));
+
             $db->setQuery($query);
             $registration = $db->loadObject();
 
+            // TODO: Set states as params in the XML file
             switch ($state) {
+                // Unregister the user from the event
                 case 3:
-                    if(!empty($registration->eb_registration)) {
-                        $query->clear()
+                    if (!empty($registration->eb_registration)) {
+                        $query
+                            ->clear()
                             ->delete('#__eb_registrants')
                             ->where($db->quoteName('id') . ' = ' . $db->quote($registration->eb_registration));
+
                         $db->setQuery($query);
                         $db->execute();
                     }
                 break;
+
+                // Register the user to the event
                 case 4:
-                    if(empty($registration->eb_registration)) {
-                        $query->select('event')
+                    if (empty($registration->eb_registration)) {
+                        $query
+                            ->select('event')
                             ->from($db->quoteName('#__emundus_setup_campaigns'))
                             ->where($db->quoteName('id') . ' = ' . $fnumInfos['id']);
+
                         $db->setQuery($query);
                         $event_id = $db->loadResult();
 
                         $columns = ['event_id', 'user_id', 'first_name', 'last_name', 'email', 'number_registrants', 'register_date', 'payment_date', 'published', 'language', 'user_ip'];
                         $values = [$event_id, $fnumInfos['applicant_id'], $db->quote($fnumInfos['name']), $db->quote($fnumInfos['name']), $db->quote($fnumInfos['email']), 1, $db->quote(date('Y-m-d h:i:s')), $db->quote(date('Y-m-d h:i:s')), 1, $db->quote('fr-FR'), $db->quote($ip)];
 
-                        $query->clear()
+                        $query
+                            ->clear()
                             ->insert($db->quoteName('#__eb_registrants'))
                             ->columns($columns)
                             ->values(implode(',', $values));
+
                         $db->setQuery($query);
                         $db->execute();
                         $registration = $db->insertid();
 
-                        $query->clear()
+                        $query
+                            ->clear()
                             ->update('#__emundus_campaign_candidature')
                             ->set($db->quoteName('eb_registration') . ' = ' . $db->quote($registration))
                             ->where($db->quoteName('fnum') . ' = ' . $db->quote($fnum));
+
                         $db->setQuery($query);
                         $db->execute();
                     } else {
-                        $query->clear()
+                        $query
+                            ->clear()
                             ->update('#__eb_registrants')
                             ->set($db->quoteName('published') . ' = 1')
                             ->where($db->quoteName('id') . ' = ' . $db->quote($registration->eb_registration));
+                            
                         $db->setQuery($query);
                         $db->execute();
                     }
@@ -113,7 +126,7 @@ class PlgEmundusEb_book_event_status extends JPlugin {
                 default:
                     break;
             }
-        } catch (Exception $e){
+        } catch (Exception $e) {
             JLog::add('plugin/emundus/eb_book_event_status error :'.$query->__toString().' : '.$e->getMessage(), JLog::ERROR, 'com_emundus');
         }
 
