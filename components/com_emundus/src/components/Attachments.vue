5983d52958b98074249928630177c7060bb0f454
Legendre <jeremy.legendre@emundus.fr>
2021-10-12 17:17:35 +0200

    init vue in com_emundus

---
+++ components/com_emundus/src/components/Attachments.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <h1>Liste de dossiers</h1>
+        <ul>
+            <li :v-for="attachment in attachments">
+                {{attachment.name}}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import attachmentService from '../services/attachment.js';
+
+export default {
+    name: 'Attachments',
+    props: {
+        user: {
+            type: Number,
+            required: true
+        },
+        fnum: {
+            type: Number,
+            required: true
+        },
+    },
+    data() {
+        return {
+            attachments: [],
+            loading: true,
+        }
+    },
+
+    mounted() {
+        this.getAttachments();
+    },
+    methods: {
+        async getAttachments() {
+            this.attachments = await attachmentService.getAttachments(this.user, this.fnum);
+
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ components/com_emundus/src/services/axiosClient.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+export default (headers = {
+    'Content-Type': 'application/json',
+    'Cache-Control': 'no-cache',
+}) =>
+    axios.create({
+        timeout: 30000,
+        headers,
+        responseType: 'json',
+        withCredentials: true,
+    });
