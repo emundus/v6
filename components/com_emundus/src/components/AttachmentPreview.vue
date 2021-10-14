356746c4b69d32132d56e8580eb0fb63a24add43
Legendre <jeremy.legendre@emundus.fr>
2021-10-14 10:41:40 +0200

    create modal sub components to edit attachment

---
+++ components/com_emundus/src/components/AttachmentPreview.vue
@@ -0,0 +1,33 @@
+<template>
+    <div id="attachment-preview">
+    </div>
+</template>
+
+<script>
+import attachment from '../services/attachment';
+
+export default {
+    props: {
+        attachment: {
+            type: Object,
+            required: true
+        },
+    },
+    data() {
+        return {
+            attachmentType: ""
+        }
+    },
+    mounted() {
+        this.attachmentType = this.attachment.attachment_type;
+        // this.loadAttachment();
+    },
+    methods: {
+        loadAttachment() {
+            attachment.loadAttachment(this.attachment.id, this.attachmentType).then(response => {
+                this.$refs.attachmentPreview.innerHTML = response.data;
+            });
+        }
+    }
+}
+</script>
