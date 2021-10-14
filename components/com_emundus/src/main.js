47c525360a83bfb2a3e950ce7b8a0d65b324a3fa
Legendre <jeremy.legendre@emundus.fr>
2021-10-14 09:30:09 +0200

    condition on mount attachment vue

--- components/com_emundus/src/main.js
+++ components/com_emundus/src/main.js
@@ -3,13 +3,15 @@ import Attachments from './components/Attachments.vue';
 
 Vue.config.productionTip = false;
 
-new Vue({
-  render(h) {
-    return h(Attachments, {
-      props: {
-        fnum: this.$el.attributes.fnum.value,
-        user: this.$el.attributes.user.value,
-      },
-    });
-  },
-}).$mount('#em-application-attachment');
+if (document.getElementById("em-application-attachment")) {
+  new Vue({
+    render(h) {
+      return h(Attachments, {
+        props: {
+          fnum: this.$el.attributes.fnum.value,
+          user: this.$el.attributes.user.value,
+        },
+      });
+    },
+  }).$mount('#em-application-attachment');  
+}
