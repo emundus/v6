7cd3afe4615744b346fa7ce7d7b7a5db63a34525
Legendre <jeremy.legendre@emundus.fr>
2021-10-14 12:32:08 +0200

    table for data display and css

--- components/com_emundus/src/components/Attachments.vue
+++ components/com_emundus/src/components/Attachments.vue
@@ -1,21 +1,45 @@
 <template>
-    <div>
+    <div id="em-attachments">
         <h1>Documents - ..% envoyés</h1>
-        <div class="searchbar">
-          <input type="text" ref="searchbar" placeholder="Key words" @input="searchInFiles">
+        <div id="searchbar">
+          <label for="searchbar">Rechercher : </label>
+          <input name="searchbar" type="text" ref="searchbar" placeholder="Key words" @input="searchInFiles">
         </div>
-        <ul v-if="attachments.length">
-            <li v-for="attachment in displayedAttachments" :key="attachment.aid">
-              <a @click="openModal">{{attachment.filename}}</a>
-              <span>{{attachment.description}}</span>
-              <div class="actions">
-                <span @click="openModal(attachment)">Edit</span>
-                <span @click="deleteAttachment(attachment.aid)">Delete</span>
-              </div>
-            </li>
-        </ul>
+        <table v-if="attachments.length">
+            <thead>
+                <tr>
+                    <th>Document</th>
+                    <th>Description</th>
+                    <th>Statut</th>
+                    <th>Date d'envoi</th>
+                    <th>Modifié par</th>
+                    <th>Date de dernière modifcation</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="attachment in displayedAttachments" :key="attachment.aid">
+                    <td class="td-document">{{ attachment.filename }}</td>
+                    <td>{{ attachment.description }}</td>
+                    <td>{{ formattedValidState(attachment.is_validated)}}</td>
+                    <td>{{ formattedTimeDate(attachment.timedate) }}</td>
+                    <td></td>
+                    <td>{{ formattedTimeDate(attachment.timedate) }}</td>
+                    <td>
+                        <span @click="openModal(attachment)">Edit</span>
+                        <span @click="deleteAttachment(attachment.aid)">Delete</span>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
         <p v-else>Aucun dossier rattaché à cet utilisateur</p>
-        <modal name="edit">
+        <modal 
+          id="edit-modal" 
+          name="edit"
+          height="50%"
+          width="50%"
+          styles="display:flex;flex-direction:row;justify-content:center;align-items:center;"
+        >
           <AttachmentPreview :attachment="selectedAttachment"></AttachmentPreview>
           <AttachmentEdit @closeModal="$modal.hide('edit')" @saveChanges="updateAttachment()" :attachment="selectedAttachment" :user="user" :fnum="fnum"></AttachmentEdit>
         </modal>
@@ -26,6 +50,7 @@
 import AttachmentPreview from './AttachmentPreview.vue'
 import AttachmentEdit from './AttachmentEdit.vue'
 import attachmentService from '../services/attachment.js';
+import moment from 'moment';
 
 export default {
   name: 'Attachments',
@@ -56,11 +81,25 @@ export default {
   methods: {
     async getAttachments() {
       this.attachments = await attachmentService.getAttachmentsByFnum(this.fnum);
+    },   
+    updateAttachment() {
+      this.getAttachments();
+      this.$modal.hide('edit');
+      this.selectedAttachment = null;
+    },
+    async deleteAttachment(id) {
+      this.attachments = this.attachments.filter(attachment => attachment.aid !== aid);
+
+      const response = await attachmentService.deleteAttachment(this.fnum, aid);
+      if (response.status == true) {
+        // Display tooltip deleted succesfully  
+      }
     },
     searchInFiles() {
       this.attachments.forEach((attachment, index) => {
         // if attachment description contains the search term, show it
-        if (attachment.description.includes(this.$refs["searchbar"].value) || attachment.filename.includes(this.$refs["searchbar"].value)) {
+        // lowercase the search term to avoid case sensitivity
+        if (attachment.description.toLowerCase().includes(this.$refs["searchbar"].value.toLowerCase()) || attachment.filename.toLowerCase().includes(this.$refs["searchbar"].value.toLowerCase())) {
           this.attachments[index].show = true;
         } else {
           this.attachments[index].show = false;
@@ -71,18 +110,22 @@ export default {
       this.$modal.show('edit');
       this.selectedAttachment = attachment;
     },
-    updateAttachment() {
-      this.getAttachments();
-      this.$modal.hide('edit');
-    },
-    async deleteAttachment(id) {
-      // remove attachment from attachments data
-      this.attachments = this.attachments.filter(attachment => attachment.aid !== aid);
-
-      const response = await attachmentService.deleteAttachment(this.fnum, aid);
-      if (response.status == true) {
-        // Display tooltip deleted succesfully  
+    formattedValidState(state) {
+      switch(state) {
+        case "1":
+          return 'Validé';
+          break;
+        case "-2":
+          return 'Invalide';
+          break;
+        case "0":
+        default:
+          return 'Indéfini';
+          break;
       }
+    },
+    formattedTimeDate(timedate) {
+      return moment(timedate).format('DD/MM/YYYY HH:mm');
     }
   },
   computed: {
@@ -96,14 +139,41 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+#em-attachments {
+  width: 90%;
+  margin: auto;
 
-.actions {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  span {
-    margin: 0 5px;
+  #searchbar {
+    margin-bottom: 20px;
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-end;
+    align-items: center;
+
+    input {
+      width: 350px;
+      margin-left: 5px;
+    }
   }
+
+  table {
+    .td-document {
+      width: 250px;
+      max-width: 250px;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+    }
+  }
+  
+  .actions {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    span {
+      margin: 0 5px;
+    }
+  } 
 }
 
 </style>
