f9bcfecc15adaced8e659ec4e46b42ba9551d62b
James Dean <jdean162@gmail.com>
2021-10-15 16:03:54 +0200

    JCH Optimize 7.0.0

---
+++ administrator/components/com_jchoptimize/fields/excludelazyload.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * JCH Optimize - Performs several front-end optimizations for fast downloads
+ *
+ * @package   jchoptimize/joomla-platform
+ * @author    Samuel Marshall <samuel@jch-optimize.net>
+ * @copyright Copyright (c) 2020 Samuel Marshall / JCH Optimize
+ * @license   GNU/GPLv3, or later. See LICENSE file
+ *
+ * If LICENSE file missing, see <http://www.gnu.org/licenses/>.
+ */
+
+defined('_JEXEC') or die;
+
+        include_once dirname(__FILE__) . '/exclude.php';
+
+        class JFormFieldExcludelazyload extends JFormFieldExclude
+        {
+
+                public $type = 'excludelazyload';
+		public $filetype = 'lazyload';
+		public $filegroup = 'file';
+        }
