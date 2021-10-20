4b45deea92ab9843e3a6d967b6c279a8b5099186
Legendre <jeremy.legendre@emundus.fr>
2021-10-20 12:20:26 +0200

    add svgs

---
+++ components/com_emundus/src/assets/icons/trash.vue
@@ -0,0 +1,11 @@
+<template>
+<svg width="10" height="12" viewBox="0 0 10 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M7.66665 4V10.6667H2.33331V4H7.66665ZM6.66665 0H3.33331L2.66665 0.666667H0.333313V2H9.66665V0.666667H7.33331L6.66665 0ZM8.99998 2.66667H0.99998V10.6667C0.99998 11.4 1.59998 12 2.33331 12H7.66665C8.39998 12 8.99998 11.4 8.99998 10.6667V2.66667Z" fill="#080C12"/>
+</svg>
+</template>
+
+<script>
+export default {
+    name: 'icon-trash'
+}
+</script>
---
+++ components/com_emundus/src/assets/icons/upload.vue
@@ -0,0 +1,11 @@
+<template>
+<svg width="10" height="12" viewBox="0 0 10 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M2.99998 8.66667H6.99998V4.66667H9.66665L4.99998 0L0.333313 4.66667H2.99998V8.66667ZM0.333313 10H9.66665V11.3333H0.333313V10Z" fill="#5E646E"/>
+</svg>
+</template>
+
+<script>
+export default {
+    name: 'icon-upload'
+}
+</script>
