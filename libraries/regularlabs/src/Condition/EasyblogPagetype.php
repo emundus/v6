f1ddd969dc1b2e37ab540d3d697da9b8325d4325
James Dean <jdean162@gmail.com>
2021-10-20 12:29:23 +0200

    Install Sourcerer 9.0.2

---
+++ libraries/regularlabs/src/Condition/EasyblogPagetype.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @package         Regular Labs Library
+ * @version         21.9.16879
+ * 
+ * @author          Peter van Westen <info@regularlabs.com>
+ * @link            http://regularlabs.com
+ * @copyright       Copyright © 2021 Regular Labs All Rights Reserved
+ * @license         http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+ */
+
+namespace RegularLabs\Library\Condition;
+
+defined('_JEXEC') or die;
+
+/**
+ * Class EasyblogPagetype
+ * @package RegularLabs\Library\Condition
+ */
+class EasyblogPagetype extends Easyblog
+{
+	public function pass()
+	{
+		return $this->passByPageType('com_easyblog', $this->selection, $this->include_type);
+	}
+}
---
+++ libraries/regularlabs/src/EditorButton.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @package         Regular Labs Library
+ * @version         21.9.16879
+ * 
+ * @author          Peter van Westen <info@regularlabs.com>
+ * @link            http://regularlabs.com
+ * @copyright       Copyright © 2021 Regular Labs All Rights Reserved
+ * @license         http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+ */
+
+namespace RegularLabs\Library;
+
+defined('_JEXEC') or die;
+
+/**
+ * @deprecated  2018-11-14  Use EditorButtonPlugin instead
+ */
+class EditorButton extends EditorButtonPlugin
+{
+}
