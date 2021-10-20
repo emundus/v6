2b5bc19567b95bc7b70591df17f63906ab2881fe
Legendre <jeremy.legendre@emundus.fr>
2021-10-20 14:39:31 +0200

    add i18n

---
+++ components/com_emundus/src/i18n/locales.js
@@ -0,0 +1,16 @@
+export default {
+    "search": Joomla.JText._("SEARCH"),
+    "attachments": {
+        "name": Joomla.JText._("NAME"),
+        "desc": Joomla.JText._("DESCRIPTION"),
+        "send_date": Joomla.JText._("SEND_DATE"),
+        "status": Joomla.JText._("STATUS"),
+        "modified_by": Joomla.JText._("MODIFIED_BY"),
+        "modification_date": Joomla.JText._("MODIFICATION_DATE"),
+        "validation_states": {
+            "-2": Joomla.JText._("INVALID"),
+            "0": Joomla.JText._("WAITING"),
+            "1": Joomla.JText._("VALID"),
+        }
+    }
+}
