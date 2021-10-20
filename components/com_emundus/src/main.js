70515b5b7d3035cddb9e59e00372028e7ccd4179
Legendre <jeremy.legendre@emundus.fr>
2021-10-20 10:42:08 +0200

    add vue router and app.vue

--- components/com_emundus/src/main.js
+++ components/com_emundus/src/main.js
@@ -1,26 +1,45 @@
 import Vue from 'vue';
 import store from "./store";
-import Attachments from './components/Attachments.vue';
+import router from "./router";
 import VModal from 'vue-js-modal';
+import App from './App.vue';
 
 Vue.config.productionTip = false;
 
 Vue.use(store);
 Vue.use(VModal);
 
+let mountApp = false;
+let elementId = "";
+let data = {};
+let componentName = "";
+
 if (document.getElementById("em-application-attachment")) {
+  const element = document.getElementById("em-application-attachment");
+  Array.prototype.slice.call(element.attributes).forEach(function (attr) {
+    data[attr.name] = attr.value;
+  });
+
+  componentName = "attachments";
+  elementId = "#em-application-attachment";
+  mountApp = true;
+}
+
+if (mountApp) {
   new Vue({
-    el: document.getElementById("em-application-attachment"),
+    el: elementId,
     store,
+    router,
     render(h) {
       return h(
-        Attachments,
+        App,
         {
           props: {
-            fnum: this.$el.attributes.fnum.value,
-            user: this.$el.attributes.user.value,
+            componentName: componentName,
+            data: data
           },
-      });
-    },
+        }
+      );
+    }
   })
 }
