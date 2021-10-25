e15dfbe13604ce552a5a0aaa0ceccfeaed6b961b
Legendre <jeremy.legendre@emundus.fr>
2021-10-25 14:21:29 +0200

    filter row repeater

---
+++ components/com_emundus/src/components/FilterBuilder/FilterRow.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="filter-row">
+        <label for="filter-name">Filtres</label>
+        <select name="filter-name" @change="changeFilter">
+            
+        </select>
+        
+        <label for="filter-action">Actions</label>
+        <select name="filter-action">
+            
+        </select>
+
+        <label for="filter-value">Valeurs</label>
+        <select name="filter-value">
+
+        </select>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            names: [],
+            actions: [],
+            values: []
+        };
+    },
+    methods: {
+        changeFilter() {
+            // update the actions and the values based on name selected
+
+        }
+    }
+}
+</script>
