4caf2d898dd999e9d10f9f9a4ed887c9918e5ce0
Legendre <jeremy.legendre@emundus.fr>
2021-10-26 12:24:28 +0200

    remove group + style

--- components/com_emundus/src/components/FilterBuilder/FilterBuilder.vue
+++ components/com_emundus/src/components/FilterBuilder/FilterBuilder.vue
@@ -1,97 +1,121 @@
 <template>
-    <div id="filter-builder">
-        <h2>Filtres</h2>
-        <div class="filters">
-            <div class="rows">
-                <div class="relation">
-                    <select name="and_or">
-                        <option value="and">ET</option>
-                        <option value="or">OU</option>
-                    </select>
-                </div>
-                <div class="elements">
-                    <div class="element" v-for="(element, index) in orderedElements" :key="index">
-                        <FilterRow v-if="element.type == 'filter'" class="filter" @removeFilter="removeFilter(index)" :group="0"></FilterRow>
-                        <FilterGroup v-if="element.type == 'group'" class="groups"  :id="element.group"></FilterGroup>
-                    </div>
-                </div>
-            </div>
-            <div class="actions">
-                <div class="btn-primary-vue" @click="addGroup">
-                    <span class="material-icons">
-                        add
-                    </span>
-                    Ajouter un groupe
-                </div>
-                <div class="btn-primary-vue" @click="addFilter">
-                    <span class="material-icons">
-                        add
-                    </span>
-                    Ajouter un filtre
-                </div>
-            </div>
+  <div id="filter-builder">
+    <h2>Filtres</h2>
+    <div class="filters">
+      <div class="rows">
+        <div class="relation">
+          <select name="and_or">
+            <option value="and">ET</option>
+            <option value="or">OU</option>
+          </select>
         </div>
+        <div class="elements">
+          <div
+            class="element"
+            v-for="(element, index) in orderedElements"
+            :key="index"
+          >
+            <FilterRow
+              v-if="element.type == 'filter'"
+              class="filter-row"
+              @removeFilter="removeElement(index)"
+              :group="0"
+            ></FilterRow>
+            <FilterGroup
+              v-if="element.type == 'group'"
+              :id="element.group"
+              @removeGroup="removeElement(index)"
+            ></FilterGroup>
+          </div>
+        </div>
+      </div>
+      <div class="actions">
+        <div class="btn-primary-vue" @click="addGroup">
+          <span class="material-icons"> add </span>
+          Ajouter un groupe
+        </div>
+        <div class="btn-primary-vue" @click="addFilter">
+          <span class="material-icons"> add </span>
+          Ajouter un filtre
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-import filterService from '../../services/filter';
-import FilterRow from './FilterRow.vue';
-import FilterGroup from './FilterGroup.vue';
+import filterService from "../../services/filter";
+import FilterRow from "./FilterRow.vue";
+import FilterGroup from "./FilterGroup.vue";
 
 export default {
-    name: 'FilterBuilder',
-    components: {
-        FilterRow,
-        FilterGroup
+  name: "FilterBuilder",
+  components: {
+    FilterRow,
+    FilterGroup,
+  },
+  props: {
+    type: {
+      type: String,
+      default: "list",
     },
-    props: {
-        type: {
-            type: String,
-            default: "list"
-        },
-        id: {
-            type: String,
-            default: "70"
-        },
+    id: {
+      type: String,
+      default: "70",
     },
-    data() {
-        return {
-            orderedElements: []
-        };
+  },
+  data() {
+    return {
+      orderedElements: [],
+    };
+  },
+  mounted() {
+    this.getFilters();
+  },
+  methods: {
+    async getFilters() {
+      const response = await filterService.getFilters(this.type, this.id);
+
+      if (response.status == true) {
+        this.$store.dispatch("setFilters", response.filters);
+      }
     },
-    mounted() {
-        this.getFilters();
+    addFilter() {
+      this.orderedElements.push({
+        type: "filter",
+        group: 0,
+      });
     },
-    methods: {
-        async getFilters() {
-            const response = await filterService.getFilters(this.type, this.id);
-            
-            if (response.status == true) {
-                this.$store.dispatch('setFilters', response.filters);
-            }
-        },
-        addFilter() {
-            this.orderedElements.push({
-                type: 'filter',
-                group: 0
-            });
-        },
-        addGroup() {
-            this.orderedElements.push({
-                type: 'group',
-                group: Math.floor(Math.random() * Date.now())
-            });
-        },
-        removeFilter(index) {
-            this.orderedElements.splice(index, 1);
-        }
-    }
-}
+    addGroup() {
+      this.orderedElements.push({
+        type: "group",
+        group: Math.floor(Math.random() * Date.now()),
+      });
+    },
+    removeElement(index) {
+      this.orderedElements.splice(index, 1);
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 #filter-builder {
-    width: fit-content;
+  width: fit-content;
+
+  .filter-row {
+    margin-left: 40px;
+  }
+
+  .actions {
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    margin-top: 20px;
+
+    .btn-primary-vue {
+      margin-left: 10px;
+    }
+  }
 }
 </style>
--- components/com_emundus/src/components/FilterBuilder/FilterGroup.vue
+++ components/com_emundus/src/components/FilterBuilder/FilterGroup.vue
@@ -1,51 +1,78 @@
 <template>
-    <div class="filters">
-        <div class="rows">
-            <div class="relation">
-                <select name="and_or">
-                    <option value="and">ET</option>
-                    <option value="or">OU</option>
-                </select>
-            </div>
-            <FilterRow :group="id" class="filter" v-for="(filter, index) in filters" :key="index" @removeFilter="removeFilter(index)"></FilterRow>
+  <div class="group">
+    <div class="wrapper">
+      <div class="rows">
+        <div class="relation">
+          <label>Condition</label>
+          <select name="and_or">
+            <option value="and">ET</option>
+            <option value="or">OU</option>
+          </select>
         </div>
-        <div class="actions">
-            <div class="btn-primary-vue" @click="addFilter">
-                <span class="material-icons">
-                    add
-                </span>
-                Ajouter un filtre
-            </div>
+        <FilterRow
+          :group="id"
+          class="filter"
+          v-for="(filter, index) in filters"
+          :key="index"
+          @removeFilter="removeFilter(index)"
+        ></FilterRow>
+      </div>
+      <div class="actions">
+        <div class="btn-primary-vue" @click="addFilter">
+          <span class="material-icons"> add </span>
+          Ajouter un filtre
         </div>
+      </div>
     </div>
+    <span class="material-icons delete" @click="$emit('removeGroup')">
+      clear
+    </span>
+  </div>
 </template>
 
 <script>
-import FilterRow from './FilterRow.vue';
+import FilterRow from "./FilterRow.vue";
 
 export default {
-    name: 'FilterGroup',
-    props: {
-        id: {
-            type: Number,
-            required: true
-        },
+  name: "FilterGroup",
+  props: {
+    id: {
+      type: Number,
+      required: true,
     },
-    components: {
-        FilterRow
+  },
+  components: {
+    FilterRow,
+  },
+  data() {
+    return {
+      filters: [],
+    };
+  },
+  methods: {
+    addFilter() {
+      this.filters.push(1);
     },
-    data() {
-        return {
-            filters: []
-        }
+    removeFilter(index) {
+      this.filters.splice(index, 1);
     },
-    methods: {
-        addFilter() {
-            this.filters.push(1);
-        },
-        removeFilter(index) {
-            this.filters.splice(index, 1);
-        }
-    }
-}
+  },
+};
 </script>
+
+<style lang="scss" scoped>
+.group {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  margin: 20px 0 20px 40px;
+
+  .wrapper {
+    display: flex;
+    flex-direction: column;
+    padding: 16px;
+    border: 1px solid var(--border-color);
+  }
+}
+</style>
