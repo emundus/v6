4ae69f40f863eb09394ec36ebc30d1a05b272fb6
Bazile Binson <bazile.binson@emundus.fr>
2021-11-23 12:25:22 +0100

    Added json parameter

--- administrator/components/com_admin/sql/updates/mysql/3.9.28-2021-11-05-em.sql
+++ administrator/components/com_admin/sql/updates/mysql/3.9.28-2021-11-05-em.sql
@@ -1,7 +1,10 @@
 INSERT INTO jos_emundus_setup_actions (name, label, c, r, u, d, ordering, status)
-VALUES ('logs', 'COM_EMUNDUS_ACCESS_LOGS', 1, 1, 1, 1, 30, 1);
+VALUES ('logs', 'COM_EMUNDUS_ACCESS_LOGS', 0, 1, 0, 0, 30, 1);
 
 ALTER TABLE jos_emundus_logs
     ADD CONSTRAINT jos_emundus_logs_jos_emundus_setup_actions_action_id_fk
         FOREIGN KEY (action_id) REFERENCES jos_emundus_setup_actions (id)
             ON UPDATE cascade ON DELETE cascade;
+
+ALTER TABLE jos_emundus_logs
+    ADD COLUMN params VARCHAR(255);
