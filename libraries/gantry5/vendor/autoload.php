8715aa37845b6e492efde8eb67904f4b35575e72
Brice <b.hubinet@gmail.com>
2021-11-26 13:39:33 +0100

    Update Gantry 5 to version 5.5.5 to enable comptaibility with php7.4

--- libraries/gantry5/vendor/autoload.php
+++ libraries/gantry5/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit23ee5f57b10f1f4fd7a0df379191ce50::getLoader();
+return ComposerAutoloaderInit4a416378a3ca55f9414d83b6ac45bd18::getLoader();
