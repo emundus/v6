dd456b9107c1c80d40fdef5b848a90f588657617
James Dean <jdean162@gmail.com>
2021-11-26 14:17:24 +0100

    Add language table

---
+++ administrator/components/com_admin/sql/updates/mysql/3.10.2-2021-11-26-em.sql
@@ -0,0 +1,22 @@
+create table jos_emundus_setup_languages
+(
+    id              int auto_increment
+        primary key,
+    tag             varchar(255)                          not null,
+    lang_code       varchar(10)                           null,
+    override        text                                  null,
+    original_text   text                                  null,
+    original_md5    varchar(32)                           null,
+    override_md5    varchar(32)                           null,
+    location        varchar(255)                          null,
+    type            varchar(50)                           null,
+    reference_id    int(10)                               null,
+    reference_table varchar(50)                           null,
+    published       int(1)                                null,
+    created_by      int(10)                               null,
+    created_date    timestamp default current_timestamp() null,
+    modified_by     int(10)                               null,
+    modified_date   timestamp                             null,
+    constraint jos_emundus_setup_languages_uindex
+        unique (tag, lang_code, location)
+);
