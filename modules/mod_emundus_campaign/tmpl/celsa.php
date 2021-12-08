fef6cc3f92f233f769d32c6108b09268dbae3472
Legendre <jeremy.legendre@emundus.fr>
2021-12-08 16:21:50 +0100

    one fnum by level of formation allowed

--- modules/mod_emundus_campaign/tmpl/celsa.php
+++ modules/mod_emundus_campaign/tmpl/celsa.php
@@ -16,14 +16,79 @@ if ($locallang == "fr-FR") {
 
 // Check if user don't already have an opened fnum
 $user = JFactory::getSession()->get('emundusUser');
-$hasOpenedFnum = false;
 
-foreach($user->fnums as $fnum) {
-    if ($fnum->published == '1') {
-        $hasOpenedFnum = true;
-        break;
+function userFormationLevelsAllowed() {
+    $user = JFactory::getSession()->get('emundusUser');
+
+    // get all formation levels from data_formation_level table
+    try {
+        $db = JFactory::getDbo();
+        $query = $db->getQuery(true);
+        $query->select('id');
+        $query->from('data_formation_level');
+        $db->setQuery($query);
+
+        $formationLevels = $db->loadColumn();
+
+        // check if user has an opened fnum for each formation level
+        foreach ($user->fnums as $fnum => $values) {
+            // get fnum's formation level
+            $query->clear();
+            $query->select('data_formation_level.id');
+            $query->from('data_formation_level')
+            ->leftJoin('data_formation ON data_formation_level.id = data_formation.level')
+            ->leftJoin('#__emundus_setup_campaigns ON data_formation.id = #__emundus_setup_campaigns.formation')
+            ->leftJoin('#__emundus_campaign_candidature ON #__emundus_setup_campaigns.id = #__emundus_campaign_candidature.campaign_id')
+            ->where('#__emundus_campaign_candidature.fnum = '.$fnum);
+
+            $db->setQuery($query);
+            $formationLevel = $db->loadResult();
+
+            // check if user has an opened fnum for the formation level
+            if (in_array($formationLevel, $formationLevels)) {
+                // remove value from array
+                $key = array_search($formationLevel, $formationLevels);
+                unset($formationLevels[$key]);
+            }
+        }
+    } catch (Exception $e) {
+        JLog::add('Error getting formation levels from data_formation_level table : '.$e->getMessage(), JLog::ERROR, 'com_emundus');
+        return [];
     }
+
+    return $formationLevels;
 }
+
+$allowedFormationLevels = userFormationLevelsAllowed();
+
+// remove campaigns that has a formation level that user does have an opened fnum for
+function removeNotAllowedCampaigns($campaigns, $allowedFormationLevels) {
+    foreach ($campaigns as $key => $campaign) {
+        // get campaign's formation level
+        $db = JFactory::getDbo();
+        $query = $db->getQuery(true);
+        $query->select('data_formation_level.id');
+        $query->from('data_formation_level')
+        ->leftJoin('data_formation ON data_formation_level.id = data_formation.level')
+        ->leftJoin('#__emundus_setup_campaigns ON data_formation.id = #__emundus_setup_campaigns.formation')
+        ->where('#__emundus_setup_campaigns.id = '.$campaign->id);
+    
+        $db->setQuery($query);
+        $formationLevel = $db->loadResult();
+    
+        // remove campaign if user has an opened fnum for the formation level
+        if (!in_array($formationLevel, $allowedFormationLevels)) {
+            unset($campaigns[$key]);
+        }
+    }
+
+    return $campaigns;
+}
+
+$currentCampaign = removeNotAllowedCampaigns($currentCampaign, $allowedFormationLevels);
+$allCampaign = removeNotAllowedCampaigns($allCampaign, $allowedFormationLevels);
+$pastCampaign = removeNotAllowedCampaigns($pastCampaign, $allowedFormationLevels);
+$futurCampaign = removeNotAllowedCampaigns($futurCampaign, $allowedFormationLevels);
 
 // sort arrays by label and not by date
 usort($currentCampaign, function($a, $b) {
@@ -51,6 +116,10 @@ $currentCampaign = array_map(function($item) use ($formations) {
             $item->class .= 'formation_type-' . $formation->type;
             $item->class .= ' formation_level-' . $formation->level;
 
+            if (!empty($formation->url_informations)) {
+                $item->formation_url .= $formation->url_informations;
+            }
+
             foreach ($formation->voies_d_acces as $voie) {
                 $item->class .= ' voie_d_acces-' . $voie->voie_d_acces;
             }
@@ -61,11 +130,9 @@ $currentCampaign = array_map(function($item) use ($formations) {
     return $item;
 }, $currentCampaign);
 
-
-
 ?>
 
-<?php if(!$hasOpenedFnum):?>
+<?php if(!empty($allowedFormationLevels)):?>
 
 <?= $mod_em_campaign_intro; ?>
 
@@ -265,11 +332,15 @@ $currentCampaign = array_map(function($item) use ($formations) {
             <?php $formUrl = base64_encode('index.php?option=com_fabrik&view=form&formid=102&course=' . $result->code . '&cid=' . $result->id); ?>
 
             <?php if ($result->apply_online == 1 && $m_campaign->isLimitObtained($result->id) !== true) : ?>
-                <?php if ($mod_em_campaign_get_link) : ?>
+                <?php if (!empty($result->formation_url)) : ?>
+                    <a class="btn btn-primary btn-creux btn-orange" role="button"
+                       href='<?php echo $result->formation_url ?>'
+                       target="_blank" data-toggle="sc-modal"><?php echo JText::_('MORE_INFO'); ?></a>
+                <?php elseif ($mod_em_campaign_get_link) : ?>
                     <a class="btn btn-primary btn-creux btn-orange" role="button"
                        href='<?php echo !empty($result->link) ? $result->link : "index.php?option=com_emundus&view=programme&cid=" . $result->id . "&Itemid=" . $mod_em_campaign_itemid2; ?>'
-                       target="_blank" data-toggle="sc-modal"><?php echo JText::_('MORE_INFO'); ?></a>
-                <?php else : ?>
+                       target="_blank" data-toggle="sc-modal"><?php echo JText::_('MORE_INFO'); ?></a>    
+               <?php else : ?>
                     <a class="btn btn-primary btn-creux btn-orange" role="button"
                        href='<?php echo "index.php?option=com_emundus&view=programme&cid=" . $result->id . "&Itemid=" . $mod_em_campaign_itemid2; ?>'
                        data-toggle="sc-modal"><?php echo JText::_('MORE_INFO'); ?></a>
@@ -288,7 +359,11 @@ $currentCampaign = array_map(function($item) use ($formations) {
                 <a class="btn btn-primary btn-plein btn-blue" role="button" href='<?php echo $register_url; ?>'
                    data-toggle="sc-modal"><?php echo JText::_('APPLY_NOW'); ?></a>
             <?php else : ?>
-                <?php if ($mod_em_campaign_get_link) : ?>
+                <?php if (!empty($result->formation_url)) : ?>
+                    <a class="btn btn-primary btn-plein btn-blue" role="button"
+                       href='<?php echo $result->formation_url ?>'
+                       target="_blank" data-toggle="sc-modal"><?php echo JText::_('MORE_INFO'); ?></a>
+                <?php elseif ($mod_em_campaign_get_link) : ?>
                     <a class="btn btn-primary btn-plein btn-blue" role="button"
                        href='<?php echo !empty($result->link) ? $result->link : "index.php?option=com_emundus&view=programme&cid=" . $result->id . "&Itemid=" . $mod_em_campaign_itemid2; ?>'
                        target="_blank" data-toggle="sc-modal"><?php echo JText::_('MORE_INFO'); ?></a>
@@ -369,7 +444,12 @@ $currentCampaign = array_map(function($item) use ($formations) {
                         } else {
                             $btn_class = "btn btn-primary btn-plein btn-blue";
                         } ?>
-                        <?php if ($mod_em_campaign_get_link) : ?>
+
+                        <?php if (!empty($result->formation_url)) : ?>
+                            <a class="<?php echo $btn_class; ?>" role="button"
+                               href='<?php echo $result->formation_url ?>'
+                               target="_blank" data-toggle="sc-modal"><?php echo JText::_('MORE_INFO'); ?></a>
+                        <?php elseif ($mod_em_campaign_get_link) : ?>
                             <a class="<?php echo $btn_class; ?>" role="button"
                                href='<?php echo !empty($result->link) ? $result->link : "index.php?option=com_emundus&view=programme&cid=" . $result->id . "&Itemid=" . $mod_em_campaign_itemid2; ?>'
                                target="_blank" data-toggle="sc-modal"><?php echo JText::_('MORE_INFO'); ?></a>
