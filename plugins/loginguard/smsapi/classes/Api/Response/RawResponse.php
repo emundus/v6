bcdf4771b3d81abfe6ab0c361e26629c2ae613cb
Brice <b.hubinet@gmail.com>
2021-12-16 14:50:24 +0100

    Add login guard component and plugin. Update css and email plugin

---
+++ plugins/loginguard/smsapi/classes/Api/Response/RawResponse.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace SMSApi\Api\Response;
+
+defined('_JEXEC') || die;
+
+/**
+ * Class RawResponse
+ * @package SMSApi\Api\AbstractContactsResponse
+ */
+class RawResponse implements Response
+{
+
+	/**
+	 * @var
+	 */
+	private $text;
+
+	/**
+	 * @param $data
+	 */
+	function __construct($data)
+	{
+
+		$this->text = $data;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getText()
+	{
+		return $this->text;
+	}
+
+}
---
+++ plugins/loginguard/smsapi/classes/Exception/ProxyException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace SMSApi\Exception;
+
+defined('_JEXEC') || die;
+
+class ProxyException extends SmsapiException
+{
+
+}
