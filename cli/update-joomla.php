9394bf90233de98e1eac9619d5d41423c5ff56cc
jordan.troadec <jordan.troadec@emundus.fr>
2021-12-17 16:59:44 +0100

    Fix bug

--- cli/update-joomla.php
+++ cli/update-joomla.php
@@ -1,26 +1,14 @@
 <?php
 
 use Joomla\Utilities\ArrayHelper;
-
 const _JEXEC = 1;
-
 error_reporting(E_ALL | E_NOTICE);
 ini_set('display_errors', 1);
-
 define('JPATH_BASE', dirname(__DIR__));
-
 require_once JPATH_BASE . '/includes/defines.php';
 require_once JPATH_BASE . '/includes/framework.php';
-
-require_once JPATH_LIBRARIES . '/import.legacy.php';
-require_once JPATH_LIBRARIES . '/cms.php';
-require_once JPATH_LIBRARIES . '/src/Updater/Updater.php';
-// Load the configuration
 require_once JPATH_CONFIGURATION . '/configuration.php';
-
-//define('JPATH_COMPONENT_ADMINISTRATOR', JPATH_ADMINISTRATOR . '/components/com_joomlaupdate');
 define('JPATH_COMPONENT_ADMINISTRATOR', JPATH_ADMINISTRATOR . '/components/');
-
 require_once JPATH_COMPONENT_ADMINISTRATOR . 'com_joomlaupdate/models/default.php';
 require_once JPATH_COMPONENT_ADMINISTRATOR . 'com_installer/models/update.php';
 
@@ -37,12 +25,86 @@ class Upgradejoomla extends JApplicationCli
         return $this->db->loadAssocList('','update_id');
     }
 
-    public function updateExtensions() {
+    public function purgeAndFetchUpdates(){
+        // Get the update cache time
+        $component = JComponentHelper::getComponent('com_installer');
+        $updater = JUpdater::getInstance();
+        $model = JModelLegacy::getInstance('InstallerModelUpdate');
+        $params = $component->params;
+        $cache_timeout = $params->get('cachetimeout', 6, 'int');
+        $cache_timeout = 3600 * $cache_timeout;
+
+        // Purge all updates
+        $this->out('Purge updates...');
+        $model->purge();
+        // Find all updates
+        $this->out('Fetching updates...');
+        $updater->findUpdates(0, $cache_timeout);
+
+    }
+
+    public function getInfo() {
+        $this->purgeAndFetchUpdates();
+
+        $this->out('Fetching info...');
+        $query = $this->db->getQuery(true);
+        $query->select('*')
+            ->from('#__' . 'updates')
+            //Exclude Joomla & Gantry5
+            ->where($this->db->quoteName('extension_id') . ' NOT LIKE 0 AND' . ($this->db->quoteName('extension_id') . ' NOT LIKE 700 AND') . ($this->db->quoteName('extension_id') . ' NOT LIKE 11970'));
+        $this->db->setQuery($query);
+        $arr = $this->db->loadAssocList();
+
+        $key = array_values($arr);
+        foreach ($key as $k){
+            echo $k['extension_id'] . ' --> ' . $k['name']. ' (version : ' . $k['version'] .')' . "\n";
+        }
+    }
+
+    public function doEchoHelp() {
+        echo <<<EOHELP
+            Joomla! CLI Update DB
+            
+            Operations
+              -l, --list                  Liste les composants avec une mise à jour disponible
+              -h, --help                  Help
+              
+            Update Filters
+              -i, --id                    Met à jour l'extension qui correspond à l'id en etnrée
+              -e, --extensions            Toutes les extensions avec une mise à jour disponible
+              -c, --core                  Composants Joomla
+              -s, --sql                   Mise à jour de la base de données
+            
+            EOHELP;
+    }
+
+    public function updateExtensions($uid=null) {
         $this->out('UPDATE EXTENSIONS...');
 
-        $model = JModelLegacy::getInstance('InstallerModelUpdate');
-        $uid = $this->getExtensionsId('updates');
+        # Update by extension id
+        if ($uid!=null) {
+            $query = $this->db->getQuery(true);
+            $query->select('*')
+                ->from('#__' . 'updates')
+                //Exclude Joomla & Gantry5
+                ->where($this->db->quoteName('extension_id') . ' LIKE '. $uid);
+            $this->db->setQuery($query);
+            $uid = $this->db->loadAssocList('','update_id');
+
+        } else {
+            $uid = $this->getExtensionsId('updates');
+        }
+        if($uid==null){
+            $this->out("No update for this extension");
+            return false;
+        } else {
+            $this->out("Update found");
+        }
         $uid = ArrayHelper::toInteger($uid, array());
+
+
+        $model = JModelLegacy::getInstance('InstallerModelUpdate');
+
 
         $component     = JComponentHelper::getComponent('com_installer');
         $params        = $component->params;
@@ -77,24 +139,16 @@ class Upgradejoomla extends JApplicationCli
 
     public function updateJoomla() {
         $this->out('UPDATE JOOMLA...');
-
-        // Get a Joomla-instance so createResorationFile() doesn't complain too much.
-
-        // com_joomlaupdate's model
         $updater = JModelLegacy::getInstance('JoomlaupdateModelDefault');
 
         // Make sure we know what the latest version is
-        $updater->refreshUpdates();
-        $updater->applyUpdateSite();
-
+        $this->purgeAndFetchUpdates();
         // Return a null-object if this is the case
         $version_check = $updater->getUpdateInformation();
         if (is_null($version_check['object'])) {
             echo 'No new updates available' . "\n";
             return 0;
         }
-
-        $this->out('Fetching updates...');
 
         // Grab the update (ends up in /tmp)
         $basename = $updater->download();
@@ -107,11 +161,17 @@ class Upgradejoomla extends JApplicationCli
         //TODO: Complete restoration process
 
         // Extract files to core directory
-        $zip = new ZipArchive;
-            if ($zip->open(JPATH_ROOT . '/tmp/Joomla_3.10.3-Stable-Update_Package.zip') === TRUE) {
+        $path = JPATH_ROOT . '/tmp/' . $basename['basename'];
+        try {
+
+            $zip = new ZipArchive;
+            if ($zip->open($path) === TRUE) {
                 $zip->extractTo(JPATH_ROOT);
                 $zip->close();
             }
+        } catch (Exception $e) {
+            echo "! Query for setVersion() fail\n";
+        }
         $this->out('Sucess...');
 
         // Update SQL etc based on the manifest file we got with the update
@@ -127,27 +187,7 @@ class Upgradejoomla extends JApplicationCli
         $this->out('Cleanup...');
     }
 
-    public function doEchoHelp()
-    {
-        echo <<<EOHELP
-            Joomla! CLI Update DB
-            
-            Operations
-              -u, --update                Run Update
-              -l, --list                  List Components
-              -h, --help                  Help
-              
-            Update Filters
-              -i, --id ID                 Update component/extension by ID
-              -a, --all                   All Components
-              -c, --core                  Composants Joomla
-            
-            
-            EOHELP;
-    }
-
-    public function doExecute()
-    {
+    public function doExecute() {
         $app = JFactory::getApplication('site');
         $app->initialise();
         // Set direct download mode
@@ -156,29 +196,27 @@ class Upgradejoomla extends JApplicationCli
         $this->db = JFactory::getDbo();
 
         echo "Emundus SQL Update Tool \n\n";
-
-        # List components available for update
+        if ($this->input->get('c', $this->input->get('core'))) {
+            $this->updateJoomla();
+        }
+        if ($this->input->get('s', $this->input->get('sql'))) {
+            $this->updateSQLJoomla();
+        }
+        if ($this->input->get('h', $this->input->get('help'))) {
+            $this->doEchoHelp();
+        }
+        if ($id = $this->input->get('i', $this->input->get('id'))) {
+            $this->updateExtensions($uid=$id);
+        }
+        if ($this->input->get('e', $this->input->get('extensions'))) {
+            $this->updateExtensions();
+        }
         if ($this->input->get('l', $this->input->get('list'))) {
             $this->getInfo();
         }
 
-        if ($this->input->get('e', $this->input->get('extensions'))) {
-            $this->updateExtensions();
-        }
 
-        if ($this->input->get('c', $this->input->get('core'))) {
-            $this->updateJoomla();
-        }
-
-        if ($this->input->get('s', $this->input->get('sql'))) {
-            $this->updateSQLJoomla();
-        }
-
-        if ($this->input->get('h', $this->input->get('help'))) {
-            $this->doEchoHelp();
-        }
-
-        //Delete all files in tmp folder
+        # Delete all files in tmp folder
         $path = JPATH_ROOT . '/tmp/';
         if(file_exists($path)) {
             $dir = new RecursiveDirectoryIterator($path, FilesystemIterator::SKIP_DOTS);
@@ -193,6 +231,5 @@ class Upgradejoomla extends JApplicationCli
         echo "\n" . "This script took $seconds to execute.";
     }
 }
-
 
 JApplicationCli::getInstance('Upgradejoomla')->execute();
