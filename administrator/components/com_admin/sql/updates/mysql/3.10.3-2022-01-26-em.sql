b1b088d3ee1743dde8cb3d802f746c5990ca9e9d
Bazile Binson <bazile.binson@emundus.fr>
2022-01-26 10:46:56 +0100

    remove fk constraint on action_id

--- administrator/components/com_admin/sql/updates/mysql/3.10.3-2022-01-26-em.sql
+++ administrator/components/com_admin/sql/updates/mysql/3.10.3-2022-01-26-em.sql
@@ -1,10 +1,10 @@
 INSERT INTO jos_emundus_setup_actions (name, label, c, r, u, d, ordering, status)
 VALUES ('logs', 'COM_EMUNDUS_ACCESS_LOGS', 0, 1, 0, 0, 30, 1);
 
-ALTER TABLE jos_emundus_logs
+/* ALTER TABLE jos_emundus_logs
     ADD CONSTRAINT action_id_fk
         FOREIGN KEY (action_id) REFERENCES jos_emundus_setup_actions (id)
-            ON UPDATE cascade ON DELETE cascade;
+            ON UPDATE cascade ON DELETE cascade; */
 
 ALTER TABLE jos_emundus_logs
     ADD COLUMN params TEXT;
