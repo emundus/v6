b607e006b1155f2e8c1ead7f144835d1cd5b7e88
Duy Tran <duytran92.cse@gmail.com>
2022-02-03 22:19:56 +0100

    fix Zoom API SQL Script

--- administrator/components/com_admin/sql/updates/mysql/3.10.3-2022-02-02-em.sql
+++ administrator/components/com_admin/sql/updates/mysql/3.10.3-2022-02-02-em.sql
@@ -101,7 +101,6 @@ VALUES (@element_2, 'load', 'host = this.element;
 host_val = this.get(&#039;value&#039;);
 
 if(host_val !== &quot;&quot;) {
-  console.log(this);
   host_name = host.options[host.selectedIndex].text;
 
   this.form.elements.get(&#039;data_referentiel_zoom_token___email&#039;).element.hide();
@@ -237,8 +236,25 @@ VALUES ('meeting_session', @group_2, 'field', 'Meeting session', 0, '2016-01-10 
 SET @element_4 := LAST_INSERT_ID();
 
 -- update table "jos_fabrik_jsactions" --
-INSERT INTO jos_fabrik_jsactions (element_id, action, code, params)
-VALUES (@element_4, 'load', 'this.element.style.background = &quot;#eeeeee&quot;;', '{"js_e_event":"","js_e_trigger":null,"js_e_condition":"","js_e_value":"","js_published":"1"}');
+INSERT INTO jos_fabrik_jsactions (element_id, action, code, params) 
+VALUES (@element_4, 'load', 'var zoom = this.element;
+zoom.hide();
+var _parent = zoom.parentElement;
+var _child = document.createElement(&quot;p&quot;);
+_child.setAttribute(&quot;id&quot;, &quot;zoom-msg&quot;);
+
+if(zoom.value !== &quot;&quot;) {
+  _child.innerHTML = zoom.value;
+  _child.style.background = &quot;#eeeeee&quot;;
+  _child.style.color = &quot;#16afe1&quot;;
+} else {
+  _child.innerHTML = &quot;Session va être générée quand ce formulaire est rempli&quot;;
+  _child.style.background = &quot;#eeeeee&quot;;
+  _child.style.color = &quot;red&quot;;
+}
+
+_child.style.fontWeight = &quot;bold&quot;;
+_parent.append(_child);', '{"js_e_event":"","js_e_trigger":"fabrik_trigger_group_group974","js_e_condition":"","js_e_value":"","js_published":"1"}');
 
 INSERT INTO jos_fabrik_elements (name, group_id, plugin, label, checked_out, checked_out_time, created, created_by, created_by_alias, modified, modified_by, width, height, `default`, hidden, eval, ordering, show_in_list_summary, filter_type, filter_exact_match, published, link_to_detail, primary_key, auto_increment, access, use_in_page_title, parent_id, params) 
 VALUES ('visio_link', @group_2, 'field', '', 62, '2022-02-03 14:13:22', '2022-02-02 21:26:28', 62, 'sysadmin', '2022-02-03 14:13:19', 62, 0, 0, '', 0, 0, 12, 0, '', 1, 0, 0, 0, 0, 1, 0, 0, '{"placeholder":"","password":"0","maxlength":"255","disable":"0","readonly":"1","autocomplete":"1","speech":"0","advanced_behavior":"0","bootstrap_class":"input-medium","text_format":"text","integer_length":"11","decimal_length":"2","field_use_number_format":"0","field_thousand_sep":",","field_decimal_sep":".","text_format_string":"","field_format_string_blank":"1","text_input_mask":"","text_input_mask_autoclear":"0","text_input_mask_definitions":"","render_as_qrcode":"0","scan_qrcode":"0","guess_linktype":"0","link_target_options":"default","rel":"","link_title":"","link_attributes":"","show_in_rss_feed":"0","show_label_in_rss_feed":"0","use_as_rss_enclosure":"0","rollover":"","tipseval":"0","tiplocation":"top-left","labelindetails":"0","labelinlist":"0","comment":"","edit_access":"1","edit_access_user":"","view_access":"1","view_access_user":"","list_view_access":"1","encrypt":"0","store_in_db":"1","default_on_copy":"0","can_order":"0","alt_list_heading":"","custom_link":"","custom_link_target":"","custom_link_indetails":"1","use_as_row_class":"0","include_in_list_query":"1","always_render":"0","icon_folder":"0","icon_hovertext":"1","icon_file":"","icon_subdir":"","filter_length":"20","filter_access":"1","full_words_only":"0","filter_required":"0","filter_build_method":"0","filter_groupby":"text","inc_in_adv_search":"1","filter_class":"input-medium","filter_responsive_class":"","tablecss_header_class":"","tablecss_header":"","tablecss_cell_class":"","tablecss_cell":"","sum_on":"0","sum_label":"Sum","sum_access":"1","sum_split":"","avg_on":"0","avg_label":"Average","avg_access":"1","avg_round":"0","avg_split":"","median_on":"0","median_label":"Median","median_access":"1","median_split":"","count_on":"0","count_label":"Count","count_condition":"","count_access":"1","count_split":"","custom_calc_on":"0","custom_calc_label":"Custom","custom_calc_query":"","custom_calc_access":"1","custom_calc_split":"","custom_calc_php":"","validations":[]}');
