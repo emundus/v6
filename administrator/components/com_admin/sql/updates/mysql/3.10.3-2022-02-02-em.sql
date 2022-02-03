8610014966531b2a2e122c6465b708b22a3a37f7
Duy Tran <duytran92.cse@gmail.com>
2022-02-03 21:14:33 +0100

    fix Zoom API SQL Script

--- administrator/components/com_admin/sql/updates/mysql/3.10.3-2022-02-02-em.sql
+++ administrator/components/com_admin/sql/updates/mysql/3.10.3-2022-02-02-em.sql
@@ -357,8 +357,8 @@ create table jos_emundus_jury_repeat_jury
 (
     id        int auto_increment primary key,
     parent_id int not null,
-    user      int not null,
-    email     int not null,
+    user      int null,
+    email     int null,
     constraint jos_emundus_jury_repeat_jury___fk1
         foreign key (user) references jos_users (id)
             on update cascade on delete cascade
