388d2c975283afd9d4b4094a765f9ddaee3a880e
Brice <b.hubinet@gmail.com>
2022-03-10 18:10:13 +0100

    Initialize GED Integration parameters

--- components/com_emundus/src/components/Settings/AttachmentStorage/Configuration.vue
+++ components/com_emundus/src/components/Settings/AttachmentStorage/Configuration.vue
@@ -2,21 +2,59 @@
   <div>
     <h2>{{ translate('COM_EMUNDUS_ONBOARD_ATTACHMENT_STORAGE_CONFIGURATION') }}</h2>
 
+    <div class="em-flex-row" >
+      <ul class="nav nav-tabs topnav">
+
+        <li v-for="(integration,index) in integrations" :key="'integration-' + index">
+          <a class="em-neutral-700-color em-pointer"
+              :class="currentIntegration === index ? 'w--current' : ''">
+            {{ translate(integration) }}
+          </a>
+        </li>
+      </ul>
+    </div>
+
+    <IntegrationGED v-if="currentIntegration === 0" :site="site"/>
+
     <div class="em-page-loader" v-if="loading"></div>
   </div>
 </template>
 
 <script>
+import settingsService from "../../../services/settings";
+import IntegrationGED from "./IntegrationGED";
+
 export default {
   name: "Configuration",
+  components: {IntegrationGED},
   data() {
     return {
-      loading: false
+      loading: false,
+      em_params: {},
+
+      integrations: [],
+      currentIntegration: 0,
+
+      // GED
+      site: null
     }
   },
+  created() {
+    settingsService.getEmundusParams().then((params) => {
+      this.em_params = params.data.config;
+
+      if(parseInt(this.em_params.external_storage_ged_alfresco_integration) === 1){
+        this.integrations.push('COM_EMUNDUS_ONBOARD_ATTACHMENT_STORAGE_GED_ALFRESCO');
+        this.site = this.em_params.external_storage_ged_alfresco_site;
+      }
+    });
+  }
 }
 </script>
 
 <style scoped>
-
+.w--current{
+  border: solid 1px #eeeeee;
+  background: #eeeeee;
+}
 </style>
---
+++ components/com_emundus/src/components/Settings/AttachmentStorage/IntegrationGED.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="em-mt-32">
+    <div class="em-flex-row">
+      <span class="material-icons">folder</span>
+      <span class="em-ml-8 em-mr-8">/{{site}}</span>
+      <v-popover :popoverArrowClass="'custom-popover-arrow'">
+        <span class="tooltip-target b3 material-icons">more_horiz</span>
+        <template slot="popover">
+          <div class="em-font-size-14 em-mb-8 em-pointer">Ajouter un sous-menu</div>
+          <div class="em-font-size-14 em-pointer">Supprimer</div>
+        </template>
+      </v-popover>
+    </div>
+
+    <hr/>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "IntegrationGED",
+  props:{
+    site: String
+  },
+  data() {
+    return {
+      loading: false,
+    }
+  },
+  created() {}
+}
+</script>
+
+<style scoped>
+</style>
