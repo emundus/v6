bc70383160e9a2da98215dbc8f4a759be86e8b69
Duy Tran <duy.tran@emundus.fr>
2022-03-10 18:58:11 +0100

    fix emundus-fileupload

--- plugins/fabrik_element/emundus_fileupload/emundus_fileupload.php
+++ plugins/fabrik_element/emundus_fileupload/emundus_fileupload.php
@@ -165,7 +165,7 @@ class PlgFabrik_ElementEmundus_fileupload extends PlgFabrik_Element {
         // track the LOGS (ATTACHMENT_CREATE)
         require_once(JPATH_SITE.DS.'components'.DS.'com_emundus'.DS.'models'.DS.'logs.php');
         $user = JFactory::getSession()->get('emundusUser');
-        EmundusModelLogs::log($user->id, $user->id, $fnum, 4, 'c', 'COM_EMUNDUS_ACCESS_ATTACHMENT_CREATE');
+        EmundusModelLogs::log($user->id, (int)substr($fnum, -7), $fnum, 4, 'c', 'COM_EMUNDUS_ACCESS_ATTACHMENT_CREATE');
 
         echo json_encode($result);
         return true;
@@ -293,7 +293,7 @@ class PlgFabrik_ElementEmundus_fileupload extends PlgFabrik_Element {
         // track the LOGS (ATTACHMENT_DELETE)
         require_once(JPATH_SITE.DS.'components'.DS.'com_emundus'.DS.'models'.DS.'logs.php');
         $user = JFactory::getSession()->get('emundusUser');
-        EmundusModelLogs::log($user->id, $user->id, $fnum, 4, 'd', 'COM_EMUNDUS_ACCESS_ATTACHMENT_DELETE');
+        EmundusModelLogs::log($user->id, (int)substr($fnum, -7), $fnum, 4, 'd', 'COM_EMUNDUS_ACCESS_ATTACHMENT_DELETE');
 
         echo json_encode(['status' => $status]);
         return true;
