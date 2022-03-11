a6638461e7f0b9f8eec9628ce0af95286bebb721
Brice <b.hubinet@gmail.com>
2022-03-11 17:57:21 +0100

    Create childrens node

--- components/com_emundus/src/components/Settings/AttachmentStorage/IntegrationGED.vue
+++ components/com_emundus/src/components/Settings/AttachmentStorage/IntegrationGED.vue
@@ -21,16 +21,16 @@
         <span class="material-icons" v-if="node.type !== 0">folder</span>
         <span class="material-icons-outlined" v-else>folder</span>
         <span class="em-ml-8 em-mr-8" v-if="node.type !== 0">/{{node.type}}</span>
-        <select class="em-ml-8 em-mr-8 em-clear-dropdown" v-else-if="node.level === 1">
+        <select class="em-ml-8 em-mr-8 em-clear-dropdown" v-model="node.type" v-else-if="node.level === 1">
           <option value="0" selected>/{{ translate('COM_EMUNDUS_ONBOARD_ATTACHMENT_STORAGE_GED_ALFRESCO_SELECT_TYPE') }}</option>
-          <option>Campagne</option>
+          <option value="campaign">{{ translate('COM_EMUNDUS_ONBOARD_ATTACHMENT_STORAGE_GED_ALFRESCO_CAMPAIGN_TYPE') }}</option>
         </select>
 
         <v-popover :popoverArrowClass="'custom-popover-arrow'">
           <span class="tooltip-target b3 material-icons">more_horiz</span>
           <template slot="popover">
             <div class="em-font-size-14 em-pointer em-p-8-12 em-hover-background-neutral-300" @click="addNode(node)">{{ translate('COM_EMUNDUS_ONBOARD_ATTACHMENT_STORAGE_GED_ALFRESCO_ADD_MENU') }}</div>
-            <div class="em-font-size-14 em-pointer em-p-8-12 em-hover-background-neutral-300" @click="deleteNode(node.id)">{{ translate('COM_EMUNDUS_ONBOARD_ATTACHMENT_STORAGE_GED_ALFRESCO_DELETE') }}</div>
+            <div class="em-font-size-14 em-pointer em-p-8-12 em-hover-background-neutral-300 em-red-500-color" @click="deleteNode(node.id)">{{ translate('COM_EMUNDUS_ONBOARD_ATTACHMENT_STORAGE_GED_ALFRESCO_DELETE') }}</div>
           </template>
         </v-popover>
       </div>
@@ -41,14 +41,14 @@
         <span class="em-ml-8 em-mr-8" v-if="children.type !== 0">/{{children.type}}</span>
         <select class="em-ml-8 em-mr-8 em-clear-dropdown" v-else-if="children.level === 2">
           <option value="0" selected>/{{ translate('COM_EMUNDUS_ONBOARD_ATTACHMENT_STORAGE_GED_ALFRESCO_SELECT_TYPE') }}</option>
-          <option>Formulaire</option>
+          <option value="form">{{ translate('COM_EMUNDUS_ONBOARD_ATTACHMENT_STORAGE_GED_ALFRESCO_FORM_TYPE') }}</option>
         </select>
 
         <v-popover :popoverArrowClass="'custom-popover-arrow'">
           <span class="tooltip-target b3 material-icons">more_horiz</span>
           <template slot="popover">
             <div class="em-font-size-14 em-pointer em-p-8-12 em-hover-background-neutral-300" @click="addNode(children)">{{ translate('COM_EMUNDUS_ONBOARD_ATTACHMENT_STORAGE_GED_ALFRESCO_ADD_MENU') }}</div>
-            <div class="em-font-size-14 em-pointer em-p-8-12 em-hover-background-neutral-300" @click="deleteNode(children.id)">{{ translate('COM_EMUNDUS_ONBOARD_ATTACHMENT_STORAGE_GED_ALFRESCO_DELETE') }}</div>
+            <div class="em-font-size-14 em-pointer em-p-8-12 em-hover-background-neutral-300 em-red-500-color" @click="deleteNode(children.id)">{{ translate('COM_EMUNDUS_ONBOARD_ATTACHMENT_STORAGE_GED_ALFRESCO_DELETE') }}</div>
           </template>
         </v-popover>
       </div>
@@ -76,7 +76,6 @@ export default {
 
   methods: {
     addNode(node_parent){
-      console.log(node_parent);
       if(node_parent === null) {
         let node = {
           id: 1,
@@ -87,7 +86,6 @@ export default {
 
         this.nodes.push(node);
       } else {
-
         let node = {
           id: 2,
           level: 2,
