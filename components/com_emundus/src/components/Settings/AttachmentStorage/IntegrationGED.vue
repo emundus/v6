f2740508d7f3533803af6c826d358206e98f8b3e
Brice <b.hubinet@gmail.com>
2022-03-13 19:55:07 +0100

    Create a recursive tree component

--- components/com_emundus/src/components/Settings/AttachmentStorage/IntegrationGED.vue
+++ components/com_emundus/src/components/Settings/AttachmentStorage/IntegrationGED.vue
@@ -17,41 +17,7 @@
     </div>
 
     <div v-for="node in nodes">
-      <div class="em-flex-row em-mb-16" :class="'em-level-' + node.level">
-        <span class="material-icons" v-if="node.type !== 0">folder</span>
-        <span class="material-icons-outlined" v-else>folder</span>
-        <span class="em-ml-8 em-mr-8" v-if="node.type !== 0">/{{node.type}}</span>
-        <select class="em-ml-8 em-mr-8 em-clear-dropdown" v-model="node.type" v-else-if="node.level === 1">
-          <option value="0" selected>/{{ translate('COM_EMUNDUS_ONBOARD_ATTACHMENT_STORAGE_GED_ALFRESCO_SELECT_TYPE') }}</option>
-          <option value="campaign">{{ translate('COM_EMUNDUS_ONBOARD_ATTACHMENT_STORAGE_GED_ALFRESCO_CAMPAIGN_TYPE') }}</option>
-        </select>
-
-        <v-popover :popoverArrowClass="'custom-popover-arrow'">
-          <span class="tooltip-target b3 material-icons">more_horiz</span>
-          <template slot="popover">
-            <div class="em-font-size-14 em-pointer em-p-8-12 em-hover-background-neutral-300" @click="addNode(node)">{{ translate('COM_EMUNDUS_ONBOARD_ATTACHMENT_STORAGE_GED_ALFRESCO_ADD_MENU') }}</div>
-            <div class="em-font-size-14 em-pointer em-p-8-12 em-hover-background-neutral-300 em-red-500-color" @click="deleteNode(node.id)">{{ translate('COM_EMUNDUS_ONBOARD_ATTACHMENT_STORAGE_GED_ALFRESCO_DELETE') }}</div>
-          </template>
-        </v-popover>
-      </div>
-
-      <div v-for="children in node.childrens" class="em-flex-row" :class="'em-level-' + children.level">
-        <span class="material-icons" v-if="children.type !== 0">folder</span>
-        <span class="material-icons-outlined" v-else>folder</span>
-        <span class="em-ml-8 em-mr-8" v-if="children.type !== 0">/{{children.type}}</span>
-        <select class="em-ml-8 em-mr-8 em-clear-dropdown" v-else-if="children.level === 2">
-          <option value="0" selected>/{{ translate('COM_EMUNDUS_ONBOARD_ATTACHMENT_STORAGE_GED_ALFRESCO_SELECT_TYPE') }}</option>
-          <option value="form">{{ translate('COM_EMUNDUS_ONBOARD_ATTACHMENT_STORAGE_GED_ALFRESCO_FORM_TYPE') }}</option>
-        </select>
-
-        <v-popover :popoverArrowClass="'custom-popover-arrow'">
-          <span class="tooltip-target b3 material-icons">more_horiz</span>
-          <template slot="popover">
-            <div class="em-font-size-14 em-pointer em-p-8-12 em-hover-background-neutral-300" @click="addNode(children)">{{ translate('COM_EMUNDUS_ONBOARD_ATTACHMENT_STORAGE_GED_ALFRESCO_ADD_MENU') }}</div>
-            <div class="em-font-size-14 em-pointer em-p-8-12 em-hover-background-neutral-300 em-red-500-color" @click="deleteNode(children.id)">{{ translate('COM_EMUNDUS_ONBOARD_ATTACHMENT_STORAGE_GED_ALFRESCO_DELETE') }}</div>
-          </template>
-        </v-popover>
-      </div>
+      <Tree :node="node" @addNode="addNode" @deleteNode="deleteNode" />
     </div>
 
     <hr/>
@@ -59,9 +25,11 @@
 </template>
 
 <script>
+import Tree from "./Tree";
 
 export default {
   name: "IntegrationGED",
+  components: {Tree},
   props:{
     site: String
   },
@@ -76,6 +44,7 @@ export default {
 
   methods: {
     addNode(node_parent){
+      console.log(node_parent);
       if(node_parent === null) {
         let node = {
           id: 1,
@@ -86,9 +55,10 @@ export default {
 
         this.nodes.push(node);
       } else {
+        let level = node_parent.level + 1;
         let node = {
           id: 2,
-          level: 2,
+          level: level,
           type: 0,
           childrens: []
         };
@@ -110,18 +80,4 @@ export default {
 </script>
 
 <style scoped>
-.em-level-1{
-  margin-left: 16px;
-}
-.em-level-2{
-  margin-left: 32px;
-}
-.em-clear-dropdown{
-  border: unset;
-  height: auto;
-}
-.em-clear-dropdown:focus{
-  outline: unset;
-  background: #E3E5E8;
-}
 </style>
