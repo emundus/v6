e686517415ab3090b605bb3a0d5b90f662e394d7
Retord <benjamin.retord@emundus.fr>
2022-07-06 16:48:01 +0200

    fix: Add translation for copy files and add campaign

---
+++ administrator/components/com_admin/sql/updates/mysql/3.10.18-2022-07-06-em.sql
@@ -0,0 +1,41 @@
+INSERT INTO jos_emundus_setup_languages (tag, lang_code, override, original_text, original_md5, override_md5, location, type, reference_id, reference_table, reference_field, published, created_by, created_date, modified_by, modified_date)
+VALUES ('PROGRAMME_LOGO', 'fr-FR', 'Logo du programme', 'Logo du programme', MD5('Logo du programme'), MD5('Logo du programme'), 'fr-FR.override.ini', 'override', null, 'fabrik_elements', 'label', 1, 62, '2022-06-30 08:28:03', null, null);
+
+INSERT INTO jos_emundus_setup_languages (tag, lang_code, override, original_text, original_md5, override_md5, location, type, reference_id, reference_table, reference_field, published, created_by, created_date, modified_by, modified_date)
+VALUES ('PROGRAMME_LOGO', 'en-EN', 'Programme logo', 'Programme logo', MD5('Programme logo'), MD5('Programme logo'), 'en-EN.override.ini', 'override', null, 'fabrik_elements', 'label', 1, 62, '2022-06-30 08:28:03', null, null);
+
+INSERT INTO jos_emundus_setup_languages (tag, lang_code, override, original_text, original_md5, override_md5, location, type, reference_id, reference_table, reference_field, published, created_by, created_date, modified_by, modified_date)
+VALUES ('PROGRAMME', 'en-EN', 'Programme', 'Programme', MD5('Programme'), MD5('Programme'), 'en-EN.override.ini', 'override', null, 'fabrik_elements', 'label', 1, 62, '2022-06-30 08:28:03', null, null);
+
+INSERT INTO jos_emundus_setup_languages (tag, lang_code, override, original_text, original_md5, override_md5, location, type, reference_id, reference_table, reference_field, published, created_by, created_date, modified_by, modified_date)
+VALUES ('PROGRAMME', 'fr-FR', 'Programme', 'Programme', MD5('Programme'), MD5('Programme'), 'fr-FR.override.ini', 'override', null, 'fabrik_elements', 'label', 1, 62, '2022-06-30 08:28:03', null, null);
+
+INSERT INTO jos_emundus_setup_languages (tag, lang_code, override, original_text, original_md5, override_md5, location, type, reference_id, reference_table, reference_field, published, created_by, created_date, modified_by, modified_date)
+VALUES ('IS_LIMITED', 'fr-FR', 'Limiter les candidatures', 'Limiter les candidatures', MD5('Limiter les candidatures'), MD5('Limiter les candidatures'), 'fr-FR.override.ini', 'override', null, 'fabrik_elements', 'label', 1, 62, '2022-06-30 08:28:03', null, null);
+
+INSERT INTO jos_emundus_setup_languages (tag, lang_code, override, original_text, original_md5, override_md5, location, type, reference_id, reference_table, reference_field, published, created_by, created_date, modified_by, modified_date)
+VALUES ('IS_LIMITED', 'en-EN', 'Limiting applications', 'Limiting applications', MD5('Limiting applications'), MD5('Limiting applications'), 'en-EN.override.ini', 'override', null, 'fabrik_elements', 'label', 1, 62, '2022-06-30 08:28:03', null, null);
+
+INSERT INTO jos_emundus_setup_languages (tag, lang_code, override, original_text, original_md5, override_md5, location, type, reference_id, reference_table, reference_field, published, created_by, created_date, modified_by, modified_date)
+VALUES ('EMUNDUS_COPY_APPLICATION_DOCUMENTS', 'en-EN', 'Copy attachments', 'Copy attachments', MD5('Copy attachments'), MD5('Copy attachments'), 'en-EN.override.ini', 'override', null, 'fabrik_elements', 'label', 1, 62, '2022-06-30 08:28:03', null, null);
+
+INSERT INTO jos_emundus_setup_languages (tag, lang_code, override, original_text, original_md5, override_md5, location, type, reference_id, reference_table, reference_field, published, created_by, created_date, modified_by, modified_date)
+VALUES ('EMUNDUS_COPY_APPLICATION_DELETE_FROM_FILE', 'en-EN', 'Delete the original file after copying', 'Delete the original file after copying', MD5('Delete the original file after copying'), MD5('Delete the original file after copying'), 'en-EN.override.ini', 'override', null, 'fabrik_elements', 'label', 1, 62, '2022-06-30 08:28:03', null, null);
+
+INSERT INTO jos_emundus_setup_languages (tag, lang_code, override, original_text, original_md5, override_md5, location, type, reference_id, reference_table, reference_field, published, created_by, created_date, modified_by, modified_date)
+VALUES ('EMUNDUS_COPY_APPLICATION_MOVE_HIKASHOP', 'en-EN', 'Move Hikashop commands', 'Move Hikashop commands', MD5('Move Hikashop commands'), MD5('Move Hikashop commands'), 'en-EN.override.ini', 'override', null, 'fabrik_elements', 'label', 1, 62, '2022-06-30 08:28:03', null, null);
+
+INSERT INTO jos_emundus_setup_languages (tag, lang_code, override, original_text, original_md5, override_md5, location, type, reference_id, reference_table, reference_field, published, created_by, created_date, modified_by, modified_date)
+VALUES ('EMUNDUS_COPY_TAG', 'en-EN', 'Copy the tags', 'Copy the tags', MD5('Copy the tags'), MD5('Copy the tags'), 'en-EN.override.ini', 'override', null, 'fabrik_elements', 'label', 1, 62, '2022-06-30 08:28:03', null, null);
+
+INSERT INTO jos_emundus_setup_languages (tag, lang_code, override, original_text, original_md5, override_md5, location, type, reference_id, reference_table, reference_field, published, created_by, created_date, modified_by, modified_date)
+VALUES ('LIMIT', 'fr-FR', 'Limite', 'Limite', MD5('Limite'), MD5('Limite'), 'fr-FR.override.ini', 'override', null, 'fabrik_elements', 'label', 1, 62, '2022-06-30 08:28:03', null, null);
+
+INSERT INTO jos_emundus_setup_languages (tag, lang_code, override, original_text, original_md5, override_md5, location, type, reference_id, reference_table, reference_field, published, created_by, created_date, modified_by, modified_date)
+VALUES ('LIMIT', 'en-EN', 'Limit', 'Limit', MD5('Limit'), MD5('Limit'), 'en-EN.override.ini', 'override', null, 'fabrik_elements', 'label', 1, 62, '2022-06-30 08:28:03', null, null);
+
+INSERT INTO jos_emundus_setup_languages (tag, lang_code, override, original_text, original_md5, override_md5, location, type, reference_id, reference_table, reference_field, published, created_by, created_date, modified_by, modified_date)
+VALUES ('LIMIT_STATUS', 'en-EN', 'Status to limit', 'Status to limit', MD5('Status to limit'), MD5('Status to limit'), 'en-EN.override.ini', 'override', null, 'fabrik_elements', 'label', 1, 62, '2022-06-30 08:28:03', null, null);
+
+INSERT INTO jos_emundus_setup_languages (tag, lang_code, override, original_text, original_md5, override_md5, location, type, reference_id, reference_table, reference_field, published, created_by, created_date, modified_by, modified_date)
+VALUES ('LIMIT_STATUS', 'fr-FR', 'Statut à limiter', 'Statut à limiter', MD5('Statut à limiter'), MD5('Statut à limiter'), 'fr-FR.override.ini', 'override', null, 'fabrik_elements', 'label', 1, 62, '2022-06-30 08:28:03', null, null);
