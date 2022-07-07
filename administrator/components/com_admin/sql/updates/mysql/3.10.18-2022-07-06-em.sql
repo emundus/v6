9f03a4cf2c07aa138df09a30450dc9d51ce67fd7
Retord <benjamin.retord@emundus.fr>
2022-07-07 11:15:33 +0200

    Add more translation

--- administrator/components/com_admin/sql/updates/mysql/3.10.18-2022-07-06-em.sql
+++ administrator/components/com_admin/sql/updates/mysql/3.10.18-2022-07-06-em.sql
@@ -39,3 +39,18 @@ VALUES ('LIMIT_STATUS', 'en-EN', 'Status to limit', 'Status to limit', MD5('Stat
 
 INSERT INTO jos_emundus_setup_languages (tag, lang_code, override, original_text, original_md5, override_md5, location, type, reference_id, reference_table, reference_field, published, created_by, created_date, modified_by, modified_date)
 VALUES ('LIMIT_STATUS', 'fr-FR', 'Statut à limiter', 'Statut à limiter', MD5('Statut à limiter'), MD5('Statut à limiter'), 'fr-FR.override.ini', 'override', null, 'fabrik_elements', 'label', 1, 62, '2022-06-30 08:28:03', null, null);
+
+INSERT INTO jos_emundus_setup_languages (tag, lang_code, override, original_text, original_md5, override_md5, location, type, reference_id, reference_table, reference_field, published, created_by, created_date, modified_by, modified_date)
+VALUES ('TIMEZONE', 'fr-FR', 'Timezone : ', 'Timezone : ', MD5('Timezone : '), MD5('Timezone : '), 'fr-FR.override.ini', 'override', null, 'fabrik_elements', 'label', 1, 62, '2022-06-30 08:28:03', null, null);
+
+INSERT INTO jos_emundus_setup_languages (tag, lang_code, override, original_text, original_md5, override_md5, location, type, reference_id, reference_table, reference_field, published, created_by, created_date, modified_by, modified_date)
+VALUES ('TIMEZONE', 'en-EN', 'Timezone: ', 'Timezone: ', MD5('Timezone: '), MD5('Timezone: '), 'en-EN.override.ini', 'override', null, 'fabrik_elements', 'label', 1, 62, '2022-06-30 08:28:03', null, null);
+
+INSERT INTO jos_emundus_setup_languages (tag, lang_code, override, original_text, original_md5, override_md5, location, type, reference_id, reference_table, reference_field, published, created_by, created_date, modified_by, modified_date)
+VALUES ('LOGOUT', 'en-EN', 'Logout', 'Logout', MD5('Logout'), MD5('Logout'), 'en-EN.override.ini', 'override', null, 'fabrik_elements', 'label', 1, 62, '2022-06-30 08:28:03', null, null);
+
+INSERT INTO jos_emundus_setup_languages (tag, lang_code, override, original_text, original_md5, override_md5, location, type, reference_id, reference_table, reference_field, published, created_by, created_date, modified_by, modified_date)
+VALUES ('FORMATION', 'fr-FR', 'Formation', 'Formation', MD5('Formation'), MD5('Formation'), 'fr-FR.override.ini', 'override', null, 'fabrik_elements', 'label', 1, 62, '2022-06-30 08:28:03', null, null);
+
+INSERT INTO jos_emundus_setup_languages (tag, lang_code, override, original_text, original_md5, override_md5, location, type, reference_id, reference_table, reference_field, published, created_by, created_date, modified_by, modified_date)
+VALUES ('FORMATION', 'en-EN', 'Training', 'Training', MD5('Training'), MD5('Training'), 'en-EN.override.ini', 'override', null, 'fabrik_elements', 'label', 1, 62, '2022-06-30 08:28:03', null, null);
