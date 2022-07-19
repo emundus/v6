1e9c3c7381b56cd0ae54ef613e9aa7f35c11a9d3
Bazile Binson <bazile.binson@emundus.fr>
2022-07-19 09:19:24 +0000

    minor: Add IP address of current user when logging action

--- administrator/components/com_admin/sql/updates/mysql/3.10.9-2022-07-18-em.sql
+++ administrator/components/com_admin/sql/updates/mysql/3.10.9-2022-07-18-em.sql
@@ -1,3 +1,5 @@
+ALTER TABLE `jos_emundus_logs` ADD COLUMN `ip_from` VARCHAR(26) NULL;
+
 update jos_extensions set enabled = 0 where element LIKE 'emundus_period';
 INSERT INTO jos_extensions (package_id, name, type, element, folder, client_id, enabled, access, protected, manifest_cache, params, custom_data, system_data, checked_out, checked_out_time, ordering, state)
 VALUES (0, 'System - eMundus Redirect user if block', 'plugin', 'emundus_block_user', 'system', 0, 1, 1, 0, '{"name":"System - eMundus Redirect user if block","type":"plugin","creationDate":"Septembre 2020","author":"eMundus","copyright":"Copyright (C) 2016 eMundus","authorEmail":"admin@emundus.fr","authorUrl":"www.emundus.fr","version":"6.0","description":"Redirect user if he is block","group":"","filename":"emundus_block_user"}', '{}', '', '', 0, '2022-06-30 08:28:03', 0, 0);
