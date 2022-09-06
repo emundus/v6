12d6be0e3645b8b3bc08fff9660f3b944216dcce
MEGB EMUNDUS <merveille.gbetegan@emundus.fr>
2022-09-06 16:18:58 +0200

    feat: remove console log

--- modules/mod_emundus_rsst_signalement_list/tmpl/default.php
+++ modules/mod_emundus_rsst_signalement_list/tmpl/default.php
@@ -5,8 +5,6 @@ defined('_JEXEC') or die('Restricted Access');
 JText::script('MOD_EMUNDUS_RSST_SIGNALEMENT_LIST');
 
 $user = JFactory::getSession()->get('emundusUser');
-echo $params->get('fabrik_list_particular_conditional_column_values');
-echo $params->get('fabrik_list_particular_conditional_column');
 
 echo '<div id="em-rsst-signalement-list-vue" user="'. $user->id . '" listId="'. $params->get('fabrik_list_id') . '" listActionColumn="'. $params->get('fabrik_list_particular_action_column') .'" listParticularConditionalColumn="'.$params->get('fabrik_list_particular_conditional_column').'" listParticularConditionalColumnValues="'.$params->get('fabrik_list_particular_conditional_column_values').'">
 </div>';
