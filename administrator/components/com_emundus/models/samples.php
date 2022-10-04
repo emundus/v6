db91d8cb4c3ab88ad562ee660fef49d4002384da
Brice <b.hubinet@gmail.com>
2022-10-04 16:18:02 +0200

    Add translations in emundustriggers. Add a reload in email validation page to go back in homepage if our account is enable. Create sample datas module in backoffice

---
+++ administrator/components/com_emundus/models/samples.php
@@ -0,0 +1,193 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bhubinet
+ * Date: 04/09/22
+ */
+defined('_JEXEC') or die('Restricted access');
+
+jimport('joomla.application.component.model');
+
+
+include_once(JPATH_SITE.'/components/com_emundus/models/users.php');
+include_once(JPATH_SITE.'/components/com_emundus/models/formbuilder.php');
+include_once(JPATH_SITE.'/components/com_emundus/models/settings.php');
+include_once(JPATH_SITE.'/components/com_emundus/helpers/files.php');
+
+class EmundusModelSamples extends JModelList {
+
+    public function createSampleUser($profile = 9,$username = 'user.test@emundus.fr')
+    {
+        $db = JFactory::getDbo();
+        $query = $db->getQuery(true);
+
+        $m_users = new EmundusModelUsers();
+
+        $user = clone(JFactory::getUser(0));
+        $user->name = 'USER Test';
+        $user->username = (string)rand(100000, 1000000);
+
+        do {
+            $query->clear()
+                ->select('id')
+                ->from($db->quoteName('#__users'))
+                ->where($db->quoteName('username') . ' = ' . $user->username);
+            $db->setQuery($query);
+            $existing = $db->loadResult();
+        } while(!is_null($existing));
+
+        $user->email = $username;
+        $user->password = md5('test1234');
+        $user->registerDate = date('Y-m-d H:i:s');
+        $user->lastvisitDate = date('Y-m-d H:i:s');
+        $user->groups = array();
+        $user->block = 0;
+
+        $other_param['firstname'] 		= 'Test';
+        $other_param['lastname'] 		= 'USER';
+        $other_param['profile'] 		= $profile;
+        $other_param['em_oprofiles'] 	= '';
+        $other_param['univ_id'] 		= 0;
+        $other_param['em_groups'] 		= '';
+        $other_param['em_campaigns'] 	= '1';
+        $other_param['news'] 			= '';
+
+        $acl_aro_groups = $m_users->getDefaultGroup($profile);
+        $user->groups = $acl_aro_groups;
+
+        $usertype = $m_users->found_usertype($acl_aro_groups[0]);
+        $user->usertype = $usertype;
+
+        $user->save();
+
+        $query->clear()
+            ->update($db->quoteName('#__users'))
+            ->set($db->quoteName('username') . ' = ' . $db->quote('user'.$user->id.'.test@emundus.fr'))
+            ->where($db->quoteName('id') . ' = ' . $user->id);
+        $db->setQuery($query);
+        $db->execute();
+
+        $m_users->addEmundusUser($user->id, $other_param);
+
+        return $user;
+    }
+
+    public function createSampleFile($uids = null){
+        $db = JFactory::getDbo();
+        $query = $db->getQuery(true);
+
+        if(!is_array($uids)){
+            $uids = [$uids];
+        }
+
+        $query->select('id')
+            ->from($db->quoteName('#__emundus_setup_campaigns'));
+        $db->setQuery($query);
+        $cids = $db->loadColumn();
+
+        $query->clear()
+            ->select('step')
+            ->from($db->quoteName('#__emundus_setup_status'));
+        $db->setQuery($query);
+        $status = $db->loadColumn();
+
+        if(empty($uid)){
+            $query->clear()
+                ->select('user_id')
+                ->from($db->quoteName('#__emundus_users'))
+                ->where($db->quoteName('profile') . ' = 9');
+            $db->setQuery($query);
+            $uids = $db->loadColumn();
+        }
+
+        foreach ($uids as $uid) {
+            foreach ($cids as $cid) {
+                $fnum = @EmundusHelperFiles::createFnum($cid, $uid);
+
+                try {
+                    $query->clear()
+                        ->insert($db->quoteName('#__emundus_campaign_candidature'));
+                    $query->set($db->quoteName('applicant_id') . ' = ' . $db->quote($uid))
+                        ->set($db->quoteName('user_id') . ' = ' . $db->quote($uid))
+                        ->set($db->quoteName('campaign_id') . ' = ' . $db->quote($cid))
+                        ->set($db->quoteName('fnum') . ' = ' . $db->quote($fnum))
+                        ->set($db->quoteName('status') . ' = ' . $db->quote(array_rand($status)));
+                    $db->setQuery($query);
+                    $db->execute();
+                } catch (Exception $e) {
+                    JLog::add('component/com_emundus/models/formbuilder | Error at creating a testing file in the campaign ' . $cid . ' of the user ' . $uid . ' : ' . preg_replace("/[\r\n]/", " ", $query->__toString() . ' -> ' . $e->getMessage()), JLog::ERROR, 'com_emundus');
+                    return false;
+                }
+            }
+        }
+    }
+
+    public function createSampleTag(){
+        $m_settings = new EmundusModelSettings;
+
+        return $m_settings->createTag()->id;
+    }
+
+    public function createSampleStatus(){
+        $m_settings = new EmundusModelSettings;
+
+        return $m_settings->createStatus()->step;
+    }
+
+    public function createSampleCampaign($label,$profile=9){
+        $db = JFactory::getDbo();
+        $query = $db->getQuery(true);
+
+        $query->select('code')
+            ->from($db->quoteName('#__emundus_setup_programmes'));
+        $db->setQuery($query);
+        $training = $db->loadResult();
+
+        if(empty($training)){
+            $inserting_datas = [
+                'code' => 'prog',
+                'label' => 'Programme de test',
+                'ordering' => 1,
+                'published' => 1,
+                'apply_online' => 1,
+            ];
+
+            $query->clear()
+                ->insert($db->quoteName('#__emundus_setup_programmes'))
+                ->columns($db->quoteName(array_keys($inserting_datas)))
+                ->values(implode(',',$db->quote(array_values($inserting_datas))));
+            $db->setQuery($query);
+            $db->execute();
+
+            $training = 'prog';
+        }
+
+        $start_date = new DateTime();
+        $start_date->modify('-1 day');
+
+        $end_date = new DateTime();
+        $end_date->modify('+1 year');
+
+        $inserting_datas = [
+            'user' => 62,
+            'label' => $label,
+            'description' => 'Lorem ipsum',
+            'short_description' => 'Lorem ipsum',
+            'start_date' => $start_date->format('Y-m-d H:i:s'),
+            'end_date' => $end_date->format('Y-m-d H:i:s'),
+            'profile_id' => $profile,
+            'training' => $training,
+            'year' => '2022-2023',
+            'published' => 1,
+        ];
+
+        $query->clear()
+            ->insert($db->quoteName('#__emundus_setup_campaigns'))
+            ->columns($db->quoteName(array_keys($inserting_datas)))
+            ->values(implode(',',$db->quote(array_values($inserting_datas))));
+        $db->setQuery($query);
+        $db->execute();
+
+        return $db->insertid();
+    }
+}
--- components/com_emundus/helpers/events.php
+++ components/com_emundus/helpers/events.php
@@ -188,7 +188,7 @@ class EmundusHelperEvents {
             $is_campaign_started = strtotime(date($now)) >= strtotime($current_start_date);
             if (!$is_campaign_started && !in_array($user->id, $applicants)) {
                 // STOP HERE, the campaign or step is not started yet. Redirect to main page
-                $mainframe->enqueueMessage(JText::_('APPLICATION_PERIOD_NOT_STARTED'), 'error');
+                $mainframe->enqueueMessage(JText::_('COM_EMUNDUS_EVENTS_APPLICATION_PERIOD_NOT_STARTED'), 'warning');
                 $mainframe->redirect('/');
             }
 
@@ -227,11 +227,11 @@ class EmundusHelperEvents {
                     //try to access detail view or other
                     else {
                         if (!$can_edit && $is_app_sent) {
-                            $mainframe->enqueueMessage(JText::_('APPLICATION_READ_ONLY'), 'error');
+                            $mainframe->enqueueMessage(JText::_('COM_EMUNDUS_EVENTS_APPLICATION_READ_ONLY'), 'warning');
                         } else if ($fnumDetail['published'] == -1) {
-                            $mainframe->enqueueMessage(JText::_('DELETED_FILE'), 'error');
+                            $mainframe->enqueueMessage(JText::_('COM_EMUNDUS_EVENTS_APPLICATION_DELETED_FILE'), 'warning');
                         } else if ($is_dead_line_passed) {
-                            $mainframe->enqueueMessage(JText::_('APPLICATION_PERIOD_PASSED'), 'error');
+                            $mainframe->enqueueMessage(JText::_('COM_EMUNDUS_EVENTS_APPLICATION_PERIOD_PASSED'), 'warning');
                         }
                         $reload_url = false;
                     }
@@ -267,9 +267,9 @@ class EmundusHelperEvents {
                     if (($is_dead_line_passed && $can_edit_after_deadline == 0) || $isLimitObtained === true) {
                         if ($reload_url) {
                             if ($isLimitObtained === true) {
-                                $mainframe->enqueueMessage(JText::_('APPLICATION_LIMIT_OBTAINED'), 'error');
+                                $mainframe->enqueueMessage(JText::_('COM_EMUNDUS_EVENTS_APPLICATION_LIMIT_OBTAINED'), 'warning');
                             } else {
-                                $mainframe->enqueueMessage(JText::_('APPLICATION_PERIOD_PASSED'), 'error');
+                                $mainframe->enqueueMessage(JText::_('COM_EMUNDUS_EVENTS_APPLICATION_PERIOD_PASSED'), 'warning');
                             }
                             $mainframe->redirect("index.php?option=com_fabrik&view=details&formid=".$jinput->get('formid')."&Itemid=".$itemid."&usekey=fnum&rowid=".$user->fnum."&r=".$reload);
                         }
@@ -782,7 +782,7 @@ class EmundusHelperEvents {
         // If we've passed the deadline and the user cannot submit (is not in the list of exempt users), block him.
         if ((($is_dead_line_passed && $can_edit_after_deadline != 1) || $isLimitObtained === true) && !in_array($student->id, $id_applicants)) {
             if ($isLimitObtained === true) {
-                $params['formModel']->formErrorMsg = JText::_('LIMIT_OBTAINED');
+                $params['formModel']->formErrorMsg = JText::_('COM_EMUNDUS_EVENTS_APPLICATION_LIMIT_OBTAINED');
             } else {
                 $params['formModel']->formErrorMsg = JText::_('CANDIDATURE_PERIOD_TEXT');
             }
