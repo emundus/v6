f3d02b51c9abb7b749c14ae35fae8f6db932ee53
Legendre <jeremy.legendre@emundus.fr>
2022-11-03 14:53:30 +0100

    fix skeleton component indentation

--- components/com_emundus/src/components/Skeleton.vue
+++ components/com_emundus/src/components/Skeleton.vue
@@ -1,12 +1,11 @@
 <template>
 	<span
-			class="em-skeleton"
-			:class="classes"
-			:style="{
-				height: height,
-				width: width,
-				anima
-			}"
+		class="em-skeleton"
+		:class="classes"
+		:style="{
+			height: height,
+			width: width
+		}"
 	>
 	</span>
 </template>
@@ -26,9 +25,6 @@ export default {
 		classes: {
 			type: String,
 			default: ''
-		},
-		animationSpeed: {
-
 		}
 	}
 }
@@ -38,32 +34,27 @@ export default {
 .em-skeleton {
 	display: inline-block;
 	position: relative;
-  overflow: hidden;
+	overflow: hidden;
 	vertical-align: middle;
 	background-color: #DDDBDD;
+	transition: all .3s;
 
 	&::after {
-    position: absolute;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-    transform: translateX(-100%);
-    background-image: linear-gradient(
-      90deg,
-      rgba(#fff, 0) 0,
-      rgba(#fff, 0.2) 20%,
-      rgba(#fff, 0.5) 60%,
-      rgba(#fff, 0)
-    );
-    animation: shimmer 5s infinite;
-    content: '';
-  }
+		position: absolute;
+		top: 0;
+		right: 0;
+		bottom: 0;
+		left: 0;
+		transform: translateX(-100%);
+		background-image: linear-gradient(90deg, rgba(#fff, 0) 0, rgba(#fff, 0.2) 20%, rgba(#fff, 0.5) 60%, rgba(#fff, 0));
+		animation: shimmer 4s infinite;
+		content: '';
+	}
 
-  @keyframes shimmer {
-    100% {
-      transform: translateX(100%);
-    }
-  }
+	@keyframes shimmer {
+		100% {
+			transform: translateX(100%);
+		}
+	}
 }
 </style>
