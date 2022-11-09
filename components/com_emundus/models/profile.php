f0614d5af3be1b494700bfb2ceea6f1219a6bcdf
Legendre <jeremy.legendre@emundus.fr>
2022-11-09 12:20:54 +0100

    get all profiles from code programme fix excel export

--- components/com_emundus/models/profile.php
+++ components/com_emundus/models/profile.php
@@ -644,41 +644,50 @@ class EmundusModelProfile extends JModelList {
 	 * @return array The profile IDs found
 	 */
     function getProfileIDByCourse($code = array(), $camps = array()) : array {
+        $profiles = [];
+        $query = $this->_db->getQuery(true);
 
-        if (!empty($code)>0 && isset($camps[0]) && $camps[0] != 0) {
+        if (!empty($code)) {
+            $query->select('DISTINCT(esc.profile_id)')
+                ->from($this->_db->quoteName('#__emundus_setup_campaigns', 'esc'))
+                ->where('esc.published = 1')
+                ->andWhere('esc.training IN (' . implode(',', $this->_db->quote($code)) . ')');
 
-            $query = 'SELECT DISTINCT(esc.profile_id)
-					FROM  #__emundus_setup_campaigns AS esc
-					WHERE esc.published = 1 AND esc.training IN ('.implode(",", $this->_db->quote($code)).') AND esc.id IN ('.implode(",", $camps).')';
+            if (!empty($camps[0])) {
+                $query->andWhere('esc.id IN (' . implode(',', $camps) . ')');
+            }
 
             try {
                 $this->_db->setQuery($query);
-                $res = $this->_db->loadColumn();
+                $profiles = $this->_db->loadColumn();
             } catch(Exception $e) {
-                JLog::add(JUri::getInstance().' :: USER ID : '.JFactory::getUser()->id.' -> '.$query, JLog::ERROR, 'com_emundus');
-                $res = [];
-                JError::raiseError(500, $e->getMessage());
+                JLog::add(JUri::getInstance().' :: USER ID : '.JFactory::getUser()->id.' -> '.$query, JLog::ERROR, 'com_emundus.error');
             }
 
-        } elseif (!empty($code)>0) {
+            $query->clear();
 
-            $query = 'SELECT DISTINCT(esc.profile_id)
-						FROM  #__emundus_setup_campaigns AS esc
-						WHERE esc.published = 1 AND esc.training IN ("'.implode("','", $code).'") LIMIT 1';
+            $query->select('DISTINCT(ecw.profile)')
+                ->from('#__emundus_campaign_workflow as ecw')
+                ->leftJoin('#__emundus_campaign_workflow_repeat_campaign AS ecwrc ON ecwrc.parent_id = ecw.id');
+
+            if (!empty($camps[0])) {
+                $query->where($this->_db->quoteName('#__emundus_setup_campaigns.id') . 'IN (' . implode(',', $camps) . ')');
+            } else {
+                $query->leftJoin('#__emundus_setup_campaigns AS jesc ON jesc.id = ecwrc.campaign')
+                    ->where('jesc.training IN (' . implode(',', $this->_db->quote($code)) . ')');
+            }
 
             try {
                 $this->_db->setQuery($query);
-                $res = $this->_db->loadColumn();
+                $workflow_profiles = $this->_db->loadColumn();
+
+                $profiles = array_unique(array_merge($profiles, $workflow_profiles));
             } catch(Exception $e) {
-                JLog::add(JUri::getInstance().' :: USER ID : '.JFactory::getUser()->id.' -> '.$query, JLog::ERROR, 'com_emundus');
-	            $res = [];
-                JError::raiseError(500, $e->getMessage());
+                JLog::add(JUri::getInstance().' :: USER ID : '.JFactory::getUser()->id.' -> '.$query, JLog::ERROR, 'com_emundus.error');
             }
-        } else {
-            $res = $code;
         }
 
-        return $res;
+        return $profiles;
     }
 
 	/**
@@ -771,8 +780,8 @@ class EmundusModelProfile extends JModelList {
 
     public function getProfileIDByCampaigns($campaigns, $codes) {
         $query = $this->_db->getQuery(true);
-        if(!empty($campaigns)) {
-            if(!empty($codes)) {
+        if (!empty($campaigns)) {
+            if (!empty($codes)) {
                 try {
                     $query->clear()
                         ->select('#__emundus_setup_campaigns.*')
