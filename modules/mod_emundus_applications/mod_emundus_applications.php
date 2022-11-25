d11683e1449a3c985e2d814dfbb22cf3ffd8de86
Brice <b.hubinet@gmail.com>
2022-11-25 15:45:53 +0100

    Add date format parameter in mod_emundus_applications

--- modules/mod_emundus_applications/mod_emundus_applications.php
+++ modules/mod_emundus_applications/mod_emundus_applications.php
@@ -97,6 +97,7 @@ if (empty($user->profile) || in_array($user->profile, $applicant_profiles) || (!
 
     $groups = $params->get('mod_em_application_group', null);
     $title_other_section = $params->get('mod_em_application_group_title_other', 'MOD_EMUNDUS_APPLICATIONS_OTHER_FILES');
+    $date_format = $params->get('mod_em_application_date_format', 'd/m/Y H:i');
 
 
     // Due to the face that ccirs-drh is totally different, we use a different method all together to avoid further complicating the existing one.
