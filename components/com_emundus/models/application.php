872421b29c745b9ed05a864cee97e84922d7167d
benjamin <benjamin.retord@emundus.io>
2022-12-01 14:08:42 +0100

    Add select to get id and content for database join plugin

--- components/com_emundus/models/application.php
+++ components/com_emundus/models/application.php
@@ -1789,6 +1789,10 @@ class EmundusModelApplication extends JModelList
                                             // TODO : If databasejoin checkbox or multilist get value from children table. Add a query to get join table from jos_fabrik_joins where element_id = $element->id
                                             if ($element->plugin == 'databasejoin'){
                                                 $params = json_decode($element->params);
+                                                $query = 'SELECT `id`, `' . $element->name . '` FROM `' . $itemt->db_table_name . '` WHERE fnum like ' . $this->_db->Quote($fnum);
+                                                $this->_db->setQuery($query);
+
+                                                $res = $this->_db->loadRow();
                                                 if($params->database_join_display_type == 'checkbox' || $params->database_join_display_type == 'multilist'){
                                                     $query = 'SELECT t.id, jd.'.$element->name.' FROM `' .$itemt->db_table_name  . '` as t LEFT JOIN `'.$itemt->db_table_name.'_repeat_' . $element->name.'` as jd ON jd.parent_id = t.id WHERE fnum like ' . $this->_db->Quote($fnum);
 
