0ae7a6a657985b5ed926e624fbb68afe6c9b43c8
Brice <b.hubinet@gmail.com>
2022-12-05 11:30:46 +0100

    Update plugin description

--- plugins/fabrik_form/emundusisevaluatedbyme/emundusisevaluatedbyme.php
+++ plugins/fabrik_form/emundusisevaluatedbyme/emundusisevaluatedbyme.php
@@ -2,14 +2,9 @@
 /**
  * @version 1.34.0: emundusisevaluatedbyme 2022-12-02 Brice HUBINET
  * @package Fabrik
- * @copyright Copyright (C) 2020 emundus.fr. All rights reserved.
+ * @copyright Copyright (C) 2022 emundus.fr. All rights reserved.
  * @license GNU/GPL, see LICENSE.php
- * Joomla! is free software. This version may have been modified pursuant
- * to the GNU General Public License, and as distributed it includes or
- * is derivative of works licensed under the GNU General Public License or
- * other free or open source software licenses.
- * See COPYRIGHT.php for copyright notices and details.
- * @description Locks access to a file if the file is not of a certain status.
+ * @description Check how can the connected user can access to an evaluation
  */
 
 // No direct access
