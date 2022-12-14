6d02c590c87c5dfd7a91e3fc4a54aa0d65e44abf
Brice Hubinet <brice.hubinet@emundus.io>
2022-12-14 11:13:57 +0000

    [#HOTFIX] Fix sheet data name from 'Data' to 'data'. Merge data sheet to xlsx file with...

--- libraries/emundus/vendor/phpoffice/phpspreadsheet/src/PhpSpreadsheet/Style/NumberFormat/FractionFormatter.php
+++ libraries/emundus/vendor/phpoffice/phpspreadsheet/src/PhpSpreadsheet/Style/NumberFormat/FractionFormatter.php
@@ -6,20 +6,32 @@ use PhpOffice\PhpSpreadsheet\Calculation\MathTrig;
 
 class FractionFormatter extends BaseFormatter
 {
+    /**
+     * @param mixed $value
+     */
     public static function format($value, string $format): string
     {
         $format = self::stripQuotes($format);
+        $value = (float) $value;
+        $absValue = abs($value);
 
         $sign = ($value < 0.0) ? '-' : '';
 
-        $integerPart = floor(abs($value));
-        $decimalPart = trim(fmod(abs($value), 1), '0.');
+        $integerPart = floor($absValue);
+
+        $decimalPart = self::getDecimal((string) $absValue);
+        if ($decimalPart === '0') {
+            return "{$sign}{$integerPart}";
+        }
         $decimalLength = strlen($decimalPart);
         $decimalDivisor = 10 ** $decimalLength;
 
+        /** @var float */
         $GCD = MathTrig\Gcd::evaluate($decimalPart, $decimalDivisor);
+        /** @var float */
+        $decimalPartx = $decimalPart;
 
-        $adjustedDecimalPart = $decimalPart / $GCD;
+        $adjustedDecimalPart = $decimalPartx / $GCD;
         $adjustedDecimalDivisor = $decimalDivisor / $GCD;
 
         if ((strpos($format, '0') !== false)) {
@@ -41,5 +53,15 @@ class FractionFormatter extends BaseFormatter
         $adjustedDecimalPart += $integerPart * $adjustedDecimalDivisor;
 
         return "{$sign}{$adjustedDecimalPart}/{$adjustedDecimalDivisor}";
+    }
+
+    private static function getDecimal(string $value): string
+    {
+        $decimalPart = '0';
+        if (preg_match('/^\\d*[.](\\d*[1-9])0*$/', $value, $matches) === 1) {
+            $decimalPart = $matches[1];
+        }
+
+        return $decimalPart;
     }
 }
