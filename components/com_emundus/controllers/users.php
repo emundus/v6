3dad6d2ba41c51014169b71474f773007e22b7f5
Brice <b.hubinet@gmail.com>
2022-12-14 17:03:55 +0100

    Update firstname and lastname of emundusSession when update user (via coordinator and via profile edit view)

--- components/com_emundus/controllers/users.php
+++ components/com_emundus/controllers/users.php
@@ -696,6 +696,11 @@ class EmundusControllerUsers extends JControllerLegacy {
 		if ($res === true || !is_array($res)) {
 			$res = true;
 			$msg = JText::_('COM_EMUNDUS_USERS_EDITED');
+
+			$e_user = JFactory::getSession()->get('emundusUser');
+			$e_user->firstname = $newuser['firstname'];
+			$e_user->lastname = $newuser['lastname'];
+			JFactory::getSession()->set('emundusUser', $e_user);
 		} else {
 			if (is_array($res)) {
 				$res['status'] = false;
@@ -1005,6 +1010,16 @@ class EmundusControllerUsers extends JControllerLegacy {
         if(!empty($user)) {
             $m_users = new EmundusModelUsers();
             $result = $m_users->saveUser($user,$current_user->id);
+			if($result){
+				$e_user = JFactory::getSession()->get('emundusUser');
+				if(isset($user->firstname)){
+					$e_user->firstname = $user->firstname;
+				}
+				if(isset($user->lastname)){
+					$e_user->lastname = $user->lastname;
+				}
+				JFactory::getSession()->set('emundusUser', $e_user);
+			}
         } else {
             $result = false;
         }
