6a20f18c46cd6baeec8fa828c8aa6782d1b4ce41
Legendre <jeremy.legendre@emundus.fr>
2022-12-22 16:59:28 +0100

    fix action id of logs (37 and not 39)

--- components/com_emundus/views/application/view.raw.php
+++ components/com_emundus/views/application/view.raw.php
@@ -315,8 +315,8 @@ class EmundusViewApplication extends JViewLegacy {
                     break;
 
                 case 'logs':
-                    if (EmundusHelperAccess::asAccessAction(39, 'r', $this->_user->id, $fnum)) {
-                        EmundusModelLogs::log($this->_user->id, (int)substr($fnum, -7), $fnum, 39, 'r', 'COM_EMUNDUS_ACCESS_LOGS_READ');
+                    if (EmundusHelperAccess::asAccessAction(37, 'r', $this->_user->id, $fnum)) {
+                        EmundusModelLogs::log($this->_user->id, (int)substr($fnum, -7), $fnum, 37, 'r', 'COM_EMUNDUS_ACCESS_LOGS_READ');
 
                         $fileLogs = EmundusModelLogs::getActionsOnFnum($fnum);
 
