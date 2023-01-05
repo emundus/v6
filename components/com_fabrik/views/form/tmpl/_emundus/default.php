67e7abe40d0aa74e7519848982788dec2d1ce54d
Legendre <jeremy.legendre@emundus.fr>
2023-01-05 10:44:58 +0100

    patch: fix inaccurate fabrik form title display

--- components/com_fabrik/views/form/tmpl/_emundus/default.php
+++ components/com_fabrik/views/form/tmpl/_emundus/default.php
@@ -33,10 +33,8 @@ endif;
 <div class="emundus-form">
 <?php if ($this->params->get('show-title', 1)) :?>
 <div class="page-header">
-    <h1>
-        <?php
-        $title = explode('-', $form->label);
-        echo !empty($title[1])?JText::_(trim($title[1])):JText::_(trim($title[0])); ?></h1>
+    <?php $title = preg_replace('/^([^-]+ - )/', '', $form->label);?>
+    <h1><?= $title ?></h1>
 </div>
 <?php endif; ?>
 
