4f01c7e8649296ab96e84d9d7b4aa2c44dca7277
Bazile Binson <bazile.binson@emundus.fr>
2023-03-24 11:34:32 +0100

    fix: rewrite messengernotify plugin for improvements and optimization

--- plugins/fabrik_cron/emundusmessengernotify/emundusmessengernotify.php
+++ plugins/fabrik_cron/emundusmessengernotify/emundusmessengernotify.php
@@ -54,24 +54,25 @@ class PlgFabrik_Cronemundusmessengernotify extends PlgFabrik_Cron {
 
         $this->log = '';
 
-        // Get list of applicants to notify
+        // Get list of fnums with a chatroom containing an unread message
         $db = JFactory::getDbo();
         $query = $db->getQuery(true);
 
         $query->select('distinct cc.fnum, cc.applicant_id')
-            ->from($db->quoteName('#__emundus_chatroom','c'))
-            ->leftJoin($db->quoteName('#__emundus_campaign_candidature','cc').' ON '.$db->quoteName('cc.fnum').' = '.$db->quoteName('c.fnum'));
-
+            ->from($db->quoteName('#__emundus_campaign_candidature','cc'))
+            ->innerJoin($db->quoteName('#__emundus_chatroom','c').' ON '.$db->quoteName('c.fnum').' = '.$db->quoteName('cc.fnum'))
+            ->innerJoin($db->quoteName('#__messages','m').' ON '.$db->quoteName('m.page').' = '.$db->quoteName('c.id') . ' AND ' . $db->quoteName('m.state') . ' = ' . $db->quote(0));
         $db->setQuery($query);
         $chatrooms = $db->loadObjectList();
 
-        $applicants_to_send = [];
+        // <------- Process to send email to applicants ------>
+        $applicants_to_send = array();
 
-        // Generate emails from template and store it in message table
+        // Generate emails for applicant from template
         if (!empty($chatrooms)) {
-            include_once(JPATH_SITE.'/components/com_emundus/models/emails.php');
-            include_once(JPATH_SITE.'/components/com_emundus/models/files.php');
-            include_once(JPATH_SITE.'/components/com_emundus/models/profile.php');
+            include_once(JPATH_SITE . DS . 'components' . DS . 'com_emundus' . DS . 'models' . DS . 'emails.php');
+            include_once(JPATH_SITE . DS . 'components' . DS . 'com_emundus' . DS . 'models' . DS . 'files.php');
+            include_once(JPATH_SITE . DS . 'components' . DS . 'com_emundus' . DS . 'models' . DS . 'profile.php');
             include_once(JPATH_SITE . DS . 'components' . DS . 'com_emundus' . DS . 'controllers' . DS . 'messages.php');
 
             $m_emails = new EmundusModelEmails;
@@ -88,6 +89,7 @@ class PlgFabrik_Cronemundusmessengernotify extends PlgFabrik_Cron {
             $db->setQuery($query);
             $email->Template = $db->loadResult();
 
+            // For every chatroom, check if the applicant has an unread message
             foreach ($chatrooms as $chatroom) {
                 $query->clear()
                     ->select('count(m.message_id)')
@@ -100,6 +102,7 @@ class PlgFabrik_Cronemundusmessengernotify extends PlgFabrik_Cron {
                 $db->setQuery($query);
                 $messages_not_read = $db->loadResult();
 
+                // If there is an unread message and the applicant is not already in the list of applicants to notify, add them
                 if ($messages_not_read > 0) {
                     if(!in_array($chatroom->applicant_id,$applicants_to_send)) {
                         $applicants_to_send[] = $chatroom->applicant_id;
@@ -107,6 +110,7 @@ class PlgFabrik_Cronemundusmessengernotify extends PlgFabrik_Cron {
                 }
             }
 
+            // Send the mail to each applicant
             foreach ($applicants_to_send as $applicant_to_send){
                 $mailer = JFactory::getMailer();
 
@@ -167,27 +171,52 @@ class PlgFabrik_Cronemundusmessengernotify extends PlgFabrik_Cron {
                 sleep(0.1);
             }
 
-            // Send notifications to coordinators
-            $fnums_no_readed = array();
-            $users_to_send = array();
+            // <------ Process to send email to partners ------>
+            $users_fnum_assoc = array();
 
             foreach ($chatrooms as $chatroom) {
+                $group_associated_prog = array();
+                $group_associated_direct = array();
+                $users_associated_direct = array();
+                $users_to_send = array();
+
+                $fnumInfos = $m_files->getFnumInfos($chatroom->fnum);
+
+                // Get users associated to the file by their group and the campaign program
                 $query->clear()
-                    ->select('distinct g.user_id')
+                    ->select('distinct u.id')
+                    ->from($db->quoteName('#__emundus_groups', 'g'))
+                    ->leftJoin($db->quoteName('#__emundus_setup_groups_repeat_course', 'grc') . ' ON ' . $db->quoteName('grc.parent_id') . ' = ' . $db->quoteName('g.group_id'))
+                    ->innerJoin($db->quoteName('#__users', 'u') . ' ON ' . $db->quoteName('u.id') . ' = ' . $db->quoteName('g.user_id'))
+                    ->where($db->quoteName('grc.course') . ' = ' . $db->quote($fnumInfos['training']));
+                $db->setQuery($query);
+                $group_associated_prog = $db->loadColumn();
+
+                // Get users associated to the file by their group directly
+                $query->clear()
+                    ->select('distinct u.id')
                     ->from($db->quoteName('#__emundus_groups', 'g'))
                     ->leftJoin($db->quoteName('#__emundus_group_assoc', 'ga') . ' ON ' . $db->quoteName('ga.group_id') . ' = ' . $db->quoteName('g.group_id'))
+                    ->innerJoin($db->quoteName('#__users', 'u') . ' ON ' . $db->quoteName('u.id') . ' = ' . $db->quoteName('g.user_id'))
                     ->where($db->quoteName('ga.fnum') . ' LIKE ' . $db->quote($chatroom->fnum));
                 $db->setQuery($query);
+                $group_associated_direct = $db->loadColumn();
 
-                $groups_associated = $db->loadColumn();
-                $users_associated = $m_files->getAssessorsByFnums((array)$chatroom->fnum,'uids');
-                foreach ($users_associated as $key => $user_associated){
-                    if(!is_string($user_associated)){
-                        unset($users_associated[$key]);
-                    }
-                }
-                $users_to_send = array_unique(array_merge($groups_associated,$users_associated));
+                // Get users associated to the file directly
+                $query->clear()
+                    ->select('distinct u.id')
+                    ->from($db->quoteName('#__emundus_campaign_candidature', 'cc'))
+                    ->leftJoin($db->quoteName('#__emundus_users_assoc', 'eua') . ' ON ' . $db->quoteName('eua.fnum') . ' LIKE ' . $db->quoteName('cc.fnum'))
+                    ->innerJoin($db->quoteName('#__users', 'u') . ' ON ' . $db->quoteName('u.id') . ' = ' . $db->quoteName('eua.user_id'))
+                    ->where($db->quoteName('cc.fnum') . ' LIKE ' . $db->quote($chatroom->fnum))
+                    ->group($db->quoteName('cc.fnum'));
+                $db->setQuery($query);
+                $users_associated_direct = $db->loadColumn();
 
+                // Merge the two lists
+                $users_to_send = array_unique(array_merge($group_associated_prog,$group_associated_direct,$users_associated_direct));
+
+                // If there are no users associated to the file, get a list of all coordinators
                 if(empty($users_to_send)){
                     $query->clear()
                         ->select('distinct eu.user_id')
@@ -198,26 +227,30 @@ class PlgFabrik_Cronemundusmessengernotify extends PlgFabrik_Cron {
                     $users_to_send = $db->loadColumn();
                 }
 
+                // Check if unread messages from the chatroom come from another user than the ones in our final list
                 $query->clear()
                     ->select('count(m.message_id)')
                     ->from($db->quoteName('#__messages', 'm'))
                     ->leftJoin($db->quoteName('#__emundus_chatroom', 'c') . ' ON ' . $db->quoteName('c.id') . ' = ' . $db->quoteName('m.page'))
                     ->where($db->quoteName('c.fnum') . ' LIKE ' . $db->quote($chatroom->fnum))
-                    ->andWhere($db->quoteName('m.user_id_from') . ' NOT IN (' . implode(',',$users_to_send) . ')')
+                    ->andWhere($db->quoteName('m.user_id_from') . ' = ' . $db->quote($chatroom->applicant_id))
                     ->andWhere($db->quoteName('m.state') . ' = 0');
 
                 $db->setQuery($query);
                 $messages_not_read = $db->loadResult();
 
+                // If an unread message from the chatroom was not sent by a partner from our final list, then it must be the applicant, add the users and the fnum to the list of emails to send
                 if ($messages_not_read > 0) {
-                    if(!in_array($chatroom->fnum,$fnums_no_readed)) {
-                        $fnums_no_readed[] = $chatroom->fnum;
+                    foreach ($users_to_send as $user_fnum_assoc) {
+                        $users_fnum_assoc[$user_fnum_assoc][] = $chatroom->fnum;
                     }
                 }
             }
 
-            if(!empty($fnums_no_readed)) {
-                foreach ($users_to_send as $user_to_send) {
+            // $users_fnum_assoc has for key the user_id of the partner and as a value an array of all the fnums the user must be notified for
+            if(!empty($users_fnum_assoc)) {
+                // So we have to send one email to these partners
+                foreach ($users_fnum_assoc as $user_to_send => $fnums_not_read) {
                     $query->clear()
                         ->select('id, email, name')
                         ->from($db->quoteName('#__users'))
@@ -227,43 +260,35 @@ class PlgFabrik_Cronemundusmessengernotify extends PlgFabrik_Cron {
 
                     $to = $user_info->email;
 
-                    $menu = JMenu::getInstance('site');
                     $menutype = $m_profile->getProfileByApplicant($user_info->id)['menutype'];
-                    $items = $menu->getItems('menutype', $menutype);
 
-                    // We're getting the first link in the user's menu that's from com_emundus
-                    // which is PROBABLY a files/evaluation view, but this does not guarantee it.
-                    $index = 0;
-                    foreach ($items as $k => $item) {
-                        if ($item->component === 'com_emundus') {
-                            $index = $k;
-                            break;
+                    // Get the first link from the partner's menu that corresponds to the file list.
+                    $query->clear()
+                        ->select($db->quoteName('path'))
+                        ->from($db->quoteName('#__menu'))
+                        ->where($db->quoteName('menutype') . ' = ' . $db->quote($menutype))
+                        ->andWhere($db->quoteName('published') . ' = ' . $db->quote(1))
+                        ->andWhere($db->quoteName('link') . ' LIKE ' . $db->quote('%option=com_emundus&view=files%') . ' OR ' . $db->quoteName('link') . ' LIKE ' . $db->quote('%option=com_emundus&view=evaluation%') . ' OR ' . $db->quoteName('link') . ' LIKE ' . $db->quote('%option=com_emundus&view=decision%'))
+                        ->order($db->quoteName('lft'));
+                    $db->setQuery($query);
+                    $userLink = $db->loadResult();
+
+                    // Here we build the fnumList to be added to the email
+                    $fnumList = '<ul>';
+                    // Using the fnums array stocked as a value for each user_id, we can list all the associated fnums with unread messages for each user specifically
+                    foreach ($fnums_not_read as $fnum) {
+                        if (!empty($userLink)) {
+                            $fnumList .= '<li><a href="' . JURI::root() . $userLink . '#' . $fnum . '|open">' . $fnum . '</a></li>';
+                        } else {
+                            $fnumList .= '<li>' . $fnum . '</li>';
                         }
                     }
-
-                    if (JFactory::getConfig()->get('sef') == 1) {
-                        $userLink = $items[$index]->alias;
-                    } else {
-                        $userLink = $items[$index]->link . '&Itemid=' . $items[0]->id;
-                    }
-
-                    $fnumList = '<ul>';
-                    $fnumListNoLink = '<ul>';
-                    foreach ($fnums_no_readed as $fnum) {
-                        $fnumList .= '
-                        <li>
-                            <a href="' . JURI::root() . $userLink . '#' . $fnum . '|open">' . $fnum . '</a>
-                        </li>';
-                        $fnumListNoLink .= '<li>' . $fnum . '</li>';
-                    }
                     $fnumList .= '</ul>';
-                    $fnumListNoLink .= '</ul>';
 
                     $post = array(
                         'FNUMS' => $fnumList,
-                        'FNUMS_LIST' => $fnumListNoLink,
                         'NAME' => $user_info->name,
-                        'SITE_URL' => JURI::base(),
+                        'SITE_URL' => JURI::root(),
                     );
 
                     $c_messages->sendEmailNoFnum($to, $reminder_mail_coordinator_id, $post, $user_info->id);
