c7a9e56cebbdb83d98b3c4698c6d89411756c773
Legendre <jeremy.legendre@emundus.fr>
2023-03-27 11:44:34 +0200

    fix js to expand only targeted element

--- modules/mod_emundus_checklist/tmpl/default.php
+++ modules/mod_emundus_checklist/tmpl/default.php
@@ -30,7 +30,7 @@ if (!empty($checkout_url)){
 
 if($show_preliminary_documents && !empty($preliminary_documents)): ?>
 <div class="mod_emundus_checklist em-mb-24">
-    <div class="em-flex-row em-flex-space-between em-pointer" onclick="expandForms()">
+    <div class="em-flex-row em-flex-space-between em-pointer mod_emundus_checklist_expand" >
         <div class="em-flex-row">
             <p class="em-h6"><?php echo JText::_($preliminary_documents_title) ?></p>
         </div>
@@ -49,7 +49,7 @@ if($show_preliminary_documents && !empty($preliminary_documents)): ?>
 </div>
 <?php endif; ?>
 <div class="mod_emundus_checklist">
-    <div class="em-flex-row em-flex-space-between em-pointer" onclick="expandForms()">
+    <div class="em-flex-row em-flex-space-between em-pointer mod_emundus_checklist_expand">
         <div class="em-flex-row">
             <p class="em-h6"><?php echo JText::_($forms_title) ?></p>
         </div>
@@ -216,9 +216,15 @@ $details_view = array_search('view=details',$url);
         }
     });
 
-    function expandForms(){
-        let content = document.getElementById('mod_emundus_checklist___content');
-        let icon = document.getElementById('mod_emundus_checklist___expand_icon');
+    document.addEventListener('click', function(e) {
+        if (e.target.closest('.mod_emundus_checklist_expand')) {
+            expandForms(e);
+        }
+    });
+
+    function expandForms(e){
+        let content = e.target.closest('.mod_emundus_checklist').querySelector('#mod_emundus_checklist___content');
+        let icon = e.target.closest('.mod_emundus_checklist').querySelector('#mod_emundus_checklist___expand_icon');
 
         if(typeof content !== 'undefined'){
             if(!content.classList.contains('mod_emundus_checklist___content_closed')){
