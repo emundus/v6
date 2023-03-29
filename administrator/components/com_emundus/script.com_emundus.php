8cd1b33ffd107d74e19b78a59f14cf99020cde7d
Legendre <jeremy.legendre@emundus.fr>
2023-03-29 10:40:16 +0200

    fix query to insert new fabrik element display_preliminary_documents

--- administrator/components/com_emundus/script.com_emundus.php
+++ administrator/components/com_emundus/script.com_emundus.php
@@ -77,6 +77,9 @@ class com_emundusInstallerScript
 		}
 
 		if ($this->manifest_cache) {
+			$db    = JFactory::getDbo();
+			$query = $db->getQuery(true);
+
 			# First run condition
 			if (version_compare($cache_version, '1.33.0', '<') || $firstrun) {
 				# Delete emundus sql files in con_admin
@@ -242,9 +245,6 @@ class com_emundusInstallerScript
 			}
 
 			if (version_compare($cache_version, '1.34.0', '<') || $firstrun) {
-				$db    = JFactory::getDbo();
-				$query = $db->getQuery(true);
-
 				EmundusHelperUpdate::addColumn('jos_emundus_setup_campaigns', 'pinned', 'TINYINT', 1);
 				EmundusHelperUpdate::addColumn('jos_emundus_setup_campaigns', 'eval_start_date', 'DATETIME');
 				EmundusHelperUpdate::addColumn('jos_emundus_setup_programmes', 'color', 'VARCHAR', 10);
@@ -974,11 +974,11 @@ if (password_value.match(regex) != null) {
 					$group_id = $db->loadResult();
 
 					if (!empty($group_id)) {
-						$values = ['display_preliminary_documents',$group_id,'yesno','Afficher les documents à télécharger ?',0,'0000-00-00 00:00:00','2023-03-29 07:47:05',62,'sysadmin','0000-00-00 00:00:00',0,0,0,0,0,0,10,0,"",1,1,0,0,0,1,0,0,'{"yesno_default":"0","yesno_icon_yes":"","yesno_icon_no":"","options_per_row":"4","toggle_others":"0","toggle_where":"","show_in_rss_feed":"0","show_label_in_rss_feed":"0","use_as_rss_enclosure":"0","rollover":"","tipseval":"0","tiplocation":"top-left","labelindetails":"0","labelinlist":"0","comment":"","edit_access":"1","edit_access_user":"","view_access":"1","view_access_user":"","list_view_access":"1","encrypt":"0","store_in_db":"1","default_on_copy":"0","can_order":"0","alt_list_heading":"","custom_link":"","custom_link_target":"","custom_link_indetails":"1","use_as_row_class":"0","include_in_list_query":"1","always_render":"0","icon_folder":"0","icon_hovertext":"1","icon_file":"","icon_subdir":"","filter_length":"20","filter_access":"1","full_words_only":"0","filter_required":"0","filter_build_method":"0","filter_groupby":"text","inc_in_adv_search":"1","filter_class":"input-medium","filter_responsive_class":"","tablecss_header_class":"","tablecss_header":"","tablecss_cell_class":"","tablecss_cell":"","sum_on":"0","sum_label":"Sum","sum_access":"1","sum_split":"","avg_on":"0","avg_label":"Average","avg_access":"1","avg_round":"0","avg_split":"","median_on":"0","median_label":"Median","median_access":"1","median_split":"","count_on":"0","count_label":"Count","count_condition":"","count_access":"1","count_split":"","custom_calc_on":"0","custom_calc_label":"Custom","custom_calc_query":"","custom_calc_access":"1","custom_calc_split":"","custom_calc_php":"","validations":[]}'];
+						$values = ['display_preliminary_documents',$group_id,'yesno','Afficher les documents à télécharger ?', '0','0000-00-00 00:00:00','2023-03-29 07:47:05','62','sysadmin','0000-00-00 00:00:00','0','0','0','0','0','0','10','0',"",'1','1','0','0','0','1','0','0','{"yesno_default":"0","yesno_icon_yes":"","yesno_icon_no":"","options_per_row":"4","toggle_others":"0","toggle_where":"","show_in_rss_feed":"0","show_label_in_rss_feed":"0","use_as_rss_enclosure":"0","rollover":"","tipseval":"0","tiplocation":"top-left","labelindetails":"0","labelinlist":"0","comment":"","edit_access":"1","edit_access_user":"","view_access":"1","view_access_user":"","list_view_access":"1","encrypt":"0","store_in_db":"1","default_on_copy":"0","can_order":"0","alt_list_heading":"","custom_link":"","custom_link_target":"","custom_link_indetails":"1","use_as_row_class":"0","include_in_list_query":"1","always_render":"0","icon_folder":"0","icon_hovertext":"1","icon_file":"","icon_subdir":"","filter_length":"20","filter_access":"1","full_words_only":"0","filter_required":"0","filter_build_method":"0","filter_groupby":"text","inc_in_adv_search":"1","filter_class":"input-medium","filter_responsive_class":"","tablecss_header_class":"","tablecss_header":"","tablecss_cell_class":"","tablecss_cell":"","sum_on":"0","sum_label":"Sum","sum_access":"1","sum_split":"","avg_on":"0","avg_label":"Average","avg_access":"1","avg_round":"0","avg_split":"","median_on":"0","median_label":"Median","median_access":"1","median_split":"","count_on":"0","count_label":"Count","count_condition":"","count_access":"1","count_split":"","custom_calc_on":"0","custom_calc_label":"Custom","custom_calc_query":"","custom_calc_access":"1","custom_calc_split":"","custom_calc_php":"","validations":[]}'];
 						$query->clear()
 							->insert($db->quoteName('#__fabrik_elements'))
 							->columns(['name','group_id','plugin','label','checked_out','checked_out_time','created','created_by','created_by_alias','modified','modified_by','width','height','`default`','hidden','eval','ordering','show_in_list_summary','filter_type','filter_exact_match','published','link_to_detail','primary_key','auto_increment','access','use_in_page_title','parent_id','params'])
-							->values(implode(',', $values));
+							->values(implode(',', $db->quote($values)));
 
 						$db->setQuery($query);
 						$db->execute();
