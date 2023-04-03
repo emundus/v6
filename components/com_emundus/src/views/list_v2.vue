bd2d2bf4f80d883a879a3a09dd0826b46a6f16cb
Legendre <jeremy.legendre@emundus.fr>
2023-04-03 10:44:43 +0200

    view modes available

--- components/com_emundus/src/views/list_v2.vue
+++ components/com_emundus/src/views/list_v2.vue
@@ -1,8 +1,8 @@
 <template>
 	<div id="onboarding_list" class="em-w-100">
-		<div class="head">
-			<h2>{{ currentList.title }}</h2>
-			<a v-if="currentAddAction" class="em-pointer" @click="onClickAddAction">{{ translate('COM_EMUNDUS_LIST_ADD_' + type.toUpperCase()) }}</a>
+		<div class="head em-flex-row em-flex-space-between em-mb-8 em-mt-8">
+			<h2 style="margin:0;">{{ currentList.title }}</h2>
+			<a v-if="currentAddAction" class="em-primary-button em-w-auto" @click="onClickAddAction">{{ translate('COM_EMUNDUS_LIST_ADD_' + type.toUpperCase()) }}</a>
 		</div>
 		<div class="list">
 			<nav v-if="currentList.tabs.length > 1">
@@ -20,9 +20,24 @@
 					</li>
 				</ul>
 			</nav>
+			<div id="actions" class="em-flex-row-justify-end em-mt-16 em-mb-16">
+				<div class="view-type">
+					<span
+							v-for="viewTypeOption in viewTypeOptions"
+							:key="viewTypeOption.value"
+							class="material-icons-outlined em-pointer em-ml-8"
+							:class="{'active': viewTypeOption.value === viewType}"
+							@click="changeViewType(viewTypeOption)"
+					>
+						{{ viewTypeOption.icon }}
+					</span>
+				</div>
+			</div>
 			<div>
 				<div v-if="typeof items[selectedListTab] != 'undefined' && items[selectedListTab].length > 0" id="list-items">
-					<table>
+					<table id="list-table" :class="{
+						'blocs': viewType === 'blocs',
+					}">
 						<thead>
 							<tr>
 								<th>Label</th>
@@ -60,29 +75,46 @@ export default {
 	name: "list_v2",
 	data() {
 		return {
-			'lists': {},
-			'type': 'forms',
-			'params': {},
-			'currentList': {
-				'title': '',
-				'tabs': []
-			},
-			'selectedListTab': 0,
-			'items': {},
-			'title': '',
-			'mode': 'table' // table, blocs
+			lists: {},
+			type: 'forms',
+			params: {},
+			currentList: {'title': '', 'tabs': []},
+			selectedListTab: 0,
+			items: {},
+			title: '',
+			viewType: 'table',
+			viewTypeOptions: [
+				{
+					value: 'table',
+					icon: 'dehaze'
+				},
+				{
+					value: 'blocs',
+					icon: 'grid_view'
+				}
+			],
 		}
 	},
 	created() {
 		const data = this.$store.getters['global/datas'];
 		this.params = Object.assign({}, ...Array.from(data).map(({name, value}) => ({[name]: value})));
 		this.type = this.params.type;
+
+		this.viewType = localStorage.getItem('tchooz_view_type/' + document.location.hostname)
+		if(this.viewType === null || typeof this.viewType === 'undefined' || (this.viewType !== 'blocs' && this.viewType !== 'table')){
+			this.viewType = 'blocs';
+			localStorage.setItem('tchooz_view_type/' + document.location.hostname,'blocs');
+		}
+
 		this.initList();
 	},
 	methods: {
+		changeViewType(viewType) {
+			this.viewType = viewType.value;
+			localStorage.setItem('tchooz_view_type/' + document.location.hostname,viewType.value);
+		},
 		initList() {
 			settingsService.getOnboardingLists().then(response => {
-				console.log(response);
 				if (response.data.status) {
 					this.lists = response.data.data;
 
@@ -184,6 +216,45 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss">
+#list-table {
+	&.blocs {
+		border: 0;
 
+		thead {
+			display: none;
+		}
+
+		tbody {
+			tr {
+				display: flex;
+				flex-direction: column;
+				border: 1px solid #ddd;
+				border-radius: 5px;
+				margin-bottom: 10px;
+				padding: 10px;
+
+				td {
+					display: flex;
+					flex-direction: column;
+					justify-content: space-between;
+					padding: 0;
+
+					ul {
+						display: flex;
+						flex-direction: row;
+						justify-content: space-between;
+						padding: 0;
+						margin: 0;
+
+						li {
+							list-style: none;
+							cursor: pointer;
+						}
+					}
+				}
+			}
+		}
+	}
+}
 </style>
