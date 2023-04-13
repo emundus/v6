9974f87bfd459af5882841ad6e03222e267044c3
Legendre <jeremy.legendre@emundus.fr>
2023-04-13 11:17:49 +0200

    programme display nb campaigns associated

--- components/com_emundus/models/programme.php
+++ components/com_emundus/models/programme.php
@@ -515,6 +515,18 @@ class EmundusModelProgramme extends JModelList {
 							'value' => $program->apply_online ? JText::_('JYES') : JText::_('JNO'),
 							'classes' => '',
 							'display' => 'table'
+						],
+						[
+							'key' => JText::_('COM_EMUNDUS_ONBOARD_CAMPAIGNS_ASSOCIATED_TITLE'),
+							'value' => $program->nb_campaigns,
+							'classes' => '',
+							'display' => 'table'
+						],
+						[
+							'key' => '',
+							'value' => $program->nb_campaigns > 1 ? $program->nb_campaigns . ' ' . JText::_('COM_EMUNDUS_ONBOARD_CAMPAIGNS_ASSOCIATED') : $program->nb_campaigns . ' ' . JText::_('COM_EMUNDUS_ONBOARD_CAMPAIGNS_ASSOCIATED_SINGLE'),
+							'classes' => 'em-mt-8 em-mb-8 label label-default em-p-5-12 em-font-weight-600',
+							'display' => 'blocs'
 						]
 					];
 				}
