31e47d2a7d145e22738dc4f6f47c7f47634f445a
Brice <b.hubinet@gmail.com>
2023-04-18 14:03:46 +0200

    Allow accordion in fabrik list

--- components/com_fabrik/views/list/tmpl/emundus_card/default_row.php
+++ components/com_fabrik/views/list/tmpl/emundus_card/default_row.php
@@ -10,28 +10,65 @@
 
 // No direct access
 defined('_JEXEC') or die('Restricted access');
+
+$parent_tag = $this->params->get('note', 'a')
 ?>
-<a id="<?php echo $this->_row->id;?>" class="<?php echo $this->_row->class;?> em-repeat-card-no-padding em-pb-24 em-pointer" href="<?php echo $this->_row->data->fabrik_view_url ?>">
-	<?php foreach ($this->headings as $heading => $label) {
-		$style = empty($this->cellClass[$heading]['style']) ? '' : 'style="'.$this->cellClass[$heading]['style'].'"';
-		?>
-        <?php if (isset($this->_row->data)) : ?>
-            <?php if(strpos($this->_row->data->$heading,'<img') !== false || strpos($this->_row->data->$heading,'fabrik-filetype-webp') !== false) : ?>
-                <?php
-                    $xpath = new DOMXPath(@DOMDocument::loadHTML($this->_row->data->$heading));
-                    $src = $xpath->evaluate("string(//img/@src)");
-                    if(empty($src)){
-	                    $src = $xpath->evaluate("string(//a/@href)");
-                    }
-                ?>
-                <p class="<?php echo $this->cellClass[$heading]['class']?>" <?php echo $style?>>
-                    <div class="fabrikImageBackground" style="background-image: url('<?php echo $src ?>')"></div>
-                </p>
-            <?php else : ?>
-                <p class="em-mt-12 em-p-8-12 <?php echo $this->cellClass[$heading]['class']?>" <?php echo $style?>>
-					<?php echo isset($this->_row->data) ? $this->_row->data->$heading : '';?>
-                </p>
+<?php if($parent_tag != 'details') : ?>
+    <a id="<?php echo $this->_row->id;?>" class="<?php echo $this->_row->class;?> em-repeat-card-no-padding em-pb-24 em-pointer" href="<?php echo $this->_row->data->fabrik_view_url ?>">
+        <?php foreach ($this->headings as $heading => $label) {
+            $style = empty($this->cellClass[$heading]['style']) ? '' : 'style="'.$this->cellClass[$heading]['style'].'"';
+            ?>
+            <?php if (isset($this->_row->data)) : ?>
+                <?php if(strpos($this->_row->data->$heading,'<img') !== false || strpos($this->_row->data->$heading,'fabrik-filetype-webp') !== false) : ?>
+                    <?php
+                        $xpath = new DOMXPath(@DOMDocument::loadHTML($this->_row->data->$heading));
+                        $src = $xpath->evaluate("string(//img/@src)");
+                        if(empty($src)){
+                            $src = $xpath->evaluate("string(//a/@href)");
+                        }
+                    ?>
+                    <p class="<?php echo $this->cellClass[$heading]['class']?>" <?php echo $style?>>
+                        <div class="fabrikImageBackground" style="background-image: url('<?php echo $src ?>')"></div>
+                    </p>
+                <?php else : ?>
+                    <p class="em-mt-12 em-p-8-12 <?php echo $this->cellClass[$heading]['class']?>" <?php echo $style?>>
+                        <?php echo isset($this->_row->data) ? $this->_row->data->$heading : '';?>
+                    </p>
+                <?php endif; ?>
             <?php endif; ?>
-        <?php endif; ?>
-	<?php }?>
-</a>
+        <?php }?>
+    </a>
+<?php else : ?>
+    <details id="<?php echo $this->_row->id;?>" class="<?php echo $this->_row->class;?> em-repeat-card-no-padding em-pb-24 em-pointer" href="<?php echo $this->_row->data->fabrik_view_url ?>">
+        <summary>
+            <?php foreach ($this->headings as $heading => $label) {
+                $style = empty($this->cellClass[$heading]['style']) ? '' : 'style="'.$this->cellClass[$heading]['style'].'"';
+                ?>
+                <?php if (isset($this->_row->data) && strpos($this->cellClass[$heading]['class'],'summary')) : ?>
+                    <p class="em-mt-12 em-p-8-12 <?php echo $this->cellClass[$heading]['class']?>" <?php echo $style?>>
+                        <?php echo isset($this->_row->data) ? $this->_row->data->$heading : '';?>
+                    </p>
+                <?php endif; ?>
+            <?php }?>
+        </summary>
+        <?php foreach ($this->headings as $heading => $label) {
+            $style = empty($this->cellClass[$heading]['style']) ? '' : 'style="'.$this->cellClass[$heading]['style'].'"';
+            ?>
+            <?php if (isset($this->_row->data) && strpos($this->cellClass[$heading]['class'],'summary') === false) : ?>
+                <?php if (strpos($this->headingClass[$heading]['class'],'displayed')) : ?>
+                    <div class="em-mt-12 em-p-8-12">
+                        <label class="em-font-weight-500 em-mb-0-important"><?php echo FText::_($label) ?></label>
+                        <p class="em-mt-8 <?php echo $this->cellClass[$heading]['class']?>" <?php echo $style?>>
+                            <?php echo isset($this->_row->data) ? $this->_row->data->$heading : '';?>
+                        </p>
+                    </div>
+                <?php else : ?>
+                    <p class="em-mt-12 em-p-8-12 <?php echo $this->cellClass[$heading]['class']?>" <?php echo $style?>>
+                        <?php echo isset($this->_row->data) ? $this->_row->data->$heading : '';?>
+                    </p>
+                <?php endif; ?>
+
+            <?php endif; ?>
+        <?php }?>
+    </details>
+<?php endif; ?>
