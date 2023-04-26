2432ff5d914d7bb1e5133d1cae890ebccd9cd504
Legendre <jeremy.legendre@emundus.fr>
2023-04-26 15:53:04 +0200

    unit tests on settings model call getOnboardingLists

--- components/com_emundus/unittest/EmundusModelSettingsTest.php
+++ components/com_emundus/unittest/EmundusModelSettingsTest.php
@@ -16,8 +16,9 @@ define('JPATH_BASE', dirname(__DIR__) . '/../../');
 
 include_once ( JPATH_BASE . 'includes/defines.php' );
 include_once ( JPATH_BASE . 'includes/framework.php' );
-include_once(JPATH_SITE.'/components/com_emundus/unittest/helpers/samples.php');
-include_once (JPATH_SITE . '/components/com_emundus/models/settings.php');
+include_once ( JPATH_SITE . '/components/com_emundus/unittest/helpers/samples.php');
+include_once ( JPATH_SITE . '/components/com_emundus/helpers/cache.php');
+include_once ( JPATH_SITE . '/components/com_emundus/models/settings.php');
 
 jimport('joomla.user.helper');
 jimport( 'joomla.application.application' );
@@ -41,4 +42,31 @@ class EmundusModelSettingsTest extends TestCase
         parent::__construct($name, $data, $dataName);
         $this->m_settings = new EmundusModelsettings;
     }
+
+	public function testFoo()
+	{
+		$foo = true;
+		$this->assertSame(true, $foo);
+	}
+
+	/**
+	 * @return void
+	 * @covers EmundusModelSettings::getOnboardingLists
+	 */
+	public function testgetOnboardingLists() {
+		$config = JFactory::getConfig();
+		$config->set('caching', 1);
+
+		$lists = $this->m_settings->getOnboardingLists();
+		$this->assertNotEmpty($lists);
+
+		// lists should contain at least 3 entries (campaigns, forms and emails)
+		$this->assertGreaterThanOrEqual(3, count($lists));
+
+		// if cache is enabled, lists should be cached
+		$h_cache = new EmundusHelperCache();
+		$lists_cached = $h_cache->get('onboarding_lists');
+		$this->assertNotEmpty($lists_cached);
+		$this->assertSame($lists, $lists_cached);
+	}
 }
