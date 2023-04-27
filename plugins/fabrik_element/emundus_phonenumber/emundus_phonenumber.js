5092c201b49a26404b0b050fd7f5baf5db499f5f
thibaud <thibaud.grignon@emundus.io>
2023-04-27 12:30:04 +0200

    set country default by navigator's language if country default not set and language known

--- plugins/fabrik_element/emundus_phonenumber/emundus_phonenumber.js
+++ plugins/fabrik_element/emundus_phonenumber/emundus_phonenumber.js
@@ -31,11 +31,32 @@ define(['jquery', 'fab/element'], function (jQuery, FbElement) {
         {
             const select = this.element.getElement("select");
             const input = this.element.getElement("input");
-            const allCountry = JSON.parse(atob(select.getAttribute("data-countries"))); // decode base64 + get JSON to array type
+            const allCountries = JSON.parse(atob(select.getAttribute("data-countries"))); // decode base64 + get JSON to array type
 
-            !isNaN(this.options.countrySelected) ? this.ValidatorJS = new ValidatorJS(input, select, allCountry, this.options.countrySelected) : this.ValidatorJS = new ValidatorJS(input, select, allCountry);
+            if (isNaN(this.options.countrySelected)) // if the default country isn't set
+            {
+                this.options.countrySelected = this.getCountryIndexFromIso2(allCountries, navigator.language.substring(3,navigator.language.length));
+                // try to get index from country from navigator's language
+                // work only language format : "en-US" so we get "US"
+            }
+
+            this.ValidatorJS = new ValidatorJS(input, select, allCountries, this.options.countrySelected);
             this.ValidatorJS.setColors(validColor, errorColor, unsupportedColor, defaultColor);
-        }
+        },
+
+        getCountryIndexFromIso2: function (countries, iso2Search)
+        {
+            let index = 0;
+            countries.forEach((element, key) =>
+            {
+                if (element.iso2 === iso2Search)
+                {
+                    index = key;
+                }
+            });
+            return index;
+        },
+
     });
     return window.FbPhoneNumber;
 });
