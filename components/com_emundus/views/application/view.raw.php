ff40638fd2277173d5c560d15dbc1795b71d3829
Bazile Binson <bazile.binson@emundus.fr>
2023-04-28 12:06:03 +0200

    remove filter on crud as filters already exist for the user directly

--- components/com_emundus/views/application/view.raw.php
+++ components/com_emundus/views/application/view.raw.php
@@ -318,7 +318,7 @@ class EmundusViewApplication extends JViewLegacy {
                     if (EmundusHelperAccess::asAccessAction(37, 'r', $this->_user->id, $fnum)) {
                         EmundusModelLogs::log($this->_user->id, (int)substr($fnum, -7), $fnum, 37, 'r', 'COM_EMUNDUS_ACCESS_LOGS_READ');
 
-                        $fileLogs = EmundusModelLogs::getActionsOnFnum($fnum, null, null, ["c", "u", "d"]);
+                        $fileLogs = EmundusModelLogs::getActionsOnFnum($fnum);
 
                         foreach ($fileLogs as $key => $log) {
                             $log->timestamp = EmundusHelperDate::displayDate($log->timestamp);
