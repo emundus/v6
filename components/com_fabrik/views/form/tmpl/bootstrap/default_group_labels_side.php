4492d3637c02aafd4bf7fad20f333e2bb2b31b75
Brice <b.hubinet@gmail.com>
2023-05-05 13:52:52 +0200

    fix: Add css class to element's tips to avoid :not rules

--- components/com_fabrik/views/form/tmpl/bootstrap/default_group_labels_side.php
+++ components/com_fabrik/views/form/tmpl/bootstrap/default_group_labels_side.php
@@ -18,7 +18,7 @@ $element = $this->element;
 
 <div class="controls">
 	<?php if ($this->tipLocation == 'above') : ?>
-		<span class=""><?php echo $element->tipAbove ?></span>
+		<span class="fabrikElementTip fabrikElementTipAbove"><?php echo $element->tipAbove ?></span>
 	<?php endif ?>
 
 	<div class="fabrikElement">
@@ -30,11 +30,11 @@ $element = $this->element;
 	</div>
 
 	<?php if ($this->tipLocation == 'side') : ?>
-		<span class=""><?php echo $element->tipSide ?></span>
+		<span class="fabrikElementTip"><?php echo $element->tipSide ?></span>
 	<?php endif ?>
 
 </div>
 
 <?php if ($this->tipLocation == 'below') :?>
-	<span class=""><?php echo $element->tipBelow ?></span>
+	<span class="fabrikElementTip fabrikElementTipBelow"><?php echo $element->tipBelow ?></span>
 <?php endif ?>
