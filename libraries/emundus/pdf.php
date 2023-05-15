c3212cbf86aa037c4a26053a7d9e7e10ed155d91
Brice <b.hubinet@gmail.com>
2023-05-15 15:25:40 +0200

    Fix generate letters modal

--- libraries/emundus/pdf.php
+++ libraries/emundus/pdf.php
@@ -906,7 +906,7 @@ function application_form_pdf($user_id, $fnum = null, $output = true, $form_post
 
             $allowed_attachments = EmundusHelperAccess::getUserAllowedAttachmentIDs(JFactory::getUser()->id);
 
-            if ($options[0] !== "0" && !$anonymize_data && ($allowed_attachments === true || in_array('10', $allowed_attachments))) {
+            if ($options[0] != "0" && !$anonymize_data && ($allowed_attachments === true || in_array('10', $allowed_attachments))) {
                 $date_submitted = (!empty($item->date_submitted) && strpos($item->date_submitted, '0000') === false) ? EmundusHelperDate::displayDate($item->date_submitted) : JText::_('NOT_SENT');
 
                 // Create an date object
