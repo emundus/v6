21bbd8a25340685a44e796610d2e8611ce944792
thibaud <thibaud.grignon@emundus.io>
2023-05-17 09:20:29 +0200

    add instruction and rollback to the v1

--- plugins/fabrik_element/emundus_phonenumber/assets/js/emundus_phonenumber_ValidatorJS.js
+++ plugins/fabrik_element/emundus_phonenumber/assets/js/emundus_phonenumber_ValidatorJS.js
@@ -26,6 +26,9 @@ class ValidatorJS {
         this.defaultValue = initDefaultValue;
         this.countrySelected = this.allCountry[this.indiceCountry];
 
+        //TODO change phone format depends on selected country
+        // libphonenumber.parsePhoneNumber("+12133734253").format("NATIONAL"); // result : '(213) 373-4253'
+
         this.mustValidate = initDiv.getElementById('hasValidation').checked; // does he have validation ?
         this.isValid.checked = !this.mustValidate; // if yes, set false, if no, set true
 
