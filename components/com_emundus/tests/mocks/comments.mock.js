d8cad7550df8bc521d9c1d092918943fd0b52a39
Legendre <jeremy.legendre@emundus.fr>
2023-05-17 11:38:21 +0200

    patch: getComments response format checked in front + unit tests on deletion

---
+++ components/com_emundus/tests/mocks/comments.mock.js
@@ -0,0 +1,22 @@
+export default {
+    'comments': [
+        {
+            'id': '1',
+            'reason': 'Test',
+            'comment_body': 'Commentaire',
+            'date': '2023-05-16 10:08:46',
+            'user': 'Tchooz Coordinator',
+            'user_id': '95'
+        },
+        {
+            'id': '9',
+            'reason': 'Test',
+            'comment_body': 'test',
+            'date': '2023-05-17 08:46:25',
+            'user': 'Tchooz Coordinator',
+            'user_id': '95'
+        }
+    ],
+    'users': ['95'],
+    'fnums': ['2021061715003400000010000095'],
+};
