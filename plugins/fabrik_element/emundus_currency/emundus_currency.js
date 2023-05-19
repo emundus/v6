2513380802ea86986108530301f74fdf673c7cd5
thibaud <thibaud.grignon@emundus.io>
2023-05-19 14:35:18 +0200

    add imask librairy + prepare php and js files

--- plugins/fabrik_element/emundus_currency/emundus_currency.js
+++ plugins/fabrik_element/emundus_currency/emundus_currency.js
@@ -1,38 +1,14 @@
 
 
-define(['jquery', 'fab/element', 'components/com_fabrik/libs/masked_input/jquery.maskedinput'],
-    function (jQuery, FbElement, Mask) {
+define(['jquery', 'fab/element'],
+    function (jQuery, FbElement) {
 
     window.FbCurrency = new Class({
         Extends: FbElement,
 
-        options: {
-            use_input_mask         : false,
-            input_mask_definitions : '',
-            input_mask_autoclear   : false,
-            geocomplete            : false,
-            mapKey                 : false,
-            language               : ''
-        },
-
         initialize: function (element, options) {
 
             this.parent(element, options);
-        },
-
-        select: function () {
-            var element = this.getElement();
-            if (element) {
-                this.getElement().select();
-            }
-        },
-
-        focus: function () {
-            var element = this.getElement();
-            if (element) {
-                this.getElement().focus();
-            }
-            this.parent();
         },
 
         cloned: function (c) {
