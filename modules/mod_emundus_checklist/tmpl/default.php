59be3dd39c58e21579f083e2c48a931c9fc7610b
Brice <b.hubinet@gmail.com>
2023-05-23 14:12:20 +0200

    Refactor condition

--- modules/mod_emundus_checklist/tmpl/default.php
+++ modules/mod_emundus_checklist/tmpl/default.php
@@ -71,7 +71,7 @@ if($show_preliminary_documents && !empty($preliminary_documents)): ?>
                     $cpt = $db->loadResult();
                     $class = $cpt==0?'need_missing':'need_ok';
                     $step = $index+1;
-                    $form_title = explode(' - ',$form->title)[1] ? explode(' - ',$form->title)[1] : $form->title;
+                    $form_title = explode(' - ',$form->title)[1] ?: $form->title;
                     ?>
                     <div id="mlf<?php echo $form->id; ?>"
                          class="<?php if($form->id == $menuid) echo 'active'?> mod_emundus_checklist_<?php echo $class; ?> mod_emundus_checklist___form_item">
