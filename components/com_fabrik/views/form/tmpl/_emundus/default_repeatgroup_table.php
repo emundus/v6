6871511121d4c99a98387a214ec1eede4f9e206f
Laura <laura.grandin@emundus.io>
2023-05-25 17:28:09 +0200

    "Add group" position

--- components/com_fabrik/views/form/tmpl/_emundus/default_repeatgroup_table.php
+++ components/com_fabrik/views/form/tmpl/_emundus/default_repeatgroup_table.php
@@ -58,7 +58,7 @@ $group = $this->group;
 <?php
     if ($group->editable) : ?>
     <td class="fabrikGroupRepeater">
-        <div class="pull-right">
+        <div class="pull-left">
             <?php
             if ($group->canAddRepeat) :
                 echo $this->addRepeatGroupButtonRow;
