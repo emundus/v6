ec69c8b3be0d3e94434dc25b3d7ecf046556656a
thibaud <thibaud.grignon@emundus.io>
2023-06-01 17:03:59 +0200

    hotfix : separate sql create data_currency, create new sql file for insert value in data_currency

--- administrator/components/com_emundus/sql/updates/mysql/create_data_currency.sql
+++ administrator/components/com_emundus/sql/updates/mysql/create_data_currency.sql
@@ -1,21 +1,15 @@
-CREATE TABLE data_currency LIKE jos_hikashop_currency;
-
-INSERT INTO data_currency SELECT * FROM jos_hikashop_currency;
-
-ALTER TABLE data_currency DROP COLUMN currency_percent_fee;
-ALTER TABLE data_currency DROP COLUMN currency_modified;
-ALTER TABLE data_currency DROP COLUMN currency_locale;
-ALTER TABLE data_currency DROP COLUMN currency_rate;
-
-ALTER TABLE data_currency RENAME COLUMN currency_id TO id;
-ALTER TABLE data_currency RENAME COLUMN currency_name TO name;
-ALTER TABLE data_currency RENAME COLUMN currency_symbol TO symbol;
-ALTER TABLE data_currency RENAME COLUMN currency_code TO iso3;
-ALTER TABLE data_currency RENAME COLUMN currency_format TO format;
-ALTER TABLE data_currency RENAME COLUMN currency_displayed TO displayed;
-ALTER TABLE data_currency RENAME COLUMN currency_published TO published;
-
-UPDATE data_currency SET published = 1 WHERE iso3 = 'EUR';
-UPDATE data_currency SET published = 1 WHERE iso3 = 'USD';
-UPDATE data_currency SET published = 1 WHERE iso3 = 'GBP';
-UPDATE data_currency SET published = 1 WHERE iso3 = 'JPY';
+create table data_currency
+(
+    id        int unsigned auto_increment
+        primary key,
+    symbol    varchar(255)          not null,
+    iso3      varchar(255)          not null,
+    format    char(10) default '%i' not null,
+    name      varchar(255)          not null,
+    published tinyint  default 0    not null,
+    displayed tinyint  default 0    not null,
+    constraint currency_code
+        unique (iso3)
+)
+    engine = MyISAM
+    charset = utf8mb3;
