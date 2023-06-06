117e7e992414565a70bf6744904041bed966bdb2
Legendre <jeremy.legendre@emundus.fr>
2023-06-06 12:15:33 +0200

    yousign multiple files request handling

--- plugins/fabrik_form/php/scripts/emundus-expert_check.php
+++ plugins/fabrik_form/php/scripts/emundus-expert_check.php
@@ -14,40 +14,43 @@ defined('_JEXEC') or die();
  */
 
 $app = JFactory::getApplication();
+$baseurl = JURI::base();
 $jinput = $app->input;
 $key_id = $jinput->get->get('keyid');
-$campaign_id = $jinput->get->get('cid');
-$formid = $jinput->get->get('formid');
 
-$baseurl = JURI::base();
-$db = JFactory::getDBO();
+if (!empty($key_id)) {
+	$campaign_id = $jinput->get->get('cid');
+	$formid = $jinput->get->get('formid');
+	$db = JFactory::getDBO();
 
-$query = $db->getQuery(true);
-$query->select('*')
-	->from($db->quoteName('#__emundus_files_request'))
-	->where($db->quoteName('keyid').' LIKE '.$db->quote($key_id).' AND ('.$db->quoteName('uploaded').' = 0 OR '.$db->quoteName('uploaded').' IS NULL)');
-$db->setQuery($query);
-$obj = $db->loadObject();
+	$query = $db->getQuery(true);
+	$query->select('*')
+		->from($db->quoteName('#__emundus_files_request'))
+		->where($db->quoteName('keyid').' LIKE '.$db->quote($key_id).' AND ('.$db->quoteName('uploaded').' = 0 OR '.$db->quoteName('uploaded').' IS NULL)');
+	$db->setQuery($query);
+	$obj = $db->loadObject();
 
-if (isset($obj)) {
+	if (isset($obj)) {
+		$s = $jinput->get->getInt('s');
+		if ($s !== 1) {
+			$link_upload = $baseurl.'index.php?option=com_fabrik&view=form&formid='.$formid.'&jos_emundus_files_request___attachment_id='.$obj->attachment_id.'&jos_emundus_files_request___campaign_id='.$obj->campaign_id.'&keyid='.$key_id.'&cid='.$campaign_id.'&rowid='.$obj->id.'&s=1';
+			$app->redirect($link_upload);
+		} else {
 
-	$s = $jinput->get->getInt('s');
-	if ($s !== 1) {
+			$up_attachment = $jinput->get('jos_emundus_files_request___attachment_id');
+			$attachment_id = !empty($up_attachment)?$jinput->get('jos_emundus_files_request___attachment_id'):$jinput->get->get('jos_emundus_files_request___attachment_id');
 
-		$link_upload = $baseurl.'index.php?option=com_fabrik&view=form&formid='.$formid.'&jos_emundus_files_request___attachment_id='.$obj->attachment_id.'&jos_emundus_files_request___campaign_id='.$obj->campaign_id.'&keyid='.$key_id.'&cid='.$campaign_id.'&rowid='.$obj->id.'&s=1';
-		$app->redirect($link_upload);
+			if (empty($key_id) || empty($attachment_id) || $attachment_id != $obj->attachment_id) {
+				$app->redirect(JURI::base());
+				throw new Exception(JText::_('ERROR: please try again'), 500);
+			}
+		}
 
 	} else {
-
-		$up_attachment = $jinput->get('jos_emundus_files_request___attachment_id');
-		$attachment_id = !empty($up_attachment)?$jinput->get('jos_emundus_files_request___attachment_id'):$jinput->get->get('jos_emundus_files_request___attachment_id');
-
-		if (empty($key_id) || empty($attachment_id) || $attachment_id != $obj->attachment_id) {
-			$app->redirect(JURI::base());
-			throw new Exception(JText::_('ERROR: please try again'), 500);
-		}
+		$app->enqueueMessage(JText::_('PLEASE_LOGIN'), 'message');
+		$app->redirect($baseurl.'index.php?option=com_users&view=login');
+		exit();
 	}
-
 } else {
 	$app->enqueueMessage(JText::_('PLEASE_LOGIN'), 'message');
 	$app->redirect($baseurl.'index.php?option=com_users&view=login');
