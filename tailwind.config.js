c19552c396e43c19b5a20ead6b6884d52f16e5a8
Brice <b.hubinet@gmail.com>
2023-07-07 17:20:42 +0200

    Style radio button

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@
 const plugin = require('tailwindcss/plugin')
 
 module.exports = {
+    prefix: 'em-',
     content: [
         "./templates/g5_helium/html/**/*.{html,js,php}",
         "./modules/**/*.{html,js,php}",
@@ -26,7 +27,7 @@ module.exports = {
     plugins: [
         plugin(function ({ addComponents, theme }) {
             addComponents({
-                '.em-default-title-1': {
+                '.default-title-1': {
                     color: 'var(--em-default-title-color-1)',
                     fontFamily: '$font-family-title',
                     fontSize: '24px',
