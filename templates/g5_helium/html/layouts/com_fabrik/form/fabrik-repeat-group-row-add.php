25a4460c9c31d8430b295866070def4f739a3392
Brice <b.hubinet@gmail.com>
2023-07-19 15:57:10 +0200

    Fix repeat group table

---
+++ templates/g5_helium/html/layouts/com_fabrik/form/fabrik-repeat-group-row-add.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Repeat group add button for table format
+ */
+
+defined('JPATH_BASE') or die;
+
+$d = $displayData;
+?>
+
+<a class="addGroup em-flex-row em-pointer"  href="#">
+    <span class="material-icons-outlined em-blue-500-color em-mr-8">add</span>
+    <p class="em-blue-500-color"><?php echo FText::_('COM_FABRIK_ADD_GROUP');?></p>
+</a>
