52cd00315290d2520eda80e665d34296c13c4e22
Legendre <jeremy.legendre@emundus.fr>
2023-07-24 14:02:42 +0200

    activite etu generate letter js

--- modules/mod_emundus_user_dropdown/tmpl/activitesetudiantes.php
+++ modules/mod_emundus_user_dropdown/tmpl/activitesetudiantes.php
@@ -19,32 +19,43 @@ require JModuleHelper::getLayoutPath('mod_emundus_user_dropdown', 'tchooz');
     }
 
     function generateLetter() {
-        document.getElementsByClassName('em-page-loader')[0].style.display = 'block';
-        let current_fnum = document.getElementById("current_fnum").value;
+        const loader = document.getElementsByClassName('em-page-loader');
 
-        fetch('index.php?option=com_emundus&controller=files&task=generateletter', {
-            method: 'POST',
-            body: JSON.stringify({
-                fnums: current_fnum,
-                ids_tmpl: [31],
-                cansee: 1,
-                showMode: 2,
-                mergeMode: 0,
-            }),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        }).then(response => {
-            return response.json();
-        }).then(data => {
-            if (data.status &&  data.data.files.length > 0) {
-                let file = data.data.files[0].url + data.data.files[0].filename;
-                download(file,'carte_acces.pdf');
-            }
-            document.getElementsByClassName('em-page-loader')[0].style.display = 'none';
-        }).catch(err => {
-            console.error(err);
-            document.getElementsByClassName('em-page-loader')[0].style.display = 'none';
-        });
+        if (loader) {
+            loader[0].style.display = 'block';
+        }
+        const current_fnum_el = document.getElementById('current_fnum');
+        const current_fnum = current_fnum_el ? current_fnum_el.value : false;
+
+        if (current_fnum) {
+            let formData = new FormData();
+            formData.append('fnums', current_fnum);
+            formData.append('ids_tmpl', [31]);
+            formData.append('cansee', 1);
+            formData.append('showMode', 2);
+            formData.append('mergeMode', 0);
+
+            fetch('index.php?option=com_emundus&controller=files&task=generateletter', {
+                method: 'POST',
+                body: formData
+            }).then(response => {
+                return response.json();
+            }).then(data => {
+                if (data.status &&  data.data.files.length > 0) {
+                    let file = data.data.files[0].url + data.data.files[0].filename;
+                    download(file,'carte_acces.pdf');
+                }
+
+                if (loader) {
+                    loader[0].style.display = 'none';
+                }
+            }).catch(err => {
+                console.error(err);
+
+                if (loader) {
+                    loader[0].style.display = 'none';
+                }
+            });
+        }
     }
 </script>
