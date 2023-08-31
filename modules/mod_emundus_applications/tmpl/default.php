574645e39c96a0ea9a88cc78ec3cd2342400c62d
Laura <laura.grandin@emundus.io>
2023-08-31 09:19:17 +0200

    update of folder icon in "my application files" module

--- modules/mod_emundus_applications/tmpl/default.php
+++ modules/mod_emundus_applications/tmpl/default.php
@@ -81,7 +81,7 @@ defined('_JEXEC') or die;
                 <div class="col-xs-12 col-md-6 main-page-file-info">
                     <p class="em-tags-display"><?= $file_tags_display; ?></i></p>
                     <a class="btn btn-warning" href="<?php echo JRoute::_($first_page_url); ?>" role="button">
-                        <i class="folder open outline icon"></i> <?= ($is_admission) ? JText::_('MOD_EMUNDUS_APPLICATIONS_OPEN_ADMISSION') : JText::_('MOD_EMUNDUS_APPLICATIONS_OPEN_APPLICATION'); ?>
+                        <i class="icon-folder-open"></i> <?= ($is_admission) ? JText::_('MOD_EMUNDUS_APPLICATIONS_OPEN_ADMISSION') : JText::_('MOD_EMUNDUS_APPLICATIONS_OPEN_APPLICATION'); ?>
                     </a>
 
                     <?php if (!empty($attachments) && ((int) ($attachments[$application->fnum]) >= 100 && (int) ($forms[$application->fnum]) >= 100 && in_array($application->status, $status_for_send) && !$is_dead_line_passed) || in_array($user->id, $applicants)) : ?>
