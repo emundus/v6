2f7f8473788475919389100881774409dd04b04e
Legendre <jeremy.legendre@emundus.fr>
2023-09-01 16:53:36 +0200

    add marker on click, save lat long in input field

--- plugins/fabrik_element/emundus_geolocalisation/emundus_geolocalisation.js
+++ plugins/fabrik_element/emundus_geolocalisation/emundus_geolocalisation.js
@@ -4,22 +4,30 @@ define(['jquery', 'fab/element'], function (jQuery, FbElement) {
             Extends: FbElement,
 
             mapContainer: null,
+            mapLayer: null,
 
             initialize: function (element, options) {
-                console.log(options);
-                console.log(element);
-
                 this.setPlugin('emundus_geolocalisation');
                 this.parent(element, options);
 
                 if (typeof L !== 'undefined' && L !== null) {
-                    this.mapContainer = L.map(element).setView([48.856, 2.352], 13);
+                    this.mapContainer = L.map(element + '___map_container').setView([48.856, 2.352], 13);
 
                     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
                         maxZoom: 19,
                         attribution: '© OpenStreetMap'
                     }).addTo(this.mapContainer);
+
+                    this.mapLayer = L.layerGroup().addTo(this.mapContainer);
+
+                    this.mapContainer.on('click', this.onClickMap.bind(this));
                 }
+            },
+
+            onClickMap: function(e) {
+                this.mapLayer.clearLayers();
+                this.mapLayer.addLayer(L.marker(e.latlng));
+                this.update(e.latlng.lat + ',' + e.latlng.lng);
             }
         });
 
