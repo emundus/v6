1a651674961ca5ebbc506b415d5452627fc2a6e2
Legendre <jeremy.legendre@emundus.fr>
2023-09-04 10:35:58 +0200

    hide geoloc lat lng input

--- plugins/fabrik_element/emundus_geolocalisation/layouts/fabrik-element-emundus_geolocalisation-form.php
+++ plugins/fabrik_element/emundus_geolocalisation/layouts/fabrik-element-emundus_geolocalisation-form.php
@@ -2,6 +2,7 @@
 
 defined('JPATH_BASE') or die;
 
+$displayData->attributes['class'] .= ' fabrikEmundusGeolocalisationInput';
 ?>
 <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
 <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
@@ -9,7 +10,7 @@ defined('JPATH_BASE') or die;
 <div id="<?php echo $displayData->attributes['name']; ?>___map_container" class="fabrikSubElementContainer fabrikEmundusGeolocalisation">
 </div>
 
-<input type="text"
+<input type="text" readonly
     <?php foreach ($displayData->attributes as $key => $value) :
         echo $key . '="' . $value . '" ';
     endforeach;
