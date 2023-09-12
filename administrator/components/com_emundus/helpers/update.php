4bf045f2b0a37fd24129e6f3972e64a83596c532
Laura <laura.grandin@emundus.io>
2023-09-12 14:47:52 +0200

    fix: Reducing the size of checkboxes

--- administrator/components/com_emundus/helpers/update.php
+++ administrator/components/com_emundus/helpers/update.php
@@ -3227,7 +3227,7 @@ class EmundusHelperUpdate
 				'checkbox-border-width' => '0px',
 				'checkbox-border-color' => '#a7a7a7',
 				'checkbox-padding' => '12px',
-				'checkbox-scale' => '1.7',
+				'checkbox-scale' => '1.3',
 				'checkbox-input-margin' => '12px',
 				'checkbox-color-checked' => '#007bff',
 				'checkbox-border-color-error' => '#F04437',
