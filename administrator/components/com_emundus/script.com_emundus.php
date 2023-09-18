3f0de020048d4f766391c452d4f5f89327ea09c5
Brice <b.hubinet@gmail.com>
2023-09-18 14:31:39 +0200

    Fix password standard with full parameters

--- administrator/components/com_emundus/script.com_emundus.php
+++ administrator/components/com_emundus/script.com_emundus.php
@@ -3013,8 +3013,6 @@ spanShowPassword.addEventListener(&#039;click&#039;, function () {
 					}
 				}
 
-				EmundusHelperUpdate::updateComponentParameter('com_users', 'minimum_length', 12);
-				EmundusHelperUpdate::updateComponentParameter('com_users', 'minimum_lowercase', 1);
 				EmundusHelperUpdate::updateComponentParameter('com_fabrik', 'use_fabrikdebug', 1);
 
 				EmundusHelperUpdate::installExtension('plg_fabrik_validationrule_checkpassword','checkpassword','{"name":"plg_fabrik_validationrule_checkpassword","type":"plugin","creationDate":"September 2023","author":"eMundus","copyright":"Copyright (C) 2015-2023 eMundus - All rights reserved.","authorEmail":"dev@emundus.io","authorUrl":"www.emundus.fr","version":"3.10","description":"PLG_VALIDATIONRULE_CHECKPASSWORD_DESCRIPTION","group":"","filename":"checkpassword"}','plugin',1,'fabrik_validationrule');
@@ -3166,6 +3164,16 @@ spanShowPassword.addEventListener(&#039;click&#039;, function () {
 					$db->setQuery($query);
 					$db->execute();
 				}
+			}
+
+			if (version_compare($cache_version, '1.37.2', '<=') || $firstrun) {
+				EmundusHelperUpdate::updateComponentParameter('com_users', 'minimum_length', 12);
+				EmundusHelperUpdate::updateComponentParameter('com_users', 'minimum_integers', 1);
+				EmundusHelperUpdate::updateComponentParameter('com_users', 'minimum_symbols', 1);
+				EmundusHelperUpdate::updateComponentParameter('com_users', 'minimum_uppercase', 1);
+				EmundusHelperUpdate::updateComponentParameter('com_users', 'minimum_lowercase', 1);
+				EmundusHelperUpdate::updateComponentParameter('com_users', 'reset_count', 5);
+				EmundusHelperUpdate::updateComponentParameter('com_users', 'reset_time', 1);
 			}
 		}
 
