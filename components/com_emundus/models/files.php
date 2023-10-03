4eeabae83d700e2fb65311c24e474dfcce8efe78
Bazile Binson <bazile.binson@emundus.fr>
2023-10-03 09:43:45 +0200

    fix: check anonymization on group before gettting assigned attachments

--- components/com_emundus/models/files.php
+++ components/com_emundus/models/files.php
@@ -3934,24 +3934,35 @@ class EmundusModelFiles extends JModelLegacy
 
         $result = [];
         foreach ($group_ids as $group_id) {
-            $query
-                ->clear()
-                ->select($db->quoteName('attachment_id_link'))
-                ->from($db->quoteName('#__emundus_setup_groups_repeat_attachment_id_link'))
-                ->where($db->quoteName('parent_id').' = '.$group_id);
+            $query->clear()
+                ->select($db->quoteName('anonymize'))
+                ->from($db->quoteName('#__emundus_setup_groups'))
+                ->where($db->quoteName('id').' = '.$group_id);
             $db->setQuery($query);
+            $anonymize = $db->loadResult();
 
-            try {
-                $attachments = $db->loadColumn();
+            // If the group has no anonymization, then the user can see all the attachments
+            if ($anonymize == 0) {
+                return true;
+            } else {
+                $query->clear()
+                    ->select($db->quoteName('attachment_id_link'))
+                    ->from($db->quoteName('#__emundus_setup_groups_repeat_attachment_id_link'))
+                    ->where($db->quoteName('parent_id').' = '.$group_id);
+                $db->setQuery($query);
 
-                // In the case of a group having no assigned Fabrik groups, it can get them all.
-                if (empty($attachments)) {
-                    return true;
+                try {
+                    $attachments = $db->loadColumn();
+
+                    // In the case of a group having no assigned Fabrik groups, it can get them all.
+                    if (empty($attachments)) {
+                        return true;
+                    }
+
+                    $result = array_merge($result, $attachments);
+                } catch (Exception $e) {
+                    return false;
                 }
-
-                $result = array_merge($result, $attachments);
-            } catch (Exception $e) {
-                return false;
             }
         }
 
