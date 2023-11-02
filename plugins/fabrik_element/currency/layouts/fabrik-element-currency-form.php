df18c08d205ebee2f74f136fb57ad1e4c2831632
thibaud <thibaud.grignon@emundus.io>
2023-11-02 16:09:58 +0100

    fix: unexpected arrow in select and hide unexpected element in formBuilder

--- plugins/fabrik_element/currency/layouts/fabrik-element-currency-form.php
+++ plugins/fabrik_element/currency/layouts/fabrik-element-currency-form.php
@@ -27,6 +27,6 @@ $doc->addScript(JURI::root() . "plugins/fabrik_element/currency/assets/js/emundu
         <?php endforeach; ?>
     </select>
 
-    <input id="currency_rowInputValue" name="<?php echo $displayData->attributes['name'].'[rowInputValueFront]'; ?>" class="fabrikinput com_emundus_hidden">
-    <input id="currency_displayiso3" name="<?php echo $displayData->attributes['name'].'[currency_displayiso3]'; ?>" value="<?php echo $displayData->displayiso3; ?>" class="fabrikinput com_emundus_hidden">
+    <input id="currency_rowInputValue" name="<?php echo $displayData->attributes['name'].'[rowInputValueFront]'; ?>" type="hidden" class="fabrikinput com_emundus_hidden">
+    <input id="currency_displayiso3" name="<?php echo $displayData->attributes['name'].'[currency_displayiso3]'; ?>" type="hidden" value="<?php echo $displayData->displayiso3; ?>" class="fabrikinput com_emundus_hidden">
 </div>
