db5cd3c1e283292e4e1888211bdff2db6be2289c
Brice <b.hubinet@gmail.com>
2023-11-07 09:18:16 +0100

    Revert dev merge

--- components/com_emundus/views/evaluation/view.raw.php
+++ components/com_emundus/views/evaluation/view.raw.php
@@ -154,7 +154,8 @@ class EmundusViewEvaluation extends JViewLegacy
 
 				$fnumArray = array();
 
-			    $this->assignRef('form_url_edit', '');
+				$form_url_edit = '';
+				$this->assignRef('form_url_edit', $form_url_edit);
 
 				if (!empty($users)) {
 
