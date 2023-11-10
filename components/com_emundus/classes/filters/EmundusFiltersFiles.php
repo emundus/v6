238eb8598a6936da74d505f987fab2beee1dcc6e
Legendre <jeremy.legendre@emundus.fr>
2023-11-10 10:39:00 +0100

    fix: when using cache associated elements were incomplete because of var replacement

--- components/com_emundus/classes/filters/EmundusFiltersFiles.php
+++ components/com_emundus/classes/filters/EmundusFiltersFiles.php
@@ -204,8 +204,8 @@ class EmundusFiltersFiles extends EmundusFilters
 							$elements_by_form[$element['element_form_id']][] = $element;
 						}
 
-						foreach($elements_by_form as $form_id => $elements) {
-							$this->h_cache->set('elements_from_form_' . $form_id, $elements);
+						foreach($elements_by_form as $form_id => $element_by_form) {
+							$this->h_cache->set('elements_from_form_' . $form_id, $element_by_form);
 						}
 					}
 				} catch (Exception $e) {
