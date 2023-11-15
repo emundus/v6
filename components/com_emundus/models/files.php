8cf655ebace53eb2d02c37483536522be8ac2611
Legendre <jeremy.legendre@emundus.fr>
2023-11-15 15:27:42 +0100

    fix: export xlsx method 1 specificity on overflow data

--- components/com_emundus/models/files.php
+++ components/com_emundus/models/files.php
@@ -2873,7 +2873,21 @@ class EmundusModelFiles extends JModelLegacy
 					// it means that we have repeated rows, so we need to retrieve last row all entries, because it may be incomplete (chunked by the limit)
 					$last_row = array_pop($rows);
 					$last_row_data = $this->getFnumArray2([$last_row['fnum']], $elements, $start, 0, $method);
-					$data[$last_row['fnum']] = $last_row_data[$last_row['fnum']];
+
+					if ($method !== 1) {
+						$data[$last_row['fnum']] = $last_row_data[$last_row['fnum']];
+					} else {
+						// in methode 1, data is not an associative array, so we need to do some stuff
+						// remove from $data all rows with the same fnum
+						foreach($data as $d_key => $row) {
+							if ($row['fnum'] === $last_row['fnum']) {
+								unset($data[$d_key]);
+							}
+						}
+
+						// add the last row array to the data
+						$data = array_merge($data, $last_row_data);
+					}
 				}
 	        }
         }
