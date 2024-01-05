e06ac0f697d41745f60e4686a1d1fa4c9b567bfe
Brice <b.hubinet@gmail.com>
2024-01-05 15:06:39 +0100

    Increase addpipe messages

--- components/com_emundus/views/checklist/tmpl/default.php
+++ components/com_emundus/views/checklist/tmpl/default.php
@@ -261,8 +261,8 @@ if (!empty($this->custom_title)) :?>
                         }
             
                         recorderInserted.onUploadDone = function(recorderId, streamName, streamDuration, audioCodec, videoCodec, fileType, audioOnly, location){
-                            //var args = Array.prototype.slice.call(arguments);
-                            //__log("onUploadDone("+args.join(\', \')+")");
+                            document.querySelector(".em-page-loader").style.display = "block";    
+                            __log("onUploadDone("+args.join(\', \')+")");
                             recorderInserted.save();
                         }
             
@@ -318,6 +318,7 @@ if (!empty($this->custom_title)) :?>
             
                         recorderInserted.onDesktopVideoUploadStarted = function(recorderId, filename, filetype, audioOnly){
                             //var args = Array.prototype.slice.call(arguments);
+                            document.querySelector(".em-page-loader").style.display = "block";
                             __log("'.JText::_('VIDEO_INSTR_UPLOADING').'");
                         }
             
@@ -338,6 +339,7 @@ if (!empty($this->custom_title)) :?>
                         //MOBILE EVENTS API
                         recorderInserted.onVideoUploadStarted = function(recorderId, filename, filetype, audioOnly){
                             //var args = Array.prototype.slice.call(arguments);
+                            document.querySelector(".em-page-loader").style.display = "block";
                             __log("'.JText::_('VIDEO_INSTR_RECORD_SAVED').'");
                         }
     
@@ -810,7 +812,7 @@ $(document).ready(() => {
 
 //ADDPIPE check if video is uploaded. If yes, reaload page
 function is_file_uploaded(fnum, aid, applicant_id) {
-    setInterval(function(){
+    let is_file_uploaded_timer = setInterval(function(){
 
         $.ajax({
             type: 'POST',
@@ -822,14 +824,43 @@ function is_file_uploaded(fnum, aid, applicant_id) {
                 applicant_id: applicant_id
             }),
             success: function(result) {
-                //console.log(result.status + " :: " + result.fnum + " :: " + result.aid + " :: " + result.applicant_id + " :: " + result.user_id + " :: " + result.user_fnum + " :: " + result.query);
+                //console.log(result.status + " :: " + result.fnum + " :: " + result.aid + " :: " + result.applicant_id + " :: " + result.user_id + " :: " + result.user_fnum + " :: " + result.query)
                 if (result.status) {
-                    clearInterval();
+                    document.querySelector(".em-page-loader").style.display = "none";
+
+                    clearInterval(is_file_uploaded_timer);
+
+                    Swal.fire({
+                        position: 'top',
+                        type: 'success',
+                        title: "<?= JText::_('COM_EMUNDUS_UPLOAD_SUCCESS'); ?>",
+                        showCancelButton: false,
+                        showConfirmButton: false,
+                        customClass: {
+                            title: 'em-swal-title'
+                        },
+                        timer: 3000
+                    }).then(() => {
                     window.location.reload(true);
+                    });
                 }
             },
             error: function(jqXHR) {
                 console.log("ERROR: "+jqXHR.responseText);
+
+                Swal.fire({
+                    position: 'top',
+                    type: 'error',
+                    title: "<?= JText::_('COM_EMUNDUS_ERROR_OCCURED'); ?>",
+                    showCancelButton: false,
+                    showConfirmButton: false,
+                    customClass: {
+                        title: 'em-swal-title'
+                    },
+                    timer: 3000
+                }).then(() => {
+                    window.location.reload(true);
+                });
             }
         });
     }, 500);
