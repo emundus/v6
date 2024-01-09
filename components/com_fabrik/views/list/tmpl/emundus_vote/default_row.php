42dd6dee1d0b12b386c738dcbaa4aa2189f9d4f3
Laura <laura.grandin@emundus.io>
2024-01-09 15:53:22 +0100

    style improvements

--- components/com_fabrik/views/list/tmpl/emundus_vote/default_row.php
+++ components/com_fabrik/views/list/tmpl/emundus_vote/default_row.php
@@ -81,7 +81,7 @@ foreach ($votes as $vote) {
 
         <div class="superior-infos-container">
             <?php if (!empty($gallery->title)) : ?>
-                <h2 class="line-clamp-2 h-14 em-applicant-title-font em-font-weight-700">
+                <h2 class="line-clamp-2 mb-2 h-14 em-applicant-title-font em-font-weight-700">
                     <?php echo isset($this->_row->data) ? $this->_row->data->{$gallery->title} : ''; ?>
                 </h2>
             <?php endif; ?>
