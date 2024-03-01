fb67065aeb6719336214ae8ccb6ba3279ead82aa
Brice <b.hubinet@gmail.com>
2024-03-01 17:34:53 +0100

    Add missing countries in mask render

--- plugins/fabrik_element/iban/iban.js
+++ plugins/fabrik_element/iban/iban.js
@@ -59,6 +59,59 @@ define(['jquery', 'fab/element'], function (jQuery, FbElement) {
             );
         },
 
+        isValidIBANNumber: function () {
+            var input = this.element.get('value');
+
+            var CODE_LENGTHS = {
+                AD: 24, AE: 23, AT: 20, AZ: 28, BA: 20, BE: 16, BG: 22, BH: 22, BR: 29,
+                CH: 21, CR: 21, CY: 28, CZ: 24, DE: 22, DK: 18, DO: 28, EE: 20, ES: 24,
+                FI: 18, FO: 18, FR: 27, GB: 22, GI: 23, GL: 18, GR: 27, GT: 28, HR: 21,
+                HU: 28, IE: 22, IL: 23, IS: 26, IT: 27, JO: 30, KW: 30, KZ: 20, LB: 28,
+                LI: 21, LT: 20, LU: 20, LV: 21, MC: 27, MD: 24, ME: 22, MK: 19, MR: 27,
+                MT: 31, MU: 30, NL: 18, NO: 15, PK: 24, PL: 28, PS: 29, PT: 25, QA: 29,
+                RO: 24, RS: 22, SA: 24, SE: 24, SI: 19, SK: 24, SM: 27, TN: 24, TR: 26,
+                AL: 28, BY: 28, CR: 22, EG: 29, GE: 22, IQ: 23, LC: 32, SC: 31, ST: 25,
+                SV: 28, TL: 23, UA: 29, VA: 22, VG: 24, XK: 20
+            };
+            var iban = String(input).toUpperCase().replace(/[^A-Z0-9]/g, ''), // keep only alphanumeric characters
+                code = iban.match(/^([A-Z]{2})(\d{2})([A-Z\d]+)$/), // match and capture (1) the country code, (2) the check digits, and (3) the rest
+                digits;
+            // check syntax and length
+            if (!code || iban.length !== CODE_LENGTHS[code[1]]) {
+                return false;
+            }
+            // rearrange country code and check digits, and convert chars to ints
+            digits = (code[3] + code[1] + code[2]).replace(/[A-Z]/g, function (letter) {
+                return letter.charCodeAt(0) - 55;
+            });
+
+            // final check
+            return this.mod97(digits) === 1;
+        },
+
+        mod97: function (string) {
+            var checksum = string.slice(0, 2), fragment;
+            for (var offset = 2; offset < string.length; offset += 7) {
+                fragment = String(checksum) + string.substring(offset, offset + 7);
+                checksum = parseInt(fragment, 10) % 97;
+            }
+            return checksum;
+        },
+
+        setError: function (msg) {
+            var parentofSelected = this.element.parentNode.parentNode.parentNode; // gives the parent DIV
+
+            var children = parentofSelected.childNodes;
+            for (var i = 0; i < children.length; i++) {
+                if (children[i].classList) {
+                    if (children[i].classList.contains('fabrikErrorMessage')) {
+                        children[i].innerHTML = msg;
+                        break;
+                    }
+                }
+            }
+        },
+
         initDynamicMask: function () {
             return [
                 {
@@ -116,65 +169,120 @@ define(['jquery', 'fab/element'], function (jQuery, FbElement) {
                     country: 'République thèque'
                 },
                 {
+                    mask: 'aa00 **** **** **** **** **',
+                    startsWith: 'DE',
+                    lazy: false,
+                    country: 'Allemagne'
+                },
+                {
+                    mask: 'aa00 **** **** **** **',
+                    startsWith: 'DK',
+                    lazy: false,
+                    country: 'Danemark'
+                },
+                {
+                    mask: 'aa00 **** **** **** ****',
+                    startsWith: 'EE',
+                    lazy: false,
+                    country: 'Estonie'
+                },
+                {
+                    mask: 'aa00 **** **** **** **** ****',
+                    startsWith: 'ES',
+                    lazy: false,
+                    country: 'Espagne'
+                },
+                {
+                    mask: 'aa00 **** **** **** **',
+                    startsWith: 'FI',
+                    lazy: false,
+                    country: 'Finlande'
+                },
+                {
+                    mask: 'aa00 **** **** **** **',
+                    startsWith: 'FO',
+                    lazy: false,
+                    country: 'Îles Féroé'
+                },
+                {
+                    mask: 'aa00 **** **** **** **** **',
+                    startsWith: 'GB',
+                    lazy: false,
+                    country: 'Grande-Bretagne'
+                },
+                {
+                    mask: 'aa00 **** **** **** **** ***',
+                    startsWith: 'GI',
+                    lazy: false,
+                    country: 'Gibraltar'
+                },
+                {
+                    mask: 'aa00 **** **** **** **',
+                    startsWith: 'GL',
+                    lazy: false,
+                    country: 'Gröenland'
+                },
+                {
+                    mask: 'aa00 **** **** **** **** **** ***',
+                    startsWith: 'GR',
+                    lazy: false,
+                    country: 'Grèce'
+                },
+                {
+                    mask: 'aa00 **** **** **** **** *',
+                    startsWith: 'HR',
+                    lazy: false,
+                    country: 'Croatie'
+                },
+                {
+                    mask: 'aa00 **** **** **** **** **** ****',
+                    startsWith: 'HU',
+                    lazy: false,
+                    country: 'Hongrie'
+                },
+                {
+                    mask: 'aa00 **** **** **** **** **',
+                    startsWith: 'IE',
+                    lazy: false,
+                    country: 'Irlande'
+                },
+                {
+                    mask: 'aa00 **** **** **** **** **** **',
+                    startsWith: 'IS',
+                    lazy: false,
+                    country: 'Islande'
+                },
+                {
+                    mask: 'aa00 **** **** **** **** **** ***',
+                    startsWith: 'IT',
+                    lazy: false,
+                    country: 'Italie'
+                },
+                {
+                    mask: 'aa00 **** **** **** **** *',
+                    startsWith: 'LI',
+                    lazy: false,
+                    country: 'Liechtenstein'
+                },
+                {
+                    mask: 'aa00 **** **** **** ****',
+                    startsWith: 'LT',
+                    lazy: false,
+                    country: 'Lituanie'
+                },
+                {
+                    mask: 'aa00 **** **** **** ****',
+                    startsWith: 'LU',
+                    lazy: false,
+                    country: 'Luxembourg'
+                },
+                {
                     mask: 'aa00 **** **** **** **** **** ***',
                     startsWith: '',
                     country: ''
                 }
             ]
         },
-
-        isValidIBANNumber: function () {
-            var input = this.element.get('value');
-
-            var CODE_LENGTHS = {
-                AD: 24, AE: 23, AT: 20, AZ: 28, BA: 20, BE: 16, BG: 22, BH: 22, BR: 29,
-                CH: 21, CR: 21, CY: 28, CZ: 24, DE: 22, DK: 18, DO: 28, EE: 20, ES: 24,
-                FI: 18, FO: 18, FR: 27, GB: 22, GI: 23, GL: 18, GR: 27, GT: 28, HR: 21,
-                HU: 28, IE: 22, IL: 23, IS: 26, IT: 27, JO: 30, KW: 30, KZ: 20, LB: 28,
-                LI: 21, LT: 20, LU: 20, LV: 21, MC: 27, MD: 24, ME: 22, MK: 19, MR: 27,
-                MT: 31, MU: 30, NL: 18, NO: 15, PK: 24, PL: 28, PS: 29, PT: 25, QA: 29,
-                RO: 24, RS: 22, SA: 24, SE: 24, SI: 19, SK: 24, SM: 27, TN: 24, TR: 26,
-                AL: 28, BY: 28, CR: 22, EG: 29, GE: 22, IQ: 23, LC: 32, SC: 31, ST: 25,
-                SV: 28, TL: 23, UA: 29, VA: 22, VG: 24, XK: 20
-            };
-            var iban = String(input).toUpperCase().replace(/[^A-Z0-9]/g, ''), // keep only alphanumeric characters
-                code = iban.match(/^([A-Z]{2})(\d{2})([A-Z\d]+)$/), // match and capture (1) the country code, (2) the check digits, and (3) the rest
-                digits;
-            // check syntax and length
-            if (!code || iban.length !== CODE_LENGTHS[code[1]]) {
-                return false;
-            }
-            // rearrange country code and check digits, and convert chars to ints
-            digits = (code[3] + code[1] + code[2]).replace(/[A-Z]/g, function (letter) {
-                return letter.charCodeAt(0) - 55;
-            });
-
-            // final check
-            return this.mod97(digits) === 1;
-        },
-
-        mod97: function (string) {
-            var checksum = string.slice(0, 2), fragment;
-            for (var offset = 2; offset < string.length; offset += 7) {
-                fragment = String(checksum) + string.substring(offset, offset + 7);
-                checksum = parseInt(fragment, 10) % 97;
-            }
-            return checksum;
-        },
-
-        setError: function (msg) {
-            var parentofSelected = this.element.parentNode.parentNode.parentNode; // gives the parent DIV
-
-            var children = parentofSelected.childNodes;
-            for (var i = 0; i < children.length; i++) {
-                if (children[i].classList) {
-                    if (children[i].classList.contains('fabrikErrorMessage')) {
-                        children[i].innerHTML = msg;
-                        break;
-                    }
-                }
-            }
-        }
     });
 
     return window.FbEmundusIban;
