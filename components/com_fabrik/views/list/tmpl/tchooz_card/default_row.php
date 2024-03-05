a44c785e70f5fbfe374bd3e397a551b1bf5055d6
Legendre <jeremy.legendre@emundus.fr>
2024-03-05 16:56:26 +0100

    display good icon for each elements

--- components/com_fabrik/views/list/tmpl/tchooz_card/default_row.php
+++ components/com_fabrik/views/list/tmpl/tchooz_card/default_row.php
@@ -12,62 +12,41 @@ defined('_JEXEC') or die('Restricted access');
 
 use Joomla\CMS\Session\Session;
 
-$tags               = [];
-$title              = '';
+$tags = [];
+$title = '';
 $description_fields = [];
 
-$detail_url   = $this->params->get('detailurl', '');
+$detail_url = $this->params->get('detailurl', '');
 $detail_label = $this->params->get('detaillabel', JText::_('COM_FABRIK_DETAILSLABEL'));
-$edit_url     = $this->params->get('editurl', '');
-$edit_label   = $this->params->get('editlabel', JText::_('COM_FABRIK_EDITLABEL'));
+$edit_url = $this->params->get('editurl', '');
+$edit_label = $this->params->get('editlabel', JText::_('COM_FABRIK_EDITLABEL'));
 
 $not_displayed_headings = ['fabrik_select', 'fabrik_actions'];
 
-$show_delete_btn  = strpos($this->_row->data->fabrik_actions, 'delete') !== false;
-$show_edit_btn    = strpos($this->_row->data->fabrik_actions, 'fabrik_edit') !== false;
+$show_delete_btn = strpos($this->_row->data->fabrik_actions, 'delete') !== false;
+$show_edit_btn = strpos($this->_row->data->fabrik_actions, 'fabrik_edit') !== false;
 $show_details_btn = strpos($this->_row->data->fabrik_actions, 'fabrik_view') !== false;
 
-foreach ($this->headings as $heading => $label)
-{
-	if (in_array($heading, $not_displayed_headings))
-	{
-		continue;
-	}
+foreach ($this->headings as $heading => $label) {
+    if (in_array($heading, $not_displayed_headings)) {
+        continue;
+    }
 
-	if (!empty($this->cellClass[$heading]['class']))
-	{
-
-		$material_icons = $this->cellClass[$heading]['class'];
-		preg_match('/list_material_icons-([a-z_]+)/', $material_icons , $icons_classes);
-
-		if (strpos($this->cellClass[$heading]['class'], 'list-tags') !== false)
-		{
-			$tags[] = $this->_row->data->$heading;
-		}
-
-        elseif (strpos($this->cellClass[$heading]['class'], 'list_material_icons') !== false)
-		{
-			$icon = $icons_classes[1];
-		}
-
-		else
-		{
-			if (strpos($this->cellClass[$heading]['class'], 'list-title') !== false)
-			{
-				$title = $this->_row->data->$heading;
-			}
-
-
-			else
-			{
-				$description_fields[] = $heading;
-			}
-		}
-	}
-	else
-	{
-		$description_fields[] = $heading;
-	}
+    if (!empty($this->cellClass[$heading]['class'])) {
+        if (strpos($this->cellClass[$heading]['class'], 'list-tags') !== false) {
+            $tags[] = $this->_row->data->$heading;
+        } elseif (strpos($this->cellClass[$heading]['class'], 'list_material_icons') !== false) {
+            $description_fields[] = $heading;
+        } else {
+            if (strpos($this->cellClass[$heading]['class'], 'list-title') !== false) {
+                $title = $this->_row->data->$heading;
+            } else {
+                $description_fields[] = $heading;
+            }
+        }
+    } else {
+        $description_fields[] = $heading;
+    }
 }
 
 ?>
@@ -88,64 +67,65 @@ foreach ($this->headings as $heading => $label)
                 </div>
             </div>
 
-			<?php if (!empty($tags) || !empty($title)) : ?>
+            <?php if (!empty($tags) || !empty($title)) : ?>
                 <hr>
-			<?php endif; ?>
+            <?php endif; ?>
 
             <div class="em-text-neutral-600">
-				<?php
-				foreach ($this->headings as $heading => $label)
-				{
-					if (in_array($heading, $description_fields))
-					{
-						$style = empty($this->cellClass[$heading]['style']) ? '' : 'style="' . $this->cellClass[$heading]['style'] . '"';
+                <?php
+                foreach ($this->headings as $heading => $label) {
+                    if (in_array($heading, $description_fields)) {
+                        $style = empty($this->cellClass[$heading]['style']) ? '' : 'style="' . $this->cellClass[$heading]['style'] . '"';
+                        if (preg_match('/list_material_icons-([a-z_]+)/', $this->cellClass[$heading]['class'], $matches)) {
+                            $icon = $matches[1];
+                        }
 
-						?>
+                        ?>
                         <div class="<?php echo $this->cellClass[$heading]['class'] ?> flex" <?php echo $style ?>>
-                           <?php if(!empty($icon)) : ?>
+                            <?php if (!empty($icon)) : ?>
                                 <span class="material-icons-outlined mr-2"><?php echo $icon ?></span>
-                           <?php endif; ?>
+                            <?php endif; ?>
 
-							<?php echo isset($this->_row->data) ? $this->_row->data->$heading : ''; ?>
+                            <?php echo isset($this->_row->data) ? $this->_row->data->$heading : ''; ?>
                         </div>
-						<?php
-					}
-				}
-				?>
+                        <?php
+                    }
+                }
+                ?>
             </div>
         </div>
         <div class="container-actions em-w-100">
-			<?php if (!empty($this->_row->data->fabrik_view_url && !empty($detail_url))) : ?>
+            <?php if (!empty($this->_row->data->fabrik_view_url && !empty($detail_url))) : ?>
                 <a class="btn btn-primary em-w-100 em-mt-8 em-applicant-default-font em-flex-column"
                    href="<?php echo $this->_row->data->fabrik_view_url ?>">
                     <span class="em-mb-8"><?php echo $detail_label; ?></span>
                 </a>
-			<?php endif; ?>
-			<?php if (!empty($this->_row->data->fabrik_edit_url && !empty($edit_url))) : ?>
+            <?php endif; ?>
+            <?php if (!empty($this->_row->data->fabrik_edit_url && !empty($edit_url))) : ?>
                 <a class="btn btn-primary em-w-100 em-mt-8 em-applicant-default-font em-flex-column"
                    href="<?php echo $this->_row->data->fabrik_edit_url ?>">
                     <span class="em-mb-8"><?php echo $edit_label; ?></span>
                 </a>
-			<?php endif; ?>
+            <?php endif; ?>
         </div>
     </div>
     <div id="actions-<?= $this->_row->id; ?>"
          class="modal-actions em-flex-column em-flex-align-start em-border-neutral-400 em-neutral-800-color hidden">
-		<?php if ($show_edit_btn) : ?>
+        <?php if ($show_edit_btn) : ?>
             <a href="<?= $this->_row->data->fabrik_edit_url; ?>"
                class="em-text-neutral-600 em-w-100 em-flex-row em-p-8">
                 <span class="material-icons-outlined">edit</span>
                 <span class="em-ml-8"><?php echo $edit_label; ?></span>
             </a>
-		<?php endif; ?>
-		<?php if ($show_details_btn) : ?>
+        <?php endif; ?>
+        <?php if ($show_details_btn) : ?>
             <a href="<?= $this->_row->data->fabrik_view_url; ?>"
                class="em-text-neutral-600 em-w-100 em-flex-row em-p-8">
                 <span class="material-icons-outlined">visibility</span>
                 <span class="em-ml-8"><?php echo $detail_label; ?></span>
             </a>
-		<?php endif; ?>
-		<?php if ($show_delete_btn) : ?>
+        <?php endif; ?>
+        <?php if ($show_delete_btn) : ?>
             <a href="#"
                class="em-text-neutral-600 em-w-100 em-flex-row em-p-8 delete"
                data-listref="list_<?= $this->table->renderid ?>"
@@ -156,7 +136,7 @@ foreach ($this->headings as $heading => $label)
                 <span class="material-icons-outlined">delete</span>
                 <span class="em-ml-8"><?php echo JText::_('COM_FABRIK_DELETE'); ?></span>
             </a>
-		<?php endif; ?>
+        <?php endif; ?>
     </div>
 </div>
 
