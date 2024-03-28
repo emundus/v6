5ee1e70b1a47a8b5adec0e2d33da95d18fe484aa
jlegendre <jeremy.legendre@emundus.fr>
2024-03-28 15:31:53 +0100

    stop using mock for user access

--- components/com_emundus/controllers/users.php
+++ components/com_emundus/controllers/users.php
@@ -1288,4 +1288,20 @@ class EmundusControllerUsers extends JControllerLegacy {
 		exit;
 	}
 
+    function getuseraccessrights()
+    {
+        $response = ['status' => false, 'msg' => JText::_('ACCESS_DENIED')];
+        $user = JFactory::getUser();
+
+        if (EmundusHelperAccess::asPartnerAccessLevel($user->id)) {
+            $m_users = $this->getModel('Users');
+            $response['data'] = $m_users->getUserACL($user->id);
+            $response['status'] = true;
+            $response['msg'] = JText::_('COM_EMUNDUS_SUCCESS');
+        }
+
+        echo json_encode((object)$response);
+        exit;
+    }
+
 }
