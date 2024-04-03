38f6030189bf88d7c99ada9014e1a79e02143c76
foucher corentin <corentin.foucher@emundus.io>
2024-04-03 17:02:10 +0200

    [add] navbar aside with the specific icon and subMenu with arrow

--- components/com_emundus/src/views/globalSettings.vue
+++ components/com_emundus/src/views/globalSettings.vue
@@ -1,83 +1,58 @@
 <template>
   <div class="em-w-100">
-    <div class="button-container">
-      <button v-for="(item, index) in Menus" :id="'Menu-'+index" name="BoutonMenu" :class="{ 'outlined-button': isOutlined }" @click="handleMenuButtonClick(index)">{{item.label}}</button>
-    </div>
-    <div v-if="indexMenu != null" class="submenu-div" v-for="(item, index1) in SubMenus[indexMenu]">
-      <boutton class="outlined-button">{{SubMenus[indexMenu][index1].label}} </boutton>
-      <div>
-        <p>{{SubMenus[indexMenu][index1].options}}</p>
-      </div>
-    </div>
-    <div class="em-page-loader" v-if="loading"></div>
-
-    <aside id="logo-sidebar" class="fixed left-0 z-50 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700" aria-label="Sidebar">
-      <div class="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
-        <ul class="space-y-2 font-medium">
+    <aside id="logo-sidebar"
+           class="fixed left-0 top-0 w-64 h-screen transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700"
+           aria-label="Sidebar">
+      <div class="h-full pb-4 overflow-y-auto bg-white dark:bg-gray-800">
+        <ul class="space-y-2 font-large">
           <li>
-            <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-              <svg class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
-                <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z"/>
-                <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z"/>
-              </svg>
-              <span class="ms-3">Dashboard</span>
+            <a href="/" class="flex items-center p-2 rounded-lg group">
+              <i class="material-icons-outlined" style="user-select: none; color: #008A35">arrow_back</i>
+              <span class="ms-1" style="user-select: none; color: #008A35">{{ translate('BACK') }}</span>
             </a>
           </li>
-          <li>
-            <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-              <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 18">
-                <path d="M6.143 0H1.857A1.857 1.857 0 0 0 0 1.857v4.286C0 7.169.831 8 1.857 8h4.286A1.857 1.857 0 0 0 8 6.143V1.857A1.857 1.857 0 0 0 6.143 0Zm10 0h-4.286A1.857 1.857 0 0 0 10 1.857v4.286C10 7.169 10.831 8 11.857 8h4.286A1.857 1.857 0 0 0 18 6.143V1.857A1.857 1.857 0 0 0 16.143 0Zm-10 10H1.857A1.857 1.857 0 0 0 0 11.857v4.286C0 17.169.831 18 1.857 18h4.286A1.857 1.857 0 0 0 8 16.143v-4.286A1.857 1.857 0 0 0 6.143 10Zm10 0h-4.286A1.857 1.857 0 0 0 10 11.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 18 16.143v-4.286A1.857 1.857 0 0 0 16.143 10Z"/>
-              </svg>
-              <span class="flex-1 ms-3 whitespace-nowrap">Kanban</span>
-              <span class="inline-flex items-center justify-center px-2 ms-3 text-sm font-medium text-gray-800 bg-gray-100 rounded-full dark:bg-gray-700 dark:text-gray-300">Pro</span>
-            </a>
-          </li>
-          <li>
-            <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-              <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                <path d="m17.418 3.623-.018-.008a6.713 6.713 0 0 0-2.4-.569V2h1a1 1 0 1 0 0-2h-2a1 1 0 0 0-1 1v2H9.89A6.977 6.977 0 0 1 12 8v5h-2V8A5 5 0 1 0 0 8v6a1 1 0 0 0 1 1h8v4a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-4h6a1 1 0 0 0 1-1V8a5 5 0 0 0-2.582-4.377ZM6 12H4a1 1 0 0 1 0-2h2a1 1 0 0 1 0 2Z"/>
-              </svg>
-              <span class="flex-1 ms-3 whitespace-nowrap">Inbox</span>
-              <span class="inline-flex items-center justify-center w-3 h-3 p-3 ms-3 text-sm font-medium text-blue-800 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300">3</span>
-            </a>
-          </li>
-          <li>
-            <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-              <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
-                <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z"/>
-              </svg>
-              <span class="flex-1 ms-3 whitespace-nowrap">Users</span>
-            </a>
-          </li>
-          <li>
-            <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-              <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
-                <path d="M17 5.923A1 1 0 0 0 16 5h-3V4a4 4 0 1 0-8 0v1H2a1 1 0 0 0-1 .923L.086 17.846A2 2 0 0 0 2.08 20h13.84a2 2 0 0 0 1.994-2.153L17 5.923ZM7 9a1 1 0 0 1-2 0V7h2v2Zm0-5a2 2 0 1 1 4 0v1H7V4Zm6 5a1 1 0 1 1-2 0V7h2v2Z"/>
-              </svg>
-              <span class="flex-1 ms-3 whitespace-nowrap">Products</span>
-            </a>
-          </li>
-          <li>
-            <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-              <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 16">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 8h11m0 0L8 4m4 4-4 4m4-11h3a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-3"/>
-              </svg>
-              <span class="flex-1 ms-3 whitespace-nowrap">Sign In</span>
-            </a>
-          </li>
-          <li>
-            <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-              <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.96 2.96 0 0 0 .13 5H5Z"/>
-                <path d="M6.737 11.061a2.961 2.961 0 0 1 .81-1.515l6.117-6.116A4.839 4.839 0 0 1 16 2.141V2a1.97 1.97 0 0 0-1.933-2H7v5a2 2 0 0 1-2 2H0v11a1.969 1.969 0 0 0 1.933 2h12.134A1.97 1.97 0 0 0 16 18v-3.093l-1.546 1.546c-.413.413-.94.695-1.513.81l-3.4.679a2.947 2.947 0 0 1-1.85-.227 2.96 2.96 0 0 1-1.635-3.257l.681-3.397Z"/>
-                <path d="M8.961 16a.93.93 0 0 0 .189-.019l3.4-.679a.961.961 0 0 0 .49-.263l6.118-6.117a2.884 2.884 0 0 0-4.079-4.078l-6.117 6.117a.96.96 0 0 0-.263.491l-.679 3.4A.961.961 0 0 0 8.961 16Zm7.477-9.8a.958.958 0 0 1 .68-.281.961.961 0 0 1 .682 1.644l-.315.315-1.36-1.36.313-.318Zm-5.911 5.911 4.236-4.236 1.359 1.359-4.236 4.237-1.7.339.341-1.699Z"/>
-              </svg>
-              <span class="flex-1 ms-3 whitespace-nowrap">Sign Up</span>
+          <li v-for="(item, index) in Menus">
+            <a :id="'Menu-'+index" name="bouton-Menu" @click="handleMenuButtonClick(index , item)"
+               style="user-select: none" class="flex items-center p-2  rounded-lg  group">
+              <i class="material-icons-outlined" name="icon-Menu" :id="'icon-'+index">{{ item.icon }}</i>
+              <span class="ms-1">{{ item.label }}</span>
             </a>
           </li>
         </ul>
       </div>
     </aside>
+
+    <div class="p-4 sm:ml-40" style="user-select: none" v-if="indexMenu != null">
+      <h1 class="text-2xl font-semibold" style="user-select: none; color: #008A35;">
+        <i class="material-icons-outlined" style="scale: 1.5; color:#008A35;  padding-right: 0.5em">{{
+            this.aMenu.icon
+          }}</i>
+        {{ this.aMenu.label }}
+      </h1>
+      <div id="accordion-collapse" v-for="(x, index1) in SubMenus[indexMenu]"
+           v-if="SubMenus[indexMenu][index1].type!=='Tile'"
+           @click="handleSubMenuclick(index1)"
+           class="flex flex-col justify-between w-full p-5 font-medium rtl:text-right text-black border border-gray-200 rounded bg-white mb-3 hover:bg-gray-100 gap-3 "
+           data-accordion-target="#accordion-collapse-body-1" aria-expanded="true"
+           aria-controls="accordion-collapse-body-1"
+           style="box-shadow:0.1em 0.05em 0.05em grey;">
+        <h2 id="accordion-collapse-heading-1" class="flex flex-row justify-between">
+          <span :id="'Subtile'+index1">{{ SubMenus[indexMenu][index1].label }}</span>
+          <span class="material-icons-outlined scale-150" :id="'SubtitleArrow'+index1" name="SubtitleArrows" >expand_more</span>
+        </h2>
+
+        <div :id="'SubMenu-'+index1" name="SubMenuContent" style="display: none">
+          {{ SubMenus[indexMenu][index1].options }}
+        </div>
+
+      </div>
+
+      <div v-for="(item, index1) in SubMenus[indexMenu]" v-if="SubMenus[indexMenu][index1].type==='Tile'"
+           class="flex items-center mb-3"><p>Tile for: {{ SubMenus[indexMenu][index1].label }} is comming soon</p></div>
+    </div>
+
+
+    <div class="em-page-loader" v-if="loading"></div>
   </div>
 
 </template>
@@ -109,16 +84,16 @@ export default {
   },
   props: {
     actualLanguage: {
-			type: String,
-	    default: 'fr'
+      type: String,
+      default: 'fr'
     },
     coordinatorAccess: {
-			type: Number,
-	    default: 1
+      type: Number,
+      default: 1
     },
     manyLanguages: {
-			type: Number,
-	    default: 1
+      type: Number,
+      default: 1
     }
   },
 
@@ -131,11 +106,13 @@ export default {
     loading: null,
 
     isOutlined: true, // Boolean flag to determine if buttons should be outlined
-    isClicked: {},
+    MenuisClicked: [],
+    SubMenuisClicked: [],
     indexMenu: null,
-    Menus : [],
-    SubMenus : [],
-    SubSubMenus : [],
+    aMenu: [],
+    Menus: [],
+    SubMenus: [],
+    SubSubMenus: [],
 
   }),
 
@@ -151,90 +128,99 @@ export default {
     changeCSS() {
       let navbarDisplay = document.getElementById("header-b");
       navbarDisplay.style.display = "none";
+      let headerDisplay = document.getElementById("g-navigation");
+      headerDisplay.style.display = "none";
     },
-    getParamFromjson(){
+    getParamFromjson() {
       return new Promise((resolve) => {
-        for(let i of SettingParam){
+        for (let i of SettingParam) {
           this.Menus.push(i);
           this.SubMenus.push(i.sections); // Use spread operator to flatten the array
         }
-        for(let j of this.SubMenus)
-        {
-          for(let k of j)
-          {
+        for (let j of this.SubMenus) {
+          for (let k of j) {
             this.SubSubMenus.push(k.options);
           }
         }
         resolve();
-        console.log(this.Menus)
-        console.log(this.SubMenus)
-        console.log(this.SubSubMenus)
       });
 
     },
-    handleMenuButtonClick(index) {
-      let bool = this.isClicked[index]
+    handleMenuButtonClick(index, item) {
+      this.aMenu = item;
+      let bool = this.MenuisClicked[index]
       // set all buttons to false
-      for (let key in this.isClicked) {
-        this.isClicked[key] = false;
+      for (let key in this.MenuisClicked) {
+        this.MenuisClicked[key] = false;
       }
+      for (let key in this.SubMenuisClicked) {
+        this.SubMenuisClicked[key] = false;
+      }
+      document.getElementsByName("SubtitleArrows").forEach(element => {
+        element.style.rotate = '0deg';
+      });
       //toggle the clicked button
-      this.isClicked[index] = !bool;
+      this.MenuisClicked[index] = !bool;
 
       // Remove 'green-button' class from all buttons
-      document.getElementsByName("BoutonMenu").forEach(element => {
+      document.getElementsByName("bouton-Menu").forEach(element => {
         element.classList.remove('green-button');
+
+        document.getElementsByName("SubMenuContent").forEach(element => {
+          element.style.display = 'none';
+        });
+
+        document.getElementsByName("icon-Menu").forEach(element => {
+          element.style.color = '#000000';
+        });
       });
 
-      if (this.isClicked[index]) {
+      if (this.MenuisClicked[index]) {
         // If the button is clicked, add 'green-button' class and set indexMenu
         this.indexMenu = index;
-        document.getElementById('Menu-'+index).classList.add('green-button');
+        document.getElementById('Menu-' + index).classList.add('green-button');
+        document.getElementById('icon-' + index).style.color = '#008A35';
       } else {
-        // If the button is not clicked, set indexMenu to null
         this.indexMenu = null;
       }
-    }
+    },
+
+
+    handleSubMenuclick(index) {
+      let bool = this.SubMenuisClicked[index]
+      // set all buttons to false
+      for (let key in this.SubMenuisClicked) {
+        this.SubMenuisClicked[key] = false;
+      }
+      document.getElementsByName("SubtitleArrows").forEach(element => {
+        element.style.rotate = '0deg';
+      });
+      //toggle the clicked button
+      this.SubMenuisClicked[index] = !bool;
+
+
+      document.getElementsByName("SubMenuContent").forEach(element => {
+        element.style.display = 'none';
+      });
+      if (this.SubMenuisClicked[index]) {
+        document.getElementById('SubtitleArrow' + index).style.rotate = '-180deg';
+        document.getElementById('SubMenu-' + index).style.display = 'flex';
+        console.log(document.getElementById('SubMenu-' + index));
+      }
+    },
 
   },
-	computed: {
-	},
-  watch: {
-  }
+  computed: {},
+  watch: {}
 };
 
 </script>
 
 <style scoped>
-.outlined-button {
-  border: 1px solid #333; /* Border color */
-  background-color: transparent; /* Remove background color */
-  padding: 8px 16px; /* Add padding for better spacing */
-  margin: 4px; /* Add margin for better spacing */
-  cursor: pointer; /* Show pointer cursor on hover */
-  outline: none; /* Remove default outline */
-}
-
-.outlined-button:hover {
-  background-color: #00ff00; /* Change background color on hover */
-  color: white; /* Change text color to white */
-
-}
-
 .green-button {
-  background-color: green; /* Change background color to green when clicked */
-  color: white; /* Change text color to white */
-}
-
-.submenu-div {
-  display: flex;
-  flex-direction: column;
-  margin-left: 20px;
-}
-
-.button-container {
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
+  weight: bold;
+  background-color: #008A351A; /* Change background color to green when clicked */
+  background-color-opacity: 0.1;
+  color: #008A35; /* Change text color to green */
 }
 </style>
