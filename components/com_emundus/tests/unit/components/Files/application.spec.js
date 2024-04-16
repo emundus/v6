1264393c05acd509d55ba439c17c1429dde8fb8f
jlegendre <jeremy.legendre@emundus.fr>
2024-04-16 17:32:04 +0200

    assert event trigger the split modal to re-render

--- components/com_emundus/tests/unit/components/Files/application.spec.js
+++ components/com_emundus/tests/unit/components/Files/application.spec.js
@@ -45,3 +45,39 @@ describe('Vue scindée simple', () => {
         expect(wrapper.find('#evaluation-grid').exists()).toBeTruthy();
     });
 });
+
+describe('Vue scindée, changement de dossier', () => {
+    const wrapper = mount(ApplicationSingle, {
+        propsData: {
+            file: {
+                id: 1,
+                fnum: '2021061715003400000010000095',
+                applicant_id: 95,
+                student_id: 95,
+                applicant_name: 'COORDINATOR Program',
+            },
+            type: 'evaluation',
+            user: '95',
+            ratio: '66/33',
+            context: 'files'
+        },
+        localVue
+    });
+
+    wrapper.vm.$data.access = {
+        1: {r: true, c: true, u: true, d: true},
+        4: {r: true, c: true, u: true, d: true},
+        10: {r: true, c: true, u: true, d: true},
+    };
+    wrapper.vm.$data.loading = false;
+    wrapper.vm.$data.evaluation_form = 270;
+
+
+    const spyOnRender = jest.spyOn(wrapper.vm, 'render');
+    it ('on openSingleApplicationWithFnum event triggered, render function should be called', () => {
+        window.dispatchEvent(new CustomEvent('openSingleApplicationWithFnum', {
+            detail: {fnum: '2021061715003400000010000095'}
+        }));
+        expect(spyOnRender).toHaveBeenCalled();
+    });
+});
