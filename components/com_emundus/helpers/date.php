ba0bf8791da00cce0c25a1318e94efbdd7da38f4
clement <clement.pilotte@emundus.io>
2024-04-24 11:58:01 +0200

    verified if the offset is a valuable timezone

--- components/com_emundus/helpers/date.php
+++ components/com_emundus/helpers/date.php
@@ -63,7 +63,12 @@ class EmundusHelperDate {
                 $config = JFactory::getConfig();
                 $offset = $config->get('offset');
 
-                $date_time = new DateTime($date, new DateTimeZone($offset));
+                $timezone = date_default_timezone_get();
+                if (in_array($offset, timezone_identifiers_list())) {
+                    $timezone = $offset;
+                }
+
+                $date_time = new DateTime($date, new DateTimeZone($timezone));
             } else {
                 $date_time = new DateTime($date);
                 $date_time->setTimezone(new DateTimeZone('UTC'));
