3689c9120a27d83b6ab0d28b74dab77e6304dfae
jlegendre <jeremy.legendre@emundus.fr>
2024-04-25 14:45:56 +0200

    moduleNameMapper up

--- components/jest.config.js
+++ components/jest.config.js
@@ -5,8 +5,6 @@ module.exports = {
   testTimeout: 5000,
   testEnvironment: 'jsdom',
   moduleNameMapper: {
-    '^@/components/(.*)$': '<rootDir>/com_emundus/src/components/$1',
-    '^@/views/(.*)$': '<rootDir>/com_emundus/src/views/$1',
-    '^@/services/(.*)$': '<rootDir>/com_emundus/src/services/$1',
+    '^@/(.*)$': '<rootDir>/com_emundus/src/$1',
   }
 };
