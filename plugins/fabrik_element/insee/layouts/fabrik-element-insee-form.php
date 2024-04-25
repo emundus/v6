dce5660d7ed8451404b1d5b0f2b3e31bacff0d7e
HUBINET Brice <brice.hubinet@emundus.fr>
2024-04-25 15:49:13 +0200

    fix: Add text attribute to insee element

--- plugins/fabrik_element/insee/layouts/fabrik-element-insee-form.php
+++ plugins/fabrik_element/insee/layouts/fabrik-element-insee-form.php
@@ -11,5 +11,5 @@ JText::script('PLG_ELEMENT_INSEE_SIRET_CLOSED');
 ?>
 
 <div id="<?php echo $d->attributes['id']; ?>" class="em-flex-row fabrikSubElementContainer inseeElement">
-    <input id="insee_inputValue" name="<?php echo $d->attributes['name']; ?>" class="fabrikinput input-medium" value="<?php echo $d->attributes['value']; ?>" />
+    <input id="insee_inputValue" type="text" name="<?php echo $d->attributes['name']; ?>" class="fabrikinput input-medium" value="<?php echo $d->attributes['value']; ?>" />
 </div>
