7bebbe6d4585345eaf91cd515ba4145aa664ebe5
foucher corentin <corentin.foucher@emundus.io>
2024-04-30 17:08:28 +0200

    [add] toggle for the custom config

--- components/com_emundus/src/views/globalSettings.vue
+++ components/com_emundus/src/views/globalSettings.vue
@@ -93,9 +93,12 @@
             </div>
 
 
-            <div v-else-if="(option.type_field !== 'Button')&&(option.published === true)" name="labelOfElements"
-                 class="block text-xl ">
-              {{ translate(option.label) }}
+            <div v-if="(option.type_field === 'toggle')&&(option.published === true)" class="inline-flex items-center cursor-pointer">
+              <label>
+                <input type="checkbox" class="sr-only peer" :v-model="option.value" v-model="option.value">
+                <div class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer  peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-green-600"></div>
+              </label>
+              <span>{{option.label_right}}</span>
             </div>
 
             <div class="flex flex-col" v-if="(option.type_field === 'Button') && (option.published === true)">
