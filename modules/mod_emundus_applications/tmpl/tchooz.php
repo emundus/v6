89d6262002c5fde1e434365f0d996bb2516a66b4
jlegendre <jeremy.legendre@emundus.fr>
2024-05-14 10:34:26 +0200

    use app id

--- modules/mod_emundus_applications/tmpl/tchooz.php
+++ modules/mod_emundus_applications/tmpl/tchooz.php
@@ -421,10 +421,10 @@ $current_tab = 0;
                                                                     </div>
                                                                 <?php endif; ?>
                                                                 <?php if ($show_nb_comments) {
-                                                                    $nb_comments = modemundusApplicationsHelper::getNbComments($application->ccid, $user->id);
+                                                                    $nb_comments = modemundusApplicationsHelper::getNbComments($application->application_id, $user->id);
                                                                     if ($nb_comments > 0) {
                                                                         ?>
-                                                                        <a href="<?= !empty($comments_page_alias) ? '/' . $comments_page_alias . '?ccid=' . $application->ccid . '&fnum=' . $application->fnum : '#'  ?>"  id="actions_button_comment" class="flex flex-row comments-icon-wrapper relative ml-2">
+                                                                        <a href="<?= !empty($comments_page_alias) ? '/' . $comments_page_alias . '?ccid=' . $application->application_id . '&fnum=' . $application->fnum : '#'  ?>"  id="actions_button_comment" class="flex flex-row comments-icon-wrapper relative ml-2">
                                                                             <span id="actions_button_comment_icon" class="material-icons em-neutral-300-color em-bg-main-500 p-2 rounded-full">chat_bubble_outline</span>
                                                                             <span id="actions_button_comment_nb" class="nb-comments em-border-main-500 em-font-size-12 em-main-500-color em-white-bg border-2 absolute rounded-full p-1"><?= $nb_comments; ?></span>
                                                                         </a>
