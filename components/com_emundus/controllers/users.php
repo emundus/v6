c4cd1e59a661adb9e4b58f7ab0fe36b73f553cec
jlegendre <jeremy.legendre@emundus.fr>
2024-05-22 16:12:03 +0200

    propose all profiles in ranking configuration

--- components/com_emundus/controllers/users.php
+++ components/com_emundus/controllers/users.php
@@ -1410,22 +1410,14 @@ class EmundusControllerUsers extends JControllerLegacy {
         exit;
     }
 
-	function getnonapplicantprofiles()
+	function getprofiles()
 	{
 		$response = ['status' => false, 'msg' => JText::_('ACCESS_DENIED')];
         $user = JFactory::getUser();
 
 		if (EmundusHelperAccess::asCoordinatorAccessLevel($user->id)) {
 			$m_users = $this->getModel('Users');
-            $profiles = $m_users->getProfiles();
-
-			$response['data'] = [];
-			foreach($profiles as $profile) {
-				if ($profile->published == 0) {
-					$response['data'][] = $profile;
-				}
-			}
-
+            $response['data'] = array_values($m_users->getProfiles());
             $response['status'] = true;
             $response['msg'] = JText::_('COM_EMUNDUS_SUCCESS');
 		}
