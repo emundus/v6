b531e5abd93132201fc38cbd79231497a632992b
clement <clement.pilotte@emundus.io>
2024-05-23 10:15:30 +0200

    add fetch test in home.tsx

--- plugins/fabrik_element/booking/src/Home.tsx
+++ plugins/fabrik_element/booking/src/Home.tsx
@@ -1,19 +1,38 @@
 import './App.css'
 import "@calcom/atoms/globals.min.css";
 import {AvailabilitySettings, CalProvider} from "@calcom/atoms";
+import {useEffect, useState} from "react";
 
 function Home() {
-  return (
-      <CalProvider
-          clientId={''}
-          options={{
-            apiUrl: import.meta.env.CAL_API_URL ?? "",
-            refreshUrl: ""
-          }}
-          >
-          <AvailabilitySettings />
-      </CalProvider>
-  );
+    const [data, setData] = useState("");
+
+    useEffect(() => {
+        fetch('http://localhost:3003/v1/users/37?apiKey=API_KEY')
+            .then(async (res) => {
+                const result = await res.json();
+                setData(result);
+            })
+            .catch((error) => {
+                console.error('Error fetching data:', error);
+            });
+    }, []);
+
+    console.log(data);
+
+    return (
+        <CalProvider
+            clientId={'CLIENT_ID'}
+            options={{
+                apiUrl: "https://api.cal.com/v2",
+            }}
+        >
+            <AvailabilitySettings />
+            <div>
+                <h3>Résultat de la requête:</h3>
+                <pre>{JSON.stringify(data, null, 2)}</pre>
+            </div>
+        </CalProvider>
+    );
 }
 
 export default Home
