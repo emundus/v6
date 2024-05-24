6392ada23160fb5859eff7f0ad5c1684097a2c8b
clement <clement.pilotte@emundus.io>
2024-05-24 16:41:34 +0200

    home.jsx can display a schedule

--- plugins/fabrik_element/booking/src/Home.tsx
+++ plugins/fabrik_element/booking/src/Home.tsx
@@ -7,7 +7,7 @@ function Home() {
     const [data, setData] = useState("");
 
     useEffect(() => {
-        fetch('http://localhost:3003/v1/users/37?apiKey=API_KEY')
+        fetch('http://localhost:3003/v1/users/37?apiKey=cal_edcfa25c9fae2e8d476cb6c02c4dc265')
             .then(async (res) => {
                 const result = await res.json();
                 setData(result);
@@ -17,13 +17,13 @@ function Home() {
             });
     }, []);
 
-    console.log(data);
-
     return (
         <CalProvider
-            clientId={'CLIENT_ID'}
+            accessToken={'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0eXBlIjoiYWNjZXNzX3Rva2VuIiwiY2xpZW50SWQiOiI4OTA3YTFiMDI3ZDA5YmJlMDc4MDIzNzQ3NWEwMGM3NjlkOGE5MTgyZjEzNjA5M2QxNTQ5NmVmN2RhOTA5YWIiLCJvd25lcklkIjo4NywiaWF0IjoxNzE2NTU3OTc3fQ.ybtEWWCV0EUOFF9kqWRoLB6LPAda7cIsVXps6Apl_Sw'}
+            clientId={'8907a1b027d09bbe0780237475a00c769d8a9182f136093d15496ef7da909ab'}
             options={{
-                apiUrl: "https://api.cal.com/v2",
+                apiUrl: "http://localhost:3004/v2",
+                refreshUrl : "http://127.0.0.1:5173/",
             }}
         >
             <AvailabilitySettings />
