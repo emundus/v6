f08b7c3fc0e6644d11c1a183f2d8e0aacd476a49
HUBINET Brice <brice.hubinet@emundus.fr>
2024-06-05 14:54:50 +0200

    Fix emundus-updatesession plugin to update fullname of emundus session

--- plugins/fabrik_form/php/scripts/emundus-updatesession.php
+++ plugins/fabrik_form/php/scripts/emundus-updatesession.php
@@ -16,5 +16,10 @@ if(!empty($lastname)) {
 
 if(!empty($firstname) || !empty($lastname))
 {
+	$user = JFactory::getUser();
+	$user->set('name', $firstname . ' ' . $lastname);
+	$user->save();
+
+	$emundusUser->name = $firstname . ' ' . $lastname;
 	JFactory::getSession()->set('emundusUser', $emundusUser);
 }
