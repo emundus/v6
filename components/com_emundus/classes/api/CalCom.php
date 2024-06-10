bebd516e9bb845cd9f83dd637d8f6ceb5bd6911b
clement <clement.pilotte@emundus.io>
2024-06-10 17:10:13 +0200

    add comments and add verifications before doing api requests

--- components/com_emundus/classes/api/CalCom.php
+++ components/com_emundus/classes/api/CalCom.php
@@ -19,52 +19,101 @@ class CalCom extends Api
 
         $this->setBaseUrl();
         $this->setClient();
-        // $this->setAuth();
-        $this->platform_oauth_clientId = "7774eb2e5e7d7b4387c2c3ed31c881512d07d90bd7fcee274ec6a4132f0c0e9c";
-        $this->platform_oauth_clientSecret = "e273621869bd693cfa186b790a46ad84880776dea895b905b299917642b97db6";
+        $this->platform_oauth_client_id = "7774eb2e5e7d7b4387c2c3ed31c881512d07d90bd7fcee274ec6a4132f0c0e9c";
+        $this->platform_oauth_client_secret = "e273621869bd693cfa186b790a46ad84880776dea895b905b299917642b97db6";
         $this->api_key = "cal_edcfa25c9fae2e8d476cb6c02c4dc265";
 
     }
 
+    /**
+     * @description Set the URL to Cal.com API v2
+     */
     public function setBaseUrl(): void
     {
         $this->baseUrl = "http://192.168.1.72:3004";
     }
 
+    /**
+     * @description Set the URL to Cal.com API v1
+     */
     public function setBaseUrlFirstVersionApi()
     {
         $this->baseUrl = "http://192.168.1.72:3003";
     }
 
-    public function setHeadersPostUser(): void
+    /**
+     * @description Set headers for users' requests
+     *
+     * @value Content-Type : Indicates the type of content received (json here)
+     * @value x-cal-secret-key : PlatformOAuthClient's secret
+     */
+    public function setHeadersUser(): void
     {
         $this->headers = array(
             'Content-Type' => "application/json",
-            'x-cal-secret-key' => $this->platform_oauth_clientSecret
+            'x-cal-secret-key' => $this->platform_oauth_client_secret
         );
     }
 
+    /**
+     * @description Post request to create a Cal.com user
+     *
+     * @param $name string User's name that will be created
+     * @param $time_format int Hours' format (12 or 24)
+     * @param $week_start string First day of the week
+     * @param $time_zone string User's timezone
+     *
+     */
     public function postUser($name, $time_format = 12, $week_start = "Monday", $time_zone = "Europe/Paris")
     {
-        $this->setHeadersPostUser();
+        $this->setHeadersUser();
+
+        $mail = $name;
+        $mail = str_replace(' ', '_', $mail);
+        $mail = htmlentities($mail, ENT_COMPAT, "UTF-8");
+        $mail = preg_replace('/&([a-zA-Z])(uml|acute|grave|circ|tilde|cedil);/', '$1',$mail);
+        $mail= html_entity_decode($mail);
+        $mail = preg_replace('/[^\x20-\x7E]/','', $mail);
+        $mail = preg_replace('/[^a-zA-Z0-9_]/', '', $mail);
+        $mail = strtolower($mail);
 
         $query_body = json_encode([
-            "email" => $name . "@emundus.com",
+            "email" => $mail . "@emundus.com",
             "timeFormat" => $time_format,
             "weekStart" => $week_start,
             "timeZone" => $time_zone,
             "name" => $name
         ]);
 
-        return $this->post("v2/oauth-clients/" . $this->platform_oauth_clientId . "/users", $query_body);
+        return $this->post("v2/oauth-clients/" . $this->platform_oauth_client_id . "/users", $query_body);
     }
 
+    /**
+     * @description Patch request to modify a Cal.com user
+     *
+     * @param $name string New user's name that will be modified
+     * @param $user_id int User's ID to be modified
+     * @param $schedule_id int Schedule ID related to the user
+     * @param $time_format int Hours' format (12 or 24)
+     * @param $week_start string First day of the week for user's calendar
+     * @param $time_zone string User's timezone
+     *
+     */
     public function patchUser($name, $user_id, $schedule_id, $time_format = 12, $week_start = "Monday", $time_zone = "Europe/Paris")
     {
-        $this->setHeadersPostUser();
+        $this->setHeadersUser();
+
+        $mail = $name;
+        $mail = str_replace(' ', '_', $mail);
+        $mail = htmlentities($mail, ENT_COMPAT, "UTF-8");
+        $mail = preg_replace('/&([a-zA-Z])(uml|acute|grave|circ|tilde|cedil);/', '$1',$mail);
+        $mail= html_entity_decode($mail);
+        $mail = preg_replace('/[^\x20-\x7E]/','', $mail);
+        $mail = preg_replace('/[^a-zA-Z0-9_]/', '', $mail);
+        $mail = strtolower($mail);
 
         $query_body = json_encode([
-            "email" => $name . "@emundus.com",
+            "email" => $mail . "@emundus.com",
             "timeFormat" => $time_format,
             "weekStart" => $week_start,
             "timeZone" => $time_zone,
@@ -72,24 +121,44 @@ class CalCom extends Api
             "defaultScheduleId" => (int)$schedule_id
         ]);
 
-        return $this->patch("v2/oauth-clients/" . $this->platform_oauth_clientId . "/users/" . $user_id, $query_body);
+        return $this->patch("v2/oauth-clients/" . $this->platform_oauth_client_id . "/users/" . $user_id, $query_body);
     }
 
 
-    public function setHeadersPostSchedule($access_token): void
+    /**
+     * @description Set headers for schedules' requests
+     *
+     * @param $access_token string : User's access token associated to the schedule
+     *
+     * @value accept : Indicates the type of content expected (json here)
+     * @value Content-Type : Indicates the type of content received (json here)
+     * @value Authorization : Authorize the request thanks to user's access token given
+     * @value x-cal-secret-key : PlatformOAuthClient's secret
+     */
+    public function setHeadersSchedule($access_token): void
     {
         $this->headers = array(
             'accept' => "application/json",
             'Content-Type' => "application/json",
             'Authorization' => "Bearer " . $access_token,
-            'x-cal-secret-key' => $this->platform_oauth_clientSecret
+            'x-cal-secret-key' => $this->platform_oauth_client_secret
         );
 
     }
 
-    public function postSchedule($access_token, $name="Default Schedule Name", $time_zone="Europe/Paris")
+    /**
+     * @description Post request to create a Cal.com schedule
+     *
+     * @param $access_token string : User's access token associated to the schedule
+     * @param $start_date string Schedule's start date
+     * @param $end_date string Schedule's end date
+     * @param $name string Schedule's name that will be created
+     * @param $time_zone string Schedule's timezone
+     *
+     */
+    public function postSchedule($access_token, $start_date, $end_date, $name="Default Schedule Name", $time_zone="Europe/Paris")
     {
-        $this->setHeadersPostSchedule($access_token);
+        $this->setHeadersSchedule($access_token);
 
         $query_body = json_encode([
             "name" => $name,
@@ -97,8 +166,8 @@ class CalCom extends Api
             "availabilities" => [
                 [
                     "days" => [],
-                    "startTime" => "1900-06-06T00:01:00.000Z",
-                    "endTime" => "1900-07-06T23:59:00.000Z",
+                    "startTime" => date('Y-m-d\TH:i', strtotime($start_date)) . ':00.000Z',
+                    "endTime" => date('Y-m-d\TH:i', strtotime($end_date)) . ':00.000Z',
                 ]
             ],
             "isDefault" => true
@@ -107,45 +176,68 @@ class CalCom extends Api
         return $this->post("v2/schedules", $query_body);
     }
 
+    /**
+     * @description Patch request to modify a Cal.com schedule
+     *
+     * @param $access_token string : User's access token associated to the schedule
+     * @param $schedule_id int Schedule's ID to be modified
+     * @param $start_date string Schedule's start date
+     * @param $end_date string Schedule's end date
+     * @param $name string New schedule's name that will be modified
+     * @param $time_zone string Schedule's timezone
+     *
+     */
     public function patchSchedule($access_token, $schedule_id, $start_date, $end_date, $name="Default Schedule Name", $time_zone="Europe/Paris")
     {
-        $this->setHeadersPostSchedule($access_token);
+        $this->setHeadersSchedule($access_token);
 
         $end_date_iso = date('Y-m-d\TH:i', strtotime($end_date)) . ':00.000Z';
 
-        $query_body = json_encode([
+        $query_body =[
             "timeZone" => $time_zone,
             "name" => $name,
             "isDefault" => true,
             "dateOverrides" => [
 
             ]
-        ]);
-
-        $query_body_array = json_decode($query_body, true);
+        ];
 
         $current_date = strtotime($start_date);
         while ($current_date <= strtotime($end_date)) {
             $current_iso_date = date('Y-m-d', $current_date) . "T" . date('H:i', strtotime($start_date)). ":00.000Z";
-            $query_body_array['dateOverrides'][] = [
+            $query_body['dateOverrides'][] = [
                 "start" => $current_iso_date,
                 "end" => $end_date_iso
             ];
             $current_date = strtotime('+1 day', $current_date);
         }
-
-        $query_body = json_encode($query_body_array);
+        $query_body = json_encode($query_body);
 
         return $this->patch("v2/schedules/" . $schedule_id, $query_body);
     }
 
+    /**
+     * @description Delete request to remove a Cal.com schedule
+     *
+     * @param $access_token string : User's access token associated to the schedule
+     * @param $schedule_id int Schedule's ID to be modified
+     *
+     */
     public function deleteSchedule($access_token, $schedule_id)
     {
-        $this->setHeadersPostSchedule($access_token);
+        $this->setHeadersSchedule($access_token);
 
         return $this->delete("v2/schedules/" . $schedule_id);
     }
 
+    /**
+     * @description Post request to create a Cal.com event type
+     *
+     * @param $length string Duration of one event type
+     * @param $title string Event type's name
+     * @param $user_id int User's ID of event type's owner
+     *
+     */
     public function postEventType($length, $title, $user_id)
     {
         $this->setBaseUrlFirstVersionApi();
@@ -162,7 +254,7 @@ class CalCom extends Api
         $query_body = json_encode([
             "length" => (int)$length,
             "slug" => $slug . "_event",
-            "title" => $title . " Event",
+            "title" => $title,
             "userId" => (int)$user_id
 
         ]);
@@ -170,6 +262,12 @@ class CalCom extends Api
         return $this->post("event-types?apiKey=" . $this->api_key, $query_body);
     }
 
+    /**
+     * @description Delete request to remove a Cal.com event type
+     *
+     * @param $event_type_id int : Event type's ID to delete
+     *
+     */
     public function deleteEventType($event_type_id)
     {
         $this->setBaseUrlFirstVersionApi();
@@ -177,11 +275,17 @@ class CalCom extends Api
         return $this->delete("event-types/" . $event_type_id . "?apiKey=" . $this->api_key);
     }
 
+    /**
+     * @description Force refreshing user's access token
+     *
+     * @param $user_id int : User's ID to refresh access token
+     *
+     */
     public function refreshingAccessToken($user_id)
     {
-        $this->setHeadersPostUser();
+        $this->setHeadersUser();
 
-        return $this->post("/v2/oauth-clients/" . $this->platform_oauth_clientId . "/users/" . $user_id . "/force-refresh");
+        return $this->post("/v2/oauth-clients/" . $this->platform_oauth_client_id . "/users/" . $user_id . "/force-refresh");
     }
 
 }
--- plugins/fabrik_form/calcom/calcom.php
+++ plugins/fabrik_form/calcom/calcom.php
@@ -1,15 +1,15 @@
 <?php
 /**
- * @version     2: emundusReferentLetter 2018-04-25 Hugo Moracchini
+ * @version     calcom June 2024 eMundus
  * @package     Fabrik
- * @copyright   Copyright (C) 2018 emundus.fr. All rights reserved.
+ * @copyright   Copyright (C) 2024 emundus.fr. All rights reserved.
  * @license     GNU/GPL, see LICENSE.php
  * Joomla! is free software. This version may have been modified pursuant
  * to the GNU General Public License, and as distributed it includes or
  * is derivative of works licensed under the GNU General Public License or
  * other free or open source software licenses.
  * See COPYRIGHT.php for copyright notices and details.
- * @description Redirection et chainage des formulaires suivant le profile de l'utilisateur
+ * @description Processing of availability management forms
  */
 
 // Require the abstract plugin class
@@ -18,16 +18,10 @@ require_once JPATH_SITE.'/components/com_emundus/classes/api/Api.php';
 require_once JPATH_SITE.'/components/com_emundus/classes/api/CalCom.php';
 
 // No direct access
-use Joomla\CMS\Component\ComponentHelper;
 use Joomla\CMS\Factory;
-use Joomla\CMS\Language\Text;
-use Joomla\CMS\Log\Log;
-use Joomla\CMS\Uri\Uri;
 use classes\api\CalCom;
 
 defined('_JEXEC') or die('Restricted access');
-
-
 
 /**
  * Create a Joomla user from the forms data
@@ -38,16 +32,6 @@ defined('_JEXEC') or die('Restricted access');
  */
 class PlgFabrik_FormCalcom extends plgFabrik_Form
 {
-	/**
-	 * Status field
-	 *
-	 * @var  string
-	 */
-	protected $URLfield = '';
-
-    protected $user_id;
-    protected $schedule_id;
-    protected $event_id;
 
 	/**
 	 * Get an element name
@@ -91,20 +75,22 @@ class PlgFabrik_FormCalcom extends plgFabrik_Form
 		return $params->get($pname);
 	}
 
-	/**
-	 * Main script.
-	 *
-	 * @throws Exception
-	 */
-    public function onBeforeProcess()
+    /**
+     * The event which runs before storing (saving) data to the database
+     *
+     * @return bool
+     */
+    public function onBeforeStore()
     {
         $w          = new CalCom();
         $app        = Factory::getApplication();
         $id         = $app->input->get('jos_emundus_setup_availabilities___id');
-        $name       = $app->input->get('jos_emundus_setup_availabilities___name') ?: '';
+        $name       = $app->input->get('jos_emundus_setup_availabilities___name', null, "raw") ?: '';
         $start_date = $app->input->get('jos_emundus_setup_availabilities___start_date') ?: '';
         $end_date   = $app->input->get('jos_emundus_setup_availabilities___end_date') ?: '';
-        $length     = $app->input->get('jos_emundus_setup_availabilities___event_length') ?: '';
+        $length     = $app->input->get('jos_emundus_setup_availabilities___event_length', null, "raw") ?: '';
+        $event_name     = $app->input->get('jos_emundus_setup_availabilities___event_name') ?: '';
+        $formModel  = $this->getModel();
 
         $db         = Factory::getDbo();
         $query       = $db->getQuery(true);
@@ -113,38 +99,44 @@ class PlgFabrik_FormCalcom extends plgFabrik_Form
             ->from($db->quoteName('jos_emundus_setup_availabilities'))
             ->where($db->quoteName('id') . ' = ' . $db->quote($id));
 
-            $db->setQuery($query);
-            $request = $db->loadObject();
+        $db->setQuery($query);
+        $request = $db->loadObject();
 
         if(!empty($request))
         {
             $access_token = $w->refreshingAccessToken($request->user_id);
-            $user = $w->patchUser($name, $request->user_id, $request->schedule_id);
-            $w->patchSchedule($access_token['data']->data->accessToken, $request->schedule_id, $start_date, $end_date);
-            $w->deleteEventType($request->event_id);
-            $event_type = $w->postEventType($length, $name, $user['data']->data->id);
-            $this->user_id = $request->user_id;
-            $this->schedule_id = $request->schedule_id;
+            if($name !== $request->name)
+            {
+                $w->patchUser($name, $request->user_id, $request->schedule_id);
+            }
+            $start_time = new DateTime($request->start_date);
+            $end_time = new DateTime($request->end_date);
+            if($start_date !== $start_time->format('Y-m-dHis') || $end_date !== $end_time->format('Y-m-dHis'))
+            {
+                $w->patchSchedule($access_token['data']->data->accessToken, $request->schedule_id, $start_date, $end_date);
+            }
+            if($length !== $request->event_length || $event_name !== $request->event_name)
+            {
+                $w->deleteEventType($request->event_id);
+                $event_type = $w->postEventType($length, $event_name, $request->user_id);
+            }
+            $event_id = $event_type ? $event_type['data']->event_type->id : $request->event_id;
         }
         else
         {
             $user = $w->postUser($name);
             $w->deleteSchedule($user['data']->data->accessToken, $user['data']->data->user->defaultScheduleId);
-            $schedule = $w->postSchedule($user['data']->data->accessToken);
+            $schedule = $w->postSchedule($user['data']->data->accessToken, $start_date, $end_date);
             $w->patchSchedule($user['data']->data->accessToken, $schedule['data']->data->id, $start_date, $end_date);
-            $event_type = $w->postEventType($length, $name, $user['data']->data->user->id);
-            $this->user_id = $user['data']->data->user->id;
-            $this->schedule_id = $schedule['data']->data->id;
+            $event_type = $w->postEventType($length, $event_name, $user['data']->data->user->id);
+            $event_id = $event_type['data']->event_type->id;
+            $formModel->updateFormData('jos_emundus_setup_availabilities___schedule_id', $schedule['data']->data->id);
+            $formModel->updateFormData('jos_emundus_setup_availabilities___user_id', $user['data']->data->user->id);
         }
-        $this->event_id = $event_type['data']->event_type->id;
-    }
 
-    public function onBeforeStore()
-    {
-        $formModel = $this->getModel();
-        $formModel->updateFormData('jos_emundus_setup_availabilities___schedule_id', $this->schedule_id);
-        $formModel->updateFormData('jos_emundus_setup_availabilities___user_id', $this->user_id);
-        $formModel->updateFormData('jos_emundus_setup_availabilities___event_id', $this->event_id);
+        $formModel->updateFormData('jos_emundus_setup_availabilities___event_id', $event_id);
+
+        return true;
     }
 
 }
