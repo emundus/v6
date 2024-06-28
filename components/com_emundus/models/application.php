10414233604a1db43dde9d8cc553ee683e6d641f
jlegendre <jeremy.legendre@emundus.fr>
2024-06-28 17:23:59 +0200

    import plugin in order to dispatch event file sharing successfully

--- components/com_emundus/models/application.php
+++ components/com_emundus/models/application.php
@@ -6165,6 +6165,7 @@ class EmundusModelApplication extends JModelList
         }
 
         if (!empty($emails)) {
+            JPluginHelper::importPlugin('emundus');
             $query = $this->_db->getQuery(true);
 
             $query->select('fnum,applicant_id,campaign_id')
