44dcbecc3eeee9565b59cfe8d9c5d4ccb6f0637b
HUBINET Brice <brice.hubinet@emundus.fr>
2024-07-03 16:52:52 +0200

    Fix translation of forms and attachments progress columns in excel exports

--- components/com_emundus/controllers/evaluation.php
+++ components/com_emundus/controllers/evaluation.php
@@ -9,6 +9,8 @@
 
 // No direct access
 
+use Joomla\CMS\Language\Text;
+
 defined( '_JEXEC' ) or die( 'Restricted access' );
 
 jimport('joomla.application.component.controller');
@@ -1222,7 +1224,7 @@ class EmundusControllerEvaluation extends JControllerLegacy
             }
             foreach ($colsup as $kOpt => $vOpt) {
                 if ($vOpt=="forms" || $vOpt=="attachment") {
-                    $line .= $vOpt . "(%)\t";
+	                $line .= Text::_('COM_EMUNDUS_'.strtoupper($vOpt))." (%)\t";
                 } else {
                     $line .= $vOpt . "\t";
                 }
