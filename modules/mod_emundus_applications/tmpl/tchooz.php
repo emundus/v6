346fcd89207231e7b17a2d9f030726c51cdacfef
HUBINET Brice <brice.hubinet@emundus.fr>
2024-07-03 15:12:58 +0200

    Fix searchbar on status

--- modules/mod_emundus_applications/tmpl/tchooz.php
+++ modules/mod_emundus_applications/tmpl/tchooz.php
@@ -418,12 +418,12 @@ if (!empty($applications) && !empty($title_override) && !empty(str_replace(array
                                                                 <?php if (empty($visible_status)) : ?>
                                                                     <div class="mod_emundus_applications___status_<?= $application->class; ?> flex"
                                                                          id="application_status_<?php echo $application->fnum ?>">
-                                                                        <span class="label label-<?= $application->class; ?>"><?= $application->value; ?></span>
+                                                                        <span class="mod_emundus_applications___status_label label label-<?= $application->class; ?>"><?= $application->value; ?></span>
                                                                     </div>
                                                                 <?php elseif (in_array($application->status, $visible_status)) : ?>
                                                                     <div class="mod_emundus_applications___status_<?= $application->class; ?> flex"
                                                                          id="application_status_<?php echo $application->fnum ?>">
-                                                                        <span class="label label-<?= $application->class; ?>"><?= $application->value; ?></span>
+                                                                        <span class="mod_emundus_applications___status_label label label-<?= $application->class; ?>"><?= $application->value; ?></span>
                                                                     </div>
                                                                 <?php endif; ?>
                                                                 <?php if (!empty($application->order_status)): ?>
@@ -781,12 +781,12 @@ if (!empty($applications) && !empty($title_override) && !empty(str_replace(array
 														<?php if (empty($visible_status)) : ?>
                                                             <div class="mod_emundus_applications___status_<?= $application->class; ?> em-flex-row"
                                                                  id="application_status_<?php echo $application->fnum ?>">
-                                                                <span class="label label-<?= $application->class; ?>"><?= $application->value; ?></span>
+                                                                <span class="mod_emundus_applications___status_label label label-<?= $application->class; ?>"><?= $application->value; ?></span>
                                                             </div>
 														<?php elseif (in_array($application->status, $visible_status)) : ?>
                                                             <div class="mod_emundus_applications___status_<?= $application->class; ?> em-flex-row"
                                                                  id="application_status_<?php echo $application->fnum ?>">
-                                                                <span class="label label-<?= $application->class; ?>"><?= $application->value; ?></span>
+                                                                <span class="mod_emundus_applications___status_label label label-<?= $application->class; ?>"><?= $application->value; ?></span>
                                                             </div>
 														<?php endif; ?>
 														<?php if (!empty($application->order_status)): ?>
