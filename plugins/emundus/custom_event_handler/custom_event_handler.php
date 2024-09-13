817f55424734182f97f686aa339bb2cb440ab1e8
bretord <benjamin.retord@emundus.io>
2024-09-13 14:38:35 +0200

    Add where published equal to 1 for select all published application by user

--- plugins/emundus/custom_event_handler/custom_event_handler.php
+++ plugins/emundus/custom_event_handler/custom_event_handler.php
@@ -45,7 +45,11 @@ class plgEmundusCustom_event_handler extends JPlugin {
 
         foreach ($events as $index => $caller_index) {
             try {
-                $returned_values[$caller_index] = $this->_runPHP($codes[$index], $args);
+                if (!isset($returned_values[$caller_index])) {
+                    $returned_values[$caller_index] = [];
+                }
+
+                $returned_values[$caller_index][$index] = $this->_runPHP($codes[$index], $args);
             } catch (ParseError $p) {
                 JLog::add('Error while running event ' . $caller_index . ' : "' . $p->getMessage() .'"', JLog::ERROR,'com_emundus');
                 continue;
